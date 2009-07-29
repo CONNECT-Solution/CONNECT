06fa7878db6d477b8b673dbce5578d16a7924b1a
rayj <rayj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-07-29 02:29:41 +0000

    test check-in; please ignore

--- Product/Build.bat
+++ Product/Build.bat
@@ -2,3 +2,5 @@
 IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
 ..\Build\nant\bin\nant.exe -buildfile:Personal.Build.xml %1 %2 %3 %4 %5 %6 %7 %8
 SET /P variable="Hit Enter to continue."
+
+#jason was here
