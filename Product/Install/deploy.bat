9f0722e297f64e58511359498b51f20aa4141317
flowersj <flowersj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-08-07 15:46:37 +0000

    Initial cut of new deployment scripts, just the production deployment, not the dev deployment

---
+++ Product/Install/deploy.bat
@@ -0,0 +1,8 @@
+set ANT_HOME=C:\GlassfishESB\netbeans\java2\ant
+set ANT_OPTS=-Xmx872m -XX:MaxPermSize=512m "-Dcom.sun.aas.instanceName=server"
+set JAVA_HOME=C:\Java\jdk1.6.0_13
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%ANT_HOME%\bin
+
+ant -buildfile deploy.xml %*
+
+exit /B %ERRORLEVEL%
