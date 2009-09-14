befb9f55a9ca720d531fbe8813a7e7b3f47a6a89
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-09-14 01:12:10 +0000

    This EJB is replacement for AdapterDocQueryEJB, AdapterDocRetrieveEJB, AdapterDocProvideAndRegisterEJB. These 3 EJBs will be removed after updating Deployment script.

---
+++ Product/Production/Adapters/General/AdapterDocRegistryAndRepositoryEJB/src/java/gov/hhs/fha/nhinc/adapterdocretrieve/AdapterDocRetrieveSecured.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.adapterdocretrieve;
+
+import gov.hhs.fha.nhinc.adapterdocretrievesecured.AdapterDocRetrieveSecuredPortType;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author svalluripalli
+ */
+@WebService(serviceName = "AdapterDocRetrieveSecured", portName = "AdapterDocRetrieveSecuredPortSoap11", endpointInterface = "gov.hhs.fha.nhinc.adapterdocretrievesecured.AdapterDocRetrieveSecuredPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:adapterdocretrievesecured", wsdlLocation = "META-INF/wsdl/AdapterDocRetrieveSecured/AdapterDocRetrieveSecured.wsdl")
+@Stateless
+public class AdapterDocRetrieveSecured implements AdapterDocRetrieveSecuredPortType {
+
+    @Resource
+    private WebServiceContext context;
+    
+    public ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType respondingGatewayCrossGatewayRetrieve(ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType body) {
+        return new AdapterDocRetrieveSecuredImpl().respondingGatewayCrossGatewayRetrieve(body, context);
+    }
+
+}
