c2b20b0cab35d4b3b67843a3a0926990b15f5c7a
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-09-16 16:25:16 +0000

    final changes for Release 2.2

--- Product/UndeployAllBinaries.sh
+++ Product/UndeployAllBinaries.sh
@@ -1,55 +1,61 @@
 #
-# Stop the Gateway Composite Applications
+# undeploy the Gateway EJB modules
 #
-$AS_HOME/bin/asadmin shut-down-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhinCA
-$AS_HOME/bin/asadmin shut-down-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile EntityCA
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/GatewayPolicyEngineTransformationEjb.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterDocQueryProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/GatewaySubscriptionRepositoryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AuditRepositoryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/NhincSubjectDiscoveryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityDocRetrieveEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterDocRetrieveProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AggregatorEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntitySubjectDiscoveryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/PatientCorrelationFacadeDteEjb.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/NhincDocRetrieveEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterSubjectDiscoveryProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/PatientCorrelationEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/NhincHiemSubscriptionEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/NhincDocQueryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/NhincAuditQueryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityHiemSubscriptionEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/HiemSubscriptionRepositoryUtilEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityDocQueryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPolicyEngineProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/GatewayPolicyEngineFacadeEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityAuditLogQueryEJB.jar 
 #
-# Undeploy the Gateway Composite Applications
+# undeploy the GUI modules
 #
-$AS_HOME/bin/asadmin undeploy-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhinCA
-$AS_HOME/bin/asadmin undeploy-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile EntityCA
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/UniversalClientGUI.war  
 #
-# Stop the Adapter Composite Application
+# undeploy the Adapter EJB modules
 #
-$AS_HOME/bin/asadmin shut-down-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterCA
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/MuralMpiEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/FTATransferAdapterEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityDocRetrieveProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/DocumentRepositoryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterAuthenticationEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPolicyEngineTransformEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPEPEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterMpiEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPolicyEngineOrchestratorEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntitySubjectDiscoveryProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterSubjectDiscoveryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterMPIProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPIPEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityAuditQueryProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/MpiEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterDocRepository2Soap12EJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/MpiManagerEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterReidentificationEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterDocRegistryAndRepositoryEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterPolicyEngineEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/EntityDocQueryProxyEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/AdapterAuditQueryEJB.jar 
 #
-# Undeploy the Adapter Composite Application
+# undeploy the Common EJB modules
 #
-$AS_HOME/bin/asadmin undeploy-jbi-service-assembly --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterCA
-#
-# Undeploy the Gateway EJB Modules
-#
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AggregatorEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AuditRepositoryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AuditLogEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ConnectionManagerEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile DocumentTransformEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile EntityAuditLogQueryEJB.jar
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile EntityHiemSubscriptionEJB.jar
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile GatewaySubscriptionRepositoryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile GatewayPolicyEngineFacadeEJB.jar
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile GatewayPolicyEngineTransformationEjb
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincAuditLogDteEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincAuditQueryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincDocRetrieveEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincDocQueryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincHiemSubscriptionEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincSubjectDiscoveryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile NhincSubDiscDataTransformsEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile PatientCorrelationEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile PatientCorrelationFacadeDteEjb
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile PropAccessorEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile SubscriptionDteEjb
-#$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile UDDIUpdateManagerEJB
-#
-# Undeploy the Adapter EJB modules
-#
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterMpiEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterPolicyEngineTransformEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterReidentificationEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile MpiEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile MpiManagerEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile DocumentRepositoryEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterPIPEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterPEPEJB
-$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile AdapterPolicyEngineOrchestratorEJB
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/PropAccessorEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/UDDIUpdateManagerEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/ConnectionManagerEJB.jar 
+$AS_HOME/bin/asadmin undeploy --user admin --passwordfile $HOME/NHINC_Binaries/passfile ~/NHINC_Binaries/DocumentTransformEJB.jar 
