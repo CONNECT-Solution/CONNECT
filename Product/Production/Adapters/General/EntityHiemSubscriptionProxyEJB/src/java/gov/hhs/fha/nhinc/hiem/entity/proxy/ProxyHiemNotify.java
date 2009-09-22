23cad09107167f76e3bddae0d60de707831588f1
webbn <webbn@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-09-22 23:57:47 +0000

    Bug #19770 - Notify stuck in check policy. Moved processing to unsecured notify processing as a work-around.

--- Product/Production/Adapters/General/EntityHiemSubscriptionProxyEJB/src/java/gov/hhs/fha/nhinc/hiem/entity/proxy/ProxyHiemNotify.java
+++ Product/Production/Adapters/General/EntityHiemSubscriptionProxyEJB/src/java/gov/hhs/fha/nhinc/hiem/entity/proxy/ProxyHiemNotify.java
@@ -3,6 +3,9 @@ package gov.hhs.fha.nhinc.hiem.entity.proxy;
 import gov.hhs.fha.nhinc.nhincproxynotificationconsumer.NhincProxyNotificationConsumerPortType;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+import javax.jws.HandlerChain;
 
 /**
  *
@@ -10,11 +13,14 @@ import javax.jws.WebService;
  */
 @WebService(serviceName = "NhincProxyNotificationConsumer", portName = "NhincProxyNotificationConsumerPortSoap11", endpointInterface = "gov.hhs.fha.nhinc.nhincproxynotificationconsumer.NhincProxyNotificationConsumerPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincproxynotificationconsumer", wsdlLocation = "META-INF/wsdl/ProxyHiemNotify/NhincProxyNotificationConsumer.wsdl")
 @Stateless
+@HandlerChain(file = "ProxyHiemNotifyHeaderHandler.xml")
 public class ProxyHiemNotify implements NhincProxyNotificationConsumerPortType
 {
+    @Resource
+    private WebServiceContext context;
 
     public void notify(gov.hhs.fha.nhinc.common.nhinccommonproxy.NotifyRequestType notifyRequest)
     {
-        new ProxyHiemNotifyImpl().notify(notifyRequest);
+        new ProxyHiemNotifyImpl().notify(notifyRequest, context);
     }
 }
