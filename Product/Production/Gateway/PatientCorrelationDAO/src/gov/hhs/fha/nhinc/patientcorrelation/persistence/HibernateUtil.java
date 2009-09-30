f126d4fb514e57305422a7431da086286d71fd5c
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-09-30 14:42:20 +0000

    task 19826

--- Product/Production/Gateway/PatientCorrelationDAO/src/gov/hhs/fha/nhinc/patientcorrelation/persistence/HibernateUtil.java
+++ Product/Production/Gateway/PatientCorrelationDAO/src/gov/hhs/fha/nhinc/patientcorrelation/persistence/HibernateUtil.java
@@ -4,8 +4,13 @@
  */
 package gov.hhs.fha.nhinc.patientcorrelation.persistence;
 
+import gov.hhs.fha.nhinc.properties.HibernateAccessor;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.io.File;
 
 /**
  * This class will be used as a Utility Class to access the Data Object using Hibernate SessionFactory
@@ -13,13 +18,14 @@ import org.hibernate.cfg.Configuration;
  */
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
+    private static Log log = LogFactory.getLog(HibernateUtil.class);
     static {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+            sessionFactory = new Configuration().configure(getConfigFile()).buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            log.error("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
@@ -30,5 +36,22 @@ public class HibernateUtil {
      */
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+    private static File getConfigFile(){
+        File result = null;
+
+        try
+        {
+            result = HibernateAccessor.getHibernateFile(NhincConstants.HIBERNATE_PATIENT_CORRELATION);
+        }
+        catch (Exception ex)
+        {
+            log.error("Unable to load " + NhincConstants.HIBERNATE_PATIENT_CORRELATION + " " + ex.getMessage(), ex );
+        }
+
+
+        return result;
+
+
     }
 }
