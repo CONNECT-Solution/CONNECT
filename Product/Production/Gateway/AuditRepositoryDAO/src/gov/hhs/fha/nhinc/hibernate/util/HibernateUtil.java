5c98abeeb76a9d2be8f1ecf61999d308c30ae63f
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-09-30 14:48:21 +0000

    task: 19826

--- Product/Production/Gateway/AuditRepositoryDAO/src/gov/hhs/fha/nhinc/hibernate/util/HibernateUtil.java
+++ Product/Production/Gateway/AuditRepositoryDAO/src/gov/hhs/fha/nhinc/hibernate/util/HibernateUtil.java
@@ -4,26 +4,52 @@
  */
 package gov.hhs.fha.nhinc.hibernate.util;
 
+import gov.hhs.fha.nhinc.properties.HibernateAccessor;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.io.File;
 
 /**
  * This class will be used as a Utility Class to access the Data Object using Hibernate SessionFactory
  * @author svalluripalli
  */
 public class HibernateUtil {
+
     private static final SessionFactory sessionFactory;
+    private static Log log = LogFactory.getLog(HibernateUtil.class);
     static {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+
+            sessionFactory = new Configuration().configure(getConfigFile()).buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            log.error("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+    private static File getConfigFile(){
+        File result = null;
+        
+        try
+        {
+            result = HibernateAccessor.getHibernateFile(NhincConstants.HIBERNATE_AUDIT_REPOSITORY);
+        }
+        catch (Exception ex)
+        {
+            log.error("Unable to load " + NhincConstants.HIBERNATE_AUDIT_REPOSITORY + " " + ex.getMessage(), ex );
+        }
+
+
+        return result;
+
+
+    }
+
+
     /**
      * Method returns an instance of Hibernate SessionFactory
      * @return SessionFactory
