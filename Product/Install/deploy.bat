23effcd73facd8fe3e0179457f8973865a784344
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-10-12 12:25:06 +0000

    Fix path issue

--- Product/Install/deploy.bat
+++ Product/Install/deploy.bat
@@ -1,4 +1,4 @@
-set ANT_HOME=C:\GlassfishESB\netbeans\java2\ant
+set ANT_HOME=%IDE_HOME%\java2\ant
 set ANT_OPTS=-Xmx872m -XX:MaxPermSize=512m "-Dcom.sun.aas.instanceName=server"
 set JAVA_HOME=C:\Java\jdk1.6.0_13
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%ANT_HOME%\bin
