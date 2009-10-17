540229d58c65866539931f59dc8e276d18eaa797
rayj <rayj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-10-17 20:31:42 +0000

    adding NhincLib unit tests to the build

---
+++ Product/Production/Common/NhincLib/test/gov/hhs/fha/nhinc/SampleUnitTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.hhs.fha.nhinc;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rayj
+ */
+public class SampleUnitTest {
+
+    public SampleUnitTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+//    @Test
+//    public void failTest() {
+//        System.out.println("forcing unit test to fail");
+//        fail("force build to fail");
+//    }
+
+    @Test
+    public void passTest() {
+        System.out.println("forcing unit test to pass");
+    }
+}
