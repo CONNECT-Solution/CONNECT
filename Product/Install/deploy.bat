d280d03140f414357685869231586b37ccf66da7
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-10-20 15:04:13 +0000

    Story#19599 comments: script recreation of GlassFish domain

--- Product/Install/deploy.bat
+++ Product/Install/deploy.bat
@@ -1,6 +1,6 @@
 set ANT_HOME=%IDE_HOME%\java2\ant
 set ANT_OPTS=-Xmx872m -XX:MaxPermSize=512m "-Dcom.sun.aas.instanceName=server"
-set JAVA_HOME=C:\Java\jdk1.6.0_13
+set JAVA_HOME=C:\Java\jdk1.6.0_16
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%ANT_HOME%\bin
 
 ant -buildfile deploy.xml %*
