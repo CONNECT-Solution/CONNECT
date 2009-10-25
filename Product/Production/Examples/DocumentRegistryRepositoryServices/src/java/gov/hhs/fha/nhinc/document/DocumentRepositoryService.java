1beabf5cefc1f201138b24619d35cb7ac05f0db8
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-10-25 16:03:12 +0000

    POC project converting DocumentRepositoryEJB to DocumentRegistryRepositoryServices POJO. Example project can you used in main steam by removing DocumentRepositoryEJB.

---
+++ Product/Production/Examples/DocumentRegistryRepositoryServices/src/java/gov/hhs/fha/nhinc/document/DocumentRepositoryService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.document;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author svalluripalli
+ */
+@WebService(serviceName = "DocumentRepository_Service", portName = "DocumentRepository_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.DocumentRepositoryPortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "WEB-INF/wsdl/DocumentRepositoryService/AdapterComponentDocRepository.wsdl")
+public class DocumentRepositoryService {
+
+    public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType documentRepositoryProvideAndRegisterDocumentSetB(ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType body)
+    {
+        return new DocumentRepositoryHelper().documentRepositoryProvideAndRegisterDocumentSet(body);
+    }
+
+    public ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType documentRepositoryRetrieveDocumentSet(ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType body)
+    {
+        return new DocumentRepositoryHelper().documentRepositoryRetrieveDocumentSet(body);
+    }
+
+}
