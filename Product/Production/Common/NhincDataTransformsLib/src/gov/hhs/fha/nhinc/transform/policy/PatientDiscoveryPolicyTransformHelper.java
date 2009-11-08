090e749264730fa1f054e64e75abf79713a7ea11
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-08 17:52:21 +0000

    User Story ID:20510 Task ID:20675, Task ID:20676, Task ID:20677, Task ID:20678 Status: Complete

--- Product/Production/Common/NhincDataTransformsLib/src/gov/hhs/fha/nhinc/transform/policy/PatientDiscoveryPolicyTransformHelper.java
+++ Product/Production/Common/NhincDataTransformsLib/src/gov/hhs/fha/nhinc/transform/policy/PatientDiscoveryPolicyTransformHelper.java
@@ -4,14 +4,23 @@
  */
 package gov.hhs.fha.nhinc.transform.policy;
 
-import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.HomeCommunityType;
 import gov.hhs.fha.nhinc.common.nhinccommonadapter.CheckPolicyRequestType;
+import gov.hhs.fha.nhinc.util.format.PatientIdFormatUtil;
+import java.util.List;
 import oasis.names.tc.xacml._2_0.context.schema.os.RequestType;
 import oasis.names.tc.xacml._2_0.context.schema.os.ResourceType;
 import oasis.names.tc.xacml._2_0.context.schema.os.SubjectType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hl7.v3.II;
+import org.hl7.v3.MCCIMT000100UV01Device;
+import org.hl7.v3.MCCIMT000100UV01Sender;
 import org.hl7.v3.PRPAIN201305UV;
+import org.hl7.v3.PRPAMT201306UVQueryByParameter;
+import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
+import javax.xml.bind.JAXBElement;
+import org.hl7.v3.PRPAMT201306UVLivingSubjectId;
 
 /**
  * This class is used to transform 201305 request message to a CheckPolicyRequestType
@@ -20,8 +29,11 @@ import org.hl7.v3.PRPAIN201305UV;
 public class PatientDiscoveryPolicyTransformHelper {
 
     private static Log log = null;
-    private static final String ActionValue = "PatientDiscoveryIn";
-    
+    private static final String ActionInValue = "PatientDiscoveryIn";
+    private static final String ActionOutValue = "PatientDiscoveryOut";
+    private static final String PatientAssigningAuthorityAttributeId = Constants.AssigningAuthorityAttributeId;
+    private static final String PatientIdAttributeId = Constants.ResourceIdAttributeId;
+
     /**
      * Default Constructor
      */
@@ -29,43 +41,80 @@ public class PatientDiscoveryPolicyTransformHelper {
         log = createLogger();
     }
 
-    /**
-     * Transform method to create a CheckPolicyRequest object from a 201305 request
-     * @param request
-     * @return CheckPolicyRequestType
-     */
-    public CheckPolicyRequestType transformPatientDiscoveryEntityToCheckPolicy(PRPAIN201305UV request) {
+    public CheckPolicyRequestType transformPatientDiscoveryNhincToCheckPolicy(RespondingGatewayPRPAIN201305UV02RequestType event) {
         addDebugLog("Begin -- PatientDiscoveryPolicyTransformHelper.transformPatientDiscoveryEntityToCheckPolicy()");
         CheckPolicyRequestType checkPolicyRequest = null;
-        if (request == null) {
+        if (event == null) {
             addErrorLog("Request is null.");
             return checkPolicyRequest;
         } else {
             checkPolicyRequest = new CheckPolicyRequestType();
         }
-        
-        RequestType req = new RequestType();
-        req.setAction(ActionHelper.actionFactory(ActionValue));
-        //SubjectType subject = SubjectHelper.subjectFactory(event.getSendingHomeCommunity(), event.getMessage().getAssertion());
-        SubjectType subject = new SubjectType();
-        req.getSubject().add(subject);
-//        II ii = extractPatientIdentifier(subjectAdded);
-//        if (ii != null) {
-//            ResourceType resource = new ResourceType();
-//            resource.getAttribute().add(AttributeHelper.attributeFactory(PatientAssigningAuthorityAttributeId, Constants.DataTypeString, ii.getRoot()));
-//            String sStrippedPatientId = PatientIdFormatUtil.parsePatientId(ii.getExtension());
-//            log.debug("transformSubjectAddedInToCheckPolicy: sStrippedPatientId = " + sStrippedPatientId);
-//            resource.getAttribute().add(AttributeHelper.attributeFactory(PatientIdAttributeId, Constants.DataTypeString, sStrippedPatientId));
-//            req.getResource().add(resource);
-//        }
-        ResourceType resource = new ResourceType();
-        req.getResource().add(resource);
-        log.debug("transformSubjectAddedInToCheckPolicy - adding assertion data");
-        //AssertionHelper.appendAssertionDataToRequest(req, event.getMessage().getAssertion());
-        checkPolicyRequest.setAssertion(new AssertionType());
-        checkPolicyRequest.setRequest(req);
+        RequestType request = getRequestType(event);
+        request.setAction(ActionHelper.actionFactory(ActionOutValue));
+        checkPolicyRequest.setRequest(request);
+        checkPolicyRequest.setAssertion(event.getAssertion());
         addDebugLog("End -- PatientDiscoveryPolicyTransformHelper.transformPatientDiscoveryEntityToCheckPolicy()");
         return checkPolicyRequest;
+    }
+
+    /**
+     * Transform method to create a CheckPolicyRequest object from a 201305 request
+     * @param request
+     * @return CheckPolicyRequestType
+     */
+    public CheckPolicyRequestType transformPatientDiscoveryEntityToCheckPolicy(RespondingGatewayPRPAIN201305UV02RequestType event) {
+        addDebugLog("Begin -- PatientDiscoveryPolicyTransformHelper.transformPatientDiscoveryEntityToCheckPolicy()");
+        CheckPolicyRequestType checkPolicyRequest = null;
+        if (event == null) {
+            addErrorLog("Request is null.");
+            return checkPolicyRequest;
+        } else {
+            checkPolicyRequest = new CheckPolicyRequestType();
+        }
+        RequestType request = getRequestType(event);
+        request.setAction(ActionHelper.actionFactory(ActionInValue));
+        checkPolicyRequest.setRequest(request);
+        checkPolicyRequest.setAssertion(event.getAssertion());
+        addDebugLog("End -- PatientDiscoveryPolicyTransformHelper.transformPatientDiscoveryEntityToCheckPolicy()");
+        return checkPolicyRequest;
+    }
+
+    protected HomeCommunityType getHomeCommunityFrom201305(RespondingGatewayPRPAIN201305UV02RequestType event) {
+        HomeCommunityType senderHomeCommunity = new HomeCommunityType();
+        final PRPAIN201305UV pRPAIN201305UV = event.getPRPAIN201305UV();
+        final MCCIMT000100UV01Sender sender = pRPAIN201305UV.getSender();
+        final MCCIMT000100UV01Device device = sender.getDevice();
+        final List<II> id = device.getId();
+        final II get = id.get(0);
+        final String root = get.getRoot();
+        senderHomeCommunity.setHomeCommunityId(root);
+        return senderHomeCommunity;
+    }
+
+    protected RequestType getRequestType(RespondingGatewayPRPAIN201305UV02RequestType event) {
+        addDebugLog("Begin -- PatientDiscoveryPolicyTransformHelper.getRequestType()");
+        RequestType request = new RequestType();
+        setSubjectToRequestType(event, request);
+        ResourceType resource = null;
+        II ii = extractPatientIdentifier(event.getPRPAIN201305UV());
+        if (ii != null) {
+            resource = new ResourceType();
+            resource.getAttribute().add(AttributeHelper.attributeFactory(PatientAssigningAuthorityAttributeId, Constants.DataTypeString, ii.getRoot()));
+            String sStrippedPatientId = PatientIdFormatUtil.parsePatientId(ii.getExtension());
+            log.debug("transformSubjectAddedInToCheckPolicy: sStrippedPatientId = " + sStrippedPatientId);
+            resource.getAttribute().add(AttributeHelper.attributeFactory(PatientIdAttributeId, Constants.DataTypeString, sStrippedPatientId));
+            request.getResource().add(resource);
+        }
+        request.getResource().add(resource);
+        addDebugLog("End -- PatientDiscoveryPolicyTransformHelper.getRequestType()");
+        return request;
+    }
+
+    protected void setSubjectToRequestType(RespondingGatewayPRPAIN201305UV02RequestType event, RequestType request) {
+        HomeCommunityType senderHomeCommunity = getHomeCommunityFrom201305(event);
+        SubjectType subject = SubjectHelper.subjectFactory(senderHomeCommunity, event.getAssertion());
+        request.getSubject().add(subject);
     }
 
     /**
@@ -98,5 +147,24 @@ public class PatientDiscoveryPolicyTransformHelper {
      */
     protected Log createLogger() {
         return ((log != null) ? log : LogFactory.getLog(getClass()));
+    }
+
+    private static II extractPatientIdentifier(PRPAIN201305UV message) {
+        II ii = null;
+
+        if ((message != null) && (message.getControlActProcess() != null) && (message.getControlActProcess().getQueryByParameter() != null)) {
+            JAXBElement<PRPAMT201306UVQueryByParameter> queryParam = message.getControlActProcess().getQueryByParameter();
+            PRPAMT201306UVQueryByParameter queryParam201306 = queryParam.getValue();
+            if (queryParam201306.getParameterList() != null && queryParam201306.getParameterList().getLivingSubjectId() != null) {
+                List<PRPAMT201306UVLivingSubjectId> livingSubjectIdList = queryParam201306.getParameterList().getLivingSubjectId();
+                if (livingSubjectIdList != null) {
+                    List<II> patinetIdList = livingSubjectIdList.get(0).getValue();
+                    if (patinetIdList != null) {
+                        ii = patinetIdList.get(0);
+                    }
+                }
+            }
+        }
+        return ii;
     }
 }
--- Product/Production/Common/NhincDataTransformsLib/test/gov/hhs/fha/nhinc/transform/policy/PatientDiscoveryPolicyTransformHelperTest.java
+++ Product/Production/Common/NhincDataTransformsLib/test/gov/hhs/fha/nhinc/transform/policy/PatientDiscoveryPolicyTransformHelperTest.java
@@ -5,14 +5,21 @@
 package gov.hhs.fha.nhinc.transform.policy;
 
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.HomeCommunityType;
 import gov.hhs.fha.nhinc.common.nhinccommonadapter.CheckPolicyRequestType;
 import oasis.names.tc.xacml._2_0.context.schema.os.RequestType;
 import org.apache.commons.logging.Log;
+import org.hl7.v3.II;
+import org.hl7.v3.MCCIMT000100UV01Device;
+import org.hl7.v3.MCCIMT000100UV01Sender;
 import org.hl7.v3.PRPAIN201305UV;
+import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -38,7 +45,9 @@ public class PatientDiscoveryPolicyTransformHelperTest {
 
     @Before
     public void setUp() {
-        context = new Mockery();
+        context = new Mockery(){{
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }};
     }
 
     @After
@@ -57,25 +66,31 @@ public class PatientDiscoveryPolicyTransformHelperTest {
         context.checking(new Expectations() {
             {
                 exactly(1).of(mockLogger).debug(with(any(String.class)));
+                //Input Request Object can not be null
                 exactly(1).of(mockLogger).error("Request is null.");
                 will(returnValue(with(any(CheckPolicyRequestType.class))));
             }
         });
-
         testSubject.transformPatientDiscoveryEntityToCheckPolicy(null);
         context.assertIsSatisfied();
     }
 
+    //                Expected Response should be validated.
+    //                At least one RequestType
+    //                At least one SubjectType
+    //                Set Action on RequestType
+    //                Set SubjectType on RequestType
+    //                Create Subject and Add to RequestType
+    //                ResourceType add to RequestType
+
     @Test
     public void testTransformPatientDiscoveryEntityToCheckPolicyWillPass() {
         final Log mockLogger = context.mock(Log.class);
-        PRPAIN201305UV requestPRPAIN201305UV = new PRPAIN201305UV();
-        //AssertionType assertionType = requestPRPAIN201305UV.getAssertion();
-
-        final CheckPolicyRequestType expectedReturnValue = new CheckPolicyRequestType();
+        CheckPolicyRequestType expectedReturnValue = new CheckPolicyRequestType();
         expectedReturnValue.setAssertion(new AssertionType());
         expectedReturnValue.setRequest(new RequestType());
-
+        RespondingGatewayPRPAIN201305UV02RequestType event = new RespondingGatewayPRPAIN201305UV02RequestType();
+        getHomeCommunityType(event);
         PatientDiscoveryPolicyTransformHelper testSubject = new PatientDiscoveryPolicyTransformHelper() {
             @Override
             protected Log createLogger() {
@@ -84,16 +99,57 @@ public class PatientDiscoveryPolicyTransformHelperTest {
         };
         context.checking(new Expectations() {
             {
-                exactly(3).of(mockLogger).debug(with(any(String.class)));
+                exactly(4).of(mockLogger).debug(with(any(String.class)));
                 will(returnValue(with(any(CheckPolicyRequestType.class))));
-//                aNonNull(AssertionType.class);
-//                aNonNull(ResourceType.class);
-//                aNull(ResourceType.class);
             }
         });
-
-        testSubject.transformPatientDiscoveryEntityToCheckPolicy(requestPRPAIN201305UV);
+        testSubject.transformPatientDiscoveryEntityToCheckPolicy(event);
         context.assertIsSatisfied();
-        //assertNotNull(expectedReturnValue.getAssertion());
+    }
+
+    @Test
+    public void testGetRequestTypeWillPass() {
+        final Log mockLogger = context.mock(Log.class);
+        final RequestType request = new RequestType();
+        RespondingGatewayPRPAIN201305UV02RequestType event = new RespondingGatewayPRPAIN201305UV02RequestType();
+        getHomeCommunityType(event);
+        PatientDiscoveryPolicyTransformHelper testSubject = new PatientDiscoveryPolicyTransformHelper() {
+            @Override
+            protected Log createLogger() {
+                return mockLogger;
+            }
+        };
+        context.checking(new Expectations(){{
+            exactly(2).of(mockLogger).debug(with(any(String.class)));
+            will(returnValue(with(any(RequestType.class))));
+        }});
+        testSubject.getRequestType(event);
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testGetHomeCommunityFrom201305()
+    {
+        RespondingGatewayPRPAIN201305UV02RequestType event = new RespondingGatewayPRPAIN201305UV02RequestType();
+        HomeCommunityType expectedReturnvalue = getHomeCommunityType(event);
+        PatientDiscoveryPolicyTransformHelper testSubject = new PatientDiscoveryPolicyTransformHelper();
+        Assert.assertEquals(expectedReturnvalue.getHomeCommunityId(), "1.1");
+        testSubject.getHomeCommunityFrom201305(event);
+        context.assertIsSatisfied();
+    }
+
+    protected HomeCommunityType getHomeCommunityType(RespondingGatewayPRPAIN201305UV02RequestType event) {
+        PRPAIN201305UV request201305 = new PRPAIN201305UV();
+        MCCIMT000100UV01Sender sender = new MCCIMT000100UV01Sender();
+        MCCIMT000100UV01Device device = new MCCIMT000100UV01Device();
+        II e = new II();
+        e.setRoot("1.1");
+        device.getId().add(e);
+        sender.setDevice(device);
+        request201305.setSender(sender);
+        event.setPRPAIN201305UV(request201305);
+        final HomeCommunityType expectedReturnvalue = new HomeCommunityType();
+        expectedReturnvalue.setHomeCommunityId("1.1");
+        return expectedReturnvalue;
     }
 }
