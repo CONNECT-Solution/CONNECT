06539586a835fe8c35d1c52613dac611ea6d7a45
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-11 03:58:27 +0000

    Checkin HL7 Updates

--- Product/Production/Adapters/General/AdapterSubjectDiscoveryEJB/src/java/gov/hhs/fha/nhinc/adapter/subjectdiscovery/AdapterSubjectDiscoveryImpl.java
+++ Product/Production/Adapters/General/AdapterSubjectDiscoveryEJB/src/java/gov/hhs/fha/nhinc/adapter/subjectdiscovery/AdapterSubjectDiscoveryImpl.java
@@ -11,7 +11,6 @@ import org.hl7.v3.II;
 import org.hl7.v3.MCCIIN000002UV01;
 import org.hl7.v3.PIXConsumerPRPAIN201301UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201302UVRequestType;
-import org.hl7.v3.PIXConsumerPRPAIN201303UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201304UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201309UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201309UVResponseType;
@@ -30,29 +29,29 @@ public class AdapterSubjectDiscoveryImpl {
         String receiverOID = null;
         
         if (request != null &&
-                request.getPRPAIN201301UV() != null) {
+                request.getPRPAIN201301UV02() != null) {
             // Extract the message id
-            if (request.getPRPAIN201301UV().getId() != null) {
-                msgId = request.getPRPAIN201301UV().getId();
+            if (request.getPRPAIN201301UV02().getId() != null) {
+                msgId = request.getPRPAIN201301UV02().getId();
             }
 
             // Set the sender OID to the receiver OID from the original message
-            if (NullChecker.isNotNullish(request.getPRPAIN201301UV().getReceiver()) &&
-                    request.getPRPAIN201301UV().getReceiver().get(0) != null &&
-                    request.getPRPAIN201301UV().getReceiver().get(0).getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201301UV().getReceiver().get(0).getDevice().getId()) &&
-                    request.getPRPAIN201301UV().getReceiver().get(0).getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201301UV().getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
-                senderOID = request.getPRPAIN201301UV().getReceiver().get(0).getDevice().getId().get(0).getRoot();
+            if (NullChecker.isNotNullish(request.getPRPAIN201301UV02().getReceiver()) &&
+                    request.getPRPAIN201301UV02().getReceiver().get(0) != null &&
+                    request.getPRPAIN201301UV02().getReceiver().get(0).getDevice() != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201301UV02().getReceiver().get(0).getDevice().getId()) &&
+                    request.getPRPAIN201301UV02().getReceiver().get(0).getDevice().getId().get(0) != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201301UV02().getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
+                senderOID = request.getPRPAIN201301UV02().getReceiver().get(0).getDevice().getId().get(0).getRoot();
             }
 
             // Set the receiver OID to the sender OID from the original message
-            if (request.getPRPAIN201301UV().getSender() != null &&
-                    request.getPRPAIN201301UV().getSender().getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201301UV().getSender().getDevice().getId()) &&
-                    request.getPRPAIN201301UV().getSender().getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201301UV().getSender().getDevice().getId().get(0).getRoot())) {
-                receiverOID = request.getPRPAIN201301UV().getSender().getDevice().getId().get(0).getRoot();
+            if (request.getPRPAIN201301UV02().getSender() != null &&
+                    request.getPRPAIN201301UV02().getSender().getDevice() != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201301UV02().getSender().getDevice().getId()) &&
+                    request.getPRPAIN201301UV02().getSender().getDevice().getId().get(0) != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201301UV02().getSender().getDevice().getId().get(0).getRoot())) {
+                receiverOID = request.getPRPAIN201301UV02().getSender().getDevice().getId().get(0).getRoot();
             }
 
             ack = HL7AckTransforms.createAckMessage(null, msgId, AGENCY_ACK_MSG, senderOID, receiverOID);
@@ -68,67 +67,29 @@ public class AdapterSubjectDiscoveryImpl {
         String receiverOID = null;
 
         if (request != null &&
-                request.getPRPAIN201302UV() != null) {
+                request.getPRPAIN201302UV02() != null) {
             // Extract the message id
-            if (request.getPRPAIN201302UV().getId() != null) {
-                msgId = request.getPRPAIN201302UV().getId();
+            if (request.getPRPAIN201302UV02().getId() != null) {
+                msgId = request.getPRPAIN201302UV02().getId();
             }
 
             // Set the sender OID to the receiver OID from the original message
-            if (NullChecker.isNotNullish(request.getPRPAIN201302UV().getReceiver()) &&
-                    request.getPRPAIN201302UV().getReceiver().get(0) != null &&
-                    request.getPRPAIN201302UV().getReceiver().get(0).getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201302UV().getReceiver().get(0).getDevice().getId()) &&
-                    request.getPRPAIN201302UV().getReceiver().get(0).getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201302UV().getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
-                senderOID = request.getPRPAIN201302UV().getReceiver().get(0).getDevice().getId().get(0).getRoot();
+            if (NullChecker.isNotNullish(request.getPRPAIN201302UV02().getReceiver()) &&
+                    request.getPRPAIN201302UV02().getReceiver().get(0) != null &&
+                    request.getPRPAIN201302UV02().getReceiver().get(0).getDevice() != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201302UV02().getReceiver().get(0).getDevice().getId()) &&
+                    request.getPRPAIN201302UV02().getReceiver().get(0).getDevice().getId().get(0) != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201302UV02().getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
+                senderOID = request.getPRPAIN201302UV02().getReceiver().get(0).getDevice().getId().get(0).getRoot();
             }
 
             // Set the receiver OID to the sender OID from the original message
-            if (request.getPRPAIN201302UV().getSender() != null &&
-                    request.getPRPAIN201302UV().getSender().getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201302UV().getSender().getDevice().getId()) &&
-                    request.getPRPAIN201302UV().getSender().getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201302UV().getSender().getDevice().getId().get(0).getRoot())) {
-                receiverOID = request.getPRPAIN201302UV().getSender().getDevice().getId().get(0).getRoot();
-            }
-
-            ack = HL7AckTransforms.createAckMessage(null, msgId, AGENCY_ACK_MSG, senderOID, receiverOID);
-        }
-
-        return ack;
-    }
-
-    public MCCIIN000002UV01 pixConsumerPRPAIN201303UV(PIXConsumerPRPAIN201303UVRequestType request, WebServiceContext context) {
-        MCCIIN000002UV01 ack = new MCCIIN000002UV01();
-        II msgId = new II();
-        String senderOID = null;
-        String receiverOID = null;
-
-        if (request != null &&
-                request.getPRPAIN201303UV() != null) {
-            // Extract the message id
-            if (request.getPRPAIN201303UV().getId() != null) {
-                msgId = request.getPRPAIN201303UV().getId();
-            }
-
-            // Set the sender OID to the receiver OID from the original message
-            if (NullChecker.isNotNullish(request.getPRPAIN201303UV().getReceiver()) &&
-                    request.getPRPAIN201303UV().getReceiver().get(0) != null &&
-                    request.getPRPAIN201303UV().getReceiver().get(0).getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201303UV().getReceiver().get(0).getDevice().getId()) &&
-                    request.getPRPAIN201303UV().getReceiver().get(0).getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201303UV().getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
-                senderOID = request.getPRPAIN201303UV().getReceiver().get(0).getDevice().getId().get(0).getRoot();
-            }
-
-            // Set the receiver OID to the sender OID from the original message
-            if (request.getPRPAIN201303UV().getSender() != null &&
-                    request.getPRPAIN201303UV().getSender().getDevice() != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201303UV().getSender().getDevice().getId()) &&
-                    request.getPRPAIN201303UV().getSender().getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getPRPAIN201303UV().getSender().getDevice().getId().get(0).getRoot())) {
-                receiverOID = request.getPRPAIN201303UV().getSender().getDevice().getId().get(0).getRoot();
+            if (request.getPRPAIN201302UV02().getSender() != null &&
+                    request.getPRPAIN201302UV02().getSender().getDevice() != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201302UV02().getSender().getDevice().getId()) &&
+                    request.getPRPAIN201302UV02().getSender().getDevice().getId().get(0) != null &&
+                    NullChecker.isNotNullish(request.getPRPAIN201302UV02().getSender().getDevice().getId().get(0).getRoot())) {
+                receiverOID = request.getPRPAIN201302UV02().getSender().getDevice().getId().get(0).getRoot();
             }
 
             ack = HL7AckTransforms.createAckMessage(null, msgId, AGENCY_ACK_MSG, senderOID, receiverOID);
--- Product/Production/Adapters/General/AdapterSubjectDiscoveryEJB/src/java/gov/hhs/fha/nhinc/adapter/subjectdiscovery/AdapterSubjectDiscoverySecured.java
+++ Product/Production/Adapters/General/AdapterSubjectDiscoveryEJB/src/java/gov/hhs/fha/nhinc/adapter/subjectdiscovery/AdapterSubjectDiscoverySecured.java
@@ -5,7 +5,6 @@
 
 package gov.hhs.fha.nhinc.adapter.subjectdiscovery;
 
-import gov.hhs.fha.nhinc.adaptersubjectdiscoverysecured.AdapterSubjectDiscoverySecuredPortType;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
@@ -17,22 +16,19 @@ import javax.xml.ws.WebServiceContext;
  */
 @WebService(serviceName = "AdapterSubjectDiscoverySecured", portName = "AdapterSubjectDiscoverySecuredPortSoap11", endpointInterface = "gov.hhs.fha.nhinc.adaptersubjectdiscoverysecured.AdapterSubjectDiscoverySecuredPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:adaptersubjectdiscoverysecured", wsdlLocation = "META-INF/wsdl/AdapterSubjectDiscoverySecured/AdapterSubjectDiscoverySecured.wsdl")
 @Stateless
-public class AdapterSubjectDiscoverySecured implements AdapterSubjectDiscoverySecuredPortType {
+public class AdapterSubjectDiscoverySecured {
+
     @Resource
     private WebServiceContext context;
 
     public org.hl7.v3.MCCIIN000002UV01 pixConsumerPRPAIN201301UV(org.hl7.v3.PIXConsumerPRPAIN201301UVRequestType pixConsumerPRPAIN201301UVRequest) {
-        return new AdapterSubjectDiscoveryImpl().pixConsumerPRPAIN201301UV(pixConsumerPRPAIN201301UVRequest, context);
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     public org.hl7.v3.MCCIIN000002UV01 pixConsumerPRPAIN201302UV(org.hl7.v3.PIXConsumerPRPAIN201302UVRequestType pixConsumerPRPAIN201302UVRequest) {
-        return new AdapterSubjectDiscoveryImpl().pixConsumerPRPAIN201302UV(pixConsumerPRPAIN201302UVRequest, context);
-
-    }
-
-    public org.hl7.v3.MCCIIN000002UV01 pixConsumerPRPAIN201303UV(org.hl7.v3.PIXConsumerPRPAIN201303UVRequestType pixConsumerPRPAIN201303UVRequest) {
-        return new AdapterSubjectDiscoveryImpl().pixConsumerPRPAIN201303UV(pixConsumerPRPAIN201303UVRequest, context);
-
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     public org.hl7.v3.MCCIIN000002UV01 pixConsumerPRPAIN201304UV(org.hl7.v3.PIXConsumerPRPAIN201304UVRequestType pixConsumerPRPAIN201304UVRequest) {
@@ -42,7 +38,6 @@ public class AdapterSubjectDiscoverySecured implements AdapterSubjectDiscoverySe
 
     public org.hl7.v3.PIXConsumerPRPAIN201309UVResponseType pixConsumerPRPAIN201309UV(org.hl7.v3.PIXConsumerPRPAIN201309UVRequestType pixConsumerPRPAIN201309UVRequest) {
         return new AdapterSubjectDiscoveryImpl().pixConsumerPRPAIN201309UV(pixConsumerPRPAIN201309UVRequest, context);
-
     }
 
 }
