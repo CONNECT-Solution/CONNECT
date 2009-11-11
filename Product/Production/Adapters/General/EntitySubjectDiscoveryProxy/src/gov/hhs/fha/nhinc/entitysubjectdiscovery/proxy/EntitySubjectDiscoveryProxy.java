4d7ac28f5e3fb666f7332b10c85b098690fa924c
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-11 04:03:57 +0000

    Checkin HL7 Updates

--- Product/Production/Adapters/General/EntitySubjectDiscoveryProxy/src/gov/hhs/fha/nhinc/entitysubjectdiscovery/proxy/EntitySubjectDiscoveryProxy.java
+++ Product/Production/Adapters/General/EntitySubjectDiscoveryProxy/src/gov/hhs/fha/nhinc/entitysubjectdiscovery/proxy/EntitySubjectDiscoveryProxy.java
@@ -3,7 +3,6 @@ package gov.hhs.fha.nhinc.entitysubjectdiscovery.proxy;
 import org.hl7.v3.MCCIIN000002UV01;
 import org.hl7.v3.PIXConsumerPRPAIN201301UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201302UVRequestType;
-import org.hl7.v3.PIXConsumerPRPAIN201303UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201304UVRequestType;
 import org.hl7.v3.PIXConsumerPRPAIN201309UVResponseType;
 import org.hl7.v3.PIXConsumerPRPAIN201309UVRequestType;
@@ -12,7 +11,6 @@ public interface EntitySubjectDiscoveryProxy {
 
     public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PIXConsumerPRPAIN201301UVRequestType body);
     public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PIXConsumerPRPAIN201302UVRequestType body);
-    public MCCIIN000002UV01 pixConsumerPRPAIN201303UV(PIXConsumerPRPAIN201303UVRequestType body);
     public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PIXConsumerPRPAIN201304UVRequestType body);
     public PIXConsumerPRPAIN201309UVResponseType pixConsumerPRPAIN201309UV(PIXConsumerPRPAIN201309UVRequestType request);
 
