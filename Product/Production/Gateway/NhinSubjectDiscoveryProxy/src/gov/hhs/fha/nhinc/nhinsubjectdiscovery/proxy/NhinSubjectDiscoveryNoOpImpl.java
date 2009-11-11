386e9f2503d2be3118fc15363f4a6ea70d5d6d0a
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-11 05:06:07 +0000

    Checkin HL7 Updates

--- Product/Production/Gateway/NhinSubjectDiscoveryProxy/src/gov/hhs/fha/nhinc/nhinsubjectdiscovery/proxy/NhinSubjectDiscoveryNoOpImpl.java
+++ Product/Production/Gateway/NhinSubjectDiscoveryProxy/src/gov/hhs/fha/nhinc/nhinsubjectdiscovery/proxy/NhinSubjectDiscoveryNoOpImpl.java
@@ -8,12 +8,11 @@ package gov.hhs.fha.nhinc.nhinsubjectdiscovery.proxy;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
 import org.hl7.v3.MCCIIN000002UV01;
-import org.hl7.v3.PRPAIN201301UV;
-import org.hl7.v3.PRPAIN201302UV;
-import org.hl7.v3.PRPAIN201303UV;
-import org.hl7.v3.PRPAIN201304UV;
-import org.hl7.v3.PRPAIN201309UV;
-import org.hl7.v3.PRPAIN201310UV;
+import org.hl7.v3.PRPAIN201301UV02;
+import org.hl7.v3.PRPAIN201302UV02;
+import org.hl7.v3.PRPAIN201304UV02;
+import org.hl7.v3.PRPAIN201309UV02;
+import org.hl7.v3.PRPAIN201310UV02;
 
 /**
  *
@@ -21,24 +20,20 @@ import org.hl7.v3.PRPAIN201310UV;
  */
 public class NhinSubjectDiscoveryNoOpImpl implements NhinSubjectDiscoveryProxy {
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PRPAIN201301UV body, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PRPAIN201301UV02 body, AssertionType assertion, NhinTargetSystemType target) {
         return new MCCIIN000002UV01();
     }
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PRPAIN201302UV body, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PRPAIN201302UV02 body, AssertionType assertion, NhinTargetSystemType target) {
         return new MCCIIN000002UV01();
     }
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201303UV(PRPAIN201303UV body, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PRPAIN201304UV02 body, AssertionType assertion, NhinTargetSystemType target) {
         return new MCCIIN000002UV01();
     }
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PRPAIN201304UV body, AssertionType assertion, NhinTargetSystemType target) {
-        return new MCCIIN000002UV01();
-    }
-
-    public PRPAIN201310UV pixConsumerPRPAIN201309UV(PRPAIN201309UV body, AssertionType assertion, NhinTargetSystemType target) {
-        return new PRPAIN201310UV();
+    public PRPAIN201310UV02 pixConsumerPRPAIN201309UV(PRPAIN201309UV02 body, AssertionType assertion, NhinTargetSystemType target) {
+        return new PRPAIN201310UV02();
     }
 
 }
