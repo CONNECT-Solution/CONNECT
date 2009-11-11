29bddb2dc312cec9b27256c7a11f39ce2957606c
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-11 04:02:09 +0000

    Checkin HL7 Updates

--- Product/Production/Adapters/General/DocumentRepositoryEJB/src/java/gov/hhs/fha/nhinc/document/DocumentRegistryService.java
+++ Product/Production/Adapters/General/DocumentRepositoryEJB/src/java/gov/hhs/fha/nhinc/document/DocumentRegistryService.java
@@ -5,17 +5,16 @@
 
 package gov.hhs.fha.nhinc.document;
 
-import ihe.iti.xds_b._2007.DocumentRegistryPortType;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 /**
  *
- * @author user1
+ * @author jhoppesc
  */
 @WebService(serviceName = "DocumentRegistry_Service", portName = "DocumentRegistry_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.DocumentRegistryPortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "META-INF/wsdl/DocumentRegistryService/AdapterComponentDocRegistry.wsdl")
 @Stateless
-public class DocumentRegistryService implements DocumentRegistryPortType {
+public class DocumentRegistryService {
 
     public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType documentRegistryRegisterDocumentSetB(oasis.names.tc.ebxml_regrep.xsd.lcm._3.SubmitObjectsRequest body)
     {
@@ -27,17 +26,17 @@ public class DocumentRegistryService implements DocumentRegistryPortType {
         return new DocumentRegistryHelper().documentRegistryRegistryStoredQuery(body);
     }
 
-    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201301UV02(org.hl7.v3.PRPAIN201301UV body)
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201301UV02(org.hl7.v3.PRPAIN201301UV02 body)
     {
         throw new UnsupportedOperationException("Not supported.");
     }
 
-    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201302UV02(org.hl7.v3.PRPAIN201302UV body)
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201302UV02(org.hl7.v3.PRPAIN201302UV02 body)
     {
         throw new UnsupportedOperationException("Not supported.");
     }
 
-    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201304UV02(org.hl7.v3.PRPAIN201304UV body)
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201304UV02(org.hl7.v3.PRPAIN201304UV02 body)
     {
         throw new UnsupportedOperationException("Not supported.");
     }
--- Product/Production/Adapters/General/DocumentRepositoryEJB/src/java/gov/hhs/fha/nhinc/document/DocumentRepositoryService.java
+++ Product/Production/Adapters/General/DocumentRepositoryEJB/src/java/gov/hhs/fha/nhinc/document/DocumentRepositoryService.java
@@ -5,17 +5,16 @@
 
 package gov.hhs.fha.nhinc.document;
 
-import ihe.iti.xds_b._2007.DocumentRepositoryPortType;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 /**
  *
- * @author user1
+ * @author jhoppesc
  */
 @WebService(serviceName = "DocumentRepository_Service", portName = "DocumentRepository_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.DocumentRepositoryPortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "META-INF/wsdl/DocumentRepositoryService/AdapterComponentDocRepository.wsdl")
 @Stateless
-public class DocumentRepositoryService implements DocumentRepositoryPortType {
+public class DocumentRepositoryService {
 
     public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType documentRepositoryProvideAndRegisterDocumentSetB(ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType body)
     {
