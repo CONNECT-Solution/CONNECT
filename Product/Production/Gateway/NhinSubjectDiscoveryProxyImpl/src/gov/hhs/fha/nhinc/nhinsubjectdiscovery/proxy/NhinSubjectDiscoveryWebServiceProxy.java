1e2cc7f1ae4af45c27a7920a987522a46f0fdb96
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-11 05:08:46 +0000

    Checkin HL7 Updates

--- Product/Production/Gateway/NhinSubjectDiscoveryProxyImpl/src/gov/hhs/fha/nhinc/nhinsubjectdiscovery/proxy/NhinSubjectDiscoveryWebServiceProxy.java
+++ Product/Production/Gateway/NhinSubjectDiscoveryProxyImpl/src/gov/hhs/fha/nhinc/nhinsubjectdiscovery/proxy/NhinSubjectDiscoveryWebServiceProxy.java
@@ -18,12 +18,11 @@ import javax.xml.ws.BindingProvider;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.v3.MCCIIN000002UV01;
-import org.hl7.v3.PRPAIN201301UV;
-import org.hl7.v3.PRPAIN201302UV;
-import org.hl7.v3.PRPAIN201303UV;
-import org.hl7.v3.PRPAIN201304UV;
-import org.hl7.v3.PRPAIN201309UV;
-import org.hl7.v3.PRPAIN201310UV;
+import org.hl7.v3.PRPAIN201301UV02;
+import org.hl7.v3.PRPAIN201302UV02;
+import org.hl7.v3.PRPAIN201304UV02;
+import org.hl7.v3.PRPAIN201309UV02;
+import org.hl7.v3.PRPAIN201310UV02;
 
 /**
  *
@@ -34,7 +33,7 @@ public class NhinSubjectDiscoveryWebServiceProxy implements NhinSubjectDiscovery
     private static Log log = LogFactory.getLog(NhinSubjectDiscoveryWebServiceProxy.class);
     static PIXConsumerService nhinService = new PIXConsumerService();
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PRPAIN201301UV request, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PRPAIN201301UV02 request, AssertionType assertion, NhinTargetSystemType target) {
         String url = null;
         MCCIIN000002UV01 ack = new MCCIIN000002UV01();
 
@@ -58,7 +57,7 @@ public class NhinSubjectDiscoveryWebServiceProxy implements NhinSubjectDiscovery
         return ack;
     }
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PRPAIN201302UV request, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PRPAIN201302UV02 request, AssertionType assertion, NhinTargetSystemType target) {
         String url = null;
         MCCIIN000002UV01 ack = new MCCIIN000002UV01();
 
@@ -83,38 +82,13 @@ public class NhinSubjectDiscoveryWebServiceProxy implements NhinSubjectDiscovery
         return ack;
     }
 
-    public MCCIIN000002UV01 pixConsumerPRPAIN201303UV(PRPAIN201303UV request, AssertionType assertion, NhinTargetSystemType target) {
-        String url = null;
-        MCCIIN000002UV01 ack = new MCCIIN000002UV01();
-
-        // Get the URL to the Nhin Subject Discovery Service
-        url = getUrl(target);
-
-        if (NullChecker.isNotNullish(url)) {
-            PIXConsumerPortType port = getPort(url);
-
-            SamlTokenCreator tokenCreator = new SamlTokenCreator();
-            Map requestContext = tokenCreator.CreateRequestContext(assertion, url, NhincConstants.SUBJECT_DISCOVERY_ACTION);
-
-            ((BindingProvider) port).getRequestContext().putAll(requestContext);
-
-            ack = port.pixConsumerPRPAIN201303UV(request);
-
-        } else {
-            log.error("The URL for service: " + NhincConstants.SUBJECT_DISCOVERY_SERVICE_NAME + " is null");
-        }
-
-
-        return ack;
-    }
-
-    public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PRPAIN201304UV request, AssertionType assertion, NhinTargetSystemType target) {
+    public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PRPAIN201304UV02 request, AssertionType assertion, NhinTargetSystemType target) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public PRPAIN201310UV pixConsumerPRPAIN201309UV(PRPAIN201309UV request, AssertionType assertion, NhinTargetSystemType target) {
+    public PRPAIN201310UV02 pixConsumerPRPAIN201309UV(PRPAIN201309UV02 request, AssertionType assertion, NhinTargetSystemType target) {
         String url = null;
-        PRPAIN201310UV resp = new PRPAIN201310UV();
+        PRPAIN201310UV02 resp = new PRPAIN201310UV02();
 
         // Get the URL to the Nhin Subject Discovery Service
         url = getUrl(target);
