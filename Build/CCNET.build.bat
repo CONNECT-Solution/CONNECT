1dffafde83d9c85fb5ea95e39195e3b744a72894
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-12 17:49:21 +0000

    Remove build server startup, replaced with generic one

--- Build/CCNET.build.bat
+++
@@ -1,8 +0,0 @@
-FOR /F "TOKENS=4 DELIMS=\," %%A IN ('cd') DO set CodeLineName=%%A
-FOR /F "TOKENS=3 DELIMS=\," %%A IN ('cd') DO set ProjectName=%%A
-
-title NHINC %CodeLineName% Build Server
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
-
-cd server
-CCNET.exe -config:"..\CCNet.build.xml" -remoting:on
