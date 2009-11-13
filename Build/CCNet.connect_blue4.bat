ea6757ed7753c9ed0a7ec3625373e47570d0e5d8
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-13 17:54:15 +0000

    Remove build servers separate startup files, replaced with generic one

--- Build/CCNet.connect_blue4.bat
+++
@@ -1,8 +0,0 @@
-FOR /F "TOKENS=4 DELIMS=\," %%A IN ('cd') DO set CodeLineName=%%A
-FOR /F "TOKENS=3 DELIMS=\," %%A IN ('cd') DO set ProjectName=%%A
-
-title NHINC %CodeLineName% CONNECT_BLUE4 Build Server
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
-
-cd server
-CCNET.exe -config:"..\CCNet.connect_blue4.xml" -remoting:on
