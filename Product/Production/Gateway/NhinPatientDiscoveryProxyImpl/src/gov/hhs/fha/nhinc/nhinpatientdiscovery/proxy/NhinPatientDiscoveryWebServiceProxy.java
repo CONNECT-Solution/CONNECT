239b25813a7ccbd850efdd0b8e6b305687f314bf
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-15 12:39:35 +0000

    Add Nhin patient discovery proxy projects

---
+++ Product/Production/Gateway/NhinPatientDiscoveryProxyImpl/src/gov/hhs/fha/nhinc/nhinpatientdiscovery/proxy/NhinPatientDiscoveryWebServiceProxy.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.hhs.fha.nhinc.nhinpatientdiscovery.proxy;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import gov.hhs.fha.nhinc.connectmgr.ConnectionManagerCache;
+import gov.hhs.fha.nhinc.connectmgr.ConnectionManagerException;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenCreator;
+import ihe.iti.xcpd._2009.RespondingGatewayPortType;
+import ihe.iti.xcpd._2009.RespondingGatewayService;
+import java.util.Map;
+import javax.xml.ws.BindingProvider;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hl7.v3.PRPAIN201305UV02;
+import org.hl7.v3.PRPAIN201306UV02;
+
+/**
+ *
+ * @author jhoppesc
+ */
+public class NhinPatientDiscoveryWebServiceProxy implements NhinPatientDiscoveryProxy {
+
+    private static Log log = LogFactory.getLog(NhinPatientDiscoveryWebServiceProxy.class);
+    static RespondingGatewayService nhinService = new RespondingGatewayService();
+
+    public PRPAIN201306UV02 respondingGatewayPRPAIN201305UV02(PRPAIN201305UV02 request, AssertionType assertion, NhinTargetSystemType target) {
+        String url = null;
+        PRPAIN201306UV02 response = new PRPAIN201306UV02();
+
+        // Get the URL to the Nhin Subject Discovery Service
+        url = getUrl(target);
+
+        if (NullChecker.isNotNullish(url)) {
+            RespondingGatewayPortType port = getPort(url);
+
+            SamlTokenCreator tokenCreator = new SamlTokenCreator();
+            Map requestContext = tokenCreator.CreateRequestContext(assertion, url, NhincConstants.PATIENT_DISCOVERY_ACTION);
+
+            ((BindingProvider) port).getRequestContext().putAll(requestContext);
+
+            response = port.respondingGatewayPRPAIN201305UV02(request);
+
+        } else {
+            log.error("The URL for service: " + NhincConstants.PATIENT_DISCOVERY_SERVICE_NAME + " is null");
+        }
+
+        return response;
+    }
+
+    private RespondingGatewayPortType getPort(String url) {
+        RespondingGatewayPortType port = nhinService.getRespondingGatewayPortSoap12();
+
+        log.info("Setting endpoint address to Nhin Patient Discovery Service to " + url);
+        ((BindingProvider) port).getRequestContext().put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+
+        return port;
+    }
+
+    private String getUrl(NhinTargetSystemType target) {
+        String url = null;
+
+        if (target != null) {
+            try {
+                url = ConnectionManagerCache.getEndpontURLFromNhinTarget(target, NhincConstants.PATIENT_DISCOVERY_SERVICE_NAME);
+            } catch (ConnectionManagerException ex) {
+                log.error("Error: Failed to retrieve url for service: " + NhincConstants.PATIENT_DISCOVERY_SERVICE_NAME);
+                log.error(ex.getMessage());
+            }
+        } else {
+            log.error("Target system passed into the proxy is null");
+        }
+
+        return url;
+    }
+}
