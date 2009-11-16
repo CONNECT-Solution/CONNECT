fb56d29aeddc6ebc846d2d270554d891c0c327df
shawc <shawc@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-16 15:14:53 +0000

    Task #20688 state:open Task #20690 state:open comments: initial add/checkpoint for checking patient correlation expiration date.

--- Product/Production/Gateway/PatientCorrelationDAO/test/gov/hhs/fha/nhinc/patientcorrelation/dao/CorrelatedIdentifiersDaoTest.java
+++ Product/Production/Gateway/PatientCorrelationDAO/test/gov/hhs/fha/nhinc/patientcorrelation/dao/CorrelatedIdentifiersDaoTest.java
@@ -6,6 +6,7 @@
 package gov.hhs.fha.nhinc.patientcorrelation.dao;
 
 import gov.hhs.fha.nhinc.patientcorrelation.model.CorrelatedIdentifiers;
+import java.util.Date;
 import org.junit.Test;
 
 /**
@@ -30,28 +31,34 @@ public class CorrelatedIdentifiersDaoTest {
         ci.setCorrelatedPatientId("D123401");
         ci.setPatientAssigningAuthorityId("2.16.840.1.113883.4.1");
         ci.setPatientId("123456789");
+        ci.setCorrelationExpirationDate(new Date());
         dao.addPatientCorrelation(ci);
         CorrelatedIdentifiers ci1 = new CorrelatedIdentifiers();
         ci1.setCorrelatedPatientAssigningAuthorityId("2.16.840.1.113883.3.192");
         ci1.setCorrelatedPatientId("1018");
         ci1.setPatientAssigningAuthorityId("2.16.840.1.113883.3.200");
         ci1.setPatientId("500000000");
+        ci1.setCorrelationExpirationDate(new Date());
         dao.addPatientCorrelation(ci1);
         CorrelatedIdentifiers ci2 = new CorrelatedIdentifiers();
         ci2.setCorrelatedPatientAssigningAuthorityId("2.16.840.1.113883.3.200");
         ci2.setCorrelatedPatientId("500000000");
         ci2.setPatientAssigningAuthorityId("2.16.840.1.113883.3.192");
         ci2.setPatientId("1018");
+        ci2.setCorrelationExpirationDate(new Date());
         dao.addPatientCorrelation(ci2);        CorrelatedIdentifiers ci3 = new CorrelatedIdentifiers();
         ci3.setCorrelatedPatientAssigningAuthorityId("2.16.840.1.113883.4.1");
         ci3.setCorrelatedPatientId("123456789");
         ci3.setPatientAssigningAuthorityId("2.16.840.1.113883.3.200");
         ci3.setPatientId("500000000");
-         dao.addPatientCorrelation(ci3);        CorrelatedIdentifiers ci4 = new CorrelatedIdentifiers();
+        ci3.setCorrelationExpirationDate(new Date());
+        dao.addPatientCorrelation(ci3);
+        CorrelatedIdentifiers ci4 = new CorrelatedIdentifiers();
         ci4.setCorrelatedPatientAssigningAuthorityId("2.16.840.1.113883.3.200");
         ci4.setCorrelatedPatientId("500000000");
         ci4.setPatientAssigningAuthorityId("2.16.840.1.113883.4.1");
         ci4.setPatientId("123456789");
+        ci4.setCorrelationExpirationDate(new Date());
         dao.addPatientCorrelation(ci4 );
     }
      
