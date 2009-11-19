8b5eb11bbab6021680e0f4c35a44db795cefaf89
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-19 19:42:58 +0000

    Changed project directory structure for AdapterAuthenticationProxy, AdapterAuthenticanLib, AdapterAuthenticationEJB, AdapterAuthenticationProxyImpl

---
+++ Product/Production/Adapters/General/AdapterAuthenticationProxyImpl/src/main/java/gov/hhs/fha/nhinc/adapterauthentication/proxy/AdapterAuthenticationWebServiceProxy.java
@@ -0,0 +1,95 @@
+package gov.hhs.fha.nhinc.adapterauthentication.proxy;
+
+import gov.hhs.fha.nhinc.adapterauthentication.AdapterAuthentication;
+import gov.hhs.fha.nhinc.adapterauthentication.AdapterAuthenticationPortType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AuthenticateUserRequestType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AuthenticateUserResponseType;
+import gov.hhs.fha.nhinc.connectmgr.ConnectionManagerCache;
+import gov.hhs.fha.nhinc.adapterauthentication.AdapterAuthenticationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This is the concrete implementation for the Web based call to the
+ * AdapterAuthentication.
+ */
+public class AdapterAuthenticationWebServiceProxy {
+
+    private static Log log = LogFactory.getLog(AdapterAuthenticationWebServiceProxy.class);
+    private static AdapterAuthentication authService = null;
+    private static String ADAPTER_AUTH_SERVICE_NAME = "adapterauthentication";
+    private static String ADAPTER_AUTH_DEFAULT_SERVICE_URL = "http://localhost:8080/NhinConnect/AdapterAuthentication";
+
+
+    /**
+     * Given a request to authenticate a user, this service will determine if
+     * this is an identifiable user within OpenSSO and if so will provide an
+     * identifying token.
+     * @param authenticateUserRequest The request to authenticate the user
+     * @return The response which indicates if an authentication service is
+     * implemented and if so the resulting token identifier
+     */
+    public AuthenticateUserResponseType authenticateUser(AuthenticateUserRequestType authenticateUserRequest) {
+
+        AuthenticateUserResponseType authResp = new AuthenticateUserResponseType();
+
+        try
+        {
+            AdapterAuthenticationPortType authPort = getAdapterAuthenticationPort();
+            authResp = authPort.authenticateUser(authenticateUserRequest);
+        }
+        catch (Exception ex)
+        {
+            String message = "Error occurred calling AdapterAuthenticationWebServiceProxy.authenticateUser.  Error: " +
+                                   ex.getMessage();
+            log.error(message, ex);
+            throw new RuntimeException(message, ex);
+        }
+
+        return authResp;
+    }
+
+    /**
+     * Return a handle to the AdapterAuthentication web service.
+     *
+     * @return The handle to the Adapter Authentication port web service.
+     */
+    private AdapterAuthenticationPortType getAdapterAuthenticationPort()
+        throws AdapterAuthenticationException
+    {
+        AdapterAuthenticationPortType authPort = null;
+
+        try
+        {
+            if (authService == null)
+            {
+                authService = new AdapterAuthentication();
+            }
+
+            authPort = authService.getAdapterAuthenticationPortSoap11();
+
+            // Get the real endpoint URL for this service.
+            String endpointURL = ConnectionManagerCache.getLocalEndpointURLByServiceName(ADAPTER_AUTH_SERVICE_NAME);
+
+            if ((endpointURL == null) ||
+                (endpointURL.length() <= 0))
+            {
+                endpointURL = ADAPTER_AUTH_DEFAULT_SERVICE_URL;
+                String message = "Failed to retrieve the Endpoint URL for service: '" +
+                                       ADAPTER_AUTH_SERVICE_NAME + "'.  " +
+                                       "Setting this to: '" + endpointURL + "'";
+                log.warn(message);
+            }
+            ((javax.xml.ws.BindingProvider)authPort).getRequestContext().put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpointURL);
+        }
+        catch (Exception ex)
+        {
+            String message = "Failed to retrieve a handle to the Adapter PIP web service.  Error: " +
+                                   ex.getMessage();
+            log.error(message, ex);
+            throw new AdapterAuthenticationException(message, ex);
+        }
+
+        return authPort;
+    }
+}
