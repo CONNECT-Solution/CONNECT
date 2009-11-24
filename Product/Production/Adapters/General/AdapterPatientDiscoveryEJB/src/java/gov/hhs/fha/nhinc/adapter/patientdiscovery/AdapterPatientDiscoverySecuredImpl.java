4328ebf228fbee27a0b791ef77544180372d0de0
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-24 23:32:26 +0000

    add in adapter patient discovery ejb

---
+++ Product/Production/Adapters/General/AdapterPatientDiscoveryEJB/src/java/gov/hhs/fha/nhinc/adapter/patientdiscovery/AdapterPatientDiscoverySecuredImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.adapter.patientdiscovery;
+
+import javax.xml.ws.WebServiceContext;
+import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
+import org.hl7.v3.RespondingGatewayPRPAIN201306UV02ResponseType;
+
+/**
+ *
+ * @author jhoppesc
+ */
+public class AdapterPatientDiscoverySecuredImpl {
+
+    public RespondingGatewayPRPAIN201306UV02ResponseType respondingGatewayPRPAIN201305UV02(RespondingGatewayPRPAIN201305UV02RequestType request, WebServiceContext context) {
+        RespondingGatewayPRPAIN201306UV02ResponseType response = new RespondingGatewayPRPAIN201306UV02ResponseType();
+        return response;
+    }
+
+}
