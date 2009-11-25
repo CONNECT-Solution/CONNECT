bd2b5361d72bfa54ed0f8deae15ac3378593ee04
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-11-25 23:06:12 +0000

    changed project structure for EntityDocRetrieveProxyEJB EntityHiemSubscriptionProxyEJB EntityPatientDiscoveryProxyEJB EntitySubjectDiscoveryProxy EntitySubjectDiscoveryProxyEJB EntitySubjectDiscoveryProxyImpl MpiEJB MpiLib MpiManagerEJB PatientCorrelationProxy UniversalClientGUI

---
+++ Product/Production/Adapters/General/EntitySubjectDiscoveryProxy/src/main/java/gov/hhs/fha/nhinc/entitysubjectdiscovery/proxy/EntitySubjectDiscoveryNoOpImpl.java
@@ -0,0 +1,30 @@
+package gov.hhs.fha.nhinc.entitysubjectdiscovery.proxy;
+
+import org.hl7.v3.MCCIIN000002UV01;
+import org.hl7.v3.PIXConsumerPRPAIN201301UVRequestType;
+import org.hl7.v3.PIXConsumerPRPAIN201302UVRequestType;
+import org.hl7.v3.PIXConsumerPRPAIN201304UVRequestType;
+import org.hl7.v3.PIXConsumerPRPAIN201309UVResponseType;
+import org.hl7.v3.PIXConsumerPRPAIN201309UVRequestType;
+import org.hl7.v3.PRPAIN201310UV02;
+
+public class EntitySubjectDiscoveryNoOpImpl implements EntitySubjectDiscoveryProxy {
+ 
+    public MCCIIN000002UV01 pixConsumerPRPAIN201301UV(PIXConsumerPRPAIN201301UVRequestType body) {
+        return new MCCIIN000002UV01();
+    }
+
+    public MCCIIN000002UV01 pixConsumerPRPAIN201302UV(PIXConsumerPRPAIN201302UVRequestType body) {
+        return new MCCIIN000002UV01();
+    }
+
+    public MCCIIN000002UV01 pixConsumerPRPAIN201304UV(PIXConsumerPRPAIN201304UVRequestType body) {
+        return new MCCIIN000002UV01();
+    }
+
+    public PIXConsumerPRPAIN201309UVResponseType pixConsumerPRPAIN201309UV(PIXConsumerPRPAIN201309UVRequestType request) {
+        PIXConsumerPRPAIN201309UVResponseType respType = new PIXConsumerPRPAIN201309UVResponseType();
+        respType.setPRPAIN201310UV02(new PRPAIN201310UV02());
+        return respType;
+    }
+}
