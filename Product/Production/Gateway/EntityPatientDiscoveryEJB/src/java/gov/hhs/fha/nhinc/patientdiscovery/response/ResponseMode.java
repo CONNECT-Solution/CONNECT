392db2a95c7e8585dff003c13cf30028efa7700a
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-12-03 17:17:41 +0000

    user story #20837. Patient Discovery - Sending XCPD Request (Outbound)

---
+++ Product/Production/Gateway/EntityPatientDiscoveryEJB/src/java/gov/hhs/fha/nhinc/patientdiscovery/response/ResponseMode.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.patientdiscovery.response;
+import org.hl7.v3.PRPAIN201306UV02;
+/**
+ *
+ * @author dunnek
+ */
+public interface ResponseMode {
+    public PRPAIN201306UV02 processResponse(PRPAIN201306UV02 response);
+
+}
---
+++ Product/Production/Gateway/EntityPatientDiscoveryEJB/src/java/gov/hhs/fha/nhinc/patientdiscovery/response/TrustMode.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.patientdiscovery.response;
+import org.hl7.v3.PRPAIN201306UV02;
+/**
+ *
+ * @author dunnek
+ */
+public class TrustMode implements ResponseMode{
+    public PRPAIN201306UV02 processResponse(PRPAIN201306UV02 response)
+    {
+        return response;
+    }
+}
