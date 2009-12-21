d282acb4ae769032477659b93e14cb44664144f2
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2009-12-21 04:38:40 +0000

    comments: add deploy.populateTestData target to database deployment script / update mysql driver location - use environment variable / fix populateTestData.sql syntax error author: Mark

--- Product/DBScripts/deploy.bat
+++ Product/DBScripts/deploy.bat
@@ -10,7 +10,7 @@ IF EXIST ..\..\Build\Packages\Ant\Ant\bin\ant.bat. (
 
 cd %BaseDir%
 set ANT_OPTS=-Xmx512m -XX:MaxPermSize=256m
-set JAVA_HOME=C:\Java\jdk1.6.0_13
+set JAVA_HOME=C:\Java\jdk1.6.0_16
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%JAVA_HOME%\bin;%ANT_HOME%\bin;
 
 ant -buildfile deploy.xml %*
