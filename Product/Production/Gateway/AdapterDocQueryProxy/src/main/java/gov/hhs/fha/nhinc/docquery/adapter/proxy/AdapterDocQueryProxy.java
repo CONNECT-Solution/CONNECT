17374166b3aec3d527feb4a128d9480d0cd0f883
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-01-25 18:25:49 +0000

    Consolidate projects below to NhinServiceProxy and Deleted them. Updated EJB projects dependency as required. •	NhinAuditQueryProxy •	NhinAuditQueryProxyImpl •	NhinDocQueryProxy •	NhinDocQueryProxyImpl •	NhinDocRetrieveProxy •	NhinDocRetrieveProxyImpl •	NhinPatientDiscoveryProxy •	NhinPatientDiscoveryProxyImpl •	NhinSubjectDiscoveryProxy •	NhinSubjectDiscoveryProxyImpl

--- Product/Production/Gateway/AdapterDocQueryProxy/src/main/java/gov/hhs/fha/nhinc/docquery/adapter/proxy/AdapterDocQueryProxy.java
+++
@@ -1,9 +0,0 @@
-package gov.hhs.fha.nhinc.docquery.adapter.proxy;
-
-import gov.hhs.fha.nhinc.common.nhinccommonadapter.RespondingGatewayCrossGatewayQueryRequestType;
-import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
-
-public interface AdapterDocQueryProxy
-{
-    public AdhocQueryResponse respondingGatewayCrossGatewayQuery(RespondingGatewayCrossGatewayQueryRequestType respondingGatewayCrossGatewayQueryRequest);
-}
