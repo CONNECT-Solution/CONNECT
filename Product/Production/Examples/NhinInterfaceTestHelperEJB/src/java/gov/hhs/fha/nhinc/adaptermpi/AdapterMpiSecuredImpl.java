066b77f799c889d5b2d21fcf1147ec2623d4b9fb
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-01-27 18:12:08 +0000

    k-task#27458 comments: cleanup. reverting change for location of NhinInterfaceTestHelperEJB project

---
+++ Product/Production/Examples/NhinInterfaceTestHelperEJB/src/java/gov/hhs/fha/nhinc/adaptermpi/AdapterMpiSecuredImpl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.adaptermpi;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractorHelper;
+import javax.xml.ws.WebServiceContext;
+import gov.hhs.fha.nhinc.adaptercomponentmpi.AdapterComponentMpiService;
+import gov.hhs.fha.nhinc.adaptercomponentmpi.AdapterComponentMpiPortType;
+import org.hl7.v3.PRPAIN201305UV02;
+import org.hl7.v3.PRPAIN201306UV02;
+
+/**
+ *
+ * @author dunnek
+ */
+public class AdapterMpiSecuredImpl {
+    private static Log log = LogFactory.getLog(AdapterMpiSecuredImpl.class);
+    private static final String SERVICE_NAME = "mockadaptermpi";
+    private static AdapterComponentMpiService service = new AdapterComponentMpiService();
+    
+    public org.hl7.v3.PRPAIN201306UV02 findCandidates(org.hl7.v3.PRPAIN201305UV02 findCandidatesRequest,WebServiceContext context) {
+        PRPAIN201306UV02 response =null;
+
+        String homeCommunityId = SamlTokenExtractorHelper.getHomeCommunityId();
+        if (NullChecker.isNotNullish(homeCommunityId)) {
+
+            AdapterComponentMpiPortType port = service.getAdapterComponentMpiPort();
+            response = port.findCandidates(findCandidatesRequest);
+            ((javax.xml.ws.BindingProvider) port).getRequestContext().put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, SamlTokenExtractorHelper.getEndpointURL(homeCommunityId, SERVICE_NAME));
+        }
+
+        return response;
+    }
+}
