aaca8a55e5d87fc814831d42f28e0175eb2d6607
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-01 19:53:55 +0000

    User Story#21987, Status: In progress, Description: Converting Adapter EJB projects to POJO based Services. Removed EntityDocQueryProxyEJB and integrated to AdapterWeb

--- Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/docquery/proxy/NhincProxyDocQuery.java
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/docquery/proxy/NhincProxyDocQuery.java
@@ -10,8 +10,7 @@ import javax.jws.WebService;
 public class NhincProxyDocQuery {
 
     public oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse respondingGatewayCrossGatewayQuery(gov.hhs.fha.nhinc.common.nhinccommonproxy.RespondingGatewayCrossGatewayQueryRequestType respondingGatewayCrossGatewayQueryRequest) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return new NhincProxyDocQueryImpl().respondingGatewayCrossGatewayQuery(respondingGatewayCrossGatewayQueryRequest);
     }
 
 }
