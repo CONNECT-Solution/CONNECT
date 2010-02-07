c546e6e8fedf364ece4168da17fed355b8c8b0c5
mflynn02 <mflynn02@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-07 14:09:13 +0000

    User Story: 22189 Task: 21680

--- Product/Production/Adapters/Framework/DocumentAssemblyManagerEJB/src/main/java/gov/hhs/fha/nhinc/assemblymanager/utils/XMLUtil.java
+++ Product/Production/Adapters/Framework/DocumentAssemblyManagerEJB/src/main/java/gov/hhs/fha/nhinc/assemblymanager/utils/XMLUtil.java
@@ -16,22 +16,22 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import nux.xom.pool.XOMUtil;
 import org.hl7.v3.CareRecordQUPCIN043200UV01ResponseType;
 import org.hl7.v3.ENExplicit;
-import org.hl7.v3.EnExplicitFamily;
-import org.hl7.v3.EnExplicitGiven;
-import org.hl7.v3.EnExplicitPrefix;
-import org.hl7.v3.EnExplicitSuffix;
 import org.hl7.v3.ObjectFactory;
 import org.hl7.v3.PNExplicit;
 import org.hl7.v3.POCDMT000040ClinicalDocument;
 import org.hl7.v3.POCDMT000040Person;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import gov.hhs.fha.nhinc.transform.subdisc.HL7DataTransformHelper;
 
 /**
  *
  * @author kim
  */
 public class XMLUtil {
+   private static Log log = LogFactory.getLog(XMLUtil.class);
 
    public static byte[] toCanonicalXMLBytes(POCDMT000040ClinicalDocument pObject) throws JAXBException {
       ObjectFactory factory = new ObjectFactory();
@@ -101,30 +101,12 @@ public class XMLUtil {
    }
 
    public static void setName(List<ENExplicit> names, POCDMT000040Person person) {
-      org.hl7.v3.ObjectFactory factory = new org.hl7.v3.ObjectFactory();
-      PNExplicit personName = factory.createPNExplicit();
+//      org.hl7.v3.ObjectFactory factory = new org.hl7.v3.ObjectFactory();
+      PNExplicit personName = null;
 
       if (names.size() > 0) {
          ENExplicit name = names.get(0);
-         for (int i = 0; i < name.getContent().size(); i++) {
-            JAXBElement o = (JAXBElement) name.getContent().get(i);
-            if (o != null && o.getValue().getClass().getName().equalsIgnoreCase("org.hl7.v3.EnExplicitFamily")) {
-               EnExplicitFamily ob = (EnExplicitFamily) o.getValue();
-               personName.getContent().add(ob.getContent());
-            } else if (o.getValue().getClass().getName().equalsIgnoreCase("org.hl7.v3.EnExplicitGiven")) {
-               EnExplicitGiven ob = (EnExplicitGiven) o.getValue();
-               personName.getContent().add(ob.getContent());
-            } else if (o.getValue().getClass().getName().equalsIgnoreCase("org.hl7.v3.EnExplicitPrefix")) {
-               EnExplicitPrefix ob = (EnExplicitPrefix) o.getValue();
-               personName.getContent().add(ob.getContent());
-            } else if (o.getValue().getClass().getName().equalsIgnoreCase("org.hl7.v3.EnExplicitSuffix")) {
-               EnExplicitSuffix ob = (EnExplicitSuffix) o.getValue();
-               personName.getContent().add(ob.getContent());
-            } else if (o.getValue().getClass().getName().equalsIgnoreCase("java.lang.String")) {
-               String ob = (String) o.getValue();
-               personName.getContent().add(ob);
-            }
-         }
+         personName = HL7DataTransformHelper.convertENtoPN(name);
       }
 
       person.getName().add(personName);
