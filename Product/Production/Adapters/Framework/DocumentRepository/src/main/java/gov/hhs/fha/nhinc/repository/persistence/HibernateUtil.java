838382094d8e0954d2b6109c83767c499eda095d
mflynn02 <mflynn02@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-07 15:05:58 +0000

    User Story: 22189 Task: 21680

--- Product/Production/Adapters/Framework/DocumentRepository/src/main/java/gov/hhs/fha/nhinc/repository/persistence/HibernateUtil.java
+++ Product/Production/Adapters/Framework/DocumentRepository/src/main/java/gov/hhs/fha/nhinc/repository/persistence/HibernateUtil.java
@@ -1,7 +1,12 @@
 package gov.hhs.fha.nhinc.repository.persistence;
 
+import gov.hhs.fha.nhinc.properties.HibernateAccessor;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.io.File;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Utility to obtain hibernate connections.
@@ -12,14 +17,14 @@ public class HibernateUtil
 {
 
     private static final SessionFactory sessionFactory;
-    
+    private static Log log = LogFactory.getLog(HibernateUtil.class);
 
     static
     {
         try
         {
-            // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+            // Create the SessionFactory from HIBERNATE_DOCUMENT_REPOSITORY
+            sessionFactory = new Configuration().configure(getConfigFile()).buildSessionFactory();
         } catch (Throwable ex)
         {
             // Make sure you log the exception, as it might be swallowed
@@ -32,4 +37,22 @@ public class HibernateUtil
     {
         return sessionFactory;
     }
+        private static File getConfigFile(){
+        File result = null;
+
+        try
+        {
+            result = HibernateAccessor.getHibernateFile(NhincConstants.HIBERNATE_DOCUMENT_REPOSITORY);
+        }
+        catch (Exception ex)
+        {
+            log.error("Unable to load " + NhincConstants.HIBERNATE_DOCUMENT_REPOSITORY + " " + ex.getMessage(), ex );
+        }
+
+
+        return result;
+
+
+    }
+
 }
