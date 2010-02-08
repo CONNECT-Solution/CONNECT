7035363fd45fbf037a1e5fb4e73e5fa794618989
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-08 02:56:28 +0000

    User Story 21907 - Added Unsecured Entity and Proxy classes to AdapterWeb

---
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/xdr/entity/EntityXDR.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.xdr.entity;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author dunnek
+ */
+@WebService(serviceName = "EntityXDR_Service", portName = "EntityXDR_Port", endpointInterface = "gov.hhs.fha.nhinc.nhincentityxdr.EntityXDRPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincentityxdr", wsdlLocation = "WEB-INF/wsdl/EntityXDR/EntityXDR.wsdl")
+public class EntityXDR {
+
+    public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType provideAndRegisterDocumentSetB(gov.hhs.fha.nhinc.common.nhinccommonentity.RespondingGatewayProvideAndRegisterDocumentSetRequestType body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
---
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/xdr/proxy/ProxyXDR.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.xdr.proxy;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author dunnek
+ */
+@WebService(serviceName = "ProxyXDR_Service", portName = "ProxyXDR_Port", endpointInterface = "gov.hhs.fha.nhinc.nhincproxyxdr.ProxyXDRPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincproxyxdr", wsdlLocation = "WEB-INF/wsdl/ProxyXDR/NhincProxyXDR.wsdl")
+public class ProxyXDR {
+
+    public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType provideAndRegisterDocumentSetB(gov.hhs.fha.nhinc.common.nhinccommonproxy.RespondingGatewayProvideAndRegisterDocumentSetRequestType body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
