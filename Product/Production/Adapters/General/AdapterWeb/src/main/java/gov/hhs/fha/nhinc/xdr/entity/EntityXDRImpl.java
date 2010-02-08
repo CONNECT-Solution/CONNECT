0660b68d5d484c66acb0ad55d9e149e5860ed0bf
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-08 16:42:25 +0000

    user story User Story 21907 - Added unsecured Entity and Proxy implementation classes.

---
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/xdr/entity/EntityXDRImpl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.xdr.entity;
+import gov.hhs.fha.nhinc.common.nhinccommon.AcknowledgementType;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import gov.hhs.fha.nhinc.properties.PropertyAccessException;
+import gov.hhs.fha.nhinc.properties.PropertyAccessor;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.xml.ws.WebServiceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ *
+ * @author dunnek
+ */
+public class EntityXDRImpl {
+
+    private static Log log = null;
+    public EntityXDRImpl()
+    {
+        log = createLogger();
+    }
+    protected Log createLogger()
+    {
+        return ((log != null) ? log : LogFactory.getLog(getClass()));
+    }
+}
