e8ba7104aba00ee35f218959c8c4a4bc35a6ee29
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-02-16 22:06:52 +0000

    User Story: #21907, Task: #22561 - Forward provideAndRegisterDocument message from Nhin component to AdapterCompoment

--- Product/Production/Common/NhincCommonLib/src/main/java/gov/hhs/fha/nhinc/nhinclib/NhincConstants.java
+++ Product/Production/Common/NhincCommonLib/src/main/java/gov/hhs/fha/nhinc/nhinclib/NhincConstants.java
@@ -60,6 +60,7 @@ public class NhincConstants {
     public static final String DOC_RETRIEVE_ACTION = "retrieveDocuments";
     public static final String SUBJECT_DISCOVERY_ACTION = "subjectDiscovery";
     public static final String PATIENT_DISCOVERY_ACTION = "patientDiscovery";
+    public static final String ADAPTER_XDR_ACTION = "adapterXDRSecured";
     public static final String AUDIT_REPO_ACTION = "auditrepository";
     public static final String POLICY_ENGINE_ACTION = "policyengine";
     public static final String PAT_CORR_ACTION = "patientcorrelation";
