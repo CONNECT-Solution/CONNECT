c6411e4c969ed12feeca4df5f6d16629412350f8
webbn <webbn@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-03-08 14:56:36 +0000

    story: 22864 comments: Review of JUnit integration tests for NhincCommonLib project. Updated references and moved JUnit Tests where appropriate.

--- Product/IntegrationTest/JunitIntegrationTests/Common/NhincCommonLib/src/test/java/gov/hhs/fha/nhinc/util/HomeCommunityMapTest.java
+++ Product/IntegrationTest/JunitIntegrationTests/Common/NhincCommonLib/src/test/java/gov/hhs/fha/nhinc/util/HomeCommunityMapTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,6 +13,7 @@ import static org.junit.Assert.*;
  * 
  * @author Les Westberg
  */
+@Ignore
 public class HomeCommunityMapTest
 {
 
