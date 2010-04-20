0b4ba2d36d12ed54de86d15d1b433beef4d9cae0
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-20 11:47:40 +0000

    Fix Regression Tests

--- Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/adapter/patientdiscovery/async/response/AdapterPatientDiscoverySecuredAsyncRespImpl.java
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/adapter/patientdiscovery/async/response/AdapterPatientDiscoverySecuredAsyncRespImpl.java
@@ -58,10 +58,14 @@ public class AdapterPatientDiscoverySecuredAsyncRespImpl {
             if (NullChecker.isNotNullish(request.getReceiver()) &&
                     request.getReceiver().get(0) != null &&
                     request.getReceiver().get(0).getDevice() != null &&
-                    NullChecker.isNotNullish(request.getReceiver().get(0).getDevice().getId()) &&
-                    request.getReceiver().get(0).getDevice().getId().get(0) != null &&
-                    NullChecker.isNotNullish(request.getReceiver().get(0).getDevice().getId().get(0).getRoot())) {
-                senderOID = request.getReceiver().get(0).getDevice().getId().get(0).getRoot();
+                    request.getReceiver().get(0).getDevice().getAsAgent() != null &&
+                    request.getReceiver().get(0).getDevice().getAsAgent().getValue() != null &&
+                    request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization() != null &&
+                    request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization().getValue() != null &&
+                    NullChecker.isNotNullish(request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization().getValue().getId()) &&
+                    request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization().getValue().getId().get(0) != null &&
+                    NullChecker.isNotNullish(request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization().getValue().getId().get(0).getRoot())) {
+                senderOID = request.getReceiver().get(0).getDevice().getAsAgent().getValue().getRepresentedOrganization().getValue().getId().get(0).getRoot();
             }
 
             // Set the receiver OID to the sender OID from the original message
