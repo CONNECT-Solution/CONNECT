6590a6594597745246c963170cb646abb975a8be
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-23 01:16:21 +0000

    Check in proxy updates

---
+++ Product/Production/Gateway/NhinServiceProxy/src/main/java/gov/hhs/fha/nhinc/nhinpatientdiscovery/async/response/proxy/NhinPatientDiscoveryAsyncRespProxy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.nhinpatientdiscovery.async.response.proxy;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import org.hl7.v3.MCCIIN000002UV01;
+import org.hl7.v3.PRPAIN201306UV02;
+
+/**
+ *
+ * @author JHOPPESC
+ */
+public interface NhinPatientDiscoveryAsyncRespProxy {
+
+    public MCCIIN000002UV01 respondingGatewayPRPAIN201306UV02(PRPAIN201306UV02 body, AssertionType assertion, NhinTargetSystemType target);
+
+}
---
+++ Product/Production/Gateway/NhinServiceProxy/src/test/java/gov/hhs/fha/nhinc/nhinpatientdiscovery/async/response/proxy/NhinPatientDiscoveryAsyncRespNoOpImplTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.nhinpatientdiscovery.async.response.proxy;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import org.hl7.v3.MCCIIN000002UV01;
+import org.hl7.v3.PRPAIN201306UV02;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JHOPPESC
+ */
+public class NhinPatientDiscoveryAsyncRespNoOpImplTest {
+
+    public NhinPatientDiscoveryAsyncRespNoOpImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of respondingGatewayPRPAIN201306UV02 method, of class NhinPatientDiscoveryAsyncRespNoOpImpl.
+     */
+    @Test
+    public void testRespondingGatewayPRPAIN201306UV02() {
+        System.out.println("testRespondingGatewayPRPAIN201306UV02");
+        PRPAIN201306UV02 body = new PRPAIN201306UV02();
+        AssertionType assertion = new AssertionType();
+        NhinTargetSystemType target = new NhinTargetSystemType();
+        NhinPatientDiscoveryAsyncRespNoOpImpl instance = new NhinPatientDiscoveryAsyncRespNoOpImpl();
+       
+        MCCIIN000002UV01 result = instance.respondingGatewayPRPAIN201306UV02(body, assertion, target);
+
+        assertNotNull(result);
+    }
+
+}
