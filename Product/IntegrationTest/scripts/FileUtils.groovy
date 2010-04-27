9f5942d4d2696d0d2e7d246c410572403a3fa7da
vadlamanis <vadlamanis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-27 20:04:53 +0000

    K-Task #28172. added scripts to start/stop GF from SOAP UI Test

--- Product/IntegrationTest/scripts/FileUtils.groovy
+++ Product/IntegrationTest/scripts/FileUtils.groovy
@@ -214,17 +214,48 @@ class FileUtils
 	}
 
   static InitializeNHINCProperties(context, log) { 
-	log.info( "Start directory copy "); 
-	try{ 
-		File destDir = new File(System.env['NHINC_PROPERTIES_DIR']);   
-		File sourceDir = new File(System.env['NHINC_SOURCE_DIR']+"/Product/Production/Common/Properties/Dev");   
-		org.apache.commons.io.FileUtils.copyDirectory(sourceDir, destDir,false); 
-	}catch(Throwable e) 
-	{ 
-		e.printStackTrace(); 
-		return; 
-	} 
-	log.info( "Done directory copy "); 
+    log.info( "Start directory copy "); 
+    try{ 
+      File destDir = new File(System.env['NHINC_PROPERTIES_DIR']);   
+      File sourceDir = new File(System.env['NHINC_SOURCE_DIR']+"/Product/Production/Common/Properties/Dev");   
+      org.apache.commons.io.FileUtils.copyDirectory(sourceDir, destDir,false); 
+    }catch(Throwable e) 
+    { 
+      e.printStackTrace(); 
+      return; 
+    } 
+    log.info( "Done directory copy "); 
   } 
+  
+  static stopGlassFishDomain(context, log){
+    try{
+      def os = System.getProperty("os.name");
+      def GF_HOME = System.env['AS_HOME'];
+      if (os ==~ /(?i)windows.*/){
+        def proc = "${GF_HOME}/bin/asadmin.bat stop-domain domain1".execute();
+        log.info( "Found text ${proc.text}");
+      }else {
+        def proc = "${GF_HOME}/bin/asadmin stop-domain domain1".execute();
+        log.info( "Found text ${proc.text}");
+      }
+    }catch(Throwable e){
+      e.printStackTrace(); 
+    }
+  }
+  
+    static startGlassFishDomain(context, log){
+    try{
+      def os = System.getProperty("os.name");
+      def GF_HOME = System.env['AS_HOME'];
+      if (os ==~ /(?i)windows.*/){
+        def proc = "${GF_HOME}/bin/asadmin.bat start-domain domain1".execute();
+        log.info( "Found text ${proc.text}");
+      }else {
+        def proc = "${GF_HOME}/bin/asadmin start-domain domain1".execute();
+        log.info( "Found text ${proc.text}");
+      }
+    }catch(Throwable e){
+      e.printStackTrace(); 
+    }
+  }
  } 
-
