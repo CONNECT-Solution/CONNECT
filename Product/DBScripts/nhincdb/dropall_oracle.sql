6c1504d28302d6a1631998644159c37fae9fc1b1
saundersr <saundersr@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-27 13:28:55 +0000

    Changes to support CONNECT running with Oracle.

---
+++ Product/DBScripts/nhincdb/dropall_oracle.sql
@@ -0,0 +1,14 @@
+
+DROP TABLE nhincuser.agg_transaction;
+DROP TABLE nhincuser.agg_message_results;
+DROP TABLE nhincuser.aa_to_home_community_mapping;
+DROP TABLE nhincuser.auditrepository;
+DROP TABLE nhincuser.document;
+DROP TABLE nhincuser.eventcode;
+DROP TABLE nhincuser.correlatedidentifiers;
+DROP TABLE nhincuser.subscription;
+DROP TABLE nhincuser.asyncmsgrepo;
+
+DROP USER nhincuser;
+
+commit;
