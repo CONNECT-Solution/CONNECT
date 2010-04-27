89f94a4d37db10794ec723f732e3a8752d18e8b4
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-27 03:36:07 +0000

    Task: #23852 Implement WS-Addressing container for the message id and the relates to id

--- Product/Production/Gateway/NhinInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/async/request/NhinPatientDiscoveryAsyncReq.java
+++ Product/Production/Gateway/NhinInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/async/request/NhinPatientDiscoveryAsyncReq.java
@@ -14,13 +14,13 @@ import javax.xml.ws.WebServiceContext;
  *
  * @author JHOPPESC
  */
-@WebService(serviceName = "RespondingGatewayAsyncReq_Service", portName = "RespondingGatewayAsyncReq_Port", endpointInterface = "ihe.iti.xcpd._2009.RespondingGatewayAsyncReqPortType", targetNamespace = "urn:ihe:iti:xcpd:2009", wsdlLocation = "WEB-INF/wsdl/NhinPatientDiscoveryAsyncReq/NhinPatientDiscoveryAsyncReq.wsdl")
+@WebService(serviceName = "RespondingGatewayDeferredRequest_Service", portName = "RespondingGatewayDeferredRequest_Port", endpointInterface = "ihe.iti.xcpd._2009.RespondingGatewayDeferredRequestPortType", targetNamespace = "urn:ihe:iti:xcpd:2009", wsdlLocation = "WEB-INF/wsdl/NhinPatientDiscoveryAsyncReq/NhinPatientDiscoveryDeferredRequest.wsdl")
 @BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
 public class NhinPatientDiscoveryAsyncReq {
     @Resource
     private WebServiceContext context;
 
-    public org.hl7.v3.MCCIIN000002UV01 respondingGatewayPRPAIN201305UV02(org.hl7.v3.PRPAIN201305UV02 body) {
+    public org.hl7.v3.MCCIIN000002UV01 respondingGatewayDeferredPRPAIN201305UV02(org.hl7.v3.PRPAIN201305UV02 body) {
         return new NhinPatientDiscoveryAsyncReqImpl().respondingGatewayPRPAIN201305UV02(body, context);
     }
 
