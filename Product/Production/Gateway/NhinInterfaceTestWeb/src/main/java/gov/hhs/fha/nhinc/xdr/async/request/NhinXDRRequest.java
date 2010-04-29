bbebcfa8141702cfd3c2a9fbf6246cedbbb126cc
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-04-29 14:31:22 +0000

    User Story#23777 Task#23519, 235666, 23568 Status: Completed. Comments:Code Upgraded to Soap1.2

--- Product/Production/Gateway/NhinInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/request/NhinXDRRequest.java
+++ Product/Production/Gateway/NhinInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/request/NhinXDRRequest.java
@@ -1,19 +1,15 @@
 package gov.hhs.fha.nhinc.xdr.async.request;
 
 import javax.jws.WebService;
-import javax.xml.ws.BindingType;
 
 /**
  *
- * @author Neil Webb
+ * @author Sai Valluripalli
  */
-@WebService(serviceName = "XDRRequest_Service", portName = "XDRRequest_Port_Soap12", endpointInterface = "ihe.iti.xdr.async.request._2007.XDRRequestPortType", targetNamespace = "urn:ihe:iti:xdr:async:request:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRRequest/NhinXDRRequest.wsdl")
-@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
-public class NhinXDRRequest
-{
+@WebService(serviceName = "XDRRequest_Service", portName = "XDRRequest_Port_Soap", endpointInterface = "ihe.iti.xdr.async.request._2007.XDRRequestPortType", targetNamespace = "urn:ihe:iti:xdr:async:request:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRRequest/NhinXDRRequest.wsdl")
+public class NhinXDRRequest {
 
-    public ihe.iti.xdr._2007.AcknowledgementType provideAndRegisterDocumentSetBRequest(ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType body)
-    {
+    public ihe.iti.xdr._2007.AcknowledgementType provideAndRegisterDocumentSetBRequest(ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType body) {
         return new NhinXDRRequestImpl().provideAndRegisterDocumentSetBRequest(body);
     }
 
