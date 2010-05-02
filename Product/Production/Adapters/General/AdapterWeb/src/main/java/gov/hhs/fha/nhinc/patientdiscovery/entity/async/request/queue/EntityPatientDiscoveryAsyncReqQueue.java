9df78ff65f58e1d09959c8a9126ef6f9080b5d79
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-05-02 01:47:16 +0000

    Task: #24047 Created unsecure entity pd queue web service

--- Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/entity/async/request/queue/EntityPatientDiscoveryAsyncReqQueue.java
+++ Product/Production/Adapters/General/AdapterWeb/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/entity/async/request/queue/EntityPatientDiscoveryAsyncReqQueue.java
@@ -5,8 +5,10 @@
 
 package gov.hhs.fha.nhinc.patientdiscovery.entity.async.request.queue;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
 
 /**
  *
@@ -15,9 +17,11 @@ import javax.xml.ws.BindingType;
 @WebService(serviceName = "EntityPatientDiscoveryAsyncReqQueue", portName = "EntityPatientDiscoveryAsyncReqQueuePortSoap", endpointInterface = "gov.hhs.fha.nhinc.entitypatientdiscoveryasyncreqqueue.EntityPatientDiscoveryAsyncReqQueuePortType", targetNamespace = "urn:gov:hhs:fha:nhinc:entitypatientdiscoveryasyncreqqueue", wsdlLocation = "WEB-INF/wsdl/EntityPatientDiscoveryAsyncReqQueue/EntityPatientDiscoveryAsyncReqQueue.wsdl")
 @BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
 public class EntityPatientDiscoveryAsyncReqQueue {
+    @Resource
+    private WebServiceContext context;
 
     public org.hl7.v3.MCCIIN000002UV01 addPatientDiscoveryAsyncReq(org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType addPatientDiscoveryAsyncReqAsyncRequest) {
-        return new EntityPatientDiscoveryAsyncReqQueueImpl().addPatientDiscoveryAsyncReq(addPatientDiscoveryAsyncReqAsyncRequest);
+        return new EntityPatientDiscoveryAsyncReqQueueImpl().addPatientDiscoveryAsyncReq(addPatientDiscoveryAsyncReqAsyncRequest, context);
     }
 
 }
