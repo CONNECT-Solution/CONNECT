8ac764cf69bd0016ca75db5ac628838466d25b42
saundersr <saundersr@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-05-05 16:01:16 +0000

    Support for Oracle.

--- Product/Production/Gateway/NhincGatewayDAO/src/main/java/gov/hhs/fha/nhinc/subscription/repository/persistence/HibernateUtil.java
+++ Product/Production/Gateway/NhincGatewayDAO/src/main/java/gov/hhs/fha/nhinc/subscription/repository/persistence/HibernateUtil.java
@@ -2,6 +2,8 @@ package gov.hhs.fha.nhinc.subscription.repository.persistence;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import gov.hhs.fha.nhinc.properties.HibernateAccessor;
+
 
 /**
  * Utility to create hibernate connections
@@ -17,7 +19,7 @@ public class HibernateUtil
         try
         {
             // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure("HiemSubRepHibernate.cfg.xml").buildSessionFactory();
+            sessionFactory = new Configuration().configure(HibernateAccessor.getHibernateFile("HiemSubRepHibernate.cfg.xml")).buildSessionFactory();
         } 
         catch (Throwable ex)
         {
