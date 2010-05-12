77ecd3afc316cf9b3ca42dae5399ba8690d34e7e
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-05-12 16:50:16 +0000

    Remove unused property

--- Product/Production/Common/NhincCommonLib/src/main/java/gov/hhs/fha/nhinc/nhinclib/NhincConstants.java
+++ Product/Production/Common/NhincCommonLib/src/main/java/gov/hhs/fha/nhinc/nhinclib/NhincConstants.java
@@ -250,7 +250,6 @@ public class NhincConstants {
     public static final String PATIENT_DISCOVERY_ASYNC_RESP_SERVICE_NAME = "PatientDiscoveryAsyncResp";
     public static final String PATIENT_DISCOVERY_SERVICE_PASSTHRU_PROPERTY = "patientDiscoveryPassthrough";
     public static final String PATIENT_DISCOVERY_SERVICE_ASYNC_REQ_PASSTHRU_PROPERTY = "patientDiscoveryAsyncReqPassthrough";
-    public static final String PATIENT_DISCOVERY_SERVICE_ASYNC_RESP_PASSTHRU_PROPERTY = "patientDiscoveryAsyncRespPassthrough";
     public static final String PATIENT_DISCOVERY_DISABLED_ACK_MSG = "Patient Discovery Service is not enabled";
     public static final String PATIENT_DISCOVERY_POLICY_FAILED_ACK_MSG = "Policy Check failed for Patient Discovery";
     public static final String PATIENT_DISCOVERY_ADAPTER_SERVICE_NAME = "adapterpatientdiscovery";
