f0b9e40d04ed9af208b56ac59631d5c91e3b2ade
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-05-22 14:42:09 +0000

    Task: #24530 Check in lift database methods

--- Product/Production/Gateway/GatewayWeb/src/main/java/gov/hhs/fha/nhinc/adapter/xdr/async/request/error/AdapterXDRRequestError.java
+++ Product/Production/Gateway/GatewayWeb/src/main/java/gov/hhs/fha/nhinc/adapter/xdr/async/request/error/AdapterXDRRequestError.java
@@ -17,11 +17,10 @@ import javax.xml.ws.WebServiceContext;
 @WebService(serviceName = "AdapterXDRRequestError_Service", portName = "AdapterXDRRequestError_Port", endpointInterface = "gov.hhs.fha.nhinc.adapterxdrrequesterror.AdapterXDRRequestErrorPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:adapterxdrrequesterror", wsdlLocation = "WEB-INF/wsdl/AdapterXDRRequestError/AdapterXDRRequestError.wsdl")
 @BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
 public class AdapterXDRRequestError {
-    
     @Resource
     private WebServiceContext context;
 
-    public gov.hhs.healthit.nhin.XDRAcknowledgementType provideAndRegisterDocumentSetBRequestError(gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterProvideAndRegisterDocumentSetRequestType body) {
+    public gov.hhs.healthit.nhin.XDRAcknowledgementType provideAndRegisterDocumentSetBRequestError(gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterProvideAndRegisterDocumentSetRequestErrorType body) {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
--- Product/Production/Gateway/GatewayWeb/src/main/java/gov/hhs/fha/nhinc/adapter/xdr/async/request/error/AdapterXDRRequestErrorImpl.java
+++ Product/Production/Gateway/GatewayWeb/src/main/java/gov/hhs/fha/nhinc/adapter/xdr/async/request/error/AdapterXDRRequestErrorImpl.java
@@ -6,18 +6,26 @@
 package gov.hhs.fha.nhinc.adapter.xdr.async.request.error;
 
 import gov.hhs.fha.nhinc.async.AsyncMessageIdExtractor;
-import gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterProvideAndRegisterDocumentSetRequestType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterProvideAndRegisterDocumentSetRequestErrorType;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import gov.hhs.healthit.nhin.XDRAcknowledgementType;
 import javax.xml.ws.WebServiceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
 /**
  *
  * @author JHOPPESC
  */
 public class AdapterXDRRequestErrorImpl {
+    private static final Log log = LogFactory.getLog(AdapterXDRRequestErrorImpl.class);
 
-    public XDRAcknowledgementType provideAndRegisterDocumentSetBRequestError(AdapterProvideAndRegisterDocumentSetRequestType body, WebServiceContext context) {
+    public XDRAcknowledgementType provideAndRegisterDocumentSetBRequestError(AdapterProvideAndRegisterDocumentSetRequestErrorType body, WebServiceContext context) {
         XDRAcknowledgementType ack = new XDRAcknowledgementType();
+        RegistryResponseType regResp = new RegistryResponseType();
+        regResp.setStatus(NhincConstants.XDR_ACK_STATUS_MSG);
+        ack.setMessage(regResp);
 
         // Extract the message id value from the WS-Addressing Header and place it in the Assertion Class
         if (body != null &&
@@ -25,6 +33,8 @@ public class AdapterXDRRequestErrorImpl {
             AsyncMessageIdExtractor msgIdExtractor = new AsyncMessageIdExtractor();
             body.getAssertion().setAsyncMessageId(msgIdExtractor.GetAsyncMessageId(context));
         }
+
+        log.error("Received an error from the Gateway.  Message: " + body.getErrorMsg());
 
         return ack;
     }
