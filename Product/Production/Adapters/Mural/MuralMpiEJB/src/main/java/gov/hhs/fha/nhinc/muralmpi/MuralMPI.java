1d49ce5f9c0fc37c701c1c49d7bcfa4832e3ae28
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-05-27 20:43:39 +0000

    User Story#24626 Task#24633 Status: Completed testing and made this EJB deployable.

--- Product/Production/Adapters/Mural/MuralMpiEJB/src/main/java/gov/hhs/fha/nhinc/muralmpi/MuralMPI.java
+++ Product/Production/Adapters/Mural/MuralMpiEJB/src/main/java/gov/hhs/fha/nhinc/muralmpi/MuralMPI.java
@@ -1,18 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gov.hhs.fha.nhinc.muralmpi;
 
 import javax.ejb.Stateless;
 import javax.jws.WebService;
+import javax.xml.ws.BindingType;
 
 /**
  *
- * @author jhoppesc
+ * @author Sai Valluripalli
  */
 @WebService(serviceName = "AdapterComponentMpiService", portName = "AdapterComponentMpiPort", endpointInterface = "gov.hhs.fha.nhinc.adaptercomponentmpi.AdapterComponentMpiPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:adaptercomponentmpi", wsdlLocation = "META-INF/wsdl/MuralMPI/AdapterComponentMpi.wsdl")
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
 @Stateless
 public class MuralMPI {
 
