7d06c051d8529d324303cf23668910afa929ca46
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-06-14 22:49:07 +0000

    User Story# 23075 Task# 24908 Status: Added HIEOS contribution to Subversion repository. This is very specific Adapter code to be used only for HIEOS 1.2.

---
+++ Product/Production/Adapters/General/AdapterDocRegistry2Soap12Web/src/main/java/gov/hhs/fha/nhinc/adapterdocregistry/AdapterDocRegistry2Soap12Client.java
@@ -0,0 +1,145 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.adapterdocregistry;
+
+import gov.hhs.fha.nhinc.connectmgr.ConnectionManagerCache;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.util.UUID;
+import javax.xml.namespace.QName;
+import com.sun.xml.ws.api.message.Headers;
+import com.sun.xml.ws.api.message.Header;
+import com.sun.xml.ws.developer.WSBindingProvider;
+
+/**
+ * This class calls a SOAP 1.2 enabled document registry given a SOAP 1.1
+ * registry stored query request message.
+ *
+ * @author Anand Sastry
+ */
+public class AdapterDocRegistry2Soap12Client
+{
+    private static Log log = LogFactory.getLog(AdapterDocRegistry2Soap12Client.class);
+    private static String ADAPTER_XDS_REG_DEFAULT_SERVICE_URL = "http://localhost:8080/axis2/services/xdsregistryb";
+    private static ihe.iti.xds_b._2007.DocumentRegistryService service = null;
+
+    // CONSTANTS - Were not created in NhincConstants to simplify provisioning of this component
+    // as an adapter between CONNECT and SOAP 1.2 Registry.
+    public static final String WS_REGISTRY_STORED_QUERY_ACTION = "urn:ihe:iti:2007:RegistryStoredQuery";
+    public static final String ADAPTER_XDS_REG_SERVICE_NAME = "adapterxdsbdocregistrysoap12";
+    /**
+     * Default constructor.
+     */
+    public AdapterDocRegistry2Soap12Client()
+    {
+
+    }
+
+    /**
+     * This method connects to a soap 1.2 enabled document registry and retrieves
+     * metadata.
+     * @param body A AdhocQueryRequest object containing key criteria to query for registry metadata.
+     *
+     * @return Returns a AdhocQueryResponse containing the desired metadata.
+     *
+     */
+
+    public oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse documentRegistryRegistryStoredQuery(oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest body) {
+
+        oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse response = null;
+
+        log.debug("Entering AdapterDocRegistry2Soap12Client.documentRegistryRegistryStoredQuery() method");
+
+        try
+        {
+            //get a connection to the soap 1.2 registryStoreQuery document web service
+            ihe.iti.xds_b._2007.DocumentRegistryPortType port = getSoap12Port(WS_REGISTRY_STORED_QUERY_ACTION);
+
+            // call the soap 1.2 retrieve document web service
+            response = port.documentRegistryRegistryStoredQuery(body);
+            log.debug("RetrieveDocumentSetRequest Response = " + ((response != null) ? response.getStatus() : "null"));
+        }
+        catch (Exception e)
+        {
+            String sErrorMessage = "Failed to execute registry stored query from the soap 1.2 web service.  Error: " +
+                                   e.getMessage();
+            log.error(sErrorMessage, e);
+            throw new RuntimeException(sErrorMessage, e);
+        }
+
+        log.debug("Leaving AdapterDocRegistry2Soap12Client.documentRegistryRegistryStoredQuery() method");
+        return response;
+    }
+
+    /**
+     * This method connects to a SOAP 1.2 enabled document registry based on the
+     * configuration found in the internalConnectionInfo.xml file, creates the
+     * appropriate SOAP 1.2 header and returns a DocumentRegistryPortType object
+     * so that a registry stored query request can be made on a SOAP 1.2 enabled document registry.
+     * @param action A string representing the soap header action needed to perform a registry stored query.
+     * @return Returns a DocumentRegistryPortType object which will enable the registry stored query txn.
+     */
+    private ihe.iti.xds_b._2007.DocumentRegistryPortType getSoap12Port (String action)
+    {
+        log.debug("Entering AdapterDocRegistry2Soap12Client.getSoap12Port() method");
+
+        ihe.iti.xds_b._2007.DocumentRegistryPortType port = null;
+
+        try
+        {
+            // Call Web Service Operation
+            service = new ihe.iti.xds_b._2007.DocumentRegistryService();
+            port = service.getDocumentRegistryPortSoap();
+
+            // Get the real endpoint URL for this service.
+            //--------------------------------------------
+            //Note, set the sEndpointURL to null and comment out the ConnectionMangerCache logic if running outside of GF.
+            String sEndpointURL = ConnectionManagerCache.getLocalEndpointURLByServiceName(ADAPTER_XDS_REG_SERVICE_NAME);
+
+            if ((sEndpointURL == null) ||
+                (sEndpointURL.length() <= 0))
+            {
+                sEndpointURL = ADAPTER_XDS_REG_DEFAULT_SERVICE_URL;
+                String sErrorMessage = "Failed to retrieve the Endpoint URL for service: '" +
+                                       ADAPTER_XDS_REG_SERVICE_NAME + "'.  " +
+                                       "Setting this to: '" + sEndpointURL + "'";
+                log.warn(sErrorMessage);
+            }
+
+            ((javax.xml.ws.BindingProvider)port).getRequestContext().put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, sEndpointURL);
+
+            //add the soap header
+            List<Header> headers = new ArrayList<Header>();
+            QName qname = new QName(NhincConstants.WS_ADDRESSING_URL, NhincConstants.WS_SOAP_HEADER_ACTION);
+            Header tmpHeader = Headers.create(qname, action);
+            headers.add(tmpHeader);
+            qname = new QName(NhincConstants.WS_ADDRESSING_URL, NhincConstants.WS_SOAP_HEADER_TO);
+            tmpHeader = Headers.create(qname, sEndpointURL);
+            headers.add(tmpHeader);
+            qname = new QName(NhincConstants.WS_ADDRESSING_URL, NhincConstants.WS_SOAP_HEADER_MESSAGE_ID);
+            UUID oMessageId = UUID.randomUUID();
+            String sMessageId = oMessageId.toString();
+            tmpHeader = Headers.create(qname, NhincConstants.WS_SOAP_HEADER_MESSAGE_ID_PREFIX + sMessageId);
+            headers.add(tmpHeader);
+
+            ((WSBindingProvider)port).setOutboundHeaders(headers);
+        }
+        catch (Exception ex)
+        {
+            String sErrorMessage = "Failed to retrieve a handle to the soap 1.2 web service.  Error: " +
+                                   ex.getMessage();
+            log.error(sErrorMessage, ex);
+            throw new RuntimeException(sErrorMessage, ex);
+
+        }
+
+        log.debug("Leaving AdapterDocRegistry2Soap12Client.getSoap12Port() method");
+        return port;
+    }
+}
---
+++ Product/Production/Adapters/General/AdapterDocRegistry2Soap12Web/src/main/java/gov/hhs/fha/nhinc/adapterdocumentregistry/soap12/AdapterDocRegistry2Soap12Service.java.old
@@ -0,0 +1,73 @@
+package gov.hhs.fha.nhinc.adapterdocumentregistry.soap12;
+
+import gov.hhs.fha.nhinc.adapterdocregistry.AdapterDocRegistry2Soap12Client;
+import ihe.iti.xds_b._2007.DocumentRegistryPortType;
+import javax.jws.WebService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This web service serves as a SOAP1.1->SOAP1.2 Adapter for transforming SOAP1.1 requests initiated
+ * by CONNECT's adapters, prior to forwarding it to a SOAP 1.2 registry.
+ *
+ * @author Anand Sastry
+ */
+@WebService(serviceName = "DocumentRegistry_Service", portName = "DocumentRegistry_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.DocumentRegistryPortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "WEB-INF/wsdl/AdapterDocRegistry2Soap12Service/AdapterComponentDocRegistry.wsdl")
+public class AdapterDocRegistry2Soap12Service implements DocumentRegistryPortType {
+
+    private static Log log = LogFactory.getLog(AdapterDocRegistry2Soap12Service.class);
+
+    public oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType documentRegistryRegisterDocumentSetB(oasis.names.tc.ebxml_regrep.xsd.lcm._3.SubmitObjectsRequest body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+     public oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse documentRegistryRegistryStoredQuery(oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest body) {
+        log.debug("Entering AdapterDocRegistry2Soap12Service.documentRegistryRegistryStoredQuery() method");
+
+        oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse response = null;
+
+        try
+        {
+            if (body != null)
+            {
+                log.debug("Input request message was not null");
+
+                AdapterDocRegistry2Soap12Client oClient = new AdapterDocRegistry2Soap12Client();
+
+                response = oClient.documentRegistryRegistryStoredQuery(body);
+
+    }
+            else
+            {
+                String sErrorMessage = "Input request message was null.";
+                log.error(sErrorMessage);
+                throw new RuntimeException(sErrorMessage);
+            }
+        }
+        catch(Exception exp)
+        {
+            log.error(exp.getMessage());
+            exp.printStackTrace();
+        }
+
+        log.debug("Leaving AdapterDocRegistry2Soap12Service.documentRegistryRegistryStoredQuery() method");
+        return response;
+    }
+
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201301UV02(org.hl7.v3.PRPAIN201301UV02 body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201302UV02(org.hl7.v3.PRPAIN201302UV02 body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public org.hl7.v3.MCCIIN000002UV01 documentRegistryPRPAIN201304UV02(org.hl7.v3.PRPAIN201304UV02 body) {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
