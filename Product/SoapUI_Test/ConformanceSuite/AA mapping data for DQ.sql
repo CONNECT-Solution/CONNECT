332bff3e1cfcda8ba9bd2b6e28f3a498a58828fa
padmanabhanp <padmanabhanp@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-07-19 20:17:07 +0000

    Added new HCID to the AA mapping sql file - Parineetha

--- Product/SoapUI_Test/ConformanceSuite/AA mapping data for DQ.sql
+++ Product/SoapUI_Test/ConformanceSuite/AA mapping data for DQ.sql
@@ -9,3 +9,4 @@ DELETE FROM assigningauthoritydb.aa_to_home_community_mapping;
 
 INSERT INTO assigningauthoritydb.aa_to_home_community_mapping VALUES ('1','1.1','1.1');
 INSERT INTO assigningauthoritydb.aa_to_home_community_mapping VALUES ('2','2.2','2.2');
+INSERT INTO assigningauthoritydb.aa_to_home_community_mapping VALUES ('3','2.16.840.1.113883.3.333','2.16.840.1.113883.3.333');
