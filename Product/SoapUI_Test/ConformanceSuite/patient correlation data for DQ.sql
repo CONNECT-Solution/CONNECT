0c96a6d1abf0827d44e3747edc980c52b5801fd2
padmanabhanp <padmanabhanp@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-07-19 20:16:34 +0000

    Updated sql file with updated Patient Ids and HCID. - Parineetha

--- Product/SoapUI_Test/ConformanceSuite/patient correlation data for DQ.sql
+++ Product/SoapUI_Test/ConformanceSuite/patient correlation data for DQ.sql
@@ -7,14 +7,14 @@ LOCK TABLES patientcorrelationdb.correlatedidentifiers WRITE;
 
 DELETE from patientcorrelationdb.correlatedidentifiers;
 
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('1','1.1','0000002009','2.2','0000002009',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('2','1.1','0000002007','2.2','0000002007',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('3','1.1','0000002022','2.2','0000002022',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('4','1.1','0000002008','2.2','0000002008',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('5','1.1','0000002017','2.2','0000002017',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('6','1.1','0000002023','2.2','0000002023',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('7','1.1','0000002025','2.2','0000002025',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('8','1.1','0000002021','2.2','0000002021',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('9','1.1','0000002020','2.2','0000002020',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('10','1.1','N600011','2.2','N600011',null);
-INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('11','1.1','N999999','2.2','N999999',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('1','1.1','N600007','2.16.840.1.113883.3.333','N600007',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('2','1.1','N600002','2.16.840.1.113883.3.333','N600002',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('3','1.1','N600031','2.16.840.1.113883.3.333','N600031',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('4','1.1','N600004','2.16.840.1.113883.3.333','N600004',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('5','1.1','N600023','2.16.840.1.113883.3.333','N600023',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('6','1.1','N600038','2.16.840.1.113883.3.333','N600038',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('7','1.1','N600045','2.16.840.1.113883.3.333','N600045',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('8','1.1','N600029','2.16.840.1.113883.3.333','N600029',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('9','1.1','N600028','2.16.840.1.113883.3.333','N600028',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('10','1.1','N600011','2.16.840.1.113883.3.333','N600011',null);
+INSERT INTO patientcorrelationdb.correlatedidentifiers VALUES ('11','1.1','N999999','2.16.840.1.113883.3.333','N999999',null);
