b52bc8120a3470d198b3f6c9f899fe7fc6930b38
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-07-26 23:18:05 +0000

    User story#23156 task#26671, 26672 Status: Created EntityDocRetrieveDeferredRequest secured and unsecured classes. Also added implementation class in Core Library.

---
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/docretrievedeferred/entity/request/EntityDocRetrieveDeferredReqImplTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.docretrievedeferred.entity.request;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunitiesType;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author svalluripalli
+ */
+public class EntityDocRetrieveDeferredReqImplTest {
+
+    public EntityDocRetrieveDeferredReqImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+     @Test
+     public void testCrossGatewayRetrieveRequest() {
+        EntityDocRetrieveDeferredReqImpl testSubject = new EntityDocRetrieveDeferredReqImpl();
+        RetrieveDocumentSetRequestType message = new RetrieveDocumentSetRequestType();
+        AssertionType assertion = new AssertionType();
+        NhinTargetCommunitiesType target = new NhinTargetCommunitiesType();
+        assertNotNull(testSubject.crossGatewayRetrieveRequest(message, assertion, target));
+     }
+
+}
