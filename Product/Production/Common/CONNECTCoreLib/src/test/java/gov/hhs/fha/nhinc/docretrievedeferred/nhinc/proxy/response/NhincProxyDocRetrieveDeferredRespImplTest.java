7d15e040d8a1c39b084f62fb35200b56c109d99f
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-07-29 19:44:50 +0000

    User story# 23156 Task#26709, 26710 Status: Created Nhinc Proxy response secured/unsecured.

---
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/docretrievedeferred/nhinc/proxy/response/NhincProxyDocRetrieveDeferredRespImplTest.java
@@ -0,0 +1,48 @@
+package gov.hhs.fha.nhinc.docretrievedeferred.nhinc.proxy.response;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sai Valluripalli
+ */
+public class NhincProxyDocRetrieveDeferredRespImplTest {
+
+    public NhincProxyDocRetrieveDeferredRespImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCrossGatewayRetrieveResponse()
+    {
+        NhincProxyDocRetrieveDeferredRespImpl testSubject = new NhincProxyDocRetrieveDeferredRespImpl();
+        RetrieveDocumentSetResponseType retrieveDocumentSetResponse = new RetrieveDocumentSetResponseType();
+        AssertionType assertion = new AssertionType();
+        NhinTargetSystemType target = new NhinTargetSystemType();
+        assertNotNull(testSubject.crossGatewayRetrieveResponse(retrieveDocumentSetResponse, assertion, target));
+    }
+    
+}
