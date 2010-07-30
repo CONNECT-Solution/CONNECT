70981b5769b0d97fa1b9205bb91474c41fabc050
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-07-30 19:54:57 +0000

    Added MEssage Proxy for Admin Dist.

---
+++ Product/Production/Gateway/CONNECTMsgProxyWeb/src/main/java/gov/hhs/fha/nhinc/admindist/nhinc/NhincAdminDist.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.admindist.nhinc;
+
+import gov.hhs.fha.nhinc.admindistribution.nhinc.NhincAdminDistImpl;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author dunnek
+ */
+@WebService(serviceName = "NhincAdminDistService", portName = "NhincAdminDist_PortType", endpointInterface = "gov.hhs.fha.nhinc.nhincadmindistribution.NhincAdminDistPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincadmindistribution", wsdlLocation = "WEB-INF/wsdl/NhincProxyAdminDist/NhincAdminDist.wsdl")
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+public class NhincAdminDist {
+
+    public void sendAlertMessage(gov.hhs.fha.nhinc.common.nhinccommonproxy.RespondingGatewaySendAlertMessageType body) {
+            new NhincAdminDistImpl().sendAlertMessage(body.getEDXLDistribution(),body.getAssertion(), body.getNhinTargetSystem());
+    }
+
+}
---
+++ Product/Production/Gateway/CONNECTMsgProxyWeb/src/main/java/gov/hhs/fha/nhinc/admindist/nhinc/NhincAdminDistSecured.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.admindist.nhinc;
+
+import gov.hhs.fha.nhinc.admindistribution.nhinc.NhincAdminDistImpl;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author dunnek
+ */
+@WebService(serviceName = "NhincAdminDistSecuredService", portName = "NhincAdminDistSecured_PortType", endpointInterface = "gov.hhs.fha.nhinc.nhincadmindistribution.NhincAdminDistSecuredPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincadmindistribution", wsdlLocation = "WEB-INF/wsdl/NhincAdminDistSecured/NhincAdminDistSecured.wsdl")
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+public class NhincAdminDistSecured {
+    @Resource
+    private WebServiceContext context;
+    public void sendAlertMessage(gov.hhs.fha.nhinc.common.nhinccommonproxy.RespondingGatewaySendAlertMessageSecuredType body) {
+        AssertionType assertion = extractAssertion(context);
+
+        new NhincAdminDistImpl().sendAlertMessage(body.getEDXLDistribution(),assertion, body.getNhinTargetSystem());
+    }
+    protected AssertionType extractAssertion(WebServiceContext context)
+    {
+        return SamlTokenExtractor.GetAssertion(context);
+    }
+}
