eee6c73c2c17644959c614a13b21cf481aa6d5d2
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-05 11:28:15 +0000

    Tasks: #26894 and 26902  Add in shell services for NHIN DQ Deferred Request and Response

---
+++ Product/Production/Gateway/CONNECTNhinServicesWeb/src/main/java/gov/hhs/fha/nhinc/docquery/nhin/deferred/request/NhinDocQueryDeferredRequest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.docquery.nhin.deferred.request;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author JHOPPESC
+ */
+@WebService(serviceName = "RespondingGateway_QueryDeferredRequest_Service", portName = "RespondingGateway_QueryDeferredRequest_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.RespondingGatewayQueryDeferredRequestPortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "WEB-INF/wsdl/NhinDocQueryDeferredRequest/NhinDocQueryDeferredRequest.wsdl")
+@BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
+public class NhinDocQueryDeferredRequest {
+
+    @Resource
+    private WebServiceContext context;
+
+    public gov.hhs.healthit.nhin.DocQueryAcknowledgementType respondingGatewayCrossGatewayQuery(oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest body) {
+        AssertionType assertion = SamlTokenExtractor.GetAssertion(context);
+
+        return new gov.hhs.healthit.nhin.DocQueryAcknowledgementType();
+    }
+
+}
---
+++ Product/Production/Gateway/CONNECTNhinServicesWeb/src/main/java/gov/hhs/fha/nhinc/docquery/nhin/deferred/response/NhinDocQueryDeferredResponse.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.docquery.nhin.deferred.response;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author JHOPPESC
+ */
+@WebService(serviceName = "RespondingGateway_QueryDeferredResponse_Service", portName = "RespondingGateway_QueryDeferredResponse_Port_Soap", endpointInterface = "ihe.iti.xds_b._2007.RespondingGatewayQueryDeferredResponsePortType", targetNamespace = "urn:ihe:iti:xds-b:2007", wsdlLocation = "WEB-INF/wsdl/NhinDocQueryDeferredResponse/NhinDocQueryDeferredResponse.wsdl")
+@BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
+public class NhinDocQueryDeferredResponse {
+
+    @Resource
+    private WebServiceContext context;
+
+    public gov.hhs.healthit.nhin.DocQueryAcknowledgementType respondingGatewayCrossGatewayQuery(oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse body) {
+        AssertionType assertion = SamlTokenExtractor.GetAssertion(context);
+
+        return new gov.hhs.healthit.nhin.DocQueryAcknowledgementType();
+    }
+
+}
