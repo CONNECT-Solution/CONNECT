802d3373ae3fc1c8f050a668cfadc1140c47ffa9
goldmanm <goldmanm@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-05 12:59:25 +0000

    story#26869 comments: add EntityDocQueryDeferredRequestHelper

--- Product/Production/Gateway/CONNECTGatewayWeb/src/main/java/gov/hhs/fha/nhinc/docquerydeferred/entity/request/EntityDocQueryDeferredReqSecured.java
+++ Product/Production/Gateway/CONNECTGatewayWeb/src/main/java/gov/hhs/fha/nhinc/docquerydeferred/entity/request/EntityDocQueryDeferredReqSecured.java
@@ -1,9 +1,14 @@
 package gov.hhs.fha.nhinc.docquerydeferred.entity.request;
 
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunitiesType;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
 import gov.hhs.fha.nhinc.common.nhinccommonentity.RespondingGatewayCrossGatewayQuerySecuredRequestType;
 import gov.hhs.healthit.nhin.DocQueryAcknowledgementType;
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest;
 
 /**
  * This is an Entity Secure service for Document Query Deferred Request message
@@ -13,12 +18,46 @@ import gov.hhs.healthit.nhin.DocQueryAcknowledgementType;
 @BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
 public class EntityDocQueryDeferredReqSecured {
 
+  @Resource
+  private WebServiceContext context;
+  private EntityDocQueryDeferredRequestHelper serviceHelper;
+
   /**
    * The Entity Secured Method implementation for RespondingGatewayCrossGatewayQueryRequest makes call to actual implementation
    * @param respondingGatewayCrossGatewayQueryRequest
    * @return DocQueryAcknowledgementsType
    */
   public DocQueryAcknowledgementType respondingGatewayCrossGatewayQuery(RespondingGatewayCrossGatewayQuerySecuredRequestType respondingGatewayCrossGatewayQueryRequest) {
-    return null;
+    serviceHelper = getEntityDocQueryDeferredRequestHelper();
+
+    AdhocQueryRequest retrieveDocumentSetRequest =
+            serviceHelper.getAdhocQueryRequest(respondingGatewayCrossGatewayQueryRequest);
+    AssertionType assertion =
+            serviceHelper.getAssertion(context);
+    NhinTargetCommunitiesType nhinTargetCommunities =
+            serviceHelper.getNhinTargetCommunities(respondingGatewayCrossGatewayQueryRequest);
+
+    DocQueryAcknowledgementType ack =
+            getEntityDocQueryDeferredReqImpl().respondingGatewayCrossGatewayQuery(
+            retrieveDocumentSetRequest,
+            assertion,
+            nhinTargetCommunities);
+    return ack;
+  }
+
+  /**
+   * Create an instance of EntityDocQueryDeferredRequestHelper Class
+   * @return EntityDocQueryDeferredRequestHelper
+   */
+  protected EntityDocQueryDeferredRequestHelper getEntityDocQueryDeferredRequestHelper() {
+    return new EntityDocQueryDeferredRequestHelper();
+  }
+
+  /**
+   * Create an instance of EntityDocRetrieveDeferredReqImpl Class
+   * @return EntityDocRetrieveDeferredReqImpl
+   */
+  protected EntityDocQueryDeferredReqImpl getEntityDocQueryDeferredReqImpl() {
+    return new EntityDocQueryDeferredReqImpl();
   }
 }
--- Product/Production/Gateway/CONNECTGatewayWeb/src/test/java/gov/hhs/fha/nhinc/docquerydeferred/entity/request/EntityDocQueryDeferredReqTest.java
+++ Product/Production/Gateway/CONNECTGatewayWeb/src/test/java/gov/hhs/fha/nhinc/docquerydeferred/entity/request/EntityDocQueryDeferredReqTest.java
@@ -1,8 +1,14 @@
 package gov.hhs.fha.nhinc.docquerydeferred.entity.request;
 
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunitiesType;
 import gov.hhs.fha.nhinc.common.nhinccommonentity.RespondingGatewayCrossGatewayQueryRequestType;
 import gov.hhs.healthit.nhin.DocQueryAcknowledgementType;
+import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+
 import org.jmock.Mockery;
+import org.jmock.Expectations;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,10 +37,64 @@ public class EntityDocQueryDeferredReqTest {
    */
   @Test
   public void testRespondingGatewayCrossGatewayQuery() {
+    final EntityDocQueryDeferredRequestHelper mockRequestHelper = mockery.mock(EntityDocQueryDeferredRequestHelper.class);
+    final EntityDocQueryDeferredReqImpl mockServiceImpl = mockery.mock(EntityDocQueryDeferredReqImpl.class);
+    final DocQueryAcknowledgementType mockAcknowledgementType = mockery.mock(DocQueryAcknowledgementType.class);
+    final RegistryResponseType mockRespType = mockery.mock(RegistryResponseType.class);
+
+    EntityDocQueryDeferredReq testSubject = new EntityDocQueryDeferredReq() {
+
+      @Override
+      protected EntityDocQueryDeferredRequestHelper getEntityDocQueryDeferredRequestHelper() {
+        return mockRequestHelper;
+      }
+
+      @Override
+      protected EntityDocQueryDeferredReqImpl getEntityDocQueryDeferredReqImpl() {
+        return mockServiceImpl;
+      }
+    };
+
+    mockery.checking(new Expectations() {
+
+      {
+        one(mockRequestHelper).getAdhocQueryRequest(with(any(RespondingGatewayCrossGatewayQueryRequestType.class)));
+        one(mockRequestHelper).getAssertion(with(any(RespondingGatewayCrossGatewayQueryRequestType.class)));
+        one(mockRequestHelper).getNhinTargetCommunities(with(any(RespondingGatewayCrossGatewayQueryRequestType.class)));
+        one(mockAcknowledgementType).getMessage();
+        will(returnValue(mockRespType));
+        one(mockRespType).getStatus();
+        will(returnValue("Success"));
+        one(mockServiceImpl).respondingGatewayCrossGatewayQuery(with(any(AdhocQueryRequest.class)), with(any(AssertionType.class)), with(any(NhinTargetCommunitiesType.class)));
+        will(returnValue(mockAcknowledgementType));
+      }
+    });
+
+    DocQueryAcknowledgementType result = testSubject.respondingGatewayCrossGatewayQuery(new RespondingGatewayCrossGatewayQueryRequestType());
+    assertEquals("Success", result.getMessage().getStatus());
+
+    mockery.assertIsSatisfied();
+  }
+
+  /**
+   * Test of getEntityDocQueryDeferredRequestHelper method, of class EntityDocQueryDeferredReq.
+   */
+  @Test
+  public void testGetEntityDocQueryDeferredRequestHelper() {
     EntityDocQueryDeferredReq testSubject = new EntityDocQueryDeferredReq();
-    RespondingGatewayCrossGatewayQueryRequestType respondingGatewayCrossGatewayQueryRequest = new RespondingGatewayCrossGatewayQueryRequestType();
-    DocQueryAcknowledgementType expResult = null;
-    DocQueryAcknowledgementType result = testSubject.respondingGatewayCrossGatewayQuery(respondingGatewayCrossGatewayQueryRequest);
-    assertEquals(expResult, result);
+
+    EntityDocQueryDeferredRequestHelper result = testSubject.getEntityDocQueryDeferredRequestHelper();
+    assertNotNull(result);
+  }
+
+  /**
+   * Test of getEntityDocQueryDeferredReqImpl method, of class EntityDocQueryDeferredReq.
+   */
+  @Test
+  public void testGetEntityDocQueryDeferredReqImpl() {
+    EntityDocQueryDeferredReq testSubject = new EntityDocQueryDeferredReq();
+
+    EntityDocQueryDeferredReqImpl result = testSubject.getEntityDocQueryDeferredReqImpl();
+    assertNotNull(result);
   }
 }
