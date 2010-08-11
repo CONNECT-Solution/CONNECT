215d5376df79aa0a2ad8f5ab8ba81e9d80222695
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-11 12:13:58 +0000

    Tasks: #26733, #26734, and #26743.  Add unsecure and secure Deferred Entity Doc Query Services

--- Product/Production/Gateway/CONNECTMsgProxyWeb/src/main/java/gov/hhs/fha/nhinc/docquery/passthru/deferred/response/PassthruDocQueryDeferredResponseSecuredImpl.java
+++ Product/Production/Gateway/CONNECTMsgProxyWeb/src/main/java/gov/hhs/fha/nhinc/docquery/passthru/deferred/response/PassthruDocQueryDeferredResponseSecuredImpl.java
@@ -5,10 +5,14 @@
 
 package gov.hhs.fha.nhinc.docquery.passthru.deferred.response;
 
+import gov.hhs.fha.nhinc.async.AsyncMessageIdExtractor;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.common.nhinccommonproxy.RespondingGatewayCrossGatewayQueryResponseSecuredType;
+import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
 import gov.hhs.healthit.nhin.DocQueryAcknowledgementType;
+import java.util.List;
 import javax.xml.ws.WebServiceContext;
-import gov.hhs.fha.nhinc.service.WebServiceHelper;
 
 /**
  *
@@ -18,21 +22,19 @@ public class PassthruDocQueryDeferredResponseSecuredImpl {
     private static org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(PassthruDocQueryDeferredResponseSecuredImpl.class);
 
     public DocQueryAcknowledgementType respondingGatewayCrossGatewayQuery(RespondingGatewayCrossGatewayQueryResponseSecuredType body, WebServiceContext context) {
-        WebServiceHelper oHelper = new WebServiceHelper();
-        PassthruDocQueryDeferredResponseOrchImpl orchImpl = new PassthruDocQueryDeferredResponseOrchImpl();
-        DocQueryAcknowledgementType response = null;
+        AssertionType assertion = SamlTokenExtractor.GetAssertion(context);
 
-        try {
-            if (body != null && orchImpl != null) {
-                response = (DocQueryAcknowledgementType) oHelper.invokeSecureDeferredResponseWebService(orchImpl, orchImpl.getClass(), "respondingGatewayCrossGatewayQuery", body, context);
-            } else {
-                log.error("Failed to call the web orchestration (" + orchImpl.getClass() + ".respondingGatewayCrossGatewayQuery).  The input parameter is null.");
+        // Extract the message id value from the WS-Addressing Header and place it in the Assertion Class
+        if (assertion != null) {
+            AsyncMessageIdExtractor msgIdExtractor = new AsyncMessageIdExtractor();
+            assertion.setMessageId(msgIdExtractor.GetAsyncMessageId(context));
+            List<String> relatesToList = msgIdExtractor.GetAsyncRelatesTo(context);
+            if (NullChecker.isNotNullish(relatesToList)) {
+               assertion.getRelatesToList().add(msgIdExtractor.GetAsyncRelatesTo(context).get(0));
             }
-        } catch (Exception e) {
-            log.error("Failed to call the web orchestration (" + orchImpl.getClass() + ".respondingGatewayCrossGatewayQuery).  An unexpected exception occurred.  " +
-                    "Exception: " + e.getMessage(), e);
         }
-        return response;
+
+        return new PassthruDocQueryDeferredResponseOrchImpl().respondingGatewayCrossGatewayQuery(body.getAdhocQueryResponse(), assertion, body.getNhinTargetSystem());
     }
 
 }
