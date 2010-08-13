7a7b580d0e0d3ae029e39af4103a8b693274aa8a
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-13 16:29:27 +0000

    Fixed Addressing issue.

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/admindistribution/AdminDistributionHelper.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/admindistribution/AdminDistributionHelper.java
@@ -120,4 +120,5 @@ public class AdminDistributionHelper {
         }
         return result;
     }
+
 }
--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/admindistribution/nhin/proxy/NhinAdminDistSecuredWebServiceImpl.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/admindistribution/nhin/proxy/NhinAdminDistSecuredWebServiceImpl.java
@@ -20,13 +20,22 @@ import gov.hhs.fha.nhinc.saml.extraction.SamlTokenCreator;
 import gov.hhs.fha.nhinc.webserviceproxy.WebServiceProxyHelper;
 import java.util.Map;
 import javax.xml.ws.BindingProvider;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
 
 /**
  *
  * @author dunnek
  */
 public class NhinAdminDistSecuredWebServiceImpl implements NhinAdminDistProxy{
-   private Log log = null;
+    private Log log = null;
+    private static Service cachedService = null;
+    private static final String NAMESPACE_URI = "urn:gov:hhs:fha:nhinc:nhinadmindistribution";
+    private static final String SERVICE_LOCAL_PART = "RespondingGateway_AdministrativeDistribution";
+    private static final String PORT_LOCAL_PART = "RespondingGateway_AdministrativeDistribution_PortType";
+    private static final String WSDL_FILE = "NhinAdminDist.wsdl";
+    private static final String WS_ADDRESSING_ACTION = NAMESPACE_URI + ":SendAlertMessage_Message";
+
     static RespondingGatewayAdministrativeDistribution nhinService = new RespondingGatewayAdministrativeDistribution();
     public NhinAdminDistSecuredWebServiceImpl()
     {
@@ -48,13 +57,11 @@ public class NhinAdminDistSecuredWebServiceImpl implements NhinAdminDistProxy{
         
         if (NullChecker.isNotNullish(url))
         {
-            RespondingGatewayAdministrativeDistributionPortType port = getPort(url);
+            RespondingGatewayAdministrativeDistributionPortType port = getPort(url, assertion);
 
             SamlTokenCreator tokenCreator = new SamlTokenCreator();
             Map requestContext = tokenCreator.CreateRequestContext(assertion, url, NhincConstants.ADMIN_DIST_ACTION);
-
-            WebServiceProxyHelper oHelper = new WebServiceProxyHelper();
-            oHelper.initializePort((javax.xml.ws.BindingProvider) port, url);
+            
             ((BindingProvider) port).getRequestContext().putAll(requestContext);
             
 
@@ -71,5 +78,38 @@ public class NhinAdminDistSecuredWebServiceImpl implements NhinAdminDistProxy{
 
         return port;
     }
-
+    protected RespondingGatewayAdministrativeDistributionPortType getPort(String url, AssertionType assertion)
+    {
+        WebServiceProxyHelper proxyHelper = new WebServiceProxyHelper();
+        
+        RespondingGatewayAdministrativeDistributionPortType port = null;
+        Service cacheService = getService(WSDL_FILE,NAMESPACE_URI, SERVICE_LOCAL_PART);
+        if (cacheService != null)
+        {
+            log.debug("Obtained service - creating port.");
+            port = cacheService.getPort(new QName(NAMESPACE_URI, PORT_LOCAL_PART), RespondingGatewayAdministrativeDistributionPortType.class);
+            proxyHelper.initializeSecurePort((javax.xml.ws.BindingProvider) port, url,NhincConstants.NHIN_ADMIN_DIST_SERVICE_NAME, WS_ADDRESSING_ACTION, assertion);            
+         }
+        else
+        {
+            log.error("Unable to obtain serivce - no port created.");
+        }
+        return port;
+    }
+    protected Service getService(String wsdl, String uri, String service)
+    {
+        if (cachedService == null)
+        {
+            try
+            {
+                WebServiceProxyHelper proxyHelper = new WebServiceProxyHelper();
+                cachedService = proxyHelper.createService(wsdl, uri, service);
+            }
+            catch (Throwable t)
+            {
+                log.error("Error creating service: " + t.getMessage(), t);
+            }
+        }
+        return cachedService;
+    }
 }
