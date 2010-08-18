26f05973503ff3355eb624bbecec7ef68180be38
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-18 11:59:50 +0000

    Task: #27109 Fix adapter component wsdls for DR Deferred

---
+++ Product/Production/Adapters/General/CONNECTAdapterWeb/src/main/java/gov/hhs/fha/nhinc/docretrieve/adapter/component/deferred/response/AdapterComponentDocRetrieveDeferredResponse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.docretrieve.adapter.component.deferred.response;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author jhoppesc
+ */
+@WebService(serviceName = "AdapterComponentDocRetrieveResponseService", portName = "AdapterComponentDocRetrieveResponsePortSoap", endpointInterface = "gov.hhs.fha.nhinc.adaptercomponentdocretrievedeferredresp.AdapterComponentDocRetrieveDeferredRespPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:adaptercomponentdocretrievedeferredresp", wsdlLocation = "WEB-INF/wsdl/AdapterComponentDocRetrieveDeferredResponse/AdapterComponentDocumentRetrieveDeferredResponse.wsdl")
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+public class AdapterComponentDocRetrieveDeferredResponse {
+    @Resource
+    private WebServiceContext context;
+
+    public gov.hhs.healthit.nhin.DocRetrieveAcknowledgementType retrieveDocumentSetResponse(gov.hhs.fha.nhinc.common.nhinccommonadapter.RespondingGatewayCrossGatewayRetrieveResponseType body) {
+        return new AdapterComponentDocRetrieveDeferredResponseImpl().crossGatewayRetrieveResponse(body, context);
+    }
+
+}
