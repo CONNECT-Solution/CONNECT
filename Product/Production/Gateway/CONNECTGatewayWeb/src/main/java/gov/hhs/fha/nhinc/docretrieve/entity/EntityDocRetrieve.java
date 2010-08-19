f7b2d2a8eff03622ca7232e256347865978988b9
dunnek <dunnek@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-19 14:43:28 +0000

    Missed file in check in.

---
+++ Product/Production/Gateway/CONNECTGatewayWeb/src/main/java/gov/hhs/fha/nhinc/docretrieve/entity/EntityDocRetrieve.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.hhs.fha.nhinc.docretrieve.entity;
+
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+
+/**
+ *
+ * @author dunnek
+ */
+@WebService(serviceName = "EntityDocRetrieve", portName = "EntityDocRetrievePortSoap", endpointInterface = "gov.hhs.fha.nhinc.entitydocretrieve.EntityDocRetrievePortType", targetNamespace = "urn:gov:hhs:fha:nhinc:entitydocretrieve", wsdlLocation = "WEB-INF/wsdl/EntityDocRetrieve/EntityDocRetrieve.wsdl")
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+public class EntityDocRetrieve {
+
+    public ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType respondingGatewayCrossGatewayRetrieve(gov.hhs.fha.nhinc.common.nhinccommonentity.RespondingGatewayCrossGatewayRetrieveRequestType respondingGatewayCrossGatewayRetrieveRequest) {
+//todo: pass in context
+        return getImpl().respondingGatewayCrossGatewayQuery(respondingGatewayCrossGatewayRetrieveRequest.getRetrieveDocumentSetRequest(), respondingGatewayCrossGatewayRetrieveRequest.getAssertion());
+    }
+    protected EntityDocRetreiveImpl getImpl()
+    {
+        return new EntityDocRetreiveImpl();
+    }
+}
