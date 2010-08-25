071d68ad8e0e041aa4755535db0b049ad0b0e404
hoppeschj <hoppeschj@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-25 19:52:30 +0000

    User Story:  26200, 26210, 25677, and 25494 Implement Msg PRoxy and Nhin XDR Request and Response

---
+++ Product/Production/Gateway/CONNECTNhinServicesWeb/src/main/java/gov/hhs/fha/nhinc/docsubmission/nhin/deferred/request/NhinDocSubmissionDeferredRequestImpl.java
@@ -0,0 +1,33 @@
+package gov.hhs.fha.nhinc.docsubmission.nhin.deferred.request;
+
+import gov.hhs.fha.nhinc.async.AsyncMessageIdExtractor;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.xml.ws.WebServiceContext;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
+import gov.hhs.healthit.nhin.XDRAcknowledgementType;
+
+/**
+ *
+ * @author patlollav
+ */
+public class NhinDocSubmissionDeferredRequestImpl {
+
+    /**
+     *
+     * @param body
+     * @param context
+     * @return
+     */
+    public XDRAcknowledgementType provideAndRegisterDocumentSetBRequest(ProvideAndRegisterDocumentSetRequestType body, WebServiceContext context) {
+        AssertionType assertion = SamlTokenExtractor.GetAssertion(context);
+
+        if (assertion != null) {
+            AsyncMessageIdExtractor msgIdExtractor = new AsyncMessageIdExtractor();
+            assertion.setMessageId(msgIdExtractor.GetAsyncMessageId(context));
+        }
+
+        return new NhinDocSubmissionDeferredRequestOrchImpl().provideAndRegisterDocumentSetBRequest(body, assertion);
+    }
+
+}
---
+++ Product/Production/Gateway/CONNECTNhinServicesWeb/src/main/java/gov/hhs/fha/nhinc/docsubmission/nhin/deferred/response/NhinDocSubmissionDeferredResponseImpl.java
@@ -0,0 +1,42 @@
+package gov.hhs.fha.nhinc.docsubmission.nhin.deferred.response;
+
+import gov.hhs.fha.nhinc.async.AsyncMessageIdExtractor;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.saml.extraction.SamlTokenExtractor;
+import javax.xml.ws.WebServiceContext;
+import gov.hhs.healthit.nhin.XDRAcknowledgementType;
+import java.util.List;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+
+/**
+ *
+ * @author patlollav
+ */
+public class NhinDocSubmissionDeferredResponseImpl
+{
+
+    
+    /**
+     *
+     * @param body
+     * @param context
+     * @return
+     */
+    public XDRAcknowledgementType provideAndRegisterDocumentSetBResponse(RegistryResponseType body, WebServiceContext context)
+    {
+       AssertionType assertion = SamlTokenExtractor.GetAssertion(context);
+
+       if (assertion != null) {
+            AsyncMessageIdExtractor msgIdExtractor = new AsyncMessageIdExtractor();
+            assertion.setMessageId(msgIdExtractor.GetAsyncMessageId(context));
+            List<String> relatesToList = AsyncMessageIdExtractor.GetAsyncRelatesTo(context);
+            if (NullChecker.isNotNullish(relatesToList)) {
+                assertion.getRelatesToList().add(AsyncMessageIdExtractor.GetAsyncRelatesTo(context).get(0));
+            }
+        }
+
+       return new NhinDocSubmissionDeferredResponseOrchImpl().provideAndRegisterDocumentSetBResponse(body, assertion);
+        
+    }
+}
