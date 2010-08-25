28075164013238855d781bcd96b7379216880311
webbn <webbn@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-08-25 16:00:47 +0000

    Stories 25748 & 26170 Comments: Core refactor work for EntityPatientDiscoveryDeferredResponse and NhincProxyPatientDiscoveryDeferredResponse services

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/passthru/deferred/response/proxy/PassthruPatientDiscoveryDeferredRespProxyJavaImpl.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/passthru/deferred/response/proxy/PassthruPatientDiscoveryDeferredRespProxyJavaImpl.java
@@ -2,9 +2,11 @@ package gov.hhs.fha.nhinc.patientdiscovery.passthru.deferred.response.proxy;
 
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import gov.hhs.fha.nhinc.patientdiscovery.passthru.deferred.response.PassthruPatientDiscoveryDeferredRespOrchImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.v3.MCCIIN000002UV01;
+import org.hl7.v3.PRPAIN201306UV02;
 import org.hl7.v3.ProxyPRPAIN201306UVProxyRequestType;
 
 /**
@@ -28,7 +30,16 @@ public class PassthruPatientDiscoveryDeferredRespProxyJavaImpl implements Passth
 
     public MCCIIN000002UV01 proxyProcessPatientDiscoveryAsyncResp(ProxyPRPAIN201306UVProxyRequestType proxyProcessPatientDiscoveryAsyncRespRequest, AssertionType assertion, NhinTargetSystemType targetSystem)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        log.debug("Begin PassthruPatientDiscoveryDeferredRespProxyJavaImpl.processPatientDiscoveryAsyncResp(...)");
+        MCCIIN000002UV01 response = null;
+        PRPAIN201306UV02 request = null;
+        if(proxyProcessPatientDiscoveryAsyncRespRequest != null)
+        {
+            request = proxyProcessPatientDiscoveryAsyncRespRequest.getPRPAIN201306UV02();
+        }
+        response = new PassthruPatientDiscoveryDeferredRespOrchImpl().proxyProcessPatientDiscoveryAsyncResp(request, assertion, targetSystem);
+        log.debug("End PassthruPatientDiscoveryDeferredRespProxyJavaImpl.processPatientDiscoveryAsyncResp(...)");
+        return response;
     }
 
 }
