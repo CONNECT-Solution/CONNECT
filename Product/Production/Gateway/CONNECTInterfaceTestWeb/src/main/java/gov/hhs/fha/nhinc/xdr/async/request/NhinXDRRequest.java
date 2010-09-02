47747094da36b385a9ad8bf221be5867be41db73
valluripallis <valluripallis@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2010-09-02 15:44:40 +0000

    User Story#27413 Task#27478 Comments: Fixed the issue with WSDL and Port changes to Nhin XDR Request and Response WSDLs.

--- Product/Production/Gateway/CONNECTInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/request/NhinXDRRequest.java
+++ Product/Production/Gateway/CONNECTInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/request/NhinXDRRequest.java
@@ -1,16 +1,17 @@
 package gov.hhs.fha.nhinc.xdr.async.request;
 
 import gov.hhs.healthit.nhin.XDRAcknowledgementType;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
 import javax.jws.WebService;
 
 /**
  *
  * @author Sai Valluripalli
  */
-@WebService(serviceName = "XDRRequest_Service", portName = "XDRRequest_Port_Soap", endpointInterface = "ihe.iti.xdr.async.request._2007.XDRRequestPortType", targetNamespace = "urn:ihe:iti:xdr:async:request:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRRequest/NhinXDRRequest.wsdl")
+@WebService(serviceName = "XDRDeferredRequest_Service", portName = "XDRDeferredRequest_Port_Soap", endpointInterface = "ihe.iti.xdr._2007.XDRDeferredRequestPortType", targetNamespace = "urn:ihe:iti:xdr:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRRequest/NhinXDRDeferredRequest.wsdl")
 public class NhinXDRRequest {
 
-    public XDRAcknowledgementType provideAndRegisterDocumentSetBRequest(ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType body) {
+    public XDRAcknowledgementType provideAndRegisterDocumentSetBDeferredRequest(ProvideAndRegisterDocumentSetRequestType body) {
         return new NhinXDRRequestImpl().provideAndRegisterDocumentSetBRequest(body);
     }
 
--- Product/Production/Gateway/CONNECTInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/response/NhinXDRResponse.java
+++ Product/Production/Gateway/CONNECTInterfaceTestWeb/src/main/java/gov/hhs/fha/nhinc/xdr/async/response/NhinXDRResponse.java
@@ -3,18 +3,17 @@ package gov.hhs.fha.nhinc.xdr.async.response;
 import gov.hhs.healthit.nhin.XDRAcknowledgementType;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
 /**
  *
- * @author Neil Webb
+ * @author Sai Valluripalli
  */
-@WebService(serviceName = "XDRResponse_Service", portName = "XDRResponse_Port_Soap", endpointInterface = "ihe.iti.xdr.async.response._2007.XDRResponsePortType", targetNamespace = "urn:ihe:iti:xdr:async:response:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRResponse/NhinXDRResponse.wsdl")
+@WebService(serviceName = "XDRDeferredResponse_Service", portName = "XDRDeferredResponse_Port_Soap", endpointInterface = "ihe.iti.xdr._2007.XDRDeferredResponsePortType", targetNamespace = "urn:ihe:iti:xdr:2007", wsdlLocation = "WEB-INF/wsdl/NhinXDRResponse/NhinXDRDeferredResponse.wsdl")
 @BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
-public class NhinXDRResponse
-{
+public class NhinXDRResponse {
 
-    public XDRAcknowledgementType provideAndRegisterDocumentSetBResponse(oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType body)
-    {
+    public XDRAcknowledgementType provideAndRegisterDocumentSetBDeferredResponse(RegistryResponseType body) {
         return new NhinXDRResponseImpl().provideAndRegisterDocumentSetBResponse(body);
     }
 
