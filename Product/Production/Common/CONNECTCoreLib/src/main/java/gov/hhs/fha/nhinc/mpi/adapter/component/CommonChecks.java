3f4e233d1be0f42033bd7710c225565313e4e56c
richard.ettema <richard.ettema@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-03-25 04:34:42 +0000

    CONNECT MPI Adapter - DB implementation - implemented MPI DB Checks

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/component/CommonChecks.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/component/CommonChecks.java
@@ -4,20 +4,21 @@
  * Copyright 2010(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
  *  
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gov.hhs.fha.nhinc.mpi.adapter.component;
 
 import gov.hhs.fha.nhinc.mpilib.Patients;
+import gov.hhs.fha.nhinc.patientdb.model.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author rayj
  */
 public class CommonChecks {
+
+    // MPI XML Checks
 
     public static boolean isSingleSearchResult(Patients patients) {
         return ((patients != null) && (patients.size() == 1));
@@ -28,6 +29,33 @@ public class CommonChecks {
     }
 
     public static boolean isZeroSearchResult(Patients patients) {
+        return ((patients == null) || (patients.size() == 0));
+    }
+
+    // MPI DB Checks
+
+    public static boolean isSinglePerAASearchResult(List<Patient> patients) {
+        boolean result = true;
+
+        Map testMap = new HashMap();
+        if (patients != null) {
+            for (Patient patient : patients) {
+                if (patient.getIdentifiers() != null &&
+                        patient.getIdentifiers().size() > 0 &&
+                        patient.getIdentifiers().get(0) != null) {
+                    String orgId = patient.getIdentifiers().get(0).getOrganizationId();
+                    if (testMap.containsKey(orgId)) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isZeroPerAASearchResult(List<Patient> patients) {
         return ((patients == null) || (patients.size() == 0));
     }
 
