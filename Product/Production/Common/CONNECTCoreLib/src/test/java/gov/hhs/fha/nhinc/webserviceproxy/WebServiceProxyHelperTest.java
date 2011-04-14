2b19d7cddbae4e6027ae6e487610403bbb2cb274
richard.ettema <richard.ettema@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-04-14 17:59:28 +0000

    Added header comment block

--- Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/webserviceproxy/WebServiceProxyHelperTest.java
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/webserviceproxy/WebServiceProxyHelperTest.java
@@ -1,6 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
+ *
  */
 package gov.hhs.fha.nhinc.webserviceproxy;
 
