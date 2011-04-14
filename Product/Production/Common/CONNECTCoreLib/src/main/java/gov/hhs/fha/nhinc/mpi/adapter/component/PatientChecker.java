d016cc70ec1af8eb73d7f527b5fb321e83de6097
richard.ettema <richard.ettema@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-04-14 12:27:11 +0000

    CONNECT MPI Adapter - MAA implementation - added support for multiple patient matches

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/component/PatientChecker.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/component/PatientChecker.java
@@ -4,15 +4,13 @@
  * Copyright 2010(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
  *  
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.hhs.fha.nhinc.mpi.adapter.component;
 
 import gov.hhs.fha.nhinc.mpi.adapter.component.hl7parsers.HL7Parser201305;
 import gov.hhs.fha.nhinc.mpi.adapter.component.hl7parsers.HL7Parser201306;
 import gov.hhs.fha.nhinc.mpilib.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.v3.*;
@@ -24,6 +22,39 @@ import org.hl7.v3.PRPAIN201305UV02;
 public class PatientChecker implements AdapterComponentMpiChecker {
 
     private static Log log = LogFactory.getLog(PatientChecker.class);
+
+//    public PRPAIN201306UV02 FindPatient(PRPAIN201305UV02 query) {
+//        log.debug("Entering PatientChecker.FindPatient method...");
+//        PRPAIN201306UV02 result = null;
+//
+//        PRPAMT201306UV02ParameterList queryParams = HL7Parser201305.ExtractHL7QueryParamsFromMessage(query);
+//
+//        if (queryParams == null) {
+//            log.error("no query parameters were supplied");
+//        } else {
+//            Patient sourcePatient = HL7Parser201305.ExtractMpiPatientFromQueryParams(queryParams);
+//            log.info("perform patient lookup in mpi");
+//
+//            log.info("source patient check 1 [" + sourcePatient.toString()+ "]");
+//            Patients searchResults = MpiDataAccess.LookupPatients(sourcePatient);
+//            if (CommonChecks.isZeroSearchResult(searchResults)) {
+//                log.info("patient not found in MPI");
+//                result = null;
+//            } else if (CommonChecks.isMultipleSearchResult(searchResults)) {
+//                log.info("multiple patients found in MPI [searchResults.size()=" + searchResults.size() + "]");
+//                result = null;
+//            } else {
+//                log.info("single patient found in MPI");
+//                Patient searchResultPatient = searchResults.get(0);
+//                log.info("Found patient " + searchResultPatient.toString());
+//
+//                result = HL7Parser201306.BuildMessageFromMpiPatient (searchResultPatient, query);
+//            }
+//        }
+//
+//        log.debug("Exiting PatientChecker.FindPatient method...");
+//        return result;
+//    }
 
     public PRPAIN201306UV02 FindPatient(PRPAIN201305UV02 query) {
         log.debug("Entering PatientChecker.FindPatient method...");
@@ -37,9 +68,40 @@ public class PatientChecker implements AdapterComponentMpiChecker {
             Patient sourcePatient = HL7Parser201305.ExtractMpiPatientFromQueryParams(queryParams);
             log.info("perform patient lookup in mpi");
 
-            log.info("source patient check 1 [" + sourcePatient.toString()+ "]");
+            log.info("source patient check 1 [" + sourcePatient.toString() + "]");
             Patients searchResults = MpiDataAccess.LookupPatients(sourcePatient);
-            if (CommonChecks.isZeroSearchResult(searchResults)) {
+
+            String pId = "";
+            String aaId = "";
+            String uniquePatientId = "";
+            List<String> uniquePatientIds = new ArrayList<String>();
+            Patients filteredSearchResults = new Patients();
+            for (Patient patient : searchResults) {
+                pId = "";
+                aaId = "";
+                uniquePatientId = "";
+                if ((patient.getIdentifiers() != null) && (patient.getIdentifiers().size() > 0)) {
+                    pId = patient.getIdentifiers().get(0).getId();
+                    aaId = patient.getIdentifiers().get(0).getOrganizationId();
+                    uniquePatientId = pId + "^^^&" + aaId + "&ISO";
+                    log.debug("FindPatient - uniquePatientId: " + uniquePatientId);
+                    if (uniquePatientIds.size() > 0) {
+                        for (String str : uniquePatientIds) {
+                            if (uniquePatientId.equalsIgnoreCase(str)) {
+                                log.debug("Duplicate patient found in the same assigning authority");
+                            } else {
+                                filteredSearchResults.add(patient);
+                            }
+                        }
+                    } else {
+                        filteredSearchResults.add(patient);
+                    }
+                    uniquePatientIds.add(uniquePatientId);
+                }
+            }
+
+            log.debug("filteredSearchResults.size(): " + filteredSearchResults.size());
+            /*if (CommonChecks.isZeroSearchResult(searchResults)) {
                 log.info("patient not found in MPI");
                 result = null;
             } else if (CommonChecks.isMultipleSearchResult(searchResults)) {
@@ -49,12 +111,19 @@ public class PatientChecker implements AdapterComponentMpiChecker {
                 log.info("single patient found in MPI");
                 Patient searchResultPatient = searchResults.get(0);
                 log.info("Found patient " + searchResultPatient.toString());
-            
-                result = HL7Parser201306.BuildMessageFromMpiPatient (searchResultPatient, query);
+
+                result = HL7Parser201306.BuildMessageFromMpiPatient(searchResultPatient, query);
+            }*/
+            if (filteredSearchResults.size() > 0) {
+                result = HL7Parser201306.BuildMessageFromMpiPatient(filteredSearchResults, query);
+            } else {
+                result = null;
             }
+
         }
 
         log.debug("Exiting PatientChecker.FindPatient method...");
         return result;
     }
+
 }
