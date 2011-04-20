0eb6ae8e4b1cb53fa437b7abcd298675c645d508
richard.ettema <richard.ettema@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-04-20 15:09:02 +0000

    GATEWAY-352 - Implemented conformance changes for required attributes and values

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/transform/subdisc/HL7MessageIdGenerator.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/transform/subdisc/HL7MessageIdGenerator.java
@@ -4,24 +4,31 @@
  * Copyright 2010(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
  *  
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.hhs.fha.nhinc.transform.subdisc;
 
+import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.properties.PropertyAccessException;
+import gov.hhs.fha.nhinc.properties.PropertyAccessor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.v3.*;
-import gov.hhs.fha.nhinc.nhinclib.NullChecker;
 
 /**
  *
  * @author Jon Hoppesch
  */
 public class HL7MessageIdGenerator {
+
     private static Log log = LogFactory.getLog(HL7MessageIdGenerator.class);
-    
+    private static final String PROPERTY_FILE = "adapter";
+    private static final String PROPERTY_NAME = "assigningAuthorityId";
+
+    /**
+     * Generate the messageId based on the passed device id.  The device id is
+     * the corresponding assigning authority id.
+     * @param myDeviceId
+     * @return messageId
+     */
     public static II GenerateHL7MessageId(String myDeviceId) {
         II messageId = new II();
         
@@ -35,6 +42,11 @@ public class HL7MessageIdGenerator {
         return messageId;
     }
 
+    /**
+     * Generate the messageId based on the default device id.  The device id is
+     * the assigning authority id from the adapter.properties configuration file.
+     * @return messageId
+     */
     public static II GenerateHL7MessageId() {
         String deviceId = getDefaultLocalDeviceId();
         
@@ -42,9 +54,21 @@ public class HL7MessageIdGenerator {
     }
 
     private static String getDefaultLocalDeviceId() {
-        return HL7Constants.DEFAULT_LOCAL_DEVICE_ID;
+        String defaultLocalId = "";
+
+        try {
+            defaultLocalId = PropertyAccessor.getProperty(PROPERTY_FILE, PROPERTY_NAME);
+        } catch (PropertyAccessException e) {
+            log.error("PropertyAccessException - Default Assigning Authority property not defined in adapter.properties", e);
+        }
+
+        return defaultLocalId;
     }
     
+    /**
+     * Generate the messageId from a new UID.
+     * @return messageId
+     */
     public static String GenerateMessageId() {
         java.rmi.server.UID uid = new java.rmi.server.UID();
         log.debug("generated message id=" + uid.toString());
