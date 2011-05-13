e2e1259ead80197e0884cf705d0314f2bf168ea3
narendra.reddy <narendra.reddy@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-05-13 05:03:50 +0000

    Gateway-356 -Initial Missing Implementation of Doc Deferred Retrieve is Completed.

---
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/docretrieve/entity/deferred/request/queue/proxy/EntityDocRetrieveDeferredRequestQueueProxy.java
@@ -0,0 +1,25 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2011(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
+ *
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.hhs.fha.nhinc.docretrieve.entity.deferred.request.queue.proxy;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunitiesType;
+import gov.hhs.healthit.nhin.DocRetrieveAcknowledgementType;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
+
+/**
+ *
+ * @author narendra.reddy
+ */
+public interface EntityDocRetrieveDeferredRequestQueueProxy {
+
+    public DocRetrieveAcknowledgementType crossGatewayRetrieveResponse(RetrieveDocumentSetRequestType request, AssertionType assertion, NhinTargetCommunitiesType targets);
+}
