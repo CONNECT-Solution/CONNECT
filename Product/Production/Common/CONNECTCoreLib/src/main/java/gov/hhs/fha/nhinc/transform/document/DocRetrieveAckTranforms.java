cee18ff2ee731f87fc1f385f1031e779b6899f3a
richard.ettema <richard.ettema@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-05-17 13:53:05 +0000

    GATEWAY-353 - DeferredQueueManager - Retrieve Documents implementation

---
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/transform/document/DocRetrieveAckTranforms.java
@@ -0,0 +1,48 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2011(Year date of delivery) United States Government, as represented by the Secretary of Health and Human Services.  All rights reserved.
+ *
+ */
+package gov.hhs.fha.nhinc.transform.document;
+
+import gov.hhs.healthit.nhin.DocRetrieveAcknowledgementType;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryError;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+
+/**
+ *
+ * @author richard.ettema
+ */
+public class DocRetrieveAckTranforms {
+
+    /**
+     * Create acknowledgement message based on specific data values.
+     *
+     * @param status
+     * @param errCode
+     * @param errText
+     * @return
+     */
+    public static DocRetrieveAcknowledgementType createAckMessage(String status, String errCode, String errText) {
+        DocRetrieveAcknowledgementType ackMsg = new DocRetrieveAcknowledgementType();
+
+        RegistryResponseType regResp = new RegistryResponseType();
+        ackMsg.setMessage(regResp);
+
+        // Assign the Ack status
+        regResp.setStatus(status);
+
+        // If an error is defined, create a registry error
+        if (errCode != null && errText != null) {
+            RegistryError regError = new RegistryError();
+            regError.setErrorCode(errCode);
+            regError.setCodeContext(errText);
+            regError.setSeverity("urn:oasis:names:tc:ebxml-regrep:ErrorSeverityType:Error");
+            regResp.getRegistryErrorList().getRegistryError().add(regError);
+        }
+
+        return ackMsg;
+    }
+
+}
