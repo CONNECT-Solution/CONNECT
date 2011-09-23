0c84ab8d71f190e56db3a318fd05089b418c5be6
ekascic <ekascic@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-09-23 16:49:22 +0000

    this might be necessary? not sure TINT-41

--- Product/build/ci_util/solaris/retrieve_and_unzip_distributions.sh
+++ Product/build/ci_util/solaris/retrieve_and_unzip_distributions.sh
@@ -16,6 +16,7 @@ rm -Rf $NHINC_DIR/*
 unzip target/CONNECT_Binaries*.zip -d /nhin/NHINC
 mkdir $AS_HOME/domains/domain1/config/nhin
 cp -r $NHINC_DIR/Dev/* $AS_HOME/domains/domain1/config/nhin
+chmod -R g+w $AS_HOME/domains/domain1/config/nhin
 
 rm -Rf $NHINC_DIRC/ThirdParty/*
 unzip target/CONNECT_ThirdParty*.zip -d $NHINC_DIR/ThirdParty
