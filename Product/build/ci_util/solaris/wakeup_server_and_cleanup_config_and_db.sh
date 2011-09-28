481b4b60649a39e4bbeb405392691601edc41bfa
ekascic <ekascic@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-09-28 20:43:55 +0000

    run patched project TINT-47

--- Product/build/ci_util/solaris/wakeup_server_and_cleanup_config_and_db.sh
+++ Product/build/ci_util/solaris/wakeup_server_and_cleanup_config_and_db.sh
@@ -1,6 +1,6 @@
 #this will crap out because of timeout on first run after startup.  ignore failure
 #it will fail and scuttle the script, so add || true to force success and keep trucking
-/nhin/soapui-3.5.1/bin/testrunner.sh -Djava.awt.headless=true -f /dev/null /nhin/ValidationSuite/1-InternalSelfTest-soapui-project.xml || true
+/nhin/soapui-3.5.1/bin/testrunner.sh -Djava.awt.headless=true -f /dev/null /nhin/ValidationSuite/1-InternalSelfTest-patched.xml || true
 /nhin/soapui-3.5.1/bin/testrunner.sh -Djava.awt.headless=true -f /dev/null /nhin/ValidationSuite/2-EndToEndSelfTest-patched.xml || true
 
 
