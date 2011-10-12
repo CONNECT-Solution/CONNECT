529d5b7cbd7ce61f1364eb81f2cd0b41ede9f85f
ngoc.nguyen <ngoc.nguyen@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2011-10-12 18:59:00 +0000

    Gateway-529 Increase the OID to 64 characters (Fix syntax). Reviewed by MW & AK Gateway-379 Fix PD verify mode. Reviewed by MW

--- Product/DBScripts/nhincdb/UpdateAuthIDTo3_3.sql
+++ Product/DBScripts/nhincdb/UpdateAuthIDTo3_3.sql
@@ -6,14 +6,14 @@
 -- ------------------------------------------------------
 
 
-ALTER `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY assigningauthorityid VARCHAR(64);
-ALTER `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY homecommunityid VARCHAR(64);
-ALTER `auditrepo`.`auditrepository` MODIFY receiverPatientId VARCHAR(128);
-ALTER `auditrepo`.`auditrepository` MODIFY senderPatientId VARCHAR(128);
-ALTER `patientcorrelationdb`.`correlatedidentifiers` MODIFY PatientId VARCHAR(128);
-ALTER `patientcorrelationdb`.`correlatedidentifiers` MODIFY PatientAssigningAuthorityId VARCHAR(64);
-ALTER `patientcorrelationdb`.`correlatedidentifiers` MODIFY CorrelatedPatientId VARCHAR(128);
-ALTER `patientcorrelationdb`.`correlatedidentifiers` MODIFY CorrelatedPatientAssignAuthId VARCHAR(64)
-ALTER `patientcorrelationdb`.`pddeferredcorrelation` MODIFY AssigningAuthorityId VARCHAR(64);
-ALTER `patientcorrelationdb`.`pddeferredcorrelation` MODIFY PatientId VARCHAR(128);;
-ALTER `docrepository`.`document` MODIFY PatientId VARCHAR(128);
+ALTER TABLE `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY assigningauthorityid VARCHAR(64) NOT NULL;
+ALTER TABLE `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY homecommunityid VARCHAR(64) NOT NULL;
+ALTER TABLE `auditrepo`.`auditrepository` MODIFY receiverPatientId VARCHAR(128);
+ALTER TABLE `auditrepo`.`auditrepository` MODIFY senderPatientId VARCHAR(128);
+ALTER TABLE `patientcorrelationdb`.`correlatedidentifiers` MODIFY PatientId VARCHAR(128) NOT NULL;
+ALTER TABLE `patientcorrelationdb`.`correlatedidentifiers` MODIFY PatientAssigningAuthorityId VARCHAR(64) NOT NULL;
+ALTER TABLE `patientcorrelationdb`.`correlatedidentifiers` MODIFY CorrelatedPatientId VARCHAR(128) NOT NULL;
+ALTER TABLE `patientcorrelationdb`.`correlatedidentifiers` MODIFY CorrelatedPatientAssignAuthId VARCHAR(64) NOT NULL;
+ALTER TABLE `patientcorrelationdb`.`pddeferredcorrelation` MODIFY AssigningAuthorityId VARCHAR(64) NOT NULL;
+ALTER TABLE `patientcorrelationdb`.`pddeferredcorrelation` MODIFY PatientId VARCHAR(128) NOT NULL;
+ALTER TABLE `docrepository`.`document` MODIFY PatientId VARCHAR(128) default NULL COMMENT 'Format of HL7 2.x CX';
