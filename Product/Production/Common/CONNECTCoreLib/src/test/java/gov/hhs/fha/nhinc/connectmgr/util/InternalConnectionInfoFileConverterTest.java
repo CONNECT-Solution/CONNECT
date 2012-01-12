411c761045dfa1edf0948b9b121424cb26f5392a
kshtabnoy <kshtabnoy@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-01-12 17:35:30 +0000

    Implementation, Unit test, Code review. GATEWAY-1064

---
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/connectmgr/util/InternalConnectionInfoFileConverterTest.java
@@ -0,0 +1,82 @@
+package gov.hhs.fha.nhinc.connectmgr.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InternalConnectionInfoFileConverterTest {
+
+	private final static String TEST_INPUT =
+  "	<InternalConnectionInfos>" +
+"\n		<internalConnectionInfo>" +
+"\n			<homeCommunityId>1.1</homeCommunityId>" +
+"\n			<name>CONNECTION</name>" +
+"\n			<description>CONNECTION Description</description>" +
+"\n			<states>" +
+"\n				<state>" +
+"\n					<name>US-NY</name>" +
+"\n				</state>" +
+"\n				<state>" +
+"\n					<name>US-FL</name>" +
+"\n				</state>" +
+"\n			</states>" +
+"\n			<services>" +
+"\n				<service>" +
+"\n					<name>QueryForDocument</name>" +
+"\n					<description>documentquery</description>" +
+"\n					<endpointURL>https://localhost:8181/GatewayDocumentQuery/1_0/NhinService/RespondingGateway_Query_Service/DocQuery</endpointURL>" +
+"\n				</service>" +
+"\n		            <service>" +
+"\n	                <name>QueryForDocumentsDeferredRequest</name>" +
+"\n	                <description>NHIN Doc Query Deferred Request</description>" +
+"\n	                <endpointURL>https://localhost:8181/GatewayDocumentQuery/1_0/NhinService/DocQueryDeferredRequestService</endpointURL>" +
+"\n	            </service>" +
+"\n			</services>" +
+"\n		</internalConnectionInfo>" +
+"\n	</InternalConnectionInfos>";
+	
+	private final static String EXPECTED_TEST_OUTPUT = 
+  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+"\n<businessDetail xmlns=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">" +
+"\n	<businessEntity businessKey=\"uddi:testnhincnode:1.1\">" +
+"\n		<name xml:lang=\"en\">CONNECTION</name>" +
+"\n		<businessServices>" +
+"\n			<businessService serviceKey=\"uddi:testnhincnode:QueryForDocument\" businessKey=\"uddi:testnhieonenode:1.1\">" +
+"\n				<name xml:lang=\"en\">QueryForDocument</name>" +
+"\n				<bindingTemplates>" +
+"\n					<bindingTemplate bindingKey=\"uddi:testnhincnode:https://localhost:8181/GatewayDocumentQuery/1_0/NhinService/RespondingGateway_Query_Service/DocQuery\" serviceKey=\"uddi:testnhincnode:QueryForDocument\">" +
+"\n						<accessPoint useType=\"endPoint\">QueryForDocument</accessPoint>" +
+"\n						<categoryBag>" +
+"\n							<keyedReference tModelKey=\"uddi:nhin:versionofservice\" keyName=\"\" keyValue=\"1\"/>" +
+"\n						</categoryBag>" +
+"\n					</bindingTemplate>" +
+"\n				</bindingTemplates>" +
+"\n			</businessService>" +
+"\n			<businessService serviceKey=\"uddi:testnhincnode:QueryForDocumentsDeferredRequest\" businessKey=\"uddi:testnhieonenode:1.1\">" +
+"\n				<name xml:lang=\"en\">QueryForDocumentsDeferredRequest</name>" +
+"\n				<bindingTemplates>" +
+"\n					<bindingTemplate bindingKey=\"uddi:testnhincnode:https://localhost:8181/GatewayDocumentQuery/1_0/NhinService/DocQueryDeferredRequestService\" serviceKey=\"uddi:testnhincnode:QueryForDocumentsDeferredRequest\">" +
+"\n						<accessPoint useType=\"endPoint\">QueryForDocumentsDeferredRequest</accessPoint>" +
+"\n						<categoryBag>" +
+"\n							<keyedReference tModelKey=\"uddi:nhin:versionofservice\" keyName=\"\" keyValue=\"1\"/>" +
+"\n						</categoryBag>" +
+"\n					</bindingTemplate>" +
+"\n				</bindingTemplates>" +
+"\n			</businessService>" +
+"\n		</businessServices>" +
+"\n		<identityBag>" +
+"\n			<keyedReference tModelKey=\"uddi:nhin:nhie:homecommunityid\" keyName=\"\" keyValue=\"1.1\"/>" +
+"\n		</identityBag>" +
+"\n		<categoryBag>" +
+"\n			<keyedReference tModelKey=\"uddi:uddi.org:ubr:categorization:iso3166\" keyName=\"New York\" keyValue=\"US-NY\"/>" +
+"\n			<keyedReference tModelKey=\"uddi:uddi.org:ubr:categorization:iso3166\" keyName=\"Florida\" keyValue=\"US-FL\"/>" +
+"\n		</categoryBag>" +
+"\n	</businessEntity>" +
+"\n</businessDetail>";			
+
+	@Test
+	public void testConvert() {
+		InternalConnectionInfoFileConverter converter = new InternalConnectionInfoFileConverter();
+		String result = converter.convert(TEST_INPUT);
+		assertEquals(EXPECTED_TEST_OUTPUT, result);
+	}
+}
