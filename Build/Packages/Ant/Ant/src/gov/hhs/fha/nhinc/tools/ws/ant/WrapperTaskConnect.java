81adfa57565518fa215479d40ad31128a8c2d92b
ngoc.nguyen <ngoc.nguyen@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-01-19 20:36:04 +0000

    Merge the build folder from multi-spec-build-and-deploy branch. 1113

--- Build/Packages/Ant/Ant/src/gov/hhs/fha/nhinc/tools/ws/ant/WrapperTaskConnect.java
+++ Build/Packages/Ant/Ant/src/gov/hhs/fha/nhinc/tools/ws/ant/WrapperTaskConnect.java
@@ -1,27 +1,31 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  * 
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
+ * file and include the License file at packager/legal/LICENSE.txt.
  * 
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
  * Contributor(s):
- * 
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -38,6 +42,7 @@ package gov.hhs.fha.nhinc.tools.ws.ant;
 
 import com.sun.istack.tools.ProtectedTask;
 import com.sun.tools.ws.Invoker;
+import com.sun.tools.ws.wscompile.Options;
 import com.sun.tools.ws.resources.WscompileMessages;
 import com.sun.tools.xjc.api.util.ToolsJarNotFoundException;
 import com.sun.xml.bind.util.Which;
@@ -54,12 +59,11 @@ import java.io.IOException;
  */
 public abstract class WrapperTaskConnect extends ProtectedTask {
 
-    private boolean doEndorsedMagic = false;
-
     /**
      * Set to true to perform the endorsed directory override so that
      * Ant tasks can run on JavaSE 6. 
      */
+    private boolean doEndorsedMagic = false;
     public void setXendorsed(boolean f) {
         this.doEndorsedMagic = f;
     }
@@ -68,20 +72,42 @@ public abstract class WrapperTaskConnect extends ProtectedTask {
         return getClass().getName()+'2';
     }
 
+    private String targetVersionAttribute;
+
+
+    @Override
+    public void setDynamicAttribute(String name, String value) throws BuildException {
+        super.setDynamicAttribute(name,value);
+        if(name.equals("target"))
+            targetVersionAttribute = value;
+        else if(name.equals("xendorsed"))
+            this.doEndorsedMagic = Boolean.valueOf(value);
+
+    }
+
     protected ClassLoader createClassLoader() throws ClassNotFoundException, IOException {
         try {
             ClassLoader cl = getClass().getClassLoader();
             if(doEndorsedMagic) {
                 return Invoker.createClassLoader(cl);
             } else {
-                // check if we are indeed loading JAX-WS 2.1 API
-                if(Invoker.checkIfLoading21API())
+                Options.Target targetVersion;
+                if (targetVersionAttribute != null) {
+                    targetVersion = Options.Target.parse(targetVersionAttribute);
+                } else {
+                    targetVersion = Options.Target.getDefault();
+                }
+                Options.Target loadedVersion = Options.Target.getLoadedAPIVersion();
+                //Check if the target version is supported by the loaded API version
+                if (loadedVersion.isLaterThan(targetVersion)) {
                     return cl;
-
+                } else {
                 if(Service.class.getClassLoader()==null)
-                    throw new BuildException(WscompileMessages.WRAPPER_TASK_NEED_ENDORSED(getTaskName()));
-                else
-                    throw new BuildException(WscompileMessages.WRAPPER_TASK_LOADING_20_API(Which.which(Service.class)));
+                        throw new BuildException(WscompileMessages.WRAPPER_TASK_NEED_ENDORSED(loadedVersion.getVersion(), targetVersion.getVersion(), getTaskName()));
+                    else {
+                        throw new BuildException(WscompileMessages.WRAPPER_TASK_LOADING_INCORRECT_API(loadedVersion.getVersion(), Which.which(Service.class), targetVersion.getVersion()));
+            }
+                }
             }
         } catch (ToolsJarNotFoundException e) {
             throw new ClassNotFoundException(e.getMessage(),e);
