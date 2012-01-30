4b9314bd66ba610eb71783ba2323cb1d840543be
bhumphrey@5amsolutions.com <bhumphrey@5amsolutions.com@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-01-30 14:55:38 +0000

    GATEWAY-1226

--- Product/Production/Gateway/CONNECTGatewayPatientDiscoveryWeb10/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/gateway/ws/NhincProxyPatientDiscoveryAsyncResp.java
+++ Product/Production/Gateway/CONNECTGatewayPatientDiscoveryWeb10/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/gateway/ws/NhincProxyPatientDiscoveryAsyncResp.java
@@ -7,26 +7,25 @@
 package gov.hhs.fha.nhinc.patientdiscovery.gateway.ws;
 
 import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.soap.Addressing;
 
 /**
- *
+ * 
  * @author Neil Webb
  */
 @WebService(serviceName = "NhincProxyPatientDiscoveryAsyncResp", portName = "NhincProxyPatientDiscoveryAsyncRespPortType", endpointInterface = "gov.hhs.fha.nhinc.nhincproxypatientdiscoveryasyncresp.NhincProxyPatientDiscoveryAsyncRespPortType", targetNamespace = "urn:gov:hhs:fha:nhinc:nhincproxypatientdiscoveryasyncresp", wsdlLocation = "WEB-INF/wsdl/NhincProxyPatientDiscoveryAsyncResp/NhincProxyPatientDiscoveryAsyncResp.wsdl")
 @BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
-@Addressing(enabled=true)
-public class NhincProxyPatientDiscoveryAsyncResp extends PatientDiscoveryBase
-{
-    @Resource
-    private WebServiceContext context;
-    
-    
+@Addressing(enabled = true)
+public class NhincProxyPatientDiscoveryAsyncResp extends PatientDiscoveryBase {
+	@Resource
+	private WebServiceContext context;
 
-    public NhincProxyPatientDiscoveryAsyncResp() {
+	public NhincProxyPatientDiscoveryAsyncResp() {
 		super();
 	}
 
@@ -34,11 +33,14 @@ public class NhincProxyPatientDiscoveryAsyncResp extends PatientDiscoveryBase
 			PatientDiscoveryServiceFactory serviceFactory) {
 		super(serviceFactory);
 	}
-
-	public org.hl7.v3.MCCIIN000002UV01 proxyProcessPatientDiscoveryAsyncResp(org.hl7.v3.ProxyPRPAIN201306UVProxyRequestType proxyProcessPatientDiscoveryAsyncRespRequest)
-    {
-        return getServiceFactory().getNhincProxyPatientDiscoveryAsyncRespImpl().proxyProcessPatientDiscoveryAsyncResp(proxyProcessPatientDiscoveryAsyncRespRequest, getWebServiceContext());
-    }
+	
+	public org.hl7.v3.MCCIIN000002UV01 proxyProcessPatientDiscoveryAsyncResp(
+			org.hl7.v3.ProxyPRPAIN201306UVProxyRequestType proxyProcessPatientDiscoveryAsyncRespRequest) {
+		return getServiceFactory().getNhincProxyPatientDiscoveryAsyncRespImpl()
+				.proxyProcessPatientDiscoveryAsyncResp(
+						proxyProcessPatientDiscoveryAsyncRespRequest,
+						getWebServiceContext());
+		}
 
 	protected WebServiceContext getWebServiceContext() {
 		return context;
--- Product/Production/Gateway/CONNECTGatewayPatientDiscoveryWeb10/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/gateway/ws/NhincProxyPatientDiscoveryAsyncRespTest.java
+++ Product/Production/Gateway/CONNECTGatewayPatientDiscoveryWeb10/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/gateway/ws/NhincProxyPatientDiscoveryAsyncRespTest.java
@@ -1,10 +1,18 @@
 package gov.hhs.fha.nhinc.patientdiscovery.gateway.ws;
 
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
+
+import java.lang.reflect.Method;
+
 import gov.hhs.fha.nhinc.patientdiscovery.passthru.deferred.response.NhincProxyPatientDiscoveryAsyncRespImpl;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.soap.Addressing;
 
 import org.hl7.v3.MCCIIN000002UV01;
 import org.hl7.v3.ProxyPRPAIN201306UVProxyRequestType;
@@ -12,6 +20,7 @@ import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NhincProxyPatientDiscoveryAsyncRespTest {
@@ -51,4 +60,22 @@ public class NhincProxyPatientDiscoveryAsyncRespTest {
 		assertSame(expectedResponse, actualResponse);
 	
 	}
+	
+	@Test
+	public void verifyWebServiceAnnotation() {
+		WebService webServiceAnnotation = NhincProxyPatientDiscoveryAsyncResp.class.getAnnotation(WebService.class);
+		assertEquals("NhincProxyPatientDiscoveryAsyncRespPortType", webServiceAnnotation.portName());
+		assertEquals("NhincProxyPatientDiscoveryAsyncResp", webServiceAnnotation.serviceName());
+		assertEquals("gov.hhs.fha.nhinc.nhincproxypatientdiscoveryasyncresp.NhincProxyPatientDiscoveryAsyncRespPortType", webServiceAnnotation.endpointInterface());
+		assertEquals("urn:gov:hhs:fha:nhinc:nhincproxypatientdiscoveryasyncresp", webServiceAnnotation.targetNamespace());
+		assertEquals("WEB-INF/wsdl/NhincProxyPatientDiscoveryAsyncResp/NhincProxyPatientDiscoveryAsyncResp.wsdl", webServiceAnnotation.wsdlLocation());		
+	}
+	
+	
+	@Test
+	public void verifyAddressingIsEnabled() {
+		Addressing addressingAnnotation = NhincProxyPatientDiscoveryAsyncResp.class.getAnnotation(Addressing.class);
+		assertTrue(addressingAnnotation.enabled());
+	}
+	
 }
