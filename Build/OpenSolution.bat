276c86e77901085608bec65a8c0434a8fc14d667
bhumphrey@5amsolutions.com <bhumphrey@5amsolutions.com@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-03-06 20:38:26 +0000

    removing legacy un-used

--- Build/OpenSolution.bat
+++
@@ -1,7 +0,0 @@
-CALL SetEnv.bat
-
-set ENVBAT="%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-IF DEFINED ProgramFiles(x86) set ENVBAT="%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-call %ENVBAT%
-
-devenv "NHINCBuildScripts.sln"
--- Build/RecompileAnalytics.bat
+++
@@ -1,3 +0,0 @@
-".\nAnt\bin\nant.exe" -buildfile:RecompileAnalytics.xml
-
-SET /P variable="Hit Enter to continue."
