ad21750bdf6ef153d070cb16c8f5da2aab276aad
ngoc.nguyen <ngoc.nguyen@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-03-06 23:34:59 +0000

    Gateway-1626 IV&V: The Product/DBScripts/nhincdb folder does not contain update SQL DDL scripts for 3.3 related schema changes 1626

--- Product/DBScripts/nhincdb/UpdateNhincDbTo3_3.sql
+++ Product/DBScripts/nhincdb/UpdateNhincDbTo3_3.sql
@@ -1,10 +1,14 @@
 -- CONNECT 3.3
 --
 -- Increase all assigning authority ids and community ids to 64 character long
+-- Drop database lift
+-- Add On-Demand columns
+--
 -- Created on: 2011-10-07
 -- Author: ngoc.nguyen
 -- ------------------------------------------------------
 
+-- Update IDs length
 
 ALTER TABLE `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY assigningauthorityid VARCHAR(64) NOT NULL;
 ALTER TABLE `assigningauthoritydb`.`aa_to_home_community_mapping` MODIFY homecommunityid VARCHAR(64) NOT NULL;
@@ -17,3 +21,13 @@ ALTER TABLE `patientcorrelationdb`.`correlatedidentifiers` MODIFY CorrelatedPati
 ALTER TABLE `patientcorrelationdb`.`pddeferredcorrelation` MODIFY AssigningAuthorityId VARCHAR(64) NOT NULL;
 ALTER TABLE `patientcorrelationdb`.`pddeferredcorrelation` MODIFY PatientId VARCHAR(128) NOT NULL;
 ALTER TABLE `docrepository`.`document` MODIFY PatientId VARCHAR(128) default NULL COMMENT 'Format of HL7 2.x CX';
+
+-- Drop lift Database
+
+drop database lift;
+
+-- Add On-Demand columns
+
+ALTER TABLE `docrepository`.`document` ADD OnDemand tinyint(1) NOT NULL default 0 COMMENT 'Indicate whether document is dynamic (true or 1) or static (false or 0).';
+ALTER TABLE `docrepository`.`document` ADD NewDocumentUniqueId varchar(128) default NULL;
+ALTER TABLE `docrepository`.`document` ADD NewRepositoryUniqueId varchar(128) default NULL;
