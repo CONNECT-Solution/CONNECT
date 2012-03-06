122f5c15d17adc04834d7301a1e9389662f28aa6
bhumphrey@5amsolutions.com <bhumphrey@5amsolutions.com@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-03-06 20:37:59 +0000

    removing legacy un-used

--- Build/CCNET.bat
+++
@@ -1,9 +0,0 @@
-FOR /F "TOKENS=4 DELIMS=\," %%A IN ('cd') DO set CodeLineName=%%A
-FOR /F "TOKENS=3 DELIMS=\," %%A IN ('cd') DO set ProjectName=%%A
-FOR /F %%A IN ('hostname') DO SET HostName=%%A
-
-title %ProjectName% %CodeLineName% %HostName% Build Server
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
-
-cd server
-CCNET.exe -config:"..\CCNet.%HostName%.xml" -remoting:on
