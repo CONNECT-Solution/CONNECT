e828ad9b67f7a8cbc70f76b27ff16dc5af981511
ngoc.nguyen <ngoc.nguyen@55fedfc4-eeb4-48df-a550-72e3fef69cde>
2012-03-08 15:46:00 +0000

    Gateway-1625 IV&V: The Product/DBScripts individual schema SQL DDL script files have not been updated 1625

---
+++ Product/DBScripts/logging/logging.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS logging.log;
+CREATE TABLE logging.log (
+    dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    context varchar(100) DEFAULT NULL,
+    logLevel varchar(10) DEFAULT NULL,
+    class varchar(500) DEFAULT NULL,
+    message longtext
+);
+
+
+
