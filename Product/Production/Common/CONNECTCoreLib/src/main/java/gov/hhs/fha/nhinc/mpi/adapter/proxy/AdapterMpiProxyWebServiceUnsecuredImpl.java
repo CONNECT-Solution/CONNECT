d232f39d483e9e362b5216afdaaa1b0787a0da3f
Arthur Kong <akong@5amsolutions.com>
2012-08-17 12:07:06 -0400

    GATEWAY-2577: Moved PD adapter to use CXF

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/proxy/AdapterMpiProxyWebServiceUnsecuredImpl.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/mpi/adapter/proxy/AdapterMpiProxyWebServiceUnsecuredImpl.java
@@ -27,129 +27,99 @@
 package gov.hhs.fha.nhinc.mpi.adapter.proxy;
 
 import gov.hhs.fha.nhinc.adaptermpi.AdapterMpiPortType;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.messaging.client.CONNECTCXFClientFactory;
+import gov.hhs.fha.nhinc.messaging.client.CONNECTClient;
+import gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor;
+import gov.hhs.fha.nhinc.mpi.adapter.proxy.service.AdapterMpiUnsecuredServicePortDescriptor;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import gov.hhs.fha.nhinc.nhinclib.NullChecker;
+import gov.hhs.fha.nhinc.webserviceproxy.WebServiceProxyHelper;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.v3.PRPAIN201305UV02;
 import org.hl7.v3.PRPAIN201306UV02;
-import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
-import gov.hhs.fha.nhinc.nhinclib.NhincConstants.ADAPTER_API_LEVEL;
-import gov.hhs.fha.nhinc.webserviceproxy.WebServiceProxyHelper;
-import javax.xml.ws.Service;
-import javax.xml.namespace.QName;
 import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
 
 /**
  * This is the proxy class for the unsecued AdapterComponentProxy interface.
- * 
- * @author Les Westberg
  */
 public class AdapterMpiProxyWebServiceUnsecuredImpl implements AdapterMpiProxy {
 
-    private static Service cachedService = null;
-    private static final String NAMESPACE_URI = "urn:gov:hhs:fha:nhinc:adaptermpi";
-    private static final String SERVICE_LOCAL_PART = "AdapterMpiService";
-    private static final String PORT_LOCAL_PART = "AdapterMpiPort";
-    private static final String WSDL_FILE = "AdapterMpi.wsdl";
-    private static final String WS_ADDRESSING_ACTION = "urn:gov:hhs:fha:nhinc:adaptermpi:FindCandidatesRequest";
-    private Log log = null;
-    private WebServiceProxyHelper oProxyHelper = new WebServiceProxyHelper();
+	private Log log = null;
+	private WebServiceProxyHelper oProxyHelper = new WebServiceProxyHelper();
 
-    /**
-     * Default constructor.
-     */
-    public AdapterMpiProxyWebServiceUnsecuredImpl() {
-        log = createLogger();
-    }
+	/**
+	 * Default constructor.
+	 */
+	public AdapterMpiProxyWebServiceUnsecuredImpl() {
+		log = createLogger();
+	}
 
-    /**
-     * Creates the log object for logging.
-     * 
-     * @return The log object.
-     */
-    protected Log createLogger() {
-        return ((log != null) ? log : LogFactory.getLog(getClass()));
-    }
+	/**
+	 * Creates the log object for logging.
+	 * 
+	 * @return The log object.
+	 */
+	protected Log createLogger() {
+		return ((log != null) ? log : LogFactory.getLog(getClass()));
+	}
 
-    /**
-     * Find the matching candidates from the MPI.
-     * 
-     * @param request The information to use for matching.
-     * @param assertion The assertion data.
-     * @return The matches that are found.
-     */
-    public PRPAIN201306UV02 findCandidates(PRPAIN201305UV02 request, AssertionType assertion) {
-        String url = null;
-        PRPAIN201306UV02 response = new PRPAIN201306UV02();
-        String sServiceName = NhincConstants.ADAPTER_MPI_SERVICE_NAME;
+	protected CONNECTClient<AdapterMpiPortType> getCONNECTClientUnsecured(
+			ServicePortDescriptor<AdapterMpiPortType> portDescriptor,
+			String url, AssertionType assertion) {
 
-        try {
-            if (request != null) {
-                log.debug("Before target system URL look up.");
-                url = oProxyHelper.getAdapterEndPointFromConnectionManager(sServiceName);
-                log.debug("After target system URL look up. URL for service: " + sServiceName + " is: " + url);
+		return CONNECTCXFClientFactory.getInstance().getCONNECTClientUnsecured(
+				portDescriptor, url, assertion);
+	}
 
-                if (NullChecker.isNotNullish(url)) {
-                    RespondingGatewayPRPAIN201305UV02RequestType wsRequest = new RespondingGatewayPRPAIN201305UV02RequestType();
-                    wsRequest.setPRPAIN201305UV02(request);
-                    wsRequest.setAssertion(assertion);
-                    AdapterMpiPortType port = getPort(url, NhincConstants.ADAPTER_MPI_ACTION, WS_ADDRESSING_ACTION,
-                            assertion);
-                    response = (PRPAIN201306UV02) oProxyHelper.invokePort(port, AdapterMpiPortType.class,
-                            "findCandidates", wsRequest);
-                } else {
-                    log.error("Failed to call the web service (" + sServiceName + ").  The URL is null.");
-                }
-            } else {
-                log.error("Failed to call the web service (" + sServiceName + ").  The input parameter is null.");
-            }
-        } catch (Exception e) {
-            log.error("Failed to call the web service (" + sServiceName + ").  An unexpected exception occurred.  "
-                    + "Exception: " + e.getMessage(), e);
-        }
+	/**
+	 * Find the matching candidates from the MPI.
+	 * 
+	 * @param request
+	 *            The information to use for matching.
+	 * @param assertion
+	 *            The assertion data.
+	 * @return The matches that are found.
+	 */
+	public PRPAIN201306UV02 findCandidates(PRPAIN201305UV02 request,
+			AssertionType assertion) {
+		String url = null;
+		PRPAIN201306UV02 response = new PRPAIN201306UV02();
+		String sServiceName = NhincConstants.ADAPTER_MPI_SERVICE_NAME;
 
-        return response;
-    }
+		try {
+			if (request != null) {
+				url = oProxyHelper
+						.getAdapterEndPointFromConnectionManager(sServiceName);
 
-    /**
-     * Retrieve the service class for this web service.
-     * 
-     * @return The service class for this web service.
-     */
-    protected Service getService() {
-        if (cachedService == null) {
-            try {
-                cachedService = oProxyHelper.createService(WSDL_FILE, NAMESPACE_URI, SERVICE_LOCAL_PART);
-            } catch (Throwable t) {
-                log.error("Error creating service: " + t.getMessage(), t);
-            }
-        }
-        return cachedService;
-    }
+				if (NullChecker.isNotNullish(url)) {
+					RespondingGatewayPRPAIN201305UV02RequestType wsRequest = new RespondingGatewayPRPAIN201305UV02RequestType();
+					wsRequest.setPRPAIN201305UV02(request);
+					wsRequest.setAssertion(assertion);
 
-    /**
-     * This method retrieves and initializes the port.
-     * 
-     * @param url The URL for the web service.
-     * @param serviceAction The action for the web service.
-     * @param wsAddressingAction The action assigned to the input parameter for the web service operation.
-     * @param assertion The assertion information for the web service
-     * @return The port object for the web service.
-     */
-    protected AdapterMpiPortType getPort(String url, String serviceAction, String wsAddressingAction,
-            AssertionType assertion) {
-        AdapterMpiPortType port = null;
-        Service service = getService();
-        if (service != null) {
-            log.debug("Obtained service - creating port.");
+					ServicePortDescriptor<AdapterMpiPortType> portDescriptor = new AdapterMpiUnsecuredServicePortDescriptor();
+					CONNECTClient<AdapterMpiPortType> client = getCONNECTClientUnsecured(
+							portDescriptor, url, assertion);
 
-            port = service.getPort(new QName(NAMESPACE_URI, PORT_LOCAL_PART), AdapterMpiPortType.class);
-            oProxyHelper
-                    .initializeUnsecurePort((javax.xml.ws.BindingProvider) port, url, wsAddressingAction, assertion);
-        } else {
-            log.error("Unable to obtain serivce - no port created.");
-        }
-        return port;
-    }
+					response = (PRPAIN201306UV02) client.invokePort(
+							AdapterMpiPortType.class, "findCandidates",
+							wsRequest);
+				} else {
+					log.error("Failed to call the web service (" + sServiceName
+							+ ").  The URL is null.");
+				}
+			} else {
+				log.error("Failed to call the web service (" + sServiceName
+						+ ").  The input parameter is null.");
+			}
+		} catch (Exception e) {
+			log.error("Failed to call the web service (" + sServiceName
+					+ ").  An unexpected exception occurred.  " + "Exception: "
+					+ e.getMessage(), e);
+		}
+
+		return response;
+	}
 }
