f58b9e0598df05f80bc7624767fef5f7bf597302
Todd C. Parnell <tparnell@5amsolutions.com>
2012-11-20 14:12:43 -0500

    GATEWAY-3121: PD response audit description builder.  Added status extraction.

--- Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/aspect/PRPAIN201306UV02EventDescriptionBuilder.java
+++ Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/aspect/PRPAIN201306UV02EventDescriptionBuilder.java
@@ -27,8 +27,26 @@
 package gov.hhs.fha.nhinc.patientdiscovery.aspect;
 
 import gov.hhs.fha.nhinc.event.AssertionEventDescriptionBuilder;
+import gov.hhs.fha.nhinc.util.Base64Coder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hl7.v3.BinaryDataEncoding;
+import org.hl7.v3.EDExplicit;
+import org.hl7.v3.MCCIMT000300UV01Acknowledgement;
+import org.hl7.v3.MCCIMT000300UV01AcknowledgementDetail;
+import org.hl7.v3.PRPAIN201306UV02;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
 public class PRPAIN201306UV02EventDescriptionBuilder extends AssertionEventDescriptionBuilder {
+
+    private static final StatusExtractor STATUS_EXTRACTOR = new StatusExtractor();
+
+    private PRPAIN201306UV02 body;
 
     public PRPAIN201306UV02EventDescriptionBuilder() {
     }
@@ -51,6 +69,13 @@ public class PRPAIN201306UV02EventDescriptionBuilder extends AssertionEventDescr
 
     @Override
     public void buildStatuses() {
+        List<String> statuses = new ArrayList<String>();
+
+        for (MCCIMT000300UV01Acknowledgement acknowledgement : body.getAcknowledgement()) {
+            List<Optional<String>> tmp = Lists.transform(acknowledgement.getAcknowledgementDetail(), STATUS_EXTRACTOR);
+            statuses.addAll(Lists.newArrayList(Optional.presentInstances(tmp)));
+        }
+        setStatuses(statuses);
     }
 
     @Override
@@ -58,11 +83,41 @@ public class PRPAIN201306UV02EventDescriptionBuilder extends AssertionEventDescr
     }
 
     @Override
-    public void setArguments(Object... arguements) {
-        extractAssertion(arguements);
+    public void setArguments(Object... arguments) {
+        extractAssertion(arguments);
+        extractBody(arguments);
+    }
+
+    private void extractBody(Object[] arguments) {
+        for (int i = 0; i < arguments.length; ++i) {
+            if (arguments[i] instanceof PRPAIN201306UV02) {
+                body = (PRPAIN201306UV02) arguments[i];
+            }
+        }
     }
 
     @Override
     public void setReturnValue(Object returnValue) {
+    }
+
+    private static class StatusExtractor implements Function<MCCIMT000300UV01AcknowledgementDetail, Optional<String>> {
+
+        @Override
+        public Optional<String> apply(MCCIMT000300UV01AcknowledgementDetail detail) {
+            EDExplicit edExplicit = detail.getText();
+            if (edExplicit == null || edExplicit.getContent().size() == 0) {
+                return Optional.absent();
+            }
+            return Optional.of(convertContent(edExplicit));
+        }
+
+        private String convertContent(EDExplicit edExplicit) {
+            String rawText = edExplicit.getContent().get(0).toString(); // Not dealing with multiple contents
+            if (edExplicit.getRepresentation().equals(BinaryDataEncoding.TXT)) {
+                return rawText;
+            } else {
+                return Base64Coder.decodeString(rawText);
+            }
+        }
     }
 }
--- Product/Production/Services/PatientDiscoveryCore/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/aspect/PRPAIN201306UV02EventDescriptionBuilderTest.java
+++ Product/Production/Services/PatientDiscoveryCore/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/aspect/PRPAIN201306UV02EventDescriptionBuilderTest.java
@@ -26,16 +26,36 @@
  */
 package gov.hhs.fha.nhinc.patientdiscovery.aspect;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.event.AssertionEventDescriptionBuilder;
 import gov.hhs.fha.nhinc.event.BaseDescriptionBuilderTest;
+import gov.hhs.fha.nhinc.event.EventDescription;
+import gov.hhs.fha.nhinc.util.Base64Coder;
 
+import java.io.Serializable;
+import java.util.List;
+
+import org.hl7.v3.BinaryDataEncoding;
+import org.hl7.v3.EDExplicit;
+import org.hl7.v3.MCCIMT000300UV01Acknowledgement;
+import org.hl7.v3.MCCIMT000300UV01AcknowledgementDetail;
 import org.hl7.v3.PRPAIN201306UV02;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PRPAIN201306UV02EventDescriptionBuilderTest extends BaseDescriptionBuilderTest {
+
+    private PRPAIN201306UV02 body;
+    private List<MCCIMT000300UV01Acknowledgement> acknowledgementList;
+
+    @Before
+    public void before() {
+        body = new PRPAIN201306UV02();
+        acknowledgementList = body.getAcknowledgement();
+    }
 
     @Test
     public void handlesAssertions() {
@@ -46,7 +66,116 @@ public class PRPAIN201306UV02EventDescriptionBuilderTest extends BaseDescription
         PRPAIN201306UV02 body = mock(PRPAIN201306UV02.class);
         AssertionType assertion = mock(AssertionType.class);
 
-        builder.setArguments(new Object[] { body, assertion });
+        builder.setArguments(new Object[] { assertion, body });
         assertTrue(builder.getAssertion().isPresent());
+    }
+
+    @Test
+    public void basicStatus() {
+        addAcknowledgements(1, createDetail("Test Content"));
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(1, eventDescription.getStatuses().size());
+        assertEquals("Test Content", eventDescription.getStatuses().get(0));
+    }
+
+    @Test
+    public void twoAcknowledgements() {
+        addAcknowledgements(2, createDetail("Test Content"));
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(2, eventDescription.getStatuses().size());
+        assertEquals("Test Content", eventDescription.getStatuses().get(0));
+        assertEquals("Test Content", eventDescription.getStatuses().get(1));
+    }
+
+    @Test
+    public void multipleDetailsInAcknowledgement() {
+        addAcknowledgements(1, createDetail("Test Content"), createDetail("Test Content2"));
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(2, eventDescription.getStatuses().size());
+        assertEquals("Test Content", eventDescription.getStatuses().get(0));
+        assertEquals("Test Content2", eventDescription.getStatuses().get(1));
+    }
+
+    @Test
+    public void base64StatusContent() {
+        addAcknowledgements(1, createDetail(BinaryDataEncoding.B_64, "Test Content"));
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(1, eventDescription.getStatuses().size());
+        assertEquals("Test Content", eventDescription.getStatuses().get(0));
+    }
+
+    @Test
+    public void nullFlavoredStatus() {
+        MCCIMT000300UV01AcknowledgementDetail detail = new MCCIMT000300UV01AcknowledgementDetail();
+        detail.getNullFlavor().add("NI");
+
+        addAcknowledgements(1, detail);
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(0, eventDescription.getStatuses().size());
+    }
+
+    @Test
+    public void missingSerializedContent() {
+        addAcknowledgements(1, createDetail(BinaryDataEncoding.TXT, null));
+
+        PRPAIN201306UV02EventDescriptionBuilder builder = new PRPAIN201306UV02EventDescriptionBuilder();
+        builder.setArguments(body);
+
+        EventDescription eventDescription = getEventDescription(builder);
+        assertEquals(0, eventDescription.getStatuses().size());
+    }
+
+    private void addAcknowledgements(int count, MCCIMT000300UV01AcknowledgementDetail... details) {
+        for (int i = 0; i < count; ++i) {
+            MCCIMT000300UV01Acknowledgement acknowledgement = new MCCIMT000300UV01Acknowledgement();
+            acknowledgementList.add(acknowledgement);
+
+            List<MCCIMT000300UV01AcknowledgementDetail> acknowledgementDetailList = acknowledgement
+                    .getAcknowledgementDetail();
+            for (int j = 0; j < details.length; ++j) {
+                acknowledgementDetailList.add(details[j]);
+            }
+        }
+    }
+
+    private MCCIMT000300UV01AcknowledgementDetail createDetail(String rawContent) {
+        return createDetail(BinaryDataEncoding.TXT, rawContent);
+    }
+
+    private MCCIMT000300UV01AcknowledgementDetail createDetail(BinaryDataEncoding encoding, String rawContent) {
+        EDExplicit ed = new EDExplicit();
+        ed.setRepresentation(encoding);
+        if (rawContent != null) {
+            List<Serializable> content = ed.getContent();
+            if (encoding.equals(BinaryDataEncoding.TXT)) {
+                content.add(rawContent);
+            } else {
+                content.add(Base64Coder.encodeString(rawContent));
+            }
+        }
+        MCCIMT000300UV01AcknowledgementDetail detail = new MCCIMT000300UV01AcknowledgementDetail();
+        detail.setText(ed);
+
+        return detail;
     }
 }
