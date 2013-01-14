6f84748db89af0c790c081dde7d7f4f6fbb78ba1
Matt Weaver <matthew.weaver@cgifederal.com>
2013-01-14 10:12:08 -0500

    GATEWAY-3309: fixes after clarification on code review comment.

--- Product/Production/Services/PatientCorrelationCore/src/test/java/gov/hhs/fha/nhinc/patientcorrelation/nhinc/proxy/PatientCorrelationProxyWebServiceUnsecuredImplTest.java
+++ Product/Production/Services/PatientCorrelationCore/src/test/java/gov/hhs/fha/nhinc/patientcorrelation/nhinc/proxy/PatientCorrelationProxyWebServiceUnsecuredImplTest.java
@@ -7,6 +7,7 @@ import gov.hhs.fha.nhinc.nhinccomponentpatientcorrelation.PatientCorrelationPort
 
 import org.hl7.v3.PRPAIN201301UV02;
 import org.hl7.v3.PRPAIN201309UV02;
+import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -18,6 +19,8 @@ public class PatientCorrelationProxyWebServiceUnsecuredImplTest {
 	private CONNECTClientFactory mockCONNECTClientFactory = mock(CONNECTClientFactory.class);
 	@SuppressWarnings("unchecked")
 	private CONNECTClient<PatientCorrelationPortType> mockClient = mock (CONNECTClient.class);
+	@SuppressWarnings("unchecked")
+	private CONNECTClient<PatientCorrelationPortType> mockSecuredClient = mock (CONNECTClient.class);
 	private AssertionType mockAssertion = mock(AssertionType.class);
 	private PRPAIN201301UV02 mock301 = mock(PRPAIN201301UV02.class);
 	private PRPAIN201309UV02 mock309 = mock(PRPAIN201309UV02.class);
@@ -27,20 +30,20 @@ public class PatientCorrelationProxyWebServiceUnsecuredImplTest {
 	@Test
 	public void testAddPatientCorrelation() throws Exception {
 		PatientCorrelationProxyWebServiceUnsecuredImpl impl = getPatientCorrelationProxyWebServiceUnsecuredImpl();
-		when(mockCONNECTClientFactory.getCONNECTClientUnsecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class))).thenReturn(mockClient);
 		impl.addPatientCorrelation(mock301, mockAssertion);
 		
 		verify(mockClient).invokePort(any(Class.class), any(String.class), any(Object.class));
+		verify(mockCONNECTClientFactory).getCONNECTClientUnsecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class));
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testretrievePatientCorrelations() throws Exception {
 		PatientCorrelationProxyWebServiceUnsecuredImpl impl = getPatientCorrelationProxyWebServiceUnsecuredImpl();
-		when(mockCONNECTClientFactory.getCONNECTClientUnsecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class))).thenReturn(mockClient);
 		impl.retrievePatientCorrelations(mock309, mockAssertion);
 		
 		verify(mockClient).invokePort(any(Class.class), any(String.class), any(Object.class));
+		verify(mockCONNECTClientFactory).getCONNECTClientUnsecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class));
 	}
 
 	protected PatientCorrelationProxyWebServiceUnsecuredImpl getPatientCorrelationProxyWebServiceUnsecuredImpl() {
@@ -51,5 +54,12 @@ public class PatientCorrelationProxyWebServiceUnsecuredImplTest {
 				return mockCONNECTClientFactory;
 			}
 		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setup() {
+		when(mockCONNECTClientFactory.getCONNECTClientUnsecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class))).thenReturn(mockClient);
+		when(mockCONNECTClientFactory.getCONNECTClientSecured(any(ServicePortDescriptor.class), any(String.class), any(AssertionType.class))).thenReturn(mockSecuredClient);
 	}
 }
