85762409eb8965b14e42625a630a24c9fc037241
Michael Landis <mlandis@5amsolutions.com>
2013-01-24 15:18:15 -0500

    GATEWAY-3381: Reverted changes to passthrough, added logging to standard inbound and removed dependency on passthrough.

--- Product/Production/Services/DocumentQueryCore/src/test/java/gov/hhs/fha/nhinc/docquery/inbound/PassthroughInboundDocQueryTest.java
+++ Product/Production/Services/DocumentQueryCore/src/test/java/gov/hhs/fha/nhinc/docquery/inbound/PassthroughInboundDocQueryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, United States Government, as represented by the Secretary of Health and Human Services.
+ * Copyright (c) 2012, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,27 +26,26 @@
  */
 package gov.hhs.fha.nhinc.docquery.inbound;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import java.lang.reflect.Method;
+
 import gov.hhs.fha.nhinc.aspect.InboundProcessingEvent;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
-import gov.hhs.fha.nhinc.common.nhinccommon.HomeCommunityType;
-import gov.hhs.fha.nhinc.common.nhinccommon.UserType;
 import gov.hhs.fha.nhinc.docquery.DocQueryAuditLog;
-import gov.hhs.fha.nhinc.docquery.DocQueryUnitTestUtil;
 import gov.hhs.fha.nhinc.docquery.adapter.proxy.AdapterDocQueryProxy;
 import gov.hhs.fha.nhinc.docquery.adapter.proxy.AdapterDocQueryProxyObjectFactory;
 import gov.hhs.fha.nhinc.docquery.aspect.AdhocQueryRequestDescriptionBuilder;
 import gov.hhs.fha.nhinc.docquery.aspect.AdhocQueryResponseDescriptionBuilder;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 
-import java.lang.reflect.Method;
-import java.net.URISyntaxException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest;
 import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
@@ -58,19 +57,6 @@ import org.junit.Test;
  * 
  */
 public class PassthroughInboundDocQueryTest {
-        
-    /**
-     * Responding gateway home community id (from gateway.properties).
-     */
-    private static final String RESPONDING_HCID_FORMATTED = "1.2";    
-    
-    /**
-     * Sending gateway home community id (from the assertion org/home community).
-     */
-    private static final String SENDING_HCID_ORG = "urn:oid:7.2";
-    private static final String SENDING_HCID_ORG_FORMATTED = "7.2";
-    private static final String SENDING_HCID_HOME = "urn:oid:7.3";
-    private static final String SENDING_HCID_HOME_FORMATTED = "7.3";
     
     @Test
     public void hasInboundProcessingEvent() throws Exception {
@@ -85,31 +71,10 @@ public class PassthroughInboundDocQueryTest {
         assertEquals("", annotation.version());
     }
 
-    /**
-     * Test {@link PassthroughInboundDocQuery#respondingGatewayCrossGatewayQuery(AdhocQueryRequest, AssertionType)}
-     * when sending hcid comes from org part of the assertion.
-     * @throws URISyntaxException
-     */
     @Test
-    public void passthroughInboundDocQueryOrgHcid() throws URISyntaxException {
-        passthroughInboundDocQuery(getMockAssertion(SENDING_HCID_ORG), SENDING_HCID_ORG_FORMATTED);
-    }
-    
-    /**
-     * Test {@link PassthroughInboundDocQuery#respondingGatewayCrossGatewayQuery(AdhocQueryRequest, AssertionType)}
-     * when sending hcid comes from home community part of the assertion.
-     * @throws URISyntaxException
-     */
-    @Test
-    public void passthroughInboundDocQueryHomeHcid() throws URISyntaxException {
-        passthroughInboundDocQuery(getMockAssertion(SENDING_HCID_HOME), SENDING_HCID_HOME_FORMATTED);
-    }
-
-    private void passthroughInboundDocQuery(AssertionType assertion, String sendingHcid) throws URISyntaxException {
-        
-        DocQueryUnitTestUtil.setUpGatewayProperties();
-        
+    public void passthroughInboundDocQuery() {
         AdhocQueryRequest request = new AdhocQueryRequest();
+        AssertionType assertion = new AssertionType();
         AdhocQueryResponse expectedResponse = new AdhocQueryResponse();
 
         AdapterDocQueryProxyObjectFactory mockAdapterFactory = mock(AdapterDocQueryProxyObjectFactory.class);
@@ -117,6 +82,7 @@ public class PassthroughInboundDocQueryTest {
         DocQueryAuditLog mockAuditLogger = mock(DocQueryAuditLog.class);
 
         when(mockAdapterFactory.getAdapterDocQueryProxy()).thenReturn(mockAdapterProxy);
+
         when(mockAdapterProxy.respondingGatewayCrossGatewayQuery(request, assertion)).thenReturn(expectedResponse);
 
         PassthroughInboundDocQuery passthroughDocQuery = new PassthroughInboundDocQuery(mockAdapterFactory,
@@ -127,31 +93,18 @@ public class PassthroughInboundDocQueryTest {
 
         verify(mockAuditLogger).auditDQRequest(eq(request), eq(assertion),
                 eq(NhincConstants.AUDIT_LOG_INBOUND_DIRECTION), eq(NhincConstants.AUDIT_LOG_NHIN_INTERFACE),
-                eq(sendingHcid));
+                any(String.class));
 
         verify(mockAuditLogger).auditDQRequest(eq(request), eq(assertion),
                 eq(NhincConstants.AUDIT_LOG_OUTBOUND_DIRECTION), eq(NhincConstants.AUDIT_LOG_ADAPTER_INTERFACE),
-                eq(RESPONDING_HCID_FORMATTED));
+                any(String.class));
 
         verify(mockAuditLogger).auditDQResponse(eq(actualResponse), eq(assertion),
                 eq(NhincConstants.AUDIT_LOG_INBOUND_DIRECTION), eq(NhincConstants.AUDIT_LOG_ADAPTER_INTERFACE),
-                eq(RESPONDING_HCID_FORMATTED));
+                any(String.class));
 
         verify(mockAuditLogger).auditDQResponse(eq(actualResponse), eq(assertion),
                 eq(NhincConstants.AUDIT_LOG_OUTBOUND_DIRECTION), eq(NhincConstants.AUDIT_LOG_NHIN_INTERFACE),
-                eq(sendingHcid));
+                any(String.class));
     }
-   
-    private AssertionType getMockAssertion(String hcid) {
-        AssertionType assertion = mock(AssertionType.class);
-        UserType user = mock(UserType.class);
-        HomeCommunityType homeCommunity = mock(HomeCommunityType.class);
-        
-        when(assertion.getUserInfo()).thenReturn(user);
-        when(user.getOrg()).thenReturn(homeCommunity);        
-        when(homeCommunity.getHomeCommunityId()).thenReturn(hcid);
-        return assertion; 
-    }
-
-
 }
