ae455678198f86ca8b1481192fc08729ca94817f
Matt Weaver <matthew.weaver@cgifederal.com>
2013-01-30 16:47:19 -0500

    GATEWAY-3390 subtask GATEWAY-3424: removed dependency on JKS.

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/CertificateManagerImpl.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/CertificateManagerImpl.java
@@ -1,34 +1,33 @@
 /*
- * Copyright (c) 2012, United States Government, as represented by the Secretary of Health and Human Services. 
- * All rights reserved. 
+ * Copyright (c) 2012, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met: 
- *     * Redistributions of source code must retain the above 
- *       copyright notice, this list of conditions and the following disclaimer. 
- *     * Redistributions in binary form must reproduce the above copyright 
- *       notice, this list of conditions and the following disclaimer in the documentation 
- *       and/or other materials provided with the distribution. 
- *     * Neither the name of the United States Government nor the 
- *       names of its contributors may be used to endorse or promote products 
- *       derived from this software without specific prior written permission. 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY 
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package gov.hhs.fha.nhinc.callback.openSAML;
 
 import gov.hhs.fha.nhinc.cryptostore.StoreUtil;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
@@ -45,7 +44,7 @@ import org.apache.log4j.Logger;
 
 /**
  * @author mweaver
- * 
+ *
  */
 public class CertificateManagerImpl implements CertificateManager {
     private static final Logger LOG = Logger.getLogger(CertificateManagerImpl.class);
@@ -71,7 +70,7 @@ public class CertificateManagerImpl implements CertificateManager {
     public static CertificateManager getInstance() {
         return new CertificateManagerImpl();
     }
-    
+
     /**
      * @return the keyStore
      */
@@ -91,7 +90,7 @@ public class CertificateManagerImpl implements CertificateManager {
     /**
      * Initializes the keystore access using the system properties defined in the domain.xml javax.net.ssl.keyStore and
      * javax.net.ssl.keyStorePassword
-     * 
+     *
      * @throws Exception
      */
     private void initKeyStore() throws Exception {
@@ -117,9 +116,7 @@ public class CertificateManagerImpl implements CertificateManager {
         } else {
             try {
                 keyStore = KeyStore.getInstance(storeType);
-                if ("JKS".equals(storeType)) {
-                    is = new FileInputStream(storeLoc);
-                }
+                is = new FileInputStream(storeLoc);
                 keyStore.load(is, password.toCharArray());
             } catch (NoSuchAlgorithmException ex) {
                 LOG.error("Error initializing KeyStore: " + ex);
@@ -147,7 +144,7 @@ public class CertificateManagerImpl implements CertificateManager {
     /**
      * Initializes the truststore access using the system properties defined in the domain.xml javax.net.ssl.trustStore
      * and javax.net.ssl.trustStorePassword
-     * 
+     *
      * @throws Exception
      */
     private void initTrustStore() throws Exception {
@@ -169,9 +166,7 @@ public class CertificateManagerImpl implements CertificateManager {
             } else {
                 try {
                     trustStore = KeyStore.getInstance(storeType);
-                    if ("JKS".equals(storeType)) {
-                        is = new FileInputStream(storeLoc);
-                    }
+                    is = new FileInputStream(storeLoc);
                     trustStore.load(is, password.toCharArray());
                 } catch (NoSuchAlgorithmException ex) {
                     LOG.error("Error initializing TrustStore: " + ex);
@@ -200,7 +195,7 @@ public class CertificateManagerImpl implements CertificateManager {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see gov.hhs.fha.nhinc.callback.openSAML.CertificateManager#getDefaultCertificate()
      */
     public X509Certificate getDefaultCertificate() throws Exception {
@@ -209,7 +204,7 @@ public class CertificateManagerImpl implements CertificateManager {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see gov.hhs.fha.nhinc.callback.openSAML.CertificateManager#getDefaultPrivateKey()
      */
     @Override
@@ -254,7 +249,7 @@ public class CertificateManagerImpl implements CertificateManager {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see gov.hhs.fha.nhinc.callback.openSAML.CertificateManager#getDefaultPublicKey()
      */
     @Override
