da69d06a9eef5150cb2f9733c54dcbdaf559e94f
Christopher May <christopher.may@cgifederal.com>
2013-12-04 19:16:48 -0500

    CONN-681: Fixed syntax error with misplaced NOT NULL

--- Product/DBScripts/nhincdb/nhincdb_oracle.sql
+++ Product/DBScripts/nhincdb/nhincdb_oracle.sql
@@ -78,7 +78,7 @@ CREATE TABLE nhincuser.document (
   DocumentUri varchar2(128) default NULL,
   RawData BLOB,
   Persistent number(11) NOT NULL,
-  OnDemand number(1,0) NOT NULL default 0,
+  OnDemand number(1,0) default 0 NOT NULL,
   NewDocumentUniqueId varchar(128) default NULL,
   NewRepositoryUniqueId varchar(128) default NULL,
   PRIMARY KEY  (documentid)
