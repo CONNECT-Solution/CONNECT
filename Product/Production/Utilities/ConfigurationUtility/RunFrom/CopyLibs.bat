7bd07e6c57050d9f432936cec03b6007feced7f5
Jason Smith <jason.smith@nitorgroup.com>
2014-01-30 14:06:12 -0500

    CONN-777 Remove Utilities

--- Product/Production/Utilities/ConfigurationUtility/RunFrom/CopyLibs.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Projects\NHINC\Current\Product\Production\Utilities\ConfigurationUtility\RunFrom
-copy C:\Projects\NHINC\Current\Product\Production\Utilities\ConfigurationUtility\dist\debug\ConfigurationUtility.jar .
-copy C:\Projects\NHINC\Current\Product\Production\Common\CONNECTCoreLib\dist\debug\CONNECTCoreLib.jar .
-copy C:\Projects\NHINC\Current\ThirdParty\log4j\commons-logging-1.1.1.jar .
-copy C:\Projects\NHINC\Current\ThirdParty\log4j\log4j-1.2.15.jar .
-copy C:\Projects\NHINC\Current\ThirdParty\SpringFramework\spring.jar .
