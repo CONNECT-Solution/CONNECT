cf8d833d9ca03dba66726a75731e914ad5151694
alameluchidambaram <alamelu.chidambaram@cgifederal.com>
2014-02-13 14:58:57 -0500

    CONN-821:Address PR comments and formated classes

--- Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/mail/AbstractMailPollerTest.java
+++ Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/mail/AbstractMailPollerTest.java
@@ -14,56 +14,54 @@ import static org.mockito.Mockito.when;
 
 /**
  * @author achidambaram
- *
+ * 
  */
 public class AbstractMailPollerTest {
-    
+
     private static final String EXCEPTION_MSG = "Error Message from MailClient";
-    
+
     MailReceiver mailReceiver = mock(MailReceiver.class);
     MessageHandler messageHandler = mock(MessageHandler.class);
-    
+
     @Test
     public void testPollMethodCanInvokeHandler() throws MailClientException {
         MailClientException mockException = mock(MailClientException.class);
         when(mockException.getMessage()).thenReturn(EXCEPTION_MSG);
-        
-        AbstractMailPoller poller = new AbstractMailPoller(mailReceiver,messageHandler) {
+
+        AbstractMailPoller poller = new AbstractMailPoller(mailReceiver, messageHandler) {
 
             @Override
             public void handleException(MailClientException e) {
                 assertEquals(EXCEPTION_MSG, e.getMessage());
             }
-            
+
         };
-        
-        
+
         poller.poll();
         verify(mailReceiver).handleMessages(messageHandler);
         verify(mockException, times(0)).getMessage();
     }
-    
+
     @Test
     public void testPollMethodCanHandleException() throws MailClientException {
-        
+
         MailClientException mockException = mock(MailClientException.class);
         when(mockException.getMessage()).thenReturn(EXCEPTION_MSG);
         when(mailReceiver.handleMessages(messageHandler)).thenThrow(mockException);
-        
-        AbstractMailPoller poller = new AbstractMailPoller(mailReceiver,messageHandler) {
+
+        AbstractMailPoller poller = new AbstractMailPoller(mailReceiver, messageHandler) {
 
             @Override
             public void handleException(MailClientException e) {
                 assertEquals(EXCEPTION_MSG, e.getMessage());
             }
-            
+
         };
-        
-        
+
         poller.poll();
         verify(mailReceiver).handleMessages(messageHandler);
         verify(mockException).getMessage();
-        
+
     }
 
 }
--- Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/mail/ImapMailReceiverTest.java
+++ Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/mail/ImapMailReceiverTest.java
@@ -8,37 +8,33 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.MessagingException;
 import javax.mail.Store;
 import javax.mail.Message;
 import javax.mail.internet.MimeMessage;
-import javax.mail.MessagingException;
 
 import java.util.Properties;
 
 import org.junit.Test;
 
-
 /**
  * @author achidambaram
- *
+ * 
  */
 public class ImapMailReceiverTest {
-    
-    private static final String EXCEPTION_MSG = "Error Message from Messaging service";
-    
+
     Folder mockFolder = mock(Folder.class);
     Store mockStore = mock(Store.class);
     Properties properties = mock(Properties.class);
     MessageHandler handler = mock(MessageHandler.class);
     MimeMessage message = mock(MimeMessage.class);
     Message messageNotMIme = mock(Message.class);
-    
 
     @Test
     public void testHandleMessages() throws MailClientException, MessagingException {
-        Message[] msgs  = {message};
+        Message[] msgs = { message };
         int result = 0;
         when(properties.getProperty("connect.delete.unhandled.msgs")).thenReturn("false");
         when(properties.getProperty("connect.max.msgs.in.batch", "25")).thenReturn("30");
@@ -49,16 +45,16 @@ public class ImapMailReceiverTest {
         ImapMailReceiver receiver = new ImapMailReceiver(properties) {
             @Override
             protected Folder getInbox(Store store) {
-               return mockFolder; 
+                return mockFolder;
             }
         };
         result = receiver.handleMessages(handler);
-        assertEquals(result,1);
+        assertEquals(result, 1);
     }
-    
+
     @Test
     public void testHandleMessagesFailureCase() throws MailClientException, MessagingException {
-        Message[] msgs  = {message};
+        Message[] msgs = { message };
         int result = 0;
         when(properties.getProperty("connect.delete.unhandled.msgs")).thenReturn("false");
         when(properties.getProperty("connect.max.msgs.in.batch", "25")).thenReturn("30");
@@ -69,16 +65,16 @@ public class ImapMailReceiverTest {
         ImapMailReceiver receiver = new ImapMailReceiver(properties) {
             @Override
             protected Folder getInbox(Store store) {
-               return mockFolder; 
+                return mockFolder;
             }
         };
         result = receiver.handleMessages(handler);
-        assertEquals(result,0);
+        assertEquals(result, 0);
     }
-    
+
     @Test
     public void testHandleMessagesWhenMessageNotMIME() throws MailClientException, MessagingException {
-        Message[] msgs  = {messageNotMIme};
+        Message[] msgs = { messageNotMIme };
         int result = 0;
         when(properties.getProperty("connect.delete.unhandled.msgs")).thenReturn("false");
         when(properties.getProperty("connect.max.msgs.in.batch", "25")).thenReturn("30");
@@ -88,16 +84,16 @@ public class ImapMailReceiverTest {
         ImapMailReceiver receiver = new ImapMailReceiver(properties) {
             @Override
             protected Folder getInbox(Store store) {
-               return mockFolder; 
+                return mockFolder;
             }
         };
         result = receiver.handleMessages(handler);
-        assertEquals(result,0);
+        assertEquals(result, 0);
     }
-    
+
     @Test
     public void testHandleMessagesWhendeleteMsgs() throws MailClientException, MessagingException {
-        Message[] msgs  = {message};
+        Message[] msgs = { message };
         int result = 0;
         when(properties.getProperty("connect.delete.unhandled.msgs")).thenReturn("true");
         when(properties.getProperty("connect.max.msgs.in.batch", "25")).thenReturn("30");
@@ -107,11 +103,12 @@ public class ImapMailReceiverTest {
         ImapMailReceiver receiver = new ImapMailReceiver(properties) {
             @Override
             protected Folder getInbox(Store store) {
-               return mockFolder; 
+                return mockFolder;
             }
         };
         result = receiver.handleMessages(handler);
-        assertEquals(result,0);
+        assertEquals(result, 0);
+        verify(message).setFlag(Flags.Flag.DELETED, true);
     }
-    
+
 }
