97e0d84ba27f74531f1b26b74013bd86a8f0fe24
Sai Valluripalli <saibabu.vallurupalli@gmail.com>
2014-02-18 15:48:05 -0500

    CONN-853i: Added more JUnits to DirectCore reached to 80%.

---
+++ Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/direct/DirectSenderImplTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2009 - 2013, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.direct;
+
+import gov.hhs.fha.nhinc.direct.event.DirectEventLogger;
+import gov.hhs.fha.nhinc.mail.MailClientException;
+import gov.hhs.fha.nhinc.mail.MailSender;
+import java.util.HashSet;
+import java.util.Set;
+import javax.mail.Address;
+import javax.mail.internet.MimeMessage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.nhindirect.gateway.smtp.MessageProcessResult;
+import org.nhindirect.gateway.smtp.SmtpAgent;
+import org.nhindirect.xd.common.DirectDocuments;
+
+/**
+ *
+ * @author svalluripalli
+ */
+public class DirectSenderImplTest extends DirectBaseTest{
+
+    @Mock
+    private MailSender mockMailSender;
+    @Mock
+    private SmtpAgent mockSmtpAgent;
+    @Mock
+    private MimeMessage mockMessage;
+        
+    private DirectSenderImpl oDirectSenderImpl;
+    private final DirectEventLogger testLogger = DirectEventLogger.getInstance();
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        oDirectSenderImpl = new DirectSenderImpl(mockMailSender, mockSmtpAgent, testLogger){
+           @Override
+           protected MailSender getExternalMailSender()
+            {
+                return mockMailSender;
+            }                        
+        };
+    }
+    
+    @After
+    public void tearDown() {
+        oDirectSenderImpl = null;
+    }
+
+    /**
+     * Test of sendOutboundDirect method, of class DirectSenderImpl.
+     */
+    @Test (expected=DirectException.class)
+    public void testSendOutboundDirect_MimeMessage() throws MailClientException {        
+        Address mockAddress = mock(Address.class);
+        Set<Address> sAddress = new HashSet<Address>();
+        sAddress.add(mockAddress);
+        Address[] recipients = sAddress.toArray(new Address[0]);               
+        oDirectSenderImpl.sendOutboundDirect(mockMessage);
+        verify(mockMailSender).send(recipients, mockMessage);
+    }
+
+    /**
+     * Test of sendOutboundDirect method, of class DirectSenderImpl.
+     */
+    @Test (expected=DirectException.class)
+    public void testSendOutboundDirect_4args() {
+        System.out.println("sendOutboundDirect");
+        Address sender = mock(Address.class);
+        Address mockAddress1 = mock(Address.class);            
+        Set<Address> toAddresses = new HashSet<Address>();
+        toAddresses.add(mockAddress1);            
+        Address[] recipients = toAddresses.toArray(new Address[0]); 
+        DirectDocuments documents = mock(DirectDocuments.class);
+        MimeMessageBuilder mockMMBuilder = mock(MimeMessageBuilder.class);
+        String messageId = "";
+        when(mockMMBuilder.build()).thenReturn(mockMessage);
+        oDirectSenderImpl.sendOutboundDirect(sender, recipients, documents, messageId);
+    }
+}
---
+++ Product/Production/Services/DirectCore/src/test/java/gov/hhs/fha/nhinc/direct/xdr/DirectXDRWebServiceImplTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.hhs.fha.nhinc.direct.xdr;
+
+import gov.hhs.fha.nhinc.direct.DirectBaseTest;
+import gov.hhs.fha.nhinc.direct.DirectException;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType.Document;
+import javax.xml.ws.WebServiceContext;
+import oasis.names.tc.ebxml_regrep.xsd.lcm._3.SubmitObjectsRequest;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author svalluripalli
+ */
+public class DirectXDRWebServiceImplTest extends DirectBaseTest {
+    
+    /**
+     * Test of provideAndRegisterDocumentSet method, of class DirectXDRWebServiceImpl.
+     */
+    @Test(expected=DirectException.class)
+    public void testProvideAndRegisterDocumentSet_Failure() throws Exception {
+        WebServiceContext mockWSContext = mock(WebServiceContext.class);
+        DirectHeaderExtractor mockExtractor = mock(DirectHeaderExtractor.class);
+        SoapEdgeContext mockSoap = mock(SoapEdgeContext.class);
+        ProvideAndRegisterDocumentSetRequestType mockBody = mock(ProvideAndRegisterDocumentSetRequestType.class);
+        DirectXDRWebServiceImpl instance = new DirectXDRWebServiceImpl();
+        when(mockExtractor.getHeaderProperties(mockWSContext)).thenReturn(mockSoap);
+        RegistryResponseType oResponse = instance.provideAndRegisterDocumentSet(mockBody, mockWSContext);
+        assertNull(oResponse);
+    }
+    
+    
+    /**
+     * Test of provideAndRegisterDocumentSet method, of class DirectXDRWebServiceImpl.
+     */
+    @Test(expected=DirectException.class)
+    public void testProvideAndRegisterDocumentSet_Happy() throws Exception {
+        WebServiceContext mockWSContext = mock(WebServiceContext.class);
+        DirectHeaderExtractor mockExtractor = mock(DirectHeaderExtractor.class);
+        SoapEdgeContext mockSoap = mock(SoapEdgeContext.class);
+        ProvideAndRegisterDocumentSetRequestType body = new ProvideAndRegisterDocumentSetRequestType();
+        Document e = mock(Document.class);
+        body.getDocument().add(e);
+        SubmitObjectsRequest oSubmit = mock(SubmitObjectsRequest.class);
+        body.setSubmitObjectsRequest(oSubmit);
+        DirectXDRWebServiceImpl instance = new DirectXDRWebServiceImpl();
+        when(mockExtractor.getHeaderProperties(mockWSContext)).thenReturn(mockSoap);
+        RegistryResponseType oResponse = instance.provideAndRegisterDocumentSet(body, mockWSContext);
+        assertNull(oResponse);
+    }
+}
