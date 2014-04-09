8d75eb8b1139965c312086d310a01420b0697057
Christopher May <christopher.may@cgifederal.com>
2014-04-09 11:42:34 -0400

    CONN-1032 - Updated SQL scripts for configdb and proper line endings

--- Product/DBScripts/nhincdb/nhincdb.sql
+++ Product/DBScripts/nhincdb/nhincdb.sql
@@ -468,6 +468,7 @@ CREATE TABLE eventdb.event (
 COMMENT = 'Event Logging';
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON eventdb.* to nhincuser;
+
 GRANT SELECT,INSERT,UPDATE,DELETE ON *.* TO 'nhincuser'@'localhost' IDENTIFIED BY 'nhincpass' WITH GRANT OPTION;
 GRANT SELECT,INSERT,UPDATE,DELETE ON *.* TO 'nhincuser'@'127.0.0.1' IDENTIFIED BY 'nhincpass' WITH GRANT OPTION;
 -- end eventdb
