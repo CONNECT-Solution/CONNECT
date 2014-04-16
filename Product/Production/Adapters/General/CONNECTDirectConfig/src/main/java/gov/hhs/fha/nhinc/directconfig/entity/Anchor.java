ace8dc4a96583d75d7270e0eda1fd967f2619db4
Christopher May <christopher.may@cgifederal.com>
2014-04-16 13:56:12 -0400

    CONN-1034: Changed package structure, reverted to 3.0.1 RI entities (part 2)

---
+++ Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/entity/Anchor.java
@@ -0,0 +1,338 @@
+/* 
+Copyright (c) 2010, NHIN Direct Project
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+   in the documentation and/or other materials provided with the distribution.  
+3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote 
+   products derived from this software without specific prior written permission.
+   
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package gov.hhs.fha.nhinc.directconfig.entity;
+
+import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
+import gov.hhs.fha.nhinc.directconfig.entity.helpers.Thumbprint;
+import gov.hhs.fha.nhinc.directconfig.exception.CertificateException;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "anchor")
+/**
+ * The JPA Domain class
+ */
+public class Anchor {
+
+    private String owner;
+    private String thumbprint;
+    private long certificateId;
+    private byte[] certificateData;
+    private long id;
+    private Calendar createTime;
+    private Calendar validStartDate;
+    private Calendar validEndDate;
+    private EntityStatus status;
+    private boolean incoming;
+    private boolean outgoing;
+
+    /**
+     * Get the value of owner.
+     * 
+     * @return the value of owner.
+     */
+    @Column(name = "owner")
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * Set the value of owner.
+     * 
+     * @param owner
+     *            The value of owner.
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Get the value of thumbprint.
+     * 
+     * @return the value of thumbprint.
+     */
+    @Column(name = "thumbprint")
+    public String getThumbprint() {
+        return thumbprint;
+    }
+
+    /**
+     * Set the value of thumbprint.
+     * 
+     * @param thumbprint
+     *            The value of thumbprint.
+     */
+    public void setThumbprint(String thumbprint) {
+        this.thumbprint = thumbprint;
+    }
+
+    /**
+     * Get the value of certificateData.
+     * 
+     * @return the value of certificateData.
+     */
+    @Column(name = "certificateData", length=4096)
+    @Lob
+    public byte[] getData() {
+        return certificateData;
+    }
+
+    /**
+     * Set the value of certificateData.
+     * 
+     * @param data
+     *            The value of certificateData.
+     * @throws CertificateException
+     */
+    public void setData(byte[] data) throws CertificateException {
+        certificateData = data;
+        if (data == Certificate.NULL_CERT) {
+            setThumbprint("");
+        } else {
+            loadCertFromData();
+        }
+    }
+
+    /**
+     * Get the value of id.
+     * 
+     * @return the value of id.
+     */
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Set the value of id.
+     * 
+     * @param id
+     *            The value of id.
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the value of createTime.
+     * 
+     * @return the value of createTime.
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Calendar getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * Set the value of createTime.
+     * 
+     * @param timestamp
+     *            The value of createTime.
+     */
+    public void setCreateTime(Calendar timestamp) {
+        createTime = timestamp;
+    }
+
+    /**
+     * Get the value of validStartDate.
+     * 
+     * @return the value of validStartDate.
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Calendar getValidStartDate() {
+        return validStartDate;
+    }
+
+    /**
+     * Set the value of validStartDate.
+     * 
+     * @param validStartDate
+     *            The value of validStartDate.
+     */
+    public void setValidStartDate(Calendar validStartDate) {
+        this.validStartDate = validStartDate;
+    }
+
+    /**
+     * Get the value of validEndDate.
+     * 
+     * @return the value of validEndDate.
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Calendar getValidEndDate() {
+        return validEndDate;
+    }
+
+    /**
+     * Set the value of validEndDate.
+     * 
+     * @param validEndDate
+     *            The value of validEndDate.
+     */
+    public void setValidEndDate(Calendar validEndDate) {
+        this.validEndDate = validEndDate;
+    }
+
+    /**
+     * Get the value of status.
+     * 
+     * @return the value of status.
+     */
+    @Enumerated
+    public EntityStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Set the value of status.
+     * 
+     * @param status
+     *            The value of status.
+     */
+    public void setStatus(EntityStatus status) {
+        this.status = status;
+    }
+
+    /**
+     * Get the value of incoming.
+     * 
+     * @return the value of incoming.
+     */
+    @Column(name = "forIncoming")
+    public boolean isIncoming() {
+        return incoming;
+    }
+
+    /**
+     * Set the value of incoming.
+     * 
+     * @param incoming
+     *            The value of incoming.
+     */
+    public void setIncoming(boolean incoming) {
+        this.incoming = incoming;
+    }
+
+    /**
+     * Get the value of outgoing.
+     * 
+     * @return the value of outgoing.
+     */
+    @Column(name = "forOutgoing")
+    public boolean isOutgoing() {
+        return outgoing;
+    }
+
+    /**
+     * Set the value of outgoing.
+     * 
+     * @param outgoing
+     *            The value of outgoing.
+     */
+    public void setOutgoing(boolean outgoing) {
+        this.outgoing = outgoing;
+    }
+
+    /**
+     * Get the value of certificateId.
+     * 
+     * @return the value of certificateId.
+     */
+    public long getCertificateId() {
+        return certificateId;
+    }
+
+    /**
+     * Set the value of certificateId.
+     * 
+     * @param certificateId
+     *            The value of certificateId.
+     */
+    public void setCertificateId(long certificateId) {
+        this.certificateId = certificateId;
+    }
+
+    private X509Certificate loadCertFromData() throws CertificateException {
+        X509Certificate cert = null;
+        try {
+            validate();
+            ByteArrayInputStream bais = new ByteArrayInputStream(certificateData);
+            cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(bais);
+            setThumbprint(Thumbprint.toThumbprint(cert).toString());
+            bais.close();
+        } catch (Exception e) {
+            setData(Certificate.NULL_CERT);
+            throw new CertificateException("Data cannot be converted to a valid X.509 Certificate", e);
+        }
+        
+        return cert;
+    }
+
+    public X509Certificate toCertificate() throws CertificateException {
+        X509Certificate cert = null;
+        try {
+            validate();
+            ByteArrayInputStream bais = new ByteArrayInputStream(certificateData);
+            cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(bais);
+            bais.close();
+        } catch (Exception e) {
+            throw new CertificateException("Data cannot be converted to a valid X.509 Certificate", e);
+        }
+        
+        return cert;
+    }
+    
+    
+    
+    private boolean hasData() {
+        return ((certificateData != null) && (!certificateData.equals(Certificate.NULL_CERT))) ? true : false;
+    }
+
+    /**
+     * Validate the Anchor for the existance of data.
+     * 
+     * @throws CertificateException
+     */
+    public void validate() throws CertificateException {
+        if (!hasData()) {
+            throw new CertificateException("Invalid Certificate: no certificate data exists");
+        }
+    }
+
+}
---
+++ Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/entity/Domain.java
@@ -0,0 +1,334 @@
+/* 
+Copyright (c) 2010, NHIN Direct Project
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+   in the documentation and/or other materials provided with the distribution.  
+3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote 
+   products derived from this software without specific prior written permission.
+   
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package gov.hhs.fha.nhinc.directconfig.entity;
+
+import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@Entity
+@Table(name = "domain")
+/**
+ * The JPA Domain class
+ */
+@XmlRootElement
+public class Domain {
+
+    private String domainName;
+
+    private Calendar createTime;
+
+    private Calendar updateTime;
+
+    private Long postmasterAddressId;
+
+    private Collection<Address> addresses;
+
+    private Long id;
+
+    private EntityStatus status = EntityStatus.NEW;
+
+    /**
+     * Construct a Domain.
+     */
+    public Domain() {
+    }
+
+    /**
+     * Construct a Domain.
+     * 
+     * @param aName
+     *            The domain name.
+     */
+    public Domain(String aName) {
+        setDomainName(aName);
+        setCreateTime(Calendar.getInstance());
+        setUpdateTime(Calendar.getInstance());
+        setStatus(EntityStatus.NEW);
+    }
+
+    /**
+     * Get the value of id.
+     * 
+     * @return the value of id.
+     */
+    @Column(name = "id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @XmlAttribute
+    public Long getId() {
+        if (id == null) {
+            setId(new Long(0L));
+        }
+        return id;
+    }
+
+    /**
+     * Set the value of id.
+     * 
+     * @param anId
+     *            The value of id.
+     */
+    public void setId(Long anId) {
+        id = anId;
+    }
+
+    /**
+     * Get the value of domainName.
+     * 
+     * @return the value of domainName.
+     */
+    @Column(name = "domainName", unique = true)
+    public String getDomainName() {
+        return domainName;
+    }
+
+    /**
+     * Get the value of createTime.
+     * 
+     * @return the value of createTime.
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Calendar getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * Get the value of postmasterAddressId.
+     * 
+     * @return the value of postmasterAddressId.
+     */
+    @Column(name = "postmasterAddressId")
+    public Long getPostmasterAddressId() {
+        return postmasterAddressId;
+    }
+
+    /**
+     * Set the value of postmasterAddressId.
+     * 
+     * @param anId
+     *            The value of postmasterAddressId.
+     */
+    public void setPostmasterAddressId(Long anId) {
+        postmasterAddressId = anId;
+    }
+
+    /**
+     * Get the value of updateTime.
+     * 
+     * @return the value of updateTime.
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Calendar getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * Get the value of status.
+     * 
+     * @return the value of status.
+     */
+    @Column(name = "status")
+    @Enumerated
+    @XmlAttribute
+    public EntityStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Set the value of domainName.
+     * 
+     * @param aName
+     *            The value of domainName.
+     */
+    public void setDomainName(String aName) {
+        domainName = aName;
+    }
+
+    /**
+     * Set the value of createTime.
+     * 
+     * @param timestamp
+     *            The value of createTime.
+     */
+    public void setCreateTime(Calendar timestamp) {
+        createTime = timestamp;
+    }
+
+    /**
+     * Set the value of updateTime.
+     * 
+     * @param timestamp
+     *            The value of updateTime.
+     */
+    public void setUpdateTime(Calendar timestamp) {
+
+        updateTime = timestamp;
+    }
+
+    /**
+     * Set the value of status.
+     * 
+     * @param aStatus
+     *            The value of status.
+     */
+    public void setStatus(EntityStatus aStatus) {
+        status = aStatus;
+    }
+
+    /**
+     * If we have an email address id, then search through the collection of
+     * addresses to find an id match and return it.
+     * 
+     * @return the postmaster email address.
+     */
+    @Transient
+    public String getPostMasterEmail() {
+        String result = null;
+        // return the address that matched the ID
+        if ((getAddresses().size() > 0) && (getPostmasterAddressId() != null) && (getPostmasterAddressId() > 0)) {
+            for (Address address : getAddresses()) {
+                if (address.getId().equals(getPostmasterAddressId())) {
+                    result = address.getEmailAddress();
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Process according to the following table: <p>
+     *           id       name      action<br>
+     *           0/Null   0/Null    None<br>
+     *           Not Null 0/Null    Set Id to null.  Don't remove the Address<br>
+     *           0/Null   Not Null  Add to Address if not there, set Id<br>
+     *           Not Null Not Null  if id.address = address then None, otherwise update id </p>
+     * 
+     * @param email
+     *            The postmaster email address.
+     */
+    public void setPostMasterEmail(String email) {
+
+        if (email == null) {
+            if (getPostmasterAddressId() != null) {
+                setPostmasterAddressId(null);
+            }
+        } else {
+            Long addressId = null;
+            boolean matched = false;
+            // Check to see if we've already got the address
+            for (Address address : getAddresses()) {
+                if (address.getEmailAddress().equals(email)) {
+                    addressId = address.getId();
+                    matched = true;
+                    break;
+                }
+            }
+
+            if (!matched) { // It's a new address so add it
+                Address postmaster = new Address(this, email);
+                postmaster.setDisplayName("Postmaster");
+                postmaster.setStatus(EntityStatus.NEW);
+                getAddresses().add(postmaster);
+                addressId = postmaster.getId();
+            }
+
+            setPostmasterAddressId(addressId);
+        }
+        return;
+    }
+
+    /**
+     * Get a colection of addresses.
+     * 
+     * @return a collection of addresses.
+     */
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER, mappedBy = "domain")
+    @XmlElement(name = "address")
+    public Collection<Address> getAddresses() {
+        if (addresses == null) {
+            addresses = new ArrayList<Address>();
+        }
+        return addresses;
+    }
+
+    /**
+     * Set the value of addresses.
+     * 
+     * @param addresses
+     *            the value of addresses
+     */
+    public void setAddresses(Collection<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    /**
+     * Verify the Domain is valid.
+     * 
+     * @return true if the Domain is valid, false otherwise.
+     */
+    @Transient
+    public boolean isValid() {
+        boolean result = false;
+        if ((getDomainName() != null)
+                && (getDomainName().length() > 0)
+                && ((getStatus().equals(EntityStatus.ENABLED)) || (getStatus().equals(EntityStatus.DISABLED)) || ((getStatus()
+                        .equals(EntityStatus.NEW)) && (getId() == 0L)))) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "[ID: " + getId() + " | Domain: " + getDomainName() + " | Status: " + getStatus().toString()
+                + " | Addresses: " + getAddresses().size() + "]";
+    }
+
+}
