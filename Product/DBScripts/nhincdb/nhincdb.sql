31afcc69fd37650747267275e6dcca163eef65d8
Jason Smith <jason.a.smith@cgifederal.com>
2014-05-30 17:58:16 -0400

    CONN-1131 Updated main sql script

--- Product/DBScripts/nhincdb/nhincdb.sql
+++ Product/DBScripts/nhincdb/nhincdb.sql
@@ -300,61 +300,68 @@ CREATE DATABASE adminguidb;
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS adminguidb.UserRole (
-	id BIGINT SERIAL PRIMARY KEY,
-	roleName VARCHAR(100) NOT NULL UNIQUE,
+	roleId SERIAL PRIMARY KEY,
+	roleName VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS adminguidb.PagePreference (
-	id BIGINT SERIAL PRIMARY KEY,
-	pageName VARCHAR(100) NOT NULL UNIQUE,
+	prefId SERIAL PRIMARY KEY,
+	pageName VARCHAR(100) NOT NULL,
 	pageDesc VARCHAR(100) NOT NULL,
-	access boolean NOT NULL,
-	roleId BIGINT SERIAL,
+	access BIGINT NOT NULL,
+	prefRoleId BIGINT NOT NULL,
 	CONSTRAINT fk_role_pref
-      FOREIGN KEY (roleId)
-      REFERENCES adminguidb.UserRole (id)
+      FOREIGN KEY (prefRoleId)
+      REFERENCES adminguidb.UserRole (roleId)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION	
-)
+);
 
 CREATE TABLE IF NOT EXISTS adminguidb.UserLogin (
     id SERIAL PRIMARY KEY,
     salt varchar(100) NOT NULL,
     sha1 varchar(100) NOT NULL,
     userName varchar(100) NOT NULL UNIQUE,
-    userRole BIGINT SERIAL,
+    userRole BIGINT NOT NULL,
     CONSTRAINT fk_role_user
       FOREIGN KEY (userRole)
-      REFERENCES adminguidb.UserRole (id)
+      REFERENCES adminguidb.UserRole (roleId)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
 );
 
 INSERT INTO adminguidb.UserRole 
-(id, roleName)
+(roleId, roleName)
 VALUES
 (1, "ADMIN"),
 (2, "SUPER USER"),
 (3, "USER");
 
 INSERT INTO adminguidb.PagePreference
-(pageName, pageDesc, access, roleId)
+(pageName, pageDesc, access, prefRoleId)
 VALUES
-("status.xhtml", "Status", 1, 1),
-("status.xhtml", "Status", 1, 2),
-("status.xhtml", "Status", 1, 3),
-("StatusPrime.xhtml", "Status Prime", 1, 1),
-("StatusPrime.xhtml", "Status Prime", 1, 2),
-("StatusPrime.xhtml", "Status Prime", 1, 3),
-("acctmanage.xhtml", "Account Management", 1, 1),
-("acctmanage.xhtml", "Account Management", 0, 2),
-("acctmanage.xhtml", "Account Management", 0, 3),
-("acctmanagePrime.xhtml", "Account Management Prime", 1, 1),
-("acctmanagePrime.xhtml", "Account Management Prime", 0, 2),
-("acctmanagePrime.xhtml", "Account Management Prime", 0, 3);
+("status.xhtml", "Status", 0, 1),
+("status.xhtml", "Status", 0, 2),
+("status.xhtml", "Status", 0, 3),
+("StatusPrime.xhtml", "Status Prime", 0, 1),
+("StatusPrime.xhtml", "Status Prime", 0, 2),
+("StatusPrime.xhtml", "Status Prime", 0, 3),
+("acctmanage.xhtml", "Account Management", 0, 1),
+("acctmanage.xhtml", "Account Management", -1, 2),
+("acctmanage.xhtml", "Account Management", -1, 3),
+("acctmanagePrime.xhtml", "Account Management Prime", 0, 1),
+("acctmanagePrime.xhtml", "Account Management Prime", -1, 2),
+("acctmanagePrime.xhtml", "Account Management Prime", -1, 3),
+("ManageRole.xhtml", "Manage Role", 0, 1),
+("ManageRole.xhtml", "Manage Role", -1, 2),
+("ManageRole.xhtml", "Manage Role", -1, 3),
+("Test.xhtml", "Test", 0, 1),
+("Test.xhtml", "Test", 0, 2),
+("Test.xhtml", "Test", 0, 3);
+
 
 INSERT INTO adminguidb.UserLogin
-(id, salt, sha1, userName)
+(id, salt, sha1, userName, userRole)
 VALUES
 (1, "ABCD", "TxMu4SPUdek0XU5NovS9U2llt3Q=", "CONNECTAdmin", 1);
 
