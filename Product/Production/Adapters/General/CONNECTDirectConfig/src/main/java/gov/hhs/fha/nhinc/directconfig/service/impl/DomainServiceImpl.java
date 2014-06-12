04505736740df79f0bdc22807395dcf74c918def
Christopher May <christopher.may@cgifederal.com>
2014-06-12 15:04:24 -0400

    DirectHBM: Code cleanup and meaningful debug messages for Domain Service

--- Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/service/impl/DomainServiceImpl.java
+++ Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/service/impl/DomainServiceImpl.java
@@ -25,27 +25,33 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
-Copyright (c) 2010, NHIN Direct Project
-All rights reserved.
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the distribution.
-3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote
-products derived from this software without specific prior written permission.
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+ in the documentation and/or other materials provided with the distribution.
+ 3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package gov.hhs.fha.nhinc.directconfig.service.impl;
+
+import gov.hhs.fha.nhinc.directconfig.dao.DomainDao;
+import gov.hhs.fha.nhinc.directconfig.entity.Domain;
+import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
+import gov.hhs.fha.nhinc.directconfig.service.ConfigurationServiceException;
+import gov.hhs.fha.nhinc.directconfig.service.DomainService;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -56,13 +62,6 @@ import javax.jws.WebService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import gov.hhs.fha.nhinc.directconfig.service.ConfigurationServiceException;
-import gov.hhs.fha.nhinc.directconfig.service.DomainService;
-import gov.hhs.fha.nhinc.directconfig.entity.Domain;
-import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
-import gov.hhs.fha.nhinc.directconfig.dao.DomainDao;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
@@ -93,32 +92,26 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#addDomain(gov.hhs.fha.nhinc.directconfig.entity.Domain)
      */
+    @Override
     public void addDomain(Domain domain) throws ConfigurationServiceException {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         dao.add(domain);
         log.info("Added Domain: " + domain.getDomainName());
-        if (log.isDebugEnabled())
-            log.debug("Exit");
     }
 
     /*
      * (non-Javadoc)
      * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#updateDomain(gov.hhs.fha.nhinc.directconfig.entity.Domain)
+     * @see
+     * gov.hhs.fha.nhinc.directconfig.service.DomainService#updateDomain(gov.hhs.fha.nhinc.directconfig.entity.Domain)
      */
+    @Override
     public void updateDomain(Domain domain) throws ConfigurationServiceException {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         if (domain != null) {
             dao.update(domain);
             log.info("Modified Domain: " + domain.getDomainName());
+        } else {
+            log.debug("No domain provided");
         }
-
-        if (log.isDebugEnabled())
-            log.debug("Exit");
     }
 
     /*
@@ -126,6 +119,7 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#getDomainCount()
      */
+    @Override
     public int getDomainCount() throws ConfigurationServiceException {
         return dao.count();
     }
@@ -133,12 +127,14 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
     /*
      * (non-Javadoc)
      * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#getDomains(java.util.Collection, gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
+     * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#getDomains(java.util.Collection,
+     * gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
      */
+    @Override
     public Collection<Domain> getDomains(Collection<String> domainNames, EntityStatus status)
             throws ConfigurationServiceException {
-        ArrayList<String> domains = new ArrayList<String>(domainNames);
-        return dao.getDomains(domains, status);
+
+        return dao.getDomains(new ArrayList<String>(domainNames), status);
     }
 
     /*
@@ -146,31 +142,21 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#removeDomain(java.lang.String)
      */
+    @Override
     public void removeDomain(String domainName) throws ConfigurationServiceException {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         dao.delete(domainName);
         log.info("Modified Domain: " + domainName);
-
-        if (log.isDebugEnabled())
-            log.debug("Exit");
     }
-    
+
     /*
      * (non-Javadoc)
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#removeDomainById(java.lang.Long)
      */
+    @Override
     public void removeDomainById(Long domainId) throws ConfigurationServiceException {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         dao.delete(domainId);
         log.info("Modified Domain: " + domainId);
-
-        if (log.isDebugEnabled())
-            log.debug("Exit");
     }
 
     /*
@@ -178,18 +164,12 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#listDomains(java.lang.String, int)
      */
+    @Override
     public Collection<Domain> listDomains(String lastDomainName, int maxResults) throws ConfigurationServiceException {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         List<Domain> result = dao.listDomains(lastDomainName, maxResults);
 
-        if (log.isDebugEnabled()) {
-            if (result == null) {
-                log.debug("Exit: NULL");
-            } else {
-                log.debug("Exit: " + result.toString());
-            }
+        if (result != null) {
+            log.debug("Exit: " + result.toString());
         }
 
         return result;
@@ -198,20 +178,15 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
     /*
      * (non-Javadoc)
      * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#searchDomain(java.lang.String, gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
+     * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#searchDomain(java.lang.String,
+     * gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
      */
+    @Override
     public Collection<Domain> searchDomain(String domain, EntityStatus status) {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         List<Domain> result = dao.searchDomain(domain, status);
 
-        if (log.isDebugEnabled()) {
-            if (result == null) {
-                log.debug("Exit: NULL");
-            } else {
-                log.debug("Exit: " + result.toString());
-            }
+        if (result != null) {
+            log.debug("Exit: " + result.toString());
         }
 
         return result;
@@ -222,21 +197,14 @@ public class DomainServiceImpl extends SpringBeanAutowiringSupport implements Do
      * 
      * @see gov.hhs.fha.nhinc.directconfig.service.DomainService#getDomain(java.lang.Long)
      */
+    @Override
     public Domain getDomain(Long id) {
-        if (log.isDebugEnabled())
-            log.debug("Enter");
-
         Domain result = dao.getDomain(id);
 
-        if (log.isDebugEnabled()) {
-            if (result == null) {
-                log.debug("Exit: NULL");
-            } else {
-                log.debug("Exit: " + result.toString());
-            }
+        if (result != null) {
+            log.debug("Exit: " + result.toString());
         }
 
         return result;
     }
-
 }
