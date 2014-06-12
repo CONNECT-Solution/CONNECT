908b409e5ab8e6fe0cea5a983471da8b0bac39e0
Christopher May <christopher.may@cgifederal.com>
2014-06-12 15:35:53 -0400

    DirectHBM: Code cleanup for Certificate Service (missed the first time around)

--- Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/service/impl/CertificateServiceImpl.java
+++ Product/Production/Adapters/General/CONNECTDirectConfig/src/main/java/gov/hhs/fha/nhinc/directconfig/service/impl/CertificateServiceImpl.java
@@ -25,27 +25,34 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
-Copyright (c) 2010, NHIN Direct Project
-All rights reserved.
+ Copyright (c) 2010, NHIN Direct Project
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the distribution.
-3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote
-products derived from this software without specific prior written permission.
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+ in the documentation and/or other materials provided with the distribution.
+ 3. Neither the name of the The NHIN Direct Project (nhindirect.org) nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package gov.hhs.fha.nhinc.directconfig.service.impl;
+
+import gov.hhs.fha.nhinc.directconfig.dao.CertificateDao;
+import gov.hhs.fha.nhinc.directconfig.entity.Certificate;
+import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
+import gov.hhs.fha.nhinc.directconfig.service.CertificateService;
+import gov.hhs.fha.nhinc.directconfig.service.ConfigurationServiceException;
+import gov.hhs.fha.nhinc.directconfig.service.helpers.CertificateGetOptions;
 
 import java.io.ByteArrayInputStream;
 import java.security.Key;
@@ -68,14 +75,6 @@ import javax.security.auth.x500.X500Principal;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import gov.hhs.fha.nhinc.directconfig.service.CertificateService;
-import gov.hhs.fha.nhinc.directconfig.service.ConfigurationServiceException;
-import gov.hhs.fha.nhinc.directconfig.service.helpers.CertificateGetOptions;
-import gov.hhs.fha.nhinc.directconfig.entity.Certificate;
-import gov.hhs.fha.nhinc.directconfig.entity.helpers.EntityStatus;
-import gov.hhs.fha.nhinc.directconfig.dao.CertificateDao;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
@@ -88,19 +87,16 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 public class CertificateServiceImpl extends SpringBeanAutowiringSupport implements CertificateService {
 
     private static final int RFC822Name_TYPE = 1; // name type constant for Subject Alternative name email address
-    private static final int DNSName_TYPE = 2; // name type constant for Subject Alternative name domain name    
-    
+    private static final int DNSName_TYPE = 2; // name type constant for Subject Alternative name domain name
+
     private static final Log log = LogFactory.getLog(CertificateServiceImpl.class);
 
     @Autowired
     private CertificateDao dao;
 
-    static
-    {
-
+    static {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
     }
-    
 
     /**
      * Initialization method.
@@ -111,277 +107,243 @@ public class CertificateServiceImpl extends SpringBeanAutowiringSupport implemen
         log.info("CertificateService initialized");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#addCertificates(java.util.Collection)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public void addCertificates(Collection<Certificate> certs) throws ConfigurationServiceException {
-        
-        if (certs != null && certs.size() > 0)
-            for (Certificate cert : certs)
-            {
-                if ((cert.getOwner() == null || cert.getOwner().isEmpty()) && cert.getData() != null)
-                {
+        if (certs != null && certs.size() > 0) {
+            for (Certificate cert : certs) {
+                if ((cert.getOwner() == null || cert.getOwner().isEmpty()) && cert.getData() != null) {
                     // get the owner from the certificate information
                     // first transform into a certificate
                     CertContainer cont = toCertContainer(cert.getData());
-                    if (cont != null && cont.getCert() != null)
-                    {
-                       
+                    if (cont != null && cont.getCert() != null) {
                         // now get the owner info from the cert
                         String theOwner = getOwner(cont.getCert());
 
-                        if (theOwner != null && !theOwner.isEmpty())
+                        if (theOwner != null && !theOwner.isEmpty()) {
                             cert.setOwner(theOwner);
+                        }
                     }
-                    
+
                 }
+
                 dao.save(cert);
             }
+        }
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#getCertificate(java.lang.String, java.lang.String, gov.hhs.fha.nhinc.directconfig.service.impl.CertificateGetOptions)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public Certificate getCertificate(String owner, String thumbprint, CertificateGetOptions options)
             throws ConfigurationServiceException {
 
         return dao.load(owner, thumbprint);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#getCertificates(java.util.Collection, gov.hhs.fha.nhinc.directconfig.service.impl.CertificateGetOptions)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public Collection<Certificate> getCertificates(Collection<Long> certIds, CertificateGetOptions options)
             throws ConfigurationServiceException {
 
         return dao.list(new ArrayList<Long>(certIds));
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#getCertificatesForOwner(java.lang.String, gov.hhs.fha.nhinc.directconfig.service.impl.CertificateGetOptions)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public Collection<Certificate> getCertificatesForOwner(String owner, CertificateGetOptions options)
             throws ConfigurationServiceException {
 
         return dao.list(owner);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#setCertificateStatus(java.util.Collection, gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public void setCertificateStatus(Collection<Long> certificateIDs, EntityStatus status)
             throws ConfigurationServiceException {
-       
+
         dao.setStatus(new ArrayList<Long>(certificateIDs), status);
-
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#setCertificateStatusForOwner(java.lang.String, gov.hhs.fha.nhinc.directconfig.entity.EntityStatus)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public void setCertificateStatusForOwner(String owner, EntityStatus status) throws ConfigurationServiceException {
-        
         dao.setStatus(owner, status);
-
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#removeCertificates(java.util.Collection)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public void removeCertificates(Collection<Long> certificateIds) throws ConfigurationServiceException {
-        
         dao.delete(new ArrayList<Long>(certificateIds));
-
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#removeCertificatesForOwner(java.lang.String)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public void removeCertificatesForOwner(String owner) throws ConfigurationServiceException {
-        
         dao.delete(owner);
-
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#listCertificates(long, int, gov.hhs.fha.nhinc.directconfig.service.impl.CertificateGetOptions)
+    /**
+     * {@inheritDoc}
      */
+    @Override
     public Collection<Certificate> listCertificates(long lastCertificateID, int maxResults,
             CertificateGetOptions options) throws ConfigurationServiceException {
-      
-        // just return all for now
-        return dao.list((String)null);
+
+        // Direct RI comment: just return all for now
+        return dao.list((String) null);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see gov.hhs.fha.nhinc.directconfig.service.CertificateService#contains(gov.hhs.fha.nhinc.directconfig.entity.Certificate)
+    /**
+     * {@inheritDoc}
      */
-    public boolean contains(Certificate cert) 
-    {
+    @Override
+    public boolean contains(Certificate cert) {
         return dao.load(cert.getOwner(), cert.getThumbprint()) != null;
- 
     }
 
-    public CertContainer toCertContainer(byte[] data) throws ConfigurationServiceException 
-    {
+    public CertContainer toCertContainer(byte[] data) throws ConfigurationServiceException {
         CertContainer certContainer = null;
-        try 
-        {
+
+        try {
             ByteArrayInputStream bais = new ByteArrayInputStream(data);
-            
+
             // lets try this a as a PKCS12 data stream first
-            try
-            {
+            try {
                 KeyStore localKeyStore = KeyStore.getInstance("PKCS12", Certificate.getJCEProviderName());
-                
+
                 localKeyStore.load(bais, "".toCharArray());
                 Enumeration<String> aliases = localKeyStore.aliases();
 
-
-                // we are really expecting only one alias 
-                if (aliases.hasMoreElements())                    
-                {
+                // we are really expecting only one alias
+                if (aliases.hasMoreElements()) {
                     String alias = aliases.nextElement();
-                    X509Certificate cert = (X509Certificate)localKeyStore.getCertificate(alias);
-                    
+                    X509Certificate cert = (X509Certificate) localKeyStore.getCertificate(alias);
+
                     // check if there is private key
                     Key key = localKeyStore.getKey(alias, "".toCharArray());
-                    if (key != null && key instanceof PrivateKey) 
-                    {
+                    if (key != null && key instanceof PrivateKey) {
                         certContainer = new CertContainer(cert, key);
-                        
                     }
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 // must not be a PKCS12 stream, go on to next step
             }
-   
-            if (certContainer == null)                
-            {
-                //try X509 certificate factory next       
+
+            if (certContainer == null) {
+                // try X509 certificate factory next
                 bais.reset();
                 bais = new ByteArrayInputStream(data);
 
-                X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(bais);
+                X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(
+                        bais);
                 certContainer = new CertContainer(cert, null);
             }
             bais.close();
-        } 
-        catch (Exception e) 
-        {
+        } catch (Exception e) {
             throw new ConfigurationServiceException("Data cannot be converted to a valid X.509 Certificate", e);
         }
-        
+
         return certContainer;
     }
-    
-    public static class CertContainer
-    {
+
+    public static class CertContainer {
         private final X509Certificate cert;
         private final Key key;
-        
-        public CertContainer(X509Certificate cert, Key key)
-        {
+
+        public CertContainer(X509Certificate cert, Key key) {
             this.cert = cert;
             this.key = key;
         }
-        
-        public X509Certificate getCert() 
-        {
+
+        public X509Certificate getCert() {
             return cert;
         }
 
-        public Key getKey() 
-        {
+        public Key getKey() {
             return key;
         }
-    
+
     }
-    
-    private String getOwner(X509Certificate certificate)
-    {
+
+    private String getOwner(X509Certificate certificate) {
         String address = "";
         // check alternative names first
         Collection<List<?>> altNames = null;
-        try
-        {            
+
+        try {
             altNames = certificate.getSubjectAlternativeNames();
-        }
-        catch (CertificateParsingException ex)
-        {
+        } catch (CertificateParsingException ex) {
             /* no -op */
-        }    
-        
-        if (altNames != null)
-        {
-            for (List<?> entries : altNames)
-            {
-                if (entries.size() >= 2) // should always be the case according the altNames spec, but checking to be defensive
-                {
-                    
-                    Integer nameType = (Integer)entries.get(0);
+        }
+
+        if (altNames != null) {
+            for (List<?> entries : altNames) {
+                // should always be the case according the altNames spec, but checking to be defensive
+                if (entries.size() >= 2) {
+                    Integer nameType = (Integer) entries.get(0);
+
                     // prefer email over over domain?
-                    if (nameType == RFC822Name_TYPE)                        
-                        address = (String)entries.get(1);
-                    else if (nameType == DNSName_TYPE && address.isEmpty())
-                        address = (String)entries.get(1);                    
+                    if (nameType == RFC822Name_TYPE) {
+                        address = (String) entries.get(1);
+                    } else if (nameType == DNSName_TYPE && address.isEmpty()) {
+                        address = (String) entries.get(1);
+                    }
                 }
             }
         }
-        
-        if (!address.isEmpty())
+
+        if (!address.isEmpty()) {
             return address;
-        
-        // can't find subject address in alt names... try the principal 
+        }
+
+        // can't find subject address in alt names... try the principal
         X500Principal issuerPrin = certificate.getSubjectX500Principal();
-        
+
         // get the domain name
         Map<String, String> oidMap = new HashMap<String, String>();
-        oidMap.put("1.2.840.113549.1.9.1", "EMAILADDRESS");  // OID for email address
-        String prinName = issuerPrin.getName(X500Principal.RFC1779, oidMap);    
-        
+        oidMap.put("1.2.840.113549.1.9.1", "EMAILADDRESS"); // OID for email address
+        String prinName = issuerPrin.getName(X500Principal.RFC1779, oidMap);
+
         // see if there is an email address first in the DN
         String searchString = "EMAILADDRESS=";
         int index = prinName.indexOf(searchString);
-        if (index == -1)
-        {
+
+        if (index == -1) {
             searchString = "CN=";
+
             // no Email.. check the CN
             index = prinName.indexOf(searchString);
-            if (index == -1)
+
+            if (index == -1) {
                 return ""; // no CN... nothing else that can be done from here
+            }
         }
-        
+
         // look for a "," to find the end of this attribute
         int endIndex = prinName.indexOf(",", index);
-        if (endIndex > -1)
+
+        if (endIndex > -1) {
             address = prinName.substring(index + searchString.length(), endIndex);
-        else 
-            address= prinName.substring(index + searchString.length());
-        
+        } else {
+            address = prinName.substring(index + searchString.length());
+        }
+
         return address;
     }
-    
 }
