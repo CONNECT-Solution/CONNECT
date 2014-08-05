895e366b8e26bbae277894607df0b0fe9cab211b
Christopher May <christopher.may@cgifederal.com>
2014-08-05 13:46:53 -0400

    Moved current PortDescriptor class under realtime package in preparation of split wsdls

--- Product/Production/Services/CORE_X12DocumentSubmissionCore/src/main/java/gov/hhs/fha/nhinc/corex12/docsubmission/realtime/nhin/proxy/service/AbstractCOREServicePortDescriptor.java
+++ Product/Production/Services/CORE_X12DocumentSubmissionCore/src/main/java/gov/hhs/fha/nhinc/corex12/docsubmission/realtime/nhin/proxy/service/AbstractCOREServicePortDescriptor.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package gov.hhs.fha.nhinc.corex12.docsubmission.nhin.proxy.service;
+package gov.hhs.fha.nhinc.corex12.docsubmission.realtime.nhin.proxy.service;
 
 import gov.hhs.fha.nhinc.adaptercore.AdapterCORETransactionPortType;
 import gov.hhs.fha.nhinc.messaging.service.port.SOAP12ServicePortDescriptor;
