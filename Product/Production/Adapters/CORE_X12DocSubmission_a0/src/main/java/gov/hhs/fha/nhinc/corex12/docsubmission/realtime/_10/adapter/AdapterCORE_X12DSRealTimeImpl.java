a6d86b3bda9e34a7ceb9c1d7c55dc396e2fe30f6
Sailaja Adusumilli <sailaja.adusumilli@gmail.com>
2014-08-06 14:37:04 -0400

    CONN-1278: Resolved rebase conflict, completed X12 Adapter code

---
+++ Product/Production/Adapters/CORE_X12DocSubmission_a0/src/main/java/gov/hhs/fha/nhinc/corex12/docsubmission/realtime/_10/adapter/AdapterCORE_X12DSRealTimeImpl.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2009-2014, United States Government, as represented by the Secretary of Health and Human Services.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ *      * Neither the name of the United States Government nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.corex12.docsubmission.realtime._10.adapter;
+
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterCOREEnvelopeRealTimeRequestType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterCOREEnvelopeRealTimeResponseType;
+import gov.hhs.fha.nhinc.common.nhinccommonadapter.AdapterCOREEnvelopeRealTimeSecuredRequestType;
+import gov.hhs.fha.nhinc.corex12.docsubmission.realtime.outbound.OutboundCORE_X12DSRealTime;
+import gov.hhs.fha.nhinc.corex12.docsubmission.realtime.adapter.AdapterCORE_X12DSRealTimeOrchImpl;
+
+import gov.hhs.fha.nhinc.messaging.server.BaseService;
+import javax.xml.ws.WebServiceContext;
+
+/**
+ *
+ * @author sadusumilli
+ */
+public class AdapterCORE_X12DSRealTimeImpl extends BaseService {
+
+    private OutboundCORE_X12DSRealTime outboundCOREX12DSRealTime;
+
+    /**
+     *
+     * @param outboundCOREX12DSRealTime
+     */
+    public AdapterCORE_X12DSRealTimeImpl(OutboundCORE_X12DSRealTime outboundCOREX12DSRealTime) {
+        this.outboundCOREX12DSRealTime = outboundCOREX12DSRealTime;
+    }
+
+    /**
+     *
+     * @param adapterCOREEnvelopeRealTimeRequestType
+     * @param context
+     * @return
+     */
+    public AdapterCOREEnvelopeRealTimeResponseType realTimeTransaction(AdapterCOREEnvelopeRealTimeRequestType adapterCOREEnvelopeRealTimeRequestType, WebServiceContext context) {
+        AdapterCOREEnvelopeRealTimeResponseType oResponse = new AdapterCOREEnvelopeRealTimeResponseType();
+        oResponse.setCOREEnvelopeRealTimeResponse(new AdapterCORE_X12DSRealTimeOrchImpl().realTimeRequest(adapterCOREEnvelopeRealTimeRequestType.getCOREEnvelopeRealTimeRequest(), adapterCOREEnvelopeRealTimeRequestType.getAssertion()));
+        return oResponse;
+    }
+
+    /**
+     *
+     * @param adapterCOREEnvelopeRealTimeSecuredRequestType
+     * @param context
+     * @return
+     */
+    public AdapterCOREEnvelopeRealTimeResponseType realTimeTransactionSecured(AdapterCOREEnvelopeRealTimeSecuredRequestType adapterCOREEnvelopeRealTimeSecuredRequestType, WebServiceContext context) {
+        AdapterCOREEnvelopeRealTimeResponseType oResponse = new AdapterCOREEnvelopeRealTimeResponseType();
+        AssertionType oAssertion = getAssertion(context, null);
+        oResponse.setCOREEnvelopeRealTimeResponse(new AdapterCORE_X12DSRealTimeOrchImpl().realTimeRequest(adapterCOREEnvelopeRealTimeSecuredRequestType.getCOREEnvelopeRealTimeRequest(), oAssertion));
+        return oResponse;
+    }
+
+}
