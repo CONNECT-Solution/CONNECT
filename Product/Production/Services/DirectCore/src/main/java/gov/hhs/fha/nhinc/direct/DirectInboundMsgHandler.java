19c9f3bb35e6390b80e36801db4c41a3dbabca0e
nareshcgi <nsubrama@NSUBRAMA-F06261.cgifederal.com>
2014-10-03 17:50:49 -0400

    CONN-1351: Removed unwanted code (addressed the peer review comments)

--- Product/Production/Services/DirectCore/src/main/java/gov/hhs/fha/nhinc/direct/DirectInboundMsgHandler.java
+++ Product/Production/Services/DirectCore/src/main/java/gov/hhs/fha/nhinc/direct/DirectInboundMsgHandler.java
@@ -33,14 +33,16 @@ import javax.mail.internet.MimeMessage;
 import org.apache.log4j.Logger;
 
 /**
- * Handles inbound messages from an external mail client. Inbound messages are un-directified and either - sent to a
- * recipient on an internal mail client - SMTP+Mime - SMTP+XDM - process XDM as XDR - SOAP+XDR
+ * Handles inbound messages from an external mail client. Inbound messages are
+ * un-directified and either - sent to a recipient on an internal mail client -
+ * SMTP+Mime - SMTP+XDM - process XDM as XDR - SOAP+XDR
  */
 public class DirectInboundMsgHandler implements MessageHandler {
 
     private static final Logger LOG = Logger.getLogger(DirectInboundMsgHandler.class);
     /**
-     * Property for the external direct client used to send the outbound message.
+     * Property for the external direct client used to send the outbound
+     * message.
      */
     private final DirectReceiver directReceiver;
 
@@ -61,15 +63,10 @@ public class DirectInboundMsgHandler implements MessageHandler {
         boolean handled = false;
         try {
             directReceiver.receiveInbound(message);
-            //Run the message monitoring logic
-            //handleMessageMonitoring();
             handled = true;
         } catch (Exception e) {
             LOG.error("Exception while processing and sending outbound direct message", e);
         }
         return handled;
     }
-    //protected void handleMessageMonitoring(){
-    //    MessageMonitoringAPI.getInstance().process();
-    //}
 }
