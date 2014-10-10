3647e0717b4316a174092882ed29ef1159fcd9f9
Sailaja Adusumilli <sailaja.adusumilli@gmail.com>
2014-10-09 21:44:20 -0400

    CONN-1290:Updated TabBean,SidebarPaneForBaseTemplate.xhtml and HeaderForBaseTemplate.xhtml to handle left navigation bar access issue in WAS

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/managed/TabBean.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/managed/TabBean.java
@@ -1,28 +1,28 @@
-/**
- * Copyright (c) 2009-2014, United States Government, as represented by the
- * Secretary of Health and Human Services. All rights reserved.
+/*
+ *  Copyright (c) 2009-2014, United States Government, as represented by the Secretary of Health and Human Services.
+ *  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer. Redistributions in binary
- * form must reproduce the above copyright notice, this list of conditions and
- * the following disclaimer in the documentation and/or other materials provided
- * with the distribution. Neither the name of the United States Government nor
- * the names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ *      * Neither the name of the United States Government nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package gov.hhs.fha.nhinc.admingui.managed;
 
@@ -52,7 +52,7 @@ public class TabBean {
     public void setDirectTabIndex(int directTabIndex) {
         this.directTabIndex = directTabIndex;
     }
-    
+
     public int getDashboardTabIndex() {
         return dashboardTabIndex;
     }
@@ -71,38 +71,78 @@ public class TabBean {
 
     public int getAdminTabIndex() {
         return adminTabIndex;
-    }   
+    }
 
     public void setAdminTabIndex(int adminTabIndex) {
         this.adminTabIndex = adminTabIndex;
     }
 
-    public String setDashboardTabIndexNavigate(int dashboardTabIndex) {
-        this.dashboardTabIndex = dashboardTabIndex;
+    public String navigateToDirectDomainTab() {
+        this.directTabIndex = NavigationConstant.DIRECT_DOMAIN_TAB;
+        return NavigationConstant.DIRECT_PAGE;
+    }
+
+    public String navigateToDirectSettingTab() {
+        this.adminTabIndex = NavigationConstant.DIRECT_SETTING_TAB;
+        return NavigationConstant.DIRECT_PAGE;
+    }
+
+    public String navigateToDirectCertificateTab() {
+        this.directTabIndex = NavigationConstant.DIRECT_CERTIFICATE_TAB;
+        return NavigationConstant.DIRECT_PAGE;
+    }
+
+    public String navigateToDirectTrustbundleTab() {
+        this.directTabIndex = NavigationConstant.DIRECT_TRUSTBUNDLE_TAB;
+        return NavigationConstant.DIRECT_PAGE;
+    }
+
+    public String navigateToAccountMgmtUserAccountTab() {
+        this.adminTabIndex = NavigationConstant.ACCOUNT_MGMT_USERACC_TAB;
+        return NavigationConstant.ACCT_MGMT_PAGE;
+    }
+
+    public String navigateToAccountMgmtManageRoleTab() {
+        this.adminTabIndex = NavigationConstant.ACCOUNT_MGMT_MANAGEROLE_TAB;
+        return NavigationConstant.ACCT_MGMT_PAGE;
+    }
+
+    public String navigateToGatewayDashboardTab() {
+        this.adminTabIndex = NavigationConstant.GATEWAY_DASHBOARD_TAB;
         return NavigationConstant.STATUS_PAGE;
     }
 
-    public String setLogsTabIndexNavigate(int logsTabIndex) {
-        this.logsTabIndex = logsTabIndex;
-        return "logs";
+    public String navigateToGatewayRemoteListTab() {
+        this.adminTabIndex = NavigationConstant.GATEWAY_REMOTELIST_TAB;
+        return NavigationConstant.STATUS_PAGE;
     }
 
-    public String setAdminTabIndexNavigate(int adminTabIndex) {
-        this.adminTabIndex = adminTabIndex;
-        return NavigationConstant.ACCT_MGMT_PAGE;
-    }
-    
-    public String setDirectTabIndexNavigate(int directTabIndex){
-        this.directTabIndex = directTabIndex;
-        return NavigationConstant.DIRECT_PAGE;
-    }
-    
+    /* public String setDashboardTabIndexNavigate(int dashboardTabIndex) {
+     this.dashboardTabIndex = dashboardTabIndex;
+     return NavigationConstant.STATUS_PAGE;
+     }
+
+     public String setLogsTabIndexNavigate(int logsTabIndex) {
+     this.logsTabIndex = logsTabIndex;
+     return "logs";
+     }
+
+     public String setAdminTabIndexNavigate(int adminTabIndex) {
+     this.adminTabIndex = adminTabIndex;
+     return NavigationConstant.ACCT_MGMT_PAGE;
+     }
+
+     public String setDirectTabIndexNavigate(int directTabIndex) {
+     this.directTabIndex = directTabIndex;
+     return NavigationConstant.DIRECT_PAGE;
+     }*/
     /**
-     * Event listener for tab change to set current active index of the 
-     * direct tab view.  Needed since active index is set by menu links as well.
-     * @param tEvent 
+     * Event listener for tab change to set current active index of the direct tab view. Needed since active index is
+     * set by menu links as well.
+     *
+     * @param tEvent
      */
-    public void onDirectTabChange(TabChangeEvent tEvent){
+    public void onDirectTabChange(TabChangeEvent tEvent) {
         TabView tabView = (TabView) tEvent.getComponent();
         this.directTabIndex = tabView.getChildren().indexOf(tEvent.getTab());
     }
