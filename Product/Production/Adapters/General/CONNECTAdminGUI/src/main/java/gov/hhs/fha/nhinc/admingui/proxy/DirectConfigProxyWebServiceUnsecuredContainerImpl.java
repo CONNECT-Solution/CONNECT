7db4d99ab6dec38b30141145e9177536fac3add5
nareshcgi <nsubrama@NSUBRAMA-F06261.cgifederal.com>
2014-10-28 14:54:20 -0400

    CONN-1419: Fixed code to address review comments

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/proxy/DirectConfigProxyWebServiceUnsecuredContainerImpl.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/proxy/DirectConfigProxyWebServiceUnsecuredContainerImpl.java
@@ -1,21 +1,27 @@
 /**
- * Copyright (c) 2009-2014, United States Government, as represented by the Secretary of Health and Human Services. All
- * rights reserved.
+ * Copyright (c) 2009-2014, United States Government, as represented by the
+ * Secretary of Health and Human Services. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met: Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
- * distribution. Neither the name of the United States Government nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. Neither the name of the United States Government nor
+ * the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 package gov.hhs.fha.nhinc.admingui.proxy;
@@ -54,8 +60,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     private final WebServiceProxyHelper oProxyHelper = new WebServiceProxyHelper();
     
 
-    //private final Class<ConfigurationService> directConfigClazz = ConfigurationService.class;
-
     /*
      * (non-Javadoc)
      * 
@@ -64,7 +68,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public Domain getDomain(Long id) throws Exception {
           return getConfigService().getConfigurationServiceImplPort().getDomain(id);
-          // return (Domain) getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_GET_DOMAIN, id);
     }
 
     /*
@@ -89,8 +92,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public List<Domain> listDomains() throws Exception {
         return getConfigService().getConfigurationServiceImplPort().listDomains(null, 0);
-        //return (List<Domain>) getClient().invokePort(directConfigClazz,
-        //  DirectConfigConstants.DIRECT_CONFIG_LIST_DOMAINS, null, 0);
     }
 
     /*
@@ -103,8 +104,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
         try {
             getConfigService().getConfigurationServiceImplPort().updateDomain(updateDomain);
             return null;
-            //return (UpdateDomainResponse) getClient().invokePort(directConfigClazz,
-            //      DirectConfigConstants.DIRECT_CONFIG_UPDATE_DOMAIN, updateDomain);
         } catch (Exception e) {
             throw new DomainException("Could not update domain " + updateDomain.getDomain().getDomainName(), e);
         }
@@ -118,7 +117,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public void deleteDomain(String name) throws Exception {
         getConfigService().getConfigurationServiceImplPort().removeDomain(name);
-        //getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_DELETE_DOMAIN, name);
     }
 
     /*
@@ -129,8 +127,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public void addAnchor(AddAnchor addAnchor) throws Exception {
         getConfigService().getConfigurationServiceImplPort().addAnchor(addAnchor.getAnchor());
-        //getClient()
-        //    .invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_ADD_ANCHOR, addAnchor.getAnchor());
     }
 
     /*
@@ -140,13 +136,7 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
      */
     @Override
     public void removeAnchors(RemoveAnchors removeAnchors) throws Exception {
-        //for (long value : removeAnchors.getAnchorId()) { 
-           
-        //}
-        return;
-        //getConfigService().getConfigurationServiceImplPort().removeAnchors(removeAnchors.getAnchorId());
-        //getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_DELETE_ANCHOR,
-          //  removeAnchors.getAnchorId());
+        getConfigService().getConfigurationServiceImplPort().removeAnchors(removeAnchors.getAnchorId());
     }
 
     /*
@@ -157,10 +147,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public List<Anchor> getAnchorsForOwner(GetAnchorsForOwner getAnchorsForOwner) throws Exception {
         return getConfigService().getConfigurationServiceImplPort().getAnchorsForOwner(getAnchorsForOwner.getOwner(), getAnchorsForOwner.getOptions());
-        //return Arrays.asList(anchors);
-        //return (List<Anchor>) getClient().invokePort(directConfigClazz,
-          //  DirectConfigConstants.DIRECT_CONFIG_GET_ANCHORS_FOR_OWNER, getAnchorsForOwner.getOwner(),
-            //getAnchorsForOwner.getOptions());
     }
 
     /*
@@ -171,8 +157,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public void addCertificates(AddCertificates certificate) throws Exception {
         getConfigService().getConfigurationServiceImplPort().addCertificates(certificate.getCerts());
-        //getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_ADD_CERT, certificate.getCerts());
-
     }
 
     /*
@@ -183,9 +167,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public void removeCertificate(RemoveCertificates certificate) throws Exception {
         getConfigService().getConfigurationServiceImplPort().removeCertificates(certificate.getCertificateIds());
-        //getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_DELETE_CERT,
-               // certificate.getCertificateIds());
-
     }
 
     /*
@@ -236,7 +217,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     @Override
     public void addTrustBundle(TrustBundle tb) throws Exception {
      getConfigService().getConfigurationServiceImplPort().addTrustBundle(tb);
-     //getClient().invokePort(directConfigClazz, DirectConfigConstants.DIRECT_CONFIG_ADD_TRUST_BUNDLE, tb);
     }
 
     /**
@@ -363,7 +343,6 @@ public class DirectConfigProxyWebServiceUnsecuredContainerImpl implements Direct
     public boolean pingDirectConfig(String url) throws Exception {
         ConfigurationServiceImplService cfService = new ConfigurationServiceImplService(new URL(url+"?wsdl"));
         int count = cfService.getConfigurationServiceImplPort().getDomainCount();
-        System.out.println("Domain Count:"+count);
         return true;
     }
 }
