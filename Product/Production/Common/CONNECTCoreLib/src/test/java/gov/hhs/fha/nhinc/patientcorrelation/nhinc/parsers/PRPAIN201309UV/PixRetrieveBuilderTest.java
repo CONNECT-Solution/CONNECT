d6847c513ec78c013ec68e9658df6c69deff6f39
Tabassum Jafri <tjafri@MININT-OOH210N.cgifederal.com>
2015-02-13 14:40:15 -0500

    FHAC-139 did a formatting of code

--- Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/patientcorrelation/nhinc/parsers/PRPAIN201309UV/PixRetrieveBuilderTest.java
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/patientcorrelation/nhinc/parsers/PRPAIN201309UV/PixRetrieveBuilderTest.java
@@ -1,6 +1,5 @@
 package gov.hhs.fha.nhinc.patientcorrelation.nhinc.parsers.PRPAIN201309UV;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -18,47 +17,46 @@ import org.junit.Test;
  *
  */
 public class PixRetrieveBuilderTest {
-    
+
     @Test
     public void testcreatePixRetrieve() {
         PixRetrieveBuilder builder = new PixRetrieveBuilder();
         PRPAIN201309UV02 pixRetrieve = new PRPAIN201309UV02();
         pixRetrieve = builder.createPixRetrieve(createRetrievePatientCorrelationsRequest());
         assertEquals(pixRetrieve.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedPerson()
-                .getValue().getId().get(0).getNullFlavor().get(0), "NA");
+            .getValue().getId().get(0).getNullFlavor().get(0), "NA");
         assertEquals(pixRetrieve.getInteractionId().getExtension(), "PRPA_IN201309");
         assertEquals(pixRetrieve.getId().getRoot(), "1.1");
     }
-    
+
     @Test
     public void testcreatePixRetrieveWhenAANull() {
-        PixRetrieveBuilder builder = new PixRetrieveBuilder(); 
+        PixRetrieveBuilder builder = new PixRetrieveBuilder();
         PRPAIN201309UV02 pixRetrieve = new PRPAIN201309UV02();
         pixRetrieve = builder.createPixRetrieve(createRetrievePatientCorrelationsRequestWhenAANull());
         assertEquals(pixRetrieve.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedPerson()
-                .getValue().getId().get(0).getNullFlavor().get(0), "NA");
+            .getValue().getId().get(0).getNullFlavor().get(0), "NA");
         assertEquals(pixRetrieve.getInteractionId().getExtension(), "PRPA_IN201309");
         assertEquals(pixRetrieve.getId().getRoot(), "1.1");
-        
+
     }
-    
+
     @Test
-    public void testcreatePixRetrieveWithTargetCommunityPrefix()
-    {
+    public void testcreatePixRetrieveWithTargetCommunityPrefix() {
         PixRetrieveBuilder builder = new PixRetrieveBuilder();
         List<String> homeCommIds = null;
-        
+
         homeCommIds = builder.stripCommunityIdsPrefix(null);
-        assertEquals(homeCommIds,null);
-        
+        assertEquals(homeCommIds, null);
+
         homeCommIds = builder.stripCommunityIdsPrefix(new ArrayList<String>());
-        assertEquals(homeCommIds,null);
-        
-        homeCommIds = builder.stripCommunityIdsPrefix(getHomeCommunitiesIdsWithPrefix ());
-        assertEquals(homeCommIds.get(0),"1.1");
-        assertEquals(homeCommIds.get(1),"2.2");         
+        assertEquals(homeCommIds, null);
+
+        homeCommIds = builder.stripCommunityIdsPrefix(getHomeCommunitiesIdsWithPrefix());
+        assertEquals(homeCommIds.get(0), "1.1");
+        assertEquals(homeCommIds.get(1), "2.2");
     }
-    
+
     private RetrievePatientCorrelationsRequestType createRetrievePatientCorrelationsRequest() {
         RetrievePatientCorrelationsRequestType patcorrReq = new RetrievePatientCorrelationsRequestType();
         patcorrReq.setQualifiedPatientIdentifier(createQualifiedSubjectIdentifier());
@@ -66,34 +64,31 @@ public class PixRetrieveBuilderTest {
         patcorrReq.getTargetAssigningAuthority().add("2.2");
         return patcorrReq;
     }
-    
+
     private RetrievePatientCorrelationsRequestType createRetrievePatientCorrelationsRequestWhenAANull() {
         RetrievePatientCorrelationsRequestType patcorrReq = new RetrievePatientCorrelationsRequestType();
         patcorrReq.setQualifiedPatientIdentifier(createQualifiedSubjectIdentifier());
         patcorrReq.setAssertion(createAssertion());
         return patcorrReq;
     }
-    
-    
+
     private QualifiedSubjectIdentifierType createQualifiedSubjectIdentifier() {
         QualifiedSubjectIdentifierType subject = new QualifiedSubjectIdentifierType();
         subject.setAssigningAuthorityIdentifier("1.1");
         subject.setSubjectIdentifier("D123401");
         return subject;
     }
-    
+
     private AssertionType createAssertion() {
         AssertionType assertion = new AssertionType();
         return assertion;
     }
-       
-    private List<String> getHomeCommunitiesIdsWithPrefix()
-    {
+
+    private List<String> getHomeCommunitiesIdsWithPrefix() {
         List<String> homeCommunityIds = new ArrayList<String>();
         homeCommunityIds.add("urn:oid:1.1");
         homeCommunityIds.add("2.2");
         return homeCommunityIds;
     }
-    
-    
+
 }
