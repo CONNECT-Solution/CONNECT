68fb05c4c3582200b9a1aaa6df513b30095abecc
alameluchidambaram <achidamb@ACHIDAMB-F05745.cgifederal.com>
2015-09-10 20:49:17 -0400

    FHAC-548:Fix PD Audit Logging Regression Suite and Passthrough Audit Logging

--- Product/Production/Services/PatientDiscoveryCore/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/outbound/PassthroughOutboundPatientDiscoveryTest.java
+++ Product/Production/Services/PatientDiscoveryCore/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/outbound/PassthroughOutboundPatientDiscoveryTest.java
@@ -26,27 +26,28 @@
  */
 package gov.hhs.fha.nhinc.patientdiscovery.outbound;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.common.nhinccommon.HomeCommunityType;
 import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunitiesType;
 import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetCommunityType;
-import gov.hhs.fha.nhinc.patientdiscovery.PatientDiscoveryAuditLogger;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import gov.hhs.fha.nhinc.patientdiscovery.audit.PatientDiscoveryAuditLogger;
 import gov.hhs.fha.nhinc.patientdiscovery.entity.OutboundPatientDiscoveryDelegate;
 import gov.hhs.fha.nhinc.patientdiscovery.entity.OutboundPatientDiscoveryOrchestratable;
-
+import java.util.Properties;
 import org.hl7.v3.PRPAIN201305UV02;
 import org.hl7.v3.PRPAIN201306UV02;
 import org.hl7.v3.RespondingGatewayPRPAIN201305UV02RequestType;
 import org.hl7.v3.RespondingGatewayPRPAIN201306UV02ResponseType;
+import static org.junit.Assert.assertSame;
 import org.junit.Test;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author akong
@@ -60,40 +61,42 @@ public class PassthroughOutboundPatientDiscoveryTest {
         request.setPRPAIN201305UV02(new PRPAIN201305UV02());
         request.setNhinTargetCommunities(createNhinTargetCommunitiesType("1.1"));
         AssertionType assertion = new AssertionType();
-
+        Properties webContextProperties = null;
         PRPAIN201306UV02 expectedResponse = new PRPAIN201306UV02();
         OutboundPatientDiscoveryOrchestratable outOrchestratable = new OutboundPatientDiscoveryOrchestratable();
         outOrchestratable.setResponse(expectedResponse);
-
         OutboundPatientDiscoveryDelegate delegate = mock(OutboundPatientDiscoveryDelegate.class);
         PatientDiscoveryAuditLogger auditLogger = mock(PatientDiscoveryAuditLogger.class);
 
         when(delegate.process(any(OutboundPatientDiscoveryOrchestratable.class))).thenReturn(outOrchestratable);
 
         PassthroughOutboundPatientDiscovery passthroughPatientDiscovery = new PassthroughOutboundPatientDiscovery(
-                delegate, auditLogger);
+            delegate, auditLogger);
 
         RespondingGatewayPRPAIN201306UV02ResponseType actualMessage = passthroughPatientDiscovery
-                .respondingGatewayPRPAIN201305UV02(request, assertion);
+            .respondingGatewayPRPAIN201305UV02(request, assertion);
 
         assertSame(outOrchestratable.getResponse(), actualMessage.getCommunityResponse().get(0).getPRPAIN201306UV02());
 
-        verify(auditLogger, never()).auditEntity201305(any(RespondingGatewayPRPAIN201305UV02RequestType.class),
-                any(AssertionType.class), any(String.class));
+        verify(auditLogger).auditRequestMessage(eq(request.getPRPAIN201305UV02()), eq(assertion),
+            Mockito.any(NhinTargetSystemType.class), eq(NhincConstants.AUDIT_LOG_OUTBOUND_DIRECTION),
+            eq(NhincConstants.AUDIT_LOG_NHIN_INTERFACE), eq(Boolean.TRUE), eq(webContextProperties),
+            eq(NhincConstants.PATIENT_DISCOVERY_SERVICE_NAME));
 
-        verify(auditLogger, never()).auditEntity201306(any(RespondingGatewayPRPAIN201306UV02ResponseType.class),
-                any(AssertionType.class), any(String.class));
     }
 
     private NhinTargetCommunitiesType createNhinTargetCommunitiesType(String hcid) {
         NhinTargetCommunitiesType targetCommunities = new NhinTargetCommunitiesType();
         NhinTargetCommunityType targetCommunity = new NhinTargetCommunityType();
+        targetCommunity.setHomeCommunity(createHomeCommunity(hcid));
+        targetCommunities.getNhinTargetCommunity().add(targetCommunity);
+        return targetCommunities;
+    }
+
+    private HomeCommunityType createHomeCommunity(String hcid) {
         HomeCommunityType homeCommunity = new HomeCommunityType();
         homeCommunity.setHomeCommunityId(hcid);
-        targetCommunity.setHomeCommunity(homeCommunity);
-        targetCommunities.getNhinTargetCommunity().add(targetCommunity);
-
-        return targetCommunities;
+        return homeCommunity;
     }
 
 }
