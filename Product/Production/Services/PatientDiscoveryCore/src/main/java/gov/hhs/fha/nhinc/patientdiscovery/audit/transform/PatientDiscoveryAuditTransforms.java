108e0d4d576530df550a870571b19f3aa461ffb3
Jason <jason.a.smith@cgifederal.com>
2015-09-18 13:54:32 -0400

    FHAC-539 Added null pointer checks and pulled out private method

--- Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/audit/transform/PatientDiscoveryAuditTransforms.java
+++ Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/audit/transform/PatientDiscoveryAuditTransforms.java
@@ -138,25 +138,44 @@ public class PatientDiscoveryAuditTransforms extends AuditTransforms<PRPAIN20130
             if (ids.size() == 1) {
                 livingSubjectId = getLivingSubjectId(ids.get(0));
             } else if (ids.size() > 1) {
-                // Get assignedDevice root
-                String root = request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice()
-                    .getValue().getId().get(0).getRoot();
-
-                // Compare assignedDevice root to each livingSubjectId root
-                for (PRPAMT201306UV02LivingSubjectId id : ids) {
-                    II oII = getLivingSubjectId(id);
-
-                    if (oII != null && oII.getRoot() != null && oII.getRoot().equals(root)) {
-                        livingSubjectId = oII;
-                        break;
-                    }
-                }
+                livingSubjectId = getLivingSubjectIdFromAuthorOrPerformerValue(request, ids);
             }
         } else {
             LOG.error("PatientId doesn't exist in the received PRPAIN201305UV02 message");
         }
 
         return livingSubjectId;
+    }
+
+    private II getLivingSubjectIdFromAuthorOrPerformerValue(PRPAIN201305UV02 request, List<PRPAMT201306UV02LivingSubjectId> ids) {
+
+        // Get assignedDevice root
+        if (request.getControlActProcess().getAuthorOrPerformer() != null
+            && !request.getControlActProcess().getAuthorOrPerformer().isEmpty()
+            && request.getControlActProcess().getAuthorOrPerformer().get(0) != null
+            && request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice() != null
+            && request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice().getValue() != null
+            && request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice().getValue().getId() != null
+            && !request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice().getValue().getId()
+            .isEmpty()
+            && request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice().getValue().getId()
+            .get(0) != null
+            && request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice().getValue().getId()
+            .get(0).getRoot() != null) {
+
+            String root = request.getControlActProcess().getAuthorOrPerformer().get(0).getAssignedDevice()
+                .getValue().getId().get(0).getRoot();
+
+            // Compare assignedDevice root to each livingSubjectId root
+            for (PRPAMT201306UV02LivingSubjectId id : ids) {
+                II oII = getLivingSubjectId(id);
+
+                if (oII != null && oII.getRoot() != null && oII.getRoot().equals(root)) {
+                    return oII;
+                }
+            }
+        } //TODO Is it correct to return a NULL here if no root value or should we return the first value found?
+        return null;
     }
 
     private II getLivingSubjectId(PRPAMT201306UV02LivingSubjectId id) {
