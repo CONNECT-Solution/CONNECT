d48005787388718700cbdff38b24a0bd87d7b2a2
Vidhi <vidhi.mehta@cgifederal.com>
2015-10-21 14:33:03 -0400

    FHAC-596 removed redundant check for prefix

--- Product/Production/Services/AuditRepositoryCore/src/main/java/gov/hhs/fha/nhinc/audit/transform/AuditTransforms.java
+++ Product/Production/Services/AuditRepositoryCore/src/main/java/gov/hhs/fha/nhinc/audit/transform/AuditTransforms.java
@@ -480,13 +480,9 @@ public abstract class AuditTransforms<T, K> {
 
         // Set the Audit Source Id (community id)
         if (communityId != null) {
-            if (communityId.startsWith(NhincConstants.HCID_PREFIX)) {
-                auditSrcId.setAuditSourceID(communityId);
-
-            } else {
-                auditSrcId.setAuditSourceID(HomeCommunityMap.getHomeCommunityIdWithPrefix(communityId));
-
-            }
+            /* HomeCommunityMap.getHomeCommunityIdWithPrefix(communityId) mehtod
+             checks & takes care of prefix if not populated*/
+            auditSrcId.setAuditSourceID(HomeCommunityMap.getHomeCommunityIdWithPrefix(communityId));
         }
 
         // If specified, set the Audit Enterprise Site Id (community name)
