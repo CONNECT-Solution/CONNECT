8b498b7d6e66b48a5ae727de616f838f6d0b182c
alameluchidambaram <achidamb@ACHIDAMB-F05745.cgifederal.com>
2015-12-17 10:34:19 -0500

    FHAC-699:Publish AuditQueryLogUnsecured Service

---
+++ Product/Production/Gateway/CONNECTGatewayWeb/src/main/java/gov/hhs/fha/nhinc/auditquerylog/nhinc/AuditQueryLogUnsecured.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2009-2015, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.auditquerylog.nhinc;
+
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsBlobRequest;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsBlobResponse;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsRequestByRequestMessageId;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsRequestType;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsResponseType;
+import javax.annotation.Resource;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.soap.Addressing;
+
+/**
+ *
+ * @author achidamb
+ */
+@BindingType(value = javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+@Addressing(enabled = true)
+public class AuditQueryLogUnsecured implements
+    gov.hhs.fha.nhinc.common.nhinccomponentauditquerylog.AuditQueryLogPortType {
+
+    @Resource
+    private WebServiceContext context;
+
+    protected WebServiceContext getWebServiceContext() {
+        return context;
+    }
+
+    protected AuditQueryLogUnsecuredImpl getAuditQueryImpl() {
+        return new AuditQueryLogUnsecuredImpl();
+    }
+
+    @Override
+    public QueryAuditEventsBlobResponse queryAuditEventsBlob(QueryAuditEventsBlobRequest requestById) {
+        return getAuditQueryImpl().queryAuditEventsById(requestById, context);
+    }
+
+    @Override
+    public QueryAuditEventsResponseType queryAuditEvents(QueryAuditEventsRequestType request) {
+        return getAuditQueryImpl().queryAuditEvents(request, context);
+    }
+
+    @Override
+    public QueryAuditEventsResponseType queryAuditEventsByMessageID(
+        QueryAuditEventsRequestByRequestMessageId requestByMessageId) {
+        return getAuditQueryImpl().queryAuditEventsByMessageId(requestByMessageId, context);
+    }
+
+}
---
+++ Product/Production/Services/AuditRepositoryCore/src/main/java/gov/hhs/fha/nhinc/auditquerylog/AuditQueryLogImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009-2015, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.auditquerylog;
+
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsBlobRequest;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsBlobResponse;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsRequestType;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsResponseType;
+import gov.hhs.fha.nhinc.common.auditquerylog.QueryAuditEventsRequestByRequestMessageId;
+
+/**
+ *
+ * @author achidamb
+ */
+public class AuditQueryLogImpl {
+
+    public QueryAuditEventsResponseType queryAuditEvents(QueryAuditEventsRequestType requestAuditEvents) {
+        return new QueryAuditEventsResponseType();
+    }
+
+    public QueryAuditEventsResponseType queryAuditEventsByMessageId(
+        QueryAuditEventsRequestByRequestMessageId requestAuditEvents) {
+        return new QueryAuditEventsResponseType();
+    }
+
+    /**
+     *
+     * @param requestAuditEvents
+     * @return
+     */
+    public QueryAuditEventsBlobResponse queryAuditEventsById(QueryAuditEventsBlobRequest requestAuditEvents) {
+        return new QueryAuditEventsBlobResponse();
+    }
+
+}
