c335a99459a1022984f0196bfe69a9f27544c8b0
tabassum_jafri <tabassum.jafri@cgifederal.com>
2016-07-28 12:35:23 -0400

    Added a custom error page and exception handler to redirect all exceptions to one error page

---
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/exceptionhandler/CustomExceptionHandlerWrapper.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2009-2016, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.admingui.exceptionhandler;
+
+import java.util.Iterator;
+import java.util.Map;
+import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerWrapper;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ExceptionQueuedEvent;
+import javax.faces.event.ExceptionQueuedEventContext;
+
+/**
+ *
+ * @author tjafri
+ */
+public class CustomExceptionHandlerWrapper extends ExceptionHandlerWrapper {
+
+    private ExceptionHandler wrapped;
+
+    CustomExceptionHandlerWrapper(ExceptionHandler exception) {
+        this.wrapped = exception;
+    }
+
+    @Override
+    public ExceptionHandler getWrapped() {
+        return wrapped;
+    }
+
+    @Override
+    public void handle() throws FacesException {
+
+        final Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents().iterator();
+        while (i.hasNext()) {
+            ExceptionQueuedEvent event = i.next();
+            ExceptionQueuedEventContext context
+                = (ExceptionQueuedEventContext) event.getSource();
+
+            // get the exception from context
+            Throwable t = context.getException();
+
+            final FacesContext fc = FacesContext.getCurrentInstance();
+            final Map<String, Object> requestMap = fc.getExternalContext().getRequestMap();
+            final NavigationHandler nav = fc.getApplication().getNavigationHandler();
+
+            try {
+
+                //redirect error page
+                FacesContext.getCurrentInstance().addMessage("errorMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "An Exception occurred while performing the requested function.", ""));
+                FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+                requestMap.put("exceptionMessage", t.getMessage());
+                nav.handleNavigation(fc, null, "/customerror");
+                fc.renderResponse();
+            } finally {
+                //remove it from queue
+                i.remove();
+            }
+        }
+        //parent handle
+        getWrapped().handle();
+    }
+}
