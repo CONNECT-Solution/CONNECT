c94d26e07cbcf28ccfa4a57c5a1ba30c11f07980
Minh-Hai Nguyen <minh-hai.nguyen@cgi.com>
2016-07-29 17:40:38 -0400

    Fix findbug issues in CI nightly job scan

--- Product/Production/Services/DocumentRetrieveCore/src/main/java/gov/hhs/fha/nhinc/docretrieve/outbound/AbstractOutboundDocRetrieve.java
+++ Product/Production/Services/DocumentRetrieveCore/src/main/java/gov/hhs/fha/nhinc/docretrieve/outbound/AbstractOutboundDocRetrieve.java
@@ -44,19 +44,7 @@ import org.apache.commons.lang.StringUtils;
  */
 public abstract class AbstractOutboundDocRetrieve {
 
-    private DocRetrieveAuditLogger docRetrieveAuditLogger;
 
-    /**
-     *
-     */
-    public AbstractOutboundDocRetrieve() {
-        super();
-        docRetrieveAuditLogger = new DocRetrieveAuditLogger();
-    }
-
-    public AbstractOutboundDocRetrieve(DocRetrieveAuditLogger docRetrieveAuditLogger) {
-        docRetrieveAuditLogger = new DocRetrieveAuditLogger();
-    }
 
     /**
      * @param targets
