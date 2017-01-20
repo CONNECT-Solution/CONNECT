93b2efc443cf738d3fae9867a1240994c82416df
mikhelamdex <madun@amdexcorp.com>
2017-01-20 16:43:22 -0500

    FHAC-1314 refactor classes for CXF POC

---
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilderConstants.java
@@ -0,0 +1,10 @@
+package gov.hhs.fha.nhinc.callback.openSAML;
+
+public class SAMLAssertionBuilderConstants {
+
+    public static final String HOK_ASSERTION_TYPE = "HOK-Assertion";
+    public static final String SV_ASSERTION_TYPE = "SV-Assertion";
+
+    private SAMLAssertionBuilderConstants() {
+    }
+}
