aff6b6139a93ec55cafb07e4e0803d8d52cc6f16
mikhelamdex <madun@amdexcorp.com>
2017-01-24 09:43:41 -0500

    refactor classes

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilder.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilder.java
@@ -35,7 +35,7 @@ import org.w3c.dom.Element;
  * @author bhumphrey
  *
  */
-abstract public class SAMLAssertionBuilder {
+public abstract class SAMLAssertionBuilder {
 
     static final String ID_PREFIX = "_";
 
@@ -44,8 +44,14 @@ abstract public class SAMLAssertionBuilder {
     // Authorization Decision Action is always set to Execute
     static final String AUTHZ_DECISION_ACTION_EXECUTE = "Execute";
 
+    static final String AUTHN_SESSION_INDEX = "123456";
+
     // Valid Authorization Decision values
     static final String AUTHZ_DECISION_PERMIT = "Permit";
+    static final String X509_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName";
+    static final String X509_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:X509";
+    static final String UNSPECIFIED_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified";
+
     private static final String AUTHZ_DECISION_DENY = "Deny";
     private static final String AUTHZ_DECISION_INDETERMINATE = "Indeterminate";
     private static final String[] VALID_AUTHZ_DECISION_ARRAY = { AUTHZ_DECISION_PERMIT, AUTHZ_DECISION_DENY,
@@ -56,7 +62,6 @@ abstract public class SAMLAssertionBuilder {
     // Valid Name Identification values
     private static final String UNSPECIFIED_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified";
     private static final String EMAIL_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
-    static final String X509_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName";
     private static final String WINDOWS_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:WindowsDomainQualifiedName";
     private static final String KERBEROS_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:kerberos";
     private static final String ENTITY_NAME_ID = "urn:oasis:names:tc:SAML:1.1:nameid-format:entity";
@@ -68,8 +73,6 @@ abstract public class SAMLAssertionBuilder {
     private static final List<String> VALID_NAME_LIST = Collections
             .unmodifiableList(Arrays.asList(VALID_NAME_ID_ARRAY));
 
-    static final String AUTHN_SESSION_INDEX = "123456";
-
     // Valid Context Class references
     private static final String INTERNET_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocol";
     private static final String INTERNET_PASSWORD_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocolPassword";
@@ -79,11 +82,9 @@ abstract public class SAMLAssertionBuilder {
     private static final String PREVIOUS_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:PreviousSession";
     private static final String REMOTE_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:SecureRemotePassword";
     private static final String TLS_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:TLSClient";
-    static final String X509_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:X509";
     private static final String PGP_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:PGP";
     private static final String SPKI_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:SPKI";
     private static final String DIG_SIGN_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:XMLDSig";
-    static final String UNSPECIFIED_AUTHN_CNTX_CLS = "urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified";
     private static final String[] VALID_AUTHN_CNTX_CLS_ARRAY = { INTERNET_AUTHN_CNTX_CLS,
             INTERNET_PASSWORD_AUTHN_CNTX_CLS, PASSWORD_AUTHN_CNTX_CLS, PASSWORD_TRANS_AUTHN_CNTX_CLS,
             KERBEROS_AUTHN_CNTX_CLS, PREVIOUS_AUTHN_CNTX_CLS, REMOTE_AUTHN_CNTX_CLS, TLS_AUTHN_CNTX_CLS,
@@ -109,6 +110,6 @@ abstract public class SAMLAssertionBuilder {
         return VALID_NAME_LIST.contains(format);
     }
 
-    abstract public Element build(CallbackProperties properties) throws Exception;
+    public abstract Element build(CallbackProperties properties) throws SAMLAssertionBuilderException;
 
 }
