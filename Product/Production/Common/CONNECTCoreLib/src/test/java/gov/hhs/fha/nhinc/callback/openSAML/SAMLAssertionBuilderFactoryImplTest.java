b605bb6367888b03653101fb386211c8098c871c
mikhelamdex <madun@amdexcorp.com>
2017-01-24 09:44:55 -0500

    refactor classes

--- Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilderFactoryImplTest.java
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilderFactoryImplTest.java
@@ -35,9 +35,9 @@ public class SAMLAssertionBuilderFactoryImplTest {
     @Test
     public void getBuilderTest() {
         SAMLAssertionBuilderFactoryImpl builderFactory = new SAMLAssertionBuilderFactoryImpl();
-        SAMLAssertionBuilder hokBuilder = builderFactory.getBuilder(SAMLAssertionBuilderFactory.HOK_ASSERTION_TYPE);
+        SAMLAssertionBuilder hokBuilder = builderFactory.getBuilder(SAMLAssertionBuilderConstants.HOK_ASSERTION_TYPE);
         assertTrue(hokBuilder instanceof HOKSAMLAssertionBuilder);
-        SAMLAssertionBuilder svBuilder = builderFactory.getBuilder(SAMLAssertionBuilderFactory.SV_ASSERTION_TYPE);
+        SAMLAssertionBuilder svBuilder = builderFactory.getBuilder(SAMLAssertionBuilderConstants.SV_ASSERTION_TYPE);
         assertTrue(svBuilder instanceof SVSAMLAssertionBuilder);
     }
 }
