896eb2b333f19151eae1275ccacfc7c6c592ddd2
mikhelamdex <madun@amdexcorp.com>
2017-01-25 13:38:24 -0500

    fix sonar issues, refactored code to throw/catch concrete exception classes

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilderException.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/callback/openSAML/SAMLAssertionBuilderException.java
@@ -2,8 +2,15 @@ package gov.hhs.fha.nhinc.callback.openSAML;
 
 public class SAMLAssertionBuilderException extends Exception {
 
-    public SAMLAssertionBuilderException(String message)
-    {
+    public SAMLAssertionBuilderException(String message) {
         super(message);
+    }
+
+    public SAMLAssertionBuilderException(Throwable throwable) {
+    	super(throwable);
+    }
+
+    public SAMLAssertionBuilderException(String message, Throwable throwable) {
+    	super(message, throwable);
     }
 }
