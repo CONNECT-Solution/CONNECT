d52ee62b4d521fa10a2b96a156f70f1d42cf794a
Minh-Hai Nguyen <minh-hai.nguyen@cgi.com>
2017-02-23 22:53:30 -0500

    fix sql script to comply with sql server and update to use jdk7

--- Product/DBScripts/nhincdb/nhincdb.sql
+++ Product/DBScripts/nhincdb/nhincdb.sql
@@ -580,7 +580,7 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON adminguidb.* to nhincuser;
 -- begin message monitoringdb
 CREATE DATABASE messagemonitoringdb;
 
-CREATE TABLE messagemonitoringdb.monitoredmessage (
+CREATE TABLE IF NOT EXISTS messagemonitoringdb.monitoredmessage (
   id bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier',
   senderemailid varchar(255) DEFAULT NULL COMMENT 'sender email identifier',
   subject varchar(255) DEFAULT NULL COMMENT 'email Subject',
@@ -594,17 +594,21 @@ CREATE TABLE messagemonitoringdb.monitoredmessage (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE messagemonitoringdb.monitoredmessagenotification (
-  id bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier',
-  emailid varchar(255) NOT NULL COMMENT 'notification email identifier',
-  messageid varchar(100) DEFAULT NULL COMMENT 'unique email message identifier',
-  monitoredmessageid bigint(20) NOT NULL COMMENT 'unique trackmessage identifier',
-  status varchar(30) NOT NULL COMMENT 'Pending, Completed, Error',
-  createtime timestamp NULL DEFAULT NULL,
-  updatetime timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY fk_monitoredmessageId (monitoredmessageid),
-  CONSTRAINT fk_monitoredmessageId FOREIGN KEY (monitoredmessageid) REFERENCES monitoredmessage (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS messagemonitoringdb.monitoredmessagenotification (
+    id bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier',
+    emailid varchar(255) NOT NULL COMMENT 'notification email identifier',
+    messageid varchar(100) DEFAULT NULL COMMENT 'unique email message identifier',
+    monitoredmessageid bigint(20) NOT NULL COMMENT 'unique trackmessage identifier',
+    status varchar(30) NOT NULL COMMENT 'Pending, Completed, Error',
+    createtime timestamp NULL DEFAULT NULL,
+    updatetime timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY fk_monitoredmessageId (monitoredmessageid),
+    CONSTRAINT fk_monitoredmessageId
+        FOREIGN KEY (monitoredmessageid)
+        REFERENCES messagemonitoringdb.monitoredmessage (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table to track outbound Message Monitoring notification';
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON messagemonitoringdb.* to nhincuser;
