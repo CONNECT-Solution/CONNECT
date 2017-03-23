09338ee7ea06102c186d9b8d63ab91cb8d659e3c
sjarral112 <sjarral@amdexcorp.com>
2017-03-23 10:25:02 -0400

    Replaced 2009-2016 ro 2009-2017 , also replaced 0=HITS to O=FHA, case insensitive

--- Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/entity/deferred/request/OutboundPatientDiscoveryDeferredRequestOrchestrationContextBuilder.java
+++ Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/entity/deferred/request/OutboundPatientDiscoveryDeferredRequestOrchestrationContextBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2016, United States Government, as represented by the Secretary of Health and Human Services.
+ * Copyright (c) 2009-2017, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
