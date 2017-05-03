1486b6f67faa8825168067aaeb603eea9b86270e
balmeeki <balmeeki09@gmail.com>
2017-05-03 11:27:45 -0400

    modified code based on PR

--- Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/transform/policy/AdhocQueryTransformHelperTest.java
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/transform/policy/AdhocQueryTransformHelperTest.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2009-2017, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package gov.hhs.fha.nhinc.transform.policy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -17,13 +44,14 @@ import oasis.names.tc.xacml._2_0.context.schema.os.AttributeType;
 import oasis.names.tc.xacml._2_0.context.schema.os.SubjectType;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+
 import org.junit.Test;
 
 public class AdhocQueryTransformHelperTest {
 
     private AdhocQueryTransformHelper instance;
-
+    private static final String TEST_HC_VAL = "urn:oid:1.1.1.1.1.1.1";
+    private static final String MESSAGE_ID = "12345566";
     @Before
     public void setUp() {
         instance = new AdhocQueryTransformHelper();
@@ -36,7 +64,7 @@ public class AdhocQueryTransformHelperTest {
 
     @Test
     public void testTransformAdhocQueryToCheckPolicy() {
-
+        String direction = "DocumentQueryIn";
         AdhocQueryRequestEventType eventA = new AdhocQueryRequestEventType();
         eventA.setDirection("inbound");
         AdhocQueryRequestMessageType msg = new AdhocQueryRequestMessageType();
@@ -53,21 +81,21 @@ public class AdhocQueryTransformHelperTest {
         adhocQueryRequest.setAdhocQuery(adhocQueryType);
         msg.setAdhocQueryRequest(adhocQueryRequest);
         AssertionType assertionTypeTest = new AssertionType();
-        assertionTypeTest.setMessageId("12345566");
+        assertionTypeTest.setMessageId(MESSAGE_ID);
         HomeCommunityType homeCom = new HomeCommunityType();
-        homeCom.setHomeCommunityId("urn:oid:1.1.1.1.1.1.1");
+        homeCom.setHomeCommunityId(TEST_HC_VAL);
         assertionTypeTest.setHomeCommunity(homeCom);
         msg.setAssertion(assertionTypeTest);
         eventA.setMessage(msg);
 
         CheckPolicyRequestType result = instance.transformAdhocQueryToCheckPolicy(eventA);
-        verifycheckInboundPolicy(result);
+        verifycheckPolicy(result, direction);
 
     }
     
     @Test
     public void testTransformAdhocQueryToCheckPolicy_Outbound() {
-
+    	 String direction = "DocumentQueryOut";
         AdhocQueryRequestEventType eventA = new AdhocQueryRequestEventType();
         eventA.setDirection("outbound");
         AdhocQueryRequestMessageType msg = new AdhocQueryRequestMessageType();
@@ -84,64 +112,43 @@ public class AdhocQueryTransformHelperTest {
         adhocQueryRequest.setAdhocQuery(adhocQueryType);
         msg.setAdhocQueryRequest(adhocQueryRequest);
         AssertionType assertionTypeTest = new AssertionType();
-        assertionTypeTest.setMessageId("12345566");
+        assertionTypeTest.setMessageId(MESSAGE_ID);
         HomeCommunityType homeCom = new HomeCommunityType();
-        homeCom.setHomeCommunityId("urn:oid:1.1.1.1.1.1.1");
+        homeCom.setHomeCommunityId(TEST_HC_VAL);
         assertionTypeTest.setHomeCommunity(homeCom);
         msg.setAssertion(assertionTypeTest);
         eventA.setMessage(msg);
-
+        
         CheckPolicyRequestType result = instance.transformAdhocQueryToCheckPolicy(eventA);
-        verifycheckOutboundPolicy(result);
+        verifycheckPolicy(result, direction );
 
     }
     
-    private void verifycheckOutboundPolicy(CheckPolicyRequestType actualCheckPolicy) {
-        // Verify result
+    private void verifycheckPolicy(CheckPolicyRequestType actualCheckPolicy, String direction) {
+        
         assertNotNull(actualCheckPolicy);
-        // AssertAssertion to make sure nothing change
+        
         AssertionType assertTypeResult = actualCheckPolicy.getAssertion();
 
-        assertEquals("messageId should not change", "12345566", assertTypeResult.getMessageId());
-        // Assert Resource Session
+        assertEquals("messageId should not change", MESSAGE_ID, assertTypeResult.getMessageId());
+        
         assertEquals("urn:oasis:names:tc:xacml:1.0:resource:resource-id", actualCheckPolicy.getRequest().getResource()
             .get(0).getAttribute().get(1).getAttributeId());
         String expectResourceId = "amp;26.489.22";
         assertEquals(expectResourceId, actualCheckPolicy.getRequest().getResource().get(0).getAttribute().get(0)
             .getAttributeValue().get(0).getContent().get(0));
 
-        assertEquals("DocumentQueryOut", actualCheckPolicy.getRequest().getAction().getAttribute().get(0)
+        assertEquals(direction, actualCheckPolicy.getRequest().getAction().getAttribute().get(0)
             .getAttributeValue().get(0).getContent().get(0));
         SubjectType subjectType = actualCheckPolicy.getRequest().getSubject().get(0);
         AttributeType attributeType = subjectType.getAttribute().get(1);
         assertEquals("http://www.hhs.gov/healthit/nhin#HomeCommunityId", attributeType.getAttributeId());
         assertEquals("http://www.w3.org/2001/XMLSchema#anyURI", attributeType.getDataType());
-        assertEquals("urn:oid:1.1.1.1.1.1.1", attributeType.getAttributeValue().get(0).getContent().get(0));
+        assertEquals(TEST_HC_VAL, attributeType.getAttributeValue().get(0).getContent().get(0));
     }
   
 
-    private void verifycheckInboundPolicy(CheckPolicyRequestType actualCheckPolicy) {
-        // Verify result
-        assertNotNull(actualCheckPolicy);
-        // AssertAssertion to make sure nothing change
-        AssertionType assertTypeResult = actualCheckPolicy.getAssertion();
-
-        assertEquals("messageId should not change", "12345566", assertTypeResult.getMessageId());
-        // Assert Resource Session
-        assertEquals("urn:oasis:names:tc:xacml:1.0:resource:resource-id", actualCheckPolicy.getRequest().getResource()
-            .get(0).getAttribute().get(1).getAttributeId());
-        String expectResourceId = "amp;26.489.22";
-        assertEquals(expectResourceId, actualCheckPolicy.getRequest().getResource().get(0).getAttribute().get(0)
-            .getAttributeValue().get(0).getContent().get(0));
-
-        assertEquals("DocumentQueryIn", actualCheckPolicy.getRequest().getAction().getAttribute().get(0)
-            .getAttributeValue().get(0).getContent().get(0));
-        SubjectType subjectType = actualCheckPolicy.getRequest().getSubject().get(0);
-        AttributeType attributeType = subjectType.getAttribute().get(1);
-        assertEquals("http://www.hhs.gov/healthit/nhin#HomeCommunityId", attributeType.getAttributeId());
-        assertEquals("http://www.w3.org/2001/XMLSchema#anyURI", attributeType.getDataType());
-        assertEquals("urn:oid:1.1.1.1.1.1.1", attributeType.getAttributeValue().get(0).getContent().get(0));
-    }
+   
     
  
     @Test
@@ -157,10 +164,10 @@ public class AdhocQueryTransformHelperTest {
     
  
     @Test
-    public void testTransformAdhocQueryToCheckPolicy_outbound() {
-    	AdhocQueryTransformHelper instance = new AdhocQueryTransformHelper();
+    public void testTransformAdhocQueryToCheckPolicy_DefaultOutBound() {
+    	
         AdhocQueryRequestEventType eventA = new AdhocQueryRequestEventType();
-     //   eventA.getDirection()= "inbound";
+    
        
         eventA.setDirection("outbound");
         CheckPolicyRequestType result = instance.transformAdhocQueryToCheckPolicy(eventA);
@@ -168,10 +175,10 @@ public class AdhocQueryTransformHelperTest {
     }
 
     @Test
-    public void testTransformAdhocQueryToCheckPolicy_AssertNotNull() {
-    	AdhocQueryTransformHelper instance = new AdhocQueryTransformHelper();
+    public void testTransformAdhocQueryToCheckPolicy_DefaultInBound() {
+    	
         AdhocQueryRequestEventType eventA = new AdhocQueryRequestEventType();
-     //   eventA.getDirection()= "inbound";
+    
        
         eventA.setDirection("inbound");
         CheckPolicyRequestType result = instance.transformAdhocQueryToCheckPolicy(eventA);
@@ -179,20 +186,20 @@ public class AdhocQueryTransformHelperTest {
     }
 
     @Test
-    public void testTransformAdhocQueryResponseToCheckPolicy_OutBound() {
-    	AdhocQueryTransformHelper instance = new AdhocQueryTransformHelper();
+    public void testTransformAdhocQueryResponseToCheckPolicy_DefaultOutBound() {
+    	
     	AdhocQueryResultEventType eventB = new AdhocQueryResultEventType();
-     //   eventA.getDirection()= "inbound";
+     
         eventB.setDirection("outbound");
         CheckPolicyRequestType result = instance.transformAdhocQueryResponseToCheckPolicy(eventB);
         assertNotNull(result);
     }
     
     @Test
-    public void testTransformAdhocQueryResponseToCheckPolicy_InBound() {
-    	AdhocQueryTransformHelper instance = new AdhocQueryTransformHelper();
+    public void testTransformAdhocQueryResponseToCheckPolicy_DefaultInBound() {
+    	
     	AdhocQueryResultEventType eventB = new AdhocQueryResultEventType();
-     //   eventA.getDirection()= "inbound";
+     
         eventB.setDirection("inbound");
         CheckPolicyRequestType result = instance.transformAdhocQueryResponseToCheckPolicy(eventB);
         assertNotNull(result);
