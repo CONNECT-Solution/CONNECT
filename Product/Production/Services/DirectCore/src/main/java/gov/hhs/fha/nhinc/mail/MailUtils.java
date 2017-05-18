b41569f0289c8ec12bea3d57d6adafc7d1392b5b
balmeeki <balmeeki09@gmail.com>
2017-05-18 10:14:16 -0400

    removed tabs

--- Product/Production/Services/DirectCore/src/main/java/gov/hhs/fha/nhinc/mail/MailUtils.java
+++ Product/Production/Services/DirectCore/src/main/java/gov/hhs/fha/nhinc/mail/MailUtils.java
@@ -47,171 +47,153 @@ import org.slf4j.LoggerFactory;
  */
 public class MailUtils {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MailUtils.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MailUtils.class);
 
-	/**
-	 * Folder Name for "Inbox".
-	 */
-	public static final String FOLDER_NAME_INBOX = "Inbox";
-	/**
-	 * boolean flag when we do want to expunge the inbox.
-	 */
-	public static final boolean FOLDER_EXPUNGE_INBOX_TRUE = true;
-	/**
-	 * boolean flag when we don't want to expunge the inbox.
-	 */
-	public static final boolean FOLDER_EXPUNGE_INBOX_FALSE = false;
+    /**
+     * Folder Name for "Inbox".
+     */
+    public static final String FOLDER_NAME_INBOX = "Inbox";
+    /**
+     * boolean flag when we do want to expunge the inbox.
+     */
+    public static final boolean FOLDER_EXPUNGE_INBOX_TRUE = true;
+    /**
+     * boolean flag when we don't want to expunge the inbox.
+     */
+    public static final boolean FOLDER_EXPUNGE_INBOX_FALSE = false;
 
-	/**
-	 * Close the java mail store and folder, specifying whether deleted messages
-	 * should be expunged. Log exceptions.
-	 *
-	 * @param store
-	 *            to be closed.
-	 * @param folder
-	 *            to be closed.
-	 * @param expunge
-	 *            true if deleted messages should be expunged from the folder.
-	 */
-	public static void closeQuietly(Store store, Folder folder, boolean expunge) {
-		if (folder != null) {
-			try {
-				folder.close(expunge);
-			} catch (Exception e) {
-				LOG.warn("Exception while closing java mail folder, expunge = " + expunge + ".", e);
-			}
-		}
-		closeQuietly(store);
-	}
+    /**
+     * Close the java mail store and folder, specifying whether deleted messages should be expunged. Log exceptions.
+     *
+     * @param store to be closed.
+     * @param folder to be closed.
+     * @param expunge true if deleted messages should be expunged from the folder.
+     */
+    public static void closeQuietly(Store store, Folder folder, boolean expunge) {
+        if (folder != null) {
+            try {
+                folder.close(expunge);
+            } catch (Exception e) {
+                LOG.warn("Exception while closing java mail folder, expunge = " + expunge + ".", e);
+            }
+        }
+        closeQuietly(store);
+    }
 
-	/**
-	 * Close the java mail store. Log exceptions.
-	 *
-	 * @param store
-	 *            to be closed.
-	 */
-	public static void closeQuietly(Store store) {
-		if (store != null) {
-			try {
-				store.close();
-			} catch (Exception e) {
-				LOG.warn("Exception while closing java mail store.", e);
-			}
-		}
-	}
+    /**
+     * Close the java mail store. Log exceptions.
+     *
+     * @param store to be closed.
+     */
+    public static void closeQuietly(Store store) {
+        if (store != null) {
+            try {
+                store.close();
+            } catch (Exception e) {
+                LOG.warn("Exception while closing java mail store.", e);
+            }
+        }
+    }
 
-	/**
-	 * Return a mail session using the provided properties, username and
-	 * password.
-	 *
-	 * @param mailServerProps
-	 *            properties for the mail server
-	 * @param user
-	 *            username credential
-	 * @param pass
-	 *            password credential
-	 * @return mail session.
-	 */
-	public static Session getMailSession(Properties mailServerProps, String user, String pass) {
-		return Session.getInstance(mailServerProps, getMailAuthenticator(user, pass));
-	}
+    /**
+     * Return a mail session using the provided properties, username and password.
+     *
+     * @param mailServerProps properties for the mail server
+     * @param user username credential
+     * @param pass password credential
+     * @return mail session.
+     */
+    public static Session getMailSession(Properties mailServerProps, String user, String pass) {
+        return Session.getInstance(mailServerProps, getMailAuthenticator(user, pass));
+    }
 
-	/**
-	 * Send a mime message.
-	 *
-	 * @param recipients
-	 *            of the mime message.
-	 * @param session
-	 *            used to send the mime message.
-	 * @param message
-	 *            to be sent.
-	 * @throws MessagingException
-	 *             if there is an error.
-	 */
-	public static void sendMessage(Address[] recipients, Session session, MimeMessage message)
-			throws MessagingException {
+    /**
+     * Send a mime message.
+     *
+     * @param recipients of the mime message.
+     * @param session used to send the mime message.
+     * @param message to be sent.
+     * @throws MessagingException if there is an error.
+     */
+    public static void sendMessage(Address[] recipients, Session session, MimeMessage message)
+        throws MessagingException {
 
-		Transport transport = null;
-		try {
-			transport = session.getTransport("smtp");
-			String host = session.getProperty("mail.smtp.host");
-			String port = session.getProperty("mail.smtp.port");
-			String user = session.getProperty("connect.mail.user");
-			String pass = session.getProperty("connect.mail.pass");
-			if (!StringUtils.isBlank(port)) {
-				port = StringUtils.trim(port);
-			}
+        Transport transport = null;
+        try {
+            transport = session.getTransport("smtp");
+            String host = session.getProperty("mail.smtp.host");
+            String port = session.getProperty("mail.smtp.port");
+            String user = session.getProperty("connect.mail.user");
+            String pass = session.getProperty("connect.mail.pass");
+            if (!StringUtils.isBlank(port)) {
+                port = StringUtils.trim(port);
+            }
 
-			transport.connect(host, Integer.parseInt(port), user, pass);
-			transport.sendMessage(message, recipients);
-			logHeaders(message);
-		} catch (AssertionError e) {
-			LOG.error("Assertion Error while sending.", e);
-			throw e;
-		} finally {
-			if (transport != null) {
-				try {
-					transport.close();
-				} catch (MessagingException e) {
-					LOG.error("Exception while closing the transport: {}", e.getMessage(), e);
-				}
+            transport.connect(host, Integer.parseInt(port), user, pass);
+            transport.sendMessage(message, recipients);
+            logHeaders(message);
+        } catch (AssertionError e) {
+            LOG.error("Assertion Error while sending.", e);
+            throw e;
+        } finally {
+            if (transport != null) {
+                try {
+                    transport.close();
+                } catch (MessagingException e) {
+                    LOG.error("Exception while closing the transport: {}", e.getMessage(), e);
+                }
+            }
+        }
+    }
 
-			}
-		}
-	}
+    /**
+     * @param user username login credential
+     * @param pass password login credential
+     * @return mail Authenticator using credentials
+     */
+    private static Authenticator getMailAuthenticator(final String user, final String pass) {
+        return new Authenticator() {
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        };
+    }
 
-	/**
-	 * @param user
-	 *            username login credential
-	 * @param pass
-	 *            password login credential
-	 * @return mail Authenticator using credentials
-	 */
-	private static Authenticator getMailAuthenticator(final String user, final String pass) {
-		return new Authenticator() {
-			/**
-			 * {@inheritDoc}
-			 */
-			@Override
-			public PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(user, pass);
-			}
-		};
-	}
+    /**
+     * Log message headers.
+     *
+     * @param mimeMessage to log headers from
+     */
+    @SuppressWarnings("unchecked")
+    public static void logHeaders(final MimeMessage mimeMessage) {
+        if (LOG.isDebugEnabled()) {
+            Enumeration<String> headerLines = null;
+            try {
+                headerLines = mimeMessage.getAllHeaderLines();
+            } catch (MessagingException e) {
+                LOG.error("Could not extract headers: ", e);
+            }
+            while (headerLines != null && headerLines.hasMoreElements()) {
+                LOG.debug(headerLines.nextElement());
+            }
+        }
+    }
 
-	/**
-	 * Log message headers.
-	 *
-	 * @param mimeMessage
-	 *            to log headers from
-	 */
-	@SuppressWarnings("unchecked")
-	public static void logHeaders(final MimeMessage mimeMessage) {
-		if (LOG.isDebugEnabled()) {
-			Enumeration<String> headerLines = null;
-			try {
-				headerLines = mimeMessage.getAllHeaderLines();
-			} catch (MessagingException e) {
-				LOG.error("Could not extract headers: ", e);
-			}
-			while (headerLines != null && headerLines.hasMoreElements()) {
-				LOG.debug(headerLines.nextElement());
-			}
-		}
-	}
-
-	/**
-	 * Set the deleted flag on a message, log and swallow exceptions. Note:
-	 * deleted messages must be "expunged" to be removed from server.
-	 *
-	 * @param message
-	 *            mime message to be deleted.
-	 */
-	public static void setDeletedQuietly(MimeMessage message) {
-		try {
-			message.setFlag(Flags.Flag.DELETED, true);
-		} catch (MessagingException e) {
-			LOG.warn("Exception ", e);
-		}
-	}
+    /**
+     * Set the deleted flag on a message, log and swallow exceptions. Note: deleted messages must be "expunged" to be
+     * removed from server.
+     *
+     * @param message mime message to be deleted.
+     */
+    public static void setDeletedQuietly(MimeMessage message) {
+        try {
+            message.setFlag(Flags.Flag.DELETED, true);
+        } catch (MessagingException e) {
+            LOG.warn("Exception ", e);
+        }
+    }
 }
