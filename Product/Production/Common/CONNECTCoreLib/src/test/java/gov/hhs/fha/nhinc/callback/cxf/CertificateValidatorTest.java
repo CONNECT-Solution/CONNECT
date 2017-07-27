2aabd5e0e03dcd769b223108b7158fcc0a66faa6
Minh-Hai Nguyen <minh-hai.nguyen@cgi.com>
2017-07-27 14:35:46 -0400

    initial check

---
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/callback/cxf/CertificateValidatorTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2009-2016, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.callback.cxf;
+
+import gov.hhs.fha.nhinc.callback.openSAML.CertificateManagerImpl;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import org.apache.cxf.helpers.IOUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author mpnguyen
+ *
+ */
+public class CertificateValidatorTest {
+    private CertificateChainValidator certValidator;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty(CertificateManagerImpl.KEY_STORE_KEY, getFilePath("/gateway_self_sign.jks"));
+        System.setProperty(CertificateManagerImpl.KEY_STORE_PASSWORD_KEY, "changeit");
+        System.setProperty(CertificateManagerImpl.KEY_STORE_TYPE_KEY, "JKS");
+
+
+        System.setProperty(CertificateManagerImpl.TRUST_STORE_KEY, getFilePath("/cacerts.jks"));
+        System.setProperty(CertificateManagerImpl.TRUST_STORE_PASSWORD_KEY, "changeit");
+        System.setProperty(CertificateManagerImpl.TRUST_STORE_TYPE_KEY, "JKS");
+
+        CertificateManagerImpl certManager = (CertificateManagerImpl) CertificateManagerImpl.getInstance();
+
+        certValidator = new CertificateChainValidator(certManager);
+    }
+
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        certValidator = null;
+    }
+
+    @Test
+    public final void testWhetherChainOfTrust() {
+        X509Certificate certToCheck = getTestCert("/cert_test.cer");
+        Assert.assertTrue("Cert should be a part chain of trust", certValidator.isPartChainOfTrust(certToCheck));
+    }
+    @Test
+    public final void testSelfSignedCert(){
+        X509Certificate certToCheck = getTestCert("/self_sign.cer");
+        Assert.assertFalse("Cert is self-sign cert", certValidator.isPartChainOfTrust(certToCheck));
+    }
+    @Test
+    public final void testValidateCert(){
+        X509Certificate certToCheck = getTestCert("/epic.cer");
+        Assert.assertTrue("Our truststore should contain this cert",certValidator.validateCert(certToCheck));
+
+
+    }
+    private X509Certificate getTestCert(String certName){
+        CertificateFactory certificateFactory;
+        X509Certificate certificateToCheck = null;
+        try {
+            certificateFactory = CertificateFactory.getInstance("X.509");
+
+            FileInputStream inputStream = new FileInputStream(getFilePath(certName));
+            certificateToCheck = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(IOUtils.readBytesFromStream(inputStream)));
+            Assert.assertNotNull(certificateToCheck);
+        } catch (CertificateException | IOException e) {
+            Assert.fail(e.getLocalizedMessage());
+        }
+        return certificateToCheck;
+
+    }
+    private String getFilePath(String filename){
+        String filePath =  this.getClass().getResource(filename).getFile();
+        System.out.println(filePath);
+        return filePath;
+    }
+
+}
