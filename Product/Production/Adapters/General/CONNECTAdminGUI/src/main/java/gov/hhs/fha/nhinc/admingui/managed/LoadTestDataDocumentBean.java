bd29ec27f28ab64ab384591fedc657226d7d4659
ptambelliniAMDEX <ptambellini@amdexcorp.com>
2017-11-30 13:45:39 -0500

    CONN-1909Ver2 Updated QA issue

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/managed/LoadTestDataDocumentBean.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/managed/LoadTestDataDocumentBean.java
@@ -314,11 +314,11 @@ public class LoadTestDataDocumentBean {
     }
 
     private static FacesMessage msgForSelectDelete(String ofType) {
-        return HelperUtil.getMsgWarn(MessageFormat.format("Select a {0} for delete.", ofType.toLowerCase()));
+        return HelperUtil.getMsgWarn(MessageFormat.format("Select {0} for delete.", ofType.toLowerCase()));
     }
 
     private static FacesMessage msgForSelectEdit(String ofType) {
-        return HelperUtil.getMsgWarn(MessageFormat.format("Select a {0} for edit.", ofType.toLowerCase()));
+        return HelperUtil.getMsgWarn(MessageFormat.format("Select {0} for edit.", ofType.toLowerCase()));
     }
 
     private static FacesMessage msgForInvalidDocument(String ofType) {
