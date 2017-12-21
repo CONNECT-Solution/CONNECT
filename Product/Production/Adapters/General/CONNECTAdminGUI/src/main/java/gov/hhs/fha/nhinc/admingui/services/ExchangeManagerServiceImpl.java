894d8f860510e4b51deb78993cd9a415e05f7e85
trantang <ttang@amdexcorp.com>
2017-12-21 11:33:08 -0500

    CONN-1986 implement exchangeManager

---
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/services/ExchangeManagerServiceImpl.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2009-2017, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.admingui.services;
+
+import static gov.hhs.fha.nhinc.admingui.util.HelperUtil.TO_DO_MARKER;
+import static gov.hhs.fha.nhinc.exchangemgr.ExchangeManagerHelper.getEndpointConfigurationTypeBy;
+
+import gov.hhs.fha.nhinc.admingui.application.EndpointManagerCache;
+import gov.hhs.fha.nhinc.admingui.model.ConnectionEndpoint;
+import gov.hhs.fha.nhinc.admingui.services.impl.PingServiceImpl;
+import gov.hhs.fha.nhinc.admingui.util.HelperUtil;
+import gov.hhs.fha.nhinc.exchange.ExchangeInfoType;
+import gov.hhs.fha.nhinc.exchange.ExchangeType;
+import gov.hhs.fha.nhinc.exchange.directory.EndpointConfigurationType;
+import gov.hhs.fha.nhinc.exchange.directory.EndpointType;
+import gov.hhs.fha.nhinc.exchange.directory.OrganizationType;
+import gov.hhs.fha.nhinc.exchangemgr.ExchangeManager;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.http.impl.cookie.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Tran tang
+ *
+ */
+@Service
+public class ExchangeManagerServiceImpl implements ExchangeManagerService {
+    private static final Logger LOG = LoggerFactory.getLogger(ExchangeManagerServiceImpl.class);
+    private final ExchangeManager exchangeManager = ExchangeManager.getInstance();
+    private final PingService pingService = new PingServiceImpl();
+    private static final String DATE_FORMAT = "MM-dd-yy hh:mm:ss";
+
+    @Override
+    public boolean saveExchange(ExchangeType exchange) {
+        return exchangeManager.saveExchange(exchange);
+    }
+
+    @Override
+    public boolean deleteExchange(String exchangeName) {
+        return exchangeManager.deleteExchange(exchangeName);
+    }
+
+
+    @Override
+    public List<ExchangeType> getAllExchanges() {
+        return exchangeManager.getAllExchanges();
+    }
+
+
+    @Override
+    public List<OrganizationType> getAllOrganizations(String exchangeName) {
+        return exchangeManager.getAllOrganizationsBy(exchangeName);
+    }
+
+
+    @Override
+    public List<ConnectionEndpoint> getAllConnectionEndpoints(String exchangeName, String hcid) {
+        List<EndpointType> orgEndpoints = exchangeManager.getAllEndpointTypesBy(exchangeName, hcid);
+        List<ConnectionEndpoint> endpoints = new ArrayList<>();
+
+        for (EndpointType endpoint : orgEndpoints) {
+            List<EndpointConfigurationType> epConfigurations = getEndpointConfigurationTypeBy(endpoint);
+            for (EndpointConfigurationType epConf : epConfigurations) {
+
+                String timestamp = null;
+                String status = "None";
+                String url = epConf.getUrl();
+                EndpointManagerCache.EndpointCacheInfo info = EndpointManagerCache.getInstance().getEndpointInfo(url);
+
+                if (info != null) {
+                    timestamp = DateUtils.formatDate(info.getTimestamp(), DATE_FORMAT);
+                    status = info.isSuccessfulPing() ? "Pass" : "Fail";
+                }
+
+                endpoints.add(
+                    new ConnectionEndpoint(endpoint.getName().get(0), url, epConf.getVersion(), status, timestamp));
+            }
+        }
+        return endpoints;
+    }
+
+    @Override
+    public ExchangeInfoType getExchangeInfoView() {
+        return exchangeManager.getExchangeInfoView();
+    }
+
+    @Override
+    public boolean saveGeneralSetting(ExchangeInfoType exchangeInfo) {
+        return exchangeManager.updateExchangeInfo(exchangeInfo.getRefreshInterval(),
+            exchangeInfo.getMaxNumberOfBackups(), exchangeInfo.getDefaultExchange());
+    }
+
+    @Override
+    public boolean refreshExchangeManager() {
+        LOG.info("force the exchangeManager to refresh: {}", TO_DO_MARKER);
+        throw new UnsupportedOperationException("waiting for implementation refresh-exchangeManger");
+    }
+
+    @Override
+    public boolean pingService(ConnectionEndpoint connEndpoint) {
+        if (null != connEndpoint) {
+            boolean status = pingService.ping(connEndpoint.getServiceUrl());
+            connEndpoint.setPing(status ? "Pass" : "Fail");
+            connEndpoint.setPingTimestamp(HelperUtil.getDateNow(DATE_FORMAT));
+            EndpointManagerCache.getInstance().addOrUpdateEndpoint(connEndpoint.getServiceUrl(), new Date(), status);
+            return true;
+        }
+        return false;
+    }
+
+}
--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchangemgr/ExchangeManager.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchangemgr/ExchangeManager.java
@@ -35,6 +35,8 @@ import gov.hhs.fha.nhinc.exchange.directory.OrganizationType;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants.UDDI_SPEC_VERSION;
 import gov.hhs.fha.nhinc.util.HomeCommunityMap;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,6 +52,7 @@ import org.slf4j.LoggerFactory;
 public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExchangeManager.class);
+    private ExchangeInfoType exInfo = null;
     private Map<String, Map<String, OrganizationType>> exCache = new HashMap<>();
     private boolean exCacheLoaded = false;
     private long exFileLastUpdateTime;
@@ -74,16 +77,16 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
     }
 
     private void loadExchangeInfo() throws ExchangeManagerException {
-        ExchangeInfoType exInfo = getExchangeInfoDAO().loadExchangeInfo();
+        exInfo = getExchangeInfoDAO().loadExchangeInfo();
 
         if (exInfo != null) {
             synchronized (exCache) {
                 exCache.clear();
                 if (null != exInfo.getExchanges() && CollectionUtils.isNotEmpty(exInfo.getExchanges().getExchange())) {
                     for (ExchangeType ex : exInfo.getExchanges().getExchange()) {
-                        if (null != ex.getOrganizationList() && CollectionUtils.isNotEmpty(ex.getOrganizationList().
-                            getOrganization()) && StringUtils.isNotEmpty(ex.getType()) && StringUtils.isNotEmpty(ex.
-                            getName())) {
+                        if (null != ex.getOrganizationList()
+                            && CollectionUtils.isNotEmpty(ex.getOrganizationList().getOrganization())
+                            && StringUtils.isNotEmpty(ex.getType()) && StringUtils.isNotEmpty(ex.getName())) {
                             Map<String, OrganizationType> innerMap = new HashMap<>();
                             for (OrganizationType org : ex.getOrganizationList().getOrganization()) {
                                 innerMap.put(org.getHcid(), org);
@@ -128,8 +131,7 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
     }
 
     @Override
-    public OrganizationType getOrganization(String exchangeName, String hcid) throws
-        ExchangeManagerException {
+    public OrganizationType getOrganization(String exchangeName, String hcid) throws ExchangeManagerException {
         refreshExchangeCacheIfRequired();
         String hcidWithoutPrefix = HomeCommunityMap.formatHomeCommunityId(hcid);
         String hcidWithPrefix = HomeCommunityMap.getHomeCommunityIdWithPrefix(hcid);
@@ -154,8 +156,8 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
     }
 
     @Override
-    public String getEndpointURL(String hcid, String sServiceName,
-        UDDI_SPEC_VERSION api_spec) throws ExchangeManagerException {
+    public String getEndpointURL(String hcid, String sServiceName, UDDI_SPEC_VERSION api_spec)
+        throws ExchangeManagerException {
         OrganizationType org = getOrganization(hcid);
         EndpointType epType = ExchangeManagerHelper.getServiceEndpointType(org, sServiceName);
         if (null == epType) {
@@ -183,5 +185,128 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
     public List<OrganizationType> getAllOrganizations() throws ExchangeManagerException {
         refreshExchangeCacheIfRequired();
         return ExchangeManagerHelper.getOrganizationTypeAllByCache(exCache);
+    }
+
+    private void saveExchangeInfo() throws ExchangeManagerException {
+        getExchangeInfoDAO().saveExchangeInfo(exInfo);
+    }
+
+    public boolean updateExchangeInfo(long refreshInterval, BigInteger maxBackups, String defaultExchange) {
+        boolean bSave = false;
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            exInfo.setRefreshInterval(refreshInterval);
+            exInfo.setMaxNumberOfBackups(maxBackups);
+            exInfo.setDefaultExchange(defaultExchange);
+            saveExchangeInfo();
+            bSave = true;
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to update-exchangeInfo: {}", e.getLocalizedMessage(), e);
+        }
+        return bSave;
+    }
+
+    public boolean deleteExchange(String exchangeName) {
+        boolean bSave = false;
+        if (StringUtils.isBlank(exchangeName)) {
+            return bSave;
+        }
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            List<ExchangeType> exchanges = getAllExchanges();
+            ExchangeType exchangeFound = ExchangeManagerHelper.findExchangeTypeBy(exchanges, exchangeName);
+            if (null != exchangeFound) {
+                exchanges.remove(exchangeFound);
+            }
+            saveExchangeInfo();
+            bSave = true;
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to delete-exchange: {}", e.getLocalizedMessage(), e);
+        }
+        return bSave;
+    }
+
+    public boolean saveExchange(ExchangeType exchangeAdd) {
+        boolean bSave = false;
+        if (null == exchangeAdd) {
+            return bSave;
+        }
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            List<ExchangeType> exchanges = ExchangeManagerHelper.getExchangeTypeBy(exInfo, true);
+            ExchangeType exchangeFound = ExchangeManagerHelper.findExchangeTypeBy(exchanges, exchangeAdd.getName());
+            if (null != exchangeFound) {
+                exchanges.remove(exchangeFound);
+            }
+            exchanges.add(exchangeAdd);
+            saveExchangeInfo();
+            bSave = true;
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to save-exchangeType: {}", e.getLocalizedMessage(), e);
+        }
+        return bSave;
+    }
+
+    public List<ExchangeType> getAllExchanges() {
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            return ExchangeManagerHelper.getExchangeTypeBy(exInfo);
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to get-all-exchanges: {}", e.getLocalizedMessage(), e);
+            return new ArrayList<>();
+        }
+    }
+
+    public List<OrganizationType> getAllOrganizationsBy(String exchangeName) {
+        List<OrganizationType> emptyList = new ArrayList<>();
+        if (StringUtils.isBlank(exchangeName)) {
+            return emptyList;
+        }
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            return ExchangeManagerHelper.getOrganizationTypeBy(exInfo, exchangeName);
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to get-all-organizationsBy: {}", e.getLocalizedMessage(), e);
+            return emptyList;
+        }
+    }
+
+    public List<EndpointType> getAllEndpointTypesBy(String exchangeName, String hcid) {
+        List<EndpointType> emptyList = new ArrayList<>();
+        if (StringUtils.isBlank(hcid)) {
+            return emptyList;
+        }
+
+        if (StringUtils.isBlank(exchangeName)) {
+            return emptyList;
+        }
+
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            return ExchangeManagerHelper.getEndpointTypeBy(exInfo, exchangeName, hcid);
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to get-all-enpointTypesBy: {}", e.getLocalizedMessage(), e);
+            return emptyList;
+        }
+    }
+
+    public ExchangeInfoType getExchangeInfoView() {
+        ExchangeInfoType view = new ExchangeInfoType();
+        view.setRefreshInterval(exInfo.getRefreshInterval());
+        view.setMaxNumberOfBackups(exInfo.getMaxNumberOfBackups());
+        view.setDefaultExchange(exInfo.getDefaultExchange());
+        return view;
     }
 }
