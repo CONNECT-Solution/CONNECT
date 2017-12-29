a57d0c1a7fe03dbafb09ca0996a9a6fb010a0a70
trantang <ttang@amdexcorp.com>
2017-12-29 16:40:11 -0500

    PR#1563 agree overwrite exchange

---
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/validator/CheckboxRequiredTrueValidator.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2009-2017, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.admingui.validator;
+
+import gov.hhs.fha.nhinc.admingui.util.HelperUtil;
+import java.text.MessageFormat;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Tran Tang
+ *
+ */
+@FacesValidator("CheckboxRequiredTrueValidator")
+public class CheckboxRequiredTrueValidator implements Validator {
+    private static final Logger LOG = LoggerFactory.getLogger(CheckboxRequiredTrueValidator.class);
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) {
+
+        if (!Boolean.TRUE.equals(value)) {
+            String errorMessage = (String) component.getAttributes().get("errorMessage");
+
+            if (StringUtils.isBlank(errorMessage)) {
+                errorMessage = ((UIInput) component).getRequiredMessage();
+            }
+
+            if (StringUtils.isBlank(errorMessage)) {
+                errorMessage = MessageFormat.format("'{0}' is required.", component.getId());
+            }
+
+            throw new ValidatorException(HelperUtil.getMsgError(errorMessage));
+        }
+
+    }
+}
