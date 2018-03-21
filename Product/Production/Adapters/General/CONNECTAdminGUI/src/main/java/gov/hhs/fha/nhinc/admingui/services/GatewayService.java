4e5dc68bc75a71532b4901de0a11b137d6f87ff1
tabassum_jafri <tabassum.jafri@cgifederal.com>
2018-03-21 09:31:40 -0400

    Fixed the display for Document search in CrossGateway Query Client

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/services/GatewayService.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/services/GatewayService.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,8 +25,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package gov.hhs.fha.nhinc.admingui.services;
-
-import static gov.hhs.fha.nhinc.util.StreamUtils.closeStreamSilently;
 
 import gov.hhs.fha.nhinc.admingui.managed.PatientSearchBean;
 import gov.hhs.fha.nhinc.admingui.model.Document;
@@ -44,6 +42,7 @@ import gov.hhs.fha.nhinc.docquery.model.builder.impl.DocumentMetadataResultsMode
 import gov.hhs.fha.nhinc.docretrieve.model.DocumentRetrieve;
 import gov.hhs.fha.nhinc.docretrieve.model.DocumentRetrieveResults;
 import gov.hhs.fha.nhinc.patientdiscovery.model.PatientSearchResults;
+import static gov.hhs.fha.nhinc.util.StreamUtils.closeStreamSilently;
 import gov.hhs.fha.nhinc.util.format.UTCDateUtil;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -92,6 +91,7 @@ public class GatewayService {
     }
 
     private static class SingletonHolder {
+
         private static final GatewayService INSTANCE = new GatewayService();
     }
 
@@ -109,7 +109,8 @@ public class GatewayService {
     public boolean discoverPatient(final PatientSearchBean patientQuerySearch) {
 
         // Create the patient bean that needs to be passed to the service layer
-        final gov.hhs.fha.nhinc.patientdiscovery.model.Patient patientBean = new gov.hhs.fha.nhinc.patientdiscovery.model.Patient();
+        final gov.hhs.fha.nhinc.patientdiscovery.model.Patient patientBean
+            = new gov.hhs.fha.nhinc.patientdiscovery.model.Patient();
         // set the orgainization
         patientBean.setOrganization(patientQuerySearch.getOrganization());
         // set birth date in YYYYMMDD
@@ -228,7 +229,7 @@ public class GatewayService {
 
     /**
      * Internal method to populate the patient data to the Patient bean used in the UI.
-     *
+     * <p>
      */
     private void populatePatientBean(final PatientSearchResults patientQueryResults,
         final PatientSearchBean patientQuerySearch) {
@@ -269,7 +270,7 @@ public class GatewayService {
 
     /**
      * Internal method to populate the patient data to the Patient bean used in the UI.
-     *
+     * <p>
      */
     private void populatePatientBeanWithDQResults(final DocumentMetadataResults DocumentQueryResults,
         final PatientSearchBean patientQuerySearch) {
@@ -295,9 +296,9 @@ public class GatewayService {
 
             // populate document type name from the static list
             // this logic needs to be revisited after the demo
-            if (patientDocument.getDocumentType() != null) {
+            if (patientDocument.getDocumentClassCode() != null) {
                 patientDocument.setDocumentTypeName(
-                    patientQuerySearch.getDocumentTypeNameFromTheStaticList(patientDocument.getDocumentType()));
+                    patientQuerySearch.getDocumentTypeNameFromTheStaticList(patientDocument.getDocumentClassCode()));
             }
             // for the demo set the value from the patient
             patientDocument.setSourcePatientId(patientQuerySearch.getSelectedCurrentPatient().getPatientId());
