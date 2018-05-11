05848664961e88a0d64bc1db653e450ad2bd7204
plobre <plobre@hotmail.com>
2018-05-11 15:28:57 -0400

    CONN_2161-Threads: Removing raw threads and switch to using Spring's Scheduler beans instead

--- Product/Production/Adapters/General/CONNECTAdapterWeb/src/main/java/gov/hhs/fha/nhinc/adapter/deferred/queue/DeferredQueueManager.java
+++ Product/Production/Adapters/General/CONNECTAdapterWeb/src/main/java/gov/hhs/fha/nhinc/adapter/deferred/queue/DeferredQueueManager.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -53,13 +53,11 @@ public class DeferredQueueManager implements gov.hhs.fha.nhinc.deferredqueuemana
      */
     public DeferredQueueManager() {
 
-        try {
-            DeferredQueueTimer timer = DeferredQueueTimer.getInstance();
-            timer.startTimer();
-        } catch (Exception e) {
-            String sErrorMessage = "Failed to start DeferredQueueManager's timer.  Error: " + e.getMessage();
-            LOG.error(sErrorMessage, e);
-        }
+        /*
+         * try { DeferredQueueTimer timer = DeferredQueueTimer.getInstance(); timer.startTimer(); } catch (Exception e)
+         * { String sErrorMessage = "Failed to start DeferredQueueManager's timer.  Error: " + e.getMessage();
+         * LOG.error(sErrorMessage, e); }
+         */
     }
 
     /**
@@ -70,9 +68,9 @@ public class DeferredQueueManager implements gov.hhs.fha.nhinc.deferredqueuemana
      */
     @Override
     public DeferredQueueManagerForceProcessResponseType forceProcessOnDeferredQueue(
-            DeferredQueueManagerForceProcessRequestType deferredQueueManagerForceProcessRequest) {
+        DeferredQueueManagerForceProcessRequestType deferredQueueManagerForceProcessRequest) {
         return new DeferredQueueManagerHelper().forceProcessOnDeferredQueue(deferredQueueManagerForceProcessRequest,
-                context);
+            context);
     }
 
     /**
@@ -83,9 +81,9 @@ public class DeferredQueueManager implements gov.hhs.fha.nhinc.deferredqueuemana
      */
     @Override
     public DeferredQueueManagerForceProcessResponseType forceProcessOnDeferredRequest(
-            DeferredQueueManagerForceProcessRequestType deferredQueueManagerForceProcessRequest) {
+        DeferredQueueManagerForceProcessRequestType deferredQueueManagerForceProcessRequest) {
         return new DeferredQueueManagerHelper().forceProcessOnDeferredQueue(deferredQueueManagerForceProcessRequest,
-                context);
+            context);
     }
 
     /**
@@ -107,7 +105,7 @@ public class DeferredQueueManager implements gov.hhs.fha.nhinc.deferredqueuemana
      */
     @Override
     public RetrieveDeferredQueueResponseType retrieveDeferredQueue(
-            RetrieveDeferredQueueRequestType retrieveDeferredQueueRequest) {
+        RetrieveDeferredQueueRequestType retrieveDeferredQueueRequest) {
         return new DeferredQueueManagerHelper().retrieveDeferredQueue(retrieveDeferredQueueRequest, context);
     }
 
@@ -119,7 +117,7 @@ public class DeferredQueueManager implements gov.hhs.fha.nhinc.deferredqueuemana
      */
     @Override
     public DeferredQueueStatisticsResponseType deferredQueueStatistics(
-            DeferredQueueStatisticsRequestType deferredQueueStatisticsRequest) {
+        DeferredQueueStatisticsRequestType deferredQueueStatisticsRequest) {
         return new DeferredQueueManagerHelper().deferredQueueStatistics(deferredQueueStatisticsRequest, context);
     }
 
---
+++ Product/Production/Services/ConnectionManagerCore/src/main/java/gov/hhs/fha/nhinc/exchangemgr/ExchangeSchedulerConfig.java
@@ -0,0 +1,60 @@
+/**
+ *
+ */
+package gov.hhs.fha.nhinc.exchangemgr;
+
+import gov.hhs.fha.nhinc.connectmgr.persistance.dao.ExchangeInfoDAOFileImpl;
+import gov.hhs.fha.nhinc.exchange.ExchangeInfoType;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.PeriodicTrigger;
+
+@EnableScheduling
+@Configuration
+@ComponentScan(basePackages = { "gov.hhs.fha.nhinc" })
+public class ExchangeSchedulerConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExchangeSchedulerConfig.class);
+    private static final int DEFAULT_EXCHANGE_REFRESH_INTERVAL = 1440; // a day in minutes
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(1);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+
+
+    @Bean
+    @SuppressWarnings("unchecked")
+    public ScheduledFuture<ExchangeScheduler> deferredTaskManager(ThreadPoolTaskScheduler scheduler) {
+        long intervalMinutes = DEFAULT_EXCHANGE_REFRESH_INTERVAL;
+        try {
+            ExchangeInfoType exInfo = ExchangeInfoDAOFileImpl.getInstance().loadExchangeInfo();
+            long interval = exInfo.getRefreshInterval();
+            if (interval <= 0) {
+                intervalMinutes = DEFAULT_EXCHANGE_REFRESH_INTERVAL;
+            } else {
+                intervalMinutes = interval;
+            }
+        } catch (ExchangeManagerException e) {
+            LOG.error("Could not set interval rate. Defaulting to {} seconds by default. Error is : {}",
+                DEFAULT_EXCHANGE_REFRESH_INTERVAL, e.getMessage());
+        }
+
+        PeriodicTrigger periodicTrigger = new PeriodicTrigger(intervalMinutes, TimeUnit.MINUTES);
+        periodicTrigger.setInitialDelay(intervalMinutes);
+
+        return (ScheduledFuture<ExchangeScheduler>) scheduler.schedule(new ExchangeScheduler(),
+            periodicTrigger);
+
+    }
+}
