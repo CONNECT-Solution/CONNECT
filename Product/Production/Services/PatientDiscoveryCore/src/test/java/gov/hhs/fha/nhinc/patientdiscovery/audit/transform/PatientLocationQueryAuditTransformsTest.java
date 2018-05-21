d19311030acc6808ba9833e0853f13e604a4afe8
trantang <ttang@amdexcorp.com>
2018-05-21 15:38:32 -0400

    CONN-2177 audit-tranformer PLQ/RLS

---
+++ Product/Production/Services/PatientDiscoveryCore/src/test/java/gov/hhs/fha/nhinc/patientdiscovery/audit/transform/PatientLocationQueryAuditTransformsTest.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gov.hhs.fha.nhinc.patientdiscovery.audit.transform;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import com.services.nhinc.schema.auditmessage.ParticipantObjectIdentificationType;
+import gov.hhs.fha.nhinc.audit.AuditTransformsConstants;
+import gov.hhs.fha.nhinc.audit.transform.AuditTransforms;
+import gov.hhs.fha.nhinc.audit.transform.AuditTransformsTest;
+import gov.hhs.fha.nhinc.common.auditlog.LogEventRequestType;
+import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
+import gov.hhs.fha.nhinc.common.nhinccommon.NhinTargetSystemType;
+import gov.hhs.fha.nhinc.connectmgr.ConnectionManagerException;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import gov.hhs.fha.nhinc.patientdiscovery.audit.PatientLocationQueryAuditTransformsConstants;
+import gov.hhs.fha.nhinc.patientdiscovery.parser.TestPatientDiscoveryMessageHelper;
+import ihe.iti.xcpd._2009.PatientLocationQueryRequestType;
+import ihe.iti.xcpd._2009.PatientLocationQueryResponseType;
+import java.net.UnknownHostException;
+import java.util.Properties;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author achidamb
+ */
+public class PatientLocationQueryAuditTransformsTest
+extends AuditTransformsTest<PatientLocationQueryRequestType, PatientLocationQueryResponseType> {
+    // NhincConstants--PatientLocationQuery
+    public static final String PATIENT_LOCATION_QUERY_SERVICE_NAME = "PatientLocationQuery";
+
+    public PatientLocationQueryAuditTransformsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    @Override
+    public void setUp() {
+    }
+
+    @After
+    @Override
+    public void tearDown() {
+    }
+
+    @Test
+    public void transformRequestToAuditMsg() throws ConnectionManagerException, UnknownHostException {
+        final String localIp = "10.10.10.10";
+        final String remoteIp = "16.14.13.12";
+        final String remoteObjectUrl = "http://" + remoteIp + ":9090/source/AuditService";
+        final String wsRequestUrl = "http://" + remoteIp + ":9090/AuditService";
+
+        Properties webContextProperties = new Properties();
+        webContextProperties.setProperty(NhincConstants.WEB_SERVICE_REQUEST_URL, wsRequestUrl);
+        webContextProperties.setProperty(NhincConstants.REMOTE_HOST_ADDRESS, remoteIp);
+
+        PatientLocationQueryAuditTransforms transforms = new PatientLocationQueryAuditTransforms() {
+            @Override
+            protected String getLocalHostAddress() {
+                return localIp;
+            }
+
+            @Override
+            protected String getRemoteHostAddress(Properties webContextProperties) {
+                if (webContextProperties != null && !webContextProperties.isEmpty()
+                    && webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
+
+                    return webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS);
+                }
+                return AuditTransformsConstants.ACTIVE_PARTICIPANT_UNKNOWN_IP_ADDRESS;
+            }
+
+            @Override
+            protected String getWebServiceUrlFromRemoteObject(NhinTargetSystemType target, String serviceName) {
+                return remoteObjectUrl;
+            }
+        };
+
+        AssertionType assertion = createAssertion();
+        LogEventRequestType auditRequest = transforms.transformRequestToAuditMsg(
+            TestPatientDiscoveryMessageHelper.createPatientLocationQueryRequestType("D123401", "1.1"),
+            assertion, createNhinTarget(), NhincConstants.AUDIT_LOG_INBOUND_DIRECTION,
+            NhincConstants.AUDIT_LOG_NHIN_INTERFACE, Boolean.TRUE, webContextProperties,
+            PATIENT_LOCATION_QUERY_SERVICE_NAME);
+
+        testGetEventIdentificationType(auditRequest, PATIENT_LOCATION_QUERY_SERVICE_NAME, Boolean.TRUE);
+        testGetActiveParticipantSource(auditRequest, Boolean.TRUE, webContextProperties, localIp);
+        testGetActiveParticipantDestination(auditRequest, Boolean.TRUE, webContextProperties, remoteObjectUrl);
+        testAuditSourceIdentification(auditRequest.getAuditMessage().getAuditSourceIdentification(), assertion);
+        testCreateActiveParticipantFromUser(auditRequest, Boolean.TRUE, assertion);
+        assertParticipantObjectIdentification(auditRequest);
+        assertEquals("AuditMessage.Request ServiceName mismatch", auditRequest.getEventType(),
+            PATIENT_LOCATION_QUERY_SERVICE_NAME);
+    }
+
+    @Test
+    public void transformResponseToAuditMsg() throws ConnectionManagerException, UnknownHostException {
+        final String localIp = "10.10.10.10";
+        final String remoteIp = "16.14.13.12";
+        final String remoteObjectUrl = "http://" + remoteIp + ":9090/source/AuditService";
+        final String wsRequestUrl = "http://" + remoteIp + ":9090/AuditService";
+
+        Properties webContextProperties = new Properties();
+        webContextProperties.setProperty(NhincConstants.WEB_SERVICE_REQUEST_URL, wsRequestUrl);
+        webContextProperties.setProperty(NhincConstants.REMOTE_HOST_ADDRESS, remoteIp);
+
+        PatientLocationQueryAuditTransforms transforms = new PatientLocationQueryAuditTransforms() {
+            @Override
+            protected String getLocalHostAddress() {
+                return localIp;
+            }
+
+            @Override
+            protected String getRemoteHostAddress(Properties webContextProperties) {
+                if (webContextProperties != null && !webContextProperties.isEmpty()
+                    && webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
+
+                    return webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS);
+                }
+                return AuditTransformsConstants.ACTIVE_PARTICIPANT_UNKNOWN_IP_ADDRESS;
+            }
+
+            @Override
+            protected String getWebServiceUrlFromRemoteObject(NhinTargetSystemType target, String serviceName) {
+                return remoteObjectUrl;
+            }
+        };
+
+        AssertionType assertion = createAssertion();
+        LogEventRequestType auditResponse = transforms.transformResponseToAuditMsg(
+            TestPatientDiscoveryMessageHelper.createPatientLocationQueryRequestType("D1234013", "2.2"),
+            TestPatientDiscoveryMessageHelper.createPatientLocationQueryResponseType("D1234013", "2.2", "1.1",
+                "D123401", "1.1"),
+            assertion, createNhinTarget(), NhincConstants.AUDIT_LOG_INBOUND_DIRECTION,
+            NhincConstants.AUDIT_LOG_NHIN_INTERFACE, Boolean.FALSE, webContextProperties,
+            PATIENT_LOCATION_QUERY_SERVICE_NAME);
+
+        testGetEventIdentificationType(auditResponse, PATIENT_LOCATION_QUERY_SERVICE_NAME, Boolean.TRUE);
+        testGetActiveParticipantSource(auditResponse, Boolean.FALSE, webContextProperties, localIp);
+        testGetActiveParticipantDestination(auditResponse, Boolean.FALSE, webContextProperties, remoteObjectUrl);
+        testCreateActiveParticipantFromUser(auditResponse, Boolean.FALSE, assertion);
+        testAuditSourceIdentification(auditResponse.getAuditMessage().getAuditSourceIdentification(), assertion);
+        assertParticipantObjectIdentification(auditResponse);
+        assertEquals("AuditMessage.Response ServiceName mismatch", auditResponse.getEventType(),
+            PATIENT_LOCATION_QUERY_SERVICE_NAME);
+    }
+
+
+    private void assertParticipantObjectIdentification(LogEventRequestType auditRequest) {
+        assertNotNull("auditRequest is null", auditRequest);
+        assertNotNull("AuditMessage is null", auditRequest.getAuditMessage());
+        assertNotNull("ParticipantObjectIdentification is null",
+            auditRequest.getAuditMessage().getParticipantObjectIdentification());
+
+        ParticipantObjectIdentificationType participantPatient = auditRequest.getAuditMessage()
+            .getParticipantObjectIdentification().get(0);
+
+        assertNotNull("participantPatient is null", participantPatient);
+
+        assertEquals("ParticipantPatient.ParticipantObjectID mismatch", "D123401^^^&1.1&ISO",
+            participantPatient.getParticipantObjectID());
+
+        // TODO: assertSame vs assertEquals consistency when returning constants
+        assertSame("ParticipantPatient.ParticipantObjectTypeCode object reference mismatch",
+            PatientLocationQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_TYPE_CODE_SYSTEM,
+            participantPatient.getParticipantObjectTypeCode());
+        assertSame("ParticipantPatient.ParticipantObjectTypeCodeRole object reference mismatch",
+            PatientLocationQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_TYPE_CODE_ROLE,
+            participantPatient.getParticipantObjectTypeCodeRole());
+        assertEquals("ParticipantPatient.ParticipantObjectIDTypeCode.Code mismatch",
+            PatientLocationQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_ID_TYPE_CODE,
+            participantPatient.getParticipantObjectIDTypeCode().getCode());
+        assertEquals("ParticipantPatient.ParticipantObjectIDTypeCode.CodeSystemName mismatch",
+            PatientLocationQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_ID_TYPE_CODE_SYSTEM,
+            participantPatient.getParticipantObjectIDTypeCode().getCodeSystemName());
+        assertEquals("ParticipantPatient.ParticipantObjectIDTypeCode.DisplayName mismatch",
+            PatientLocationQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_ID_TYPE_DISPLAY_NAME,
+            participantPatient.getParticipantObjectIDTypeCode().getDisplayName());
+
+        assertNull("ParticipantQuery.ParticipantObjectName is not null", participantPatient.getParticipantObjectName());
+
+    }
+
+    @Override
+    protected AuditTransforms<PatientLocationQueryRequestType, PatientLocationQueryResponseType> getAuditTransforms() {
+        return new PatientLocationQueryAuditTransforms();
+    }
+}
+
