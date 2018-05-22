e101692ee689b9e267972f41276d24bd0d622430
tabassum_jafri <tabassum.jafri@cgifederal.com>
2018-05-22 15:21:29 -0400

    added overrides exchange type to exchange manager

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchangemgr/ExchangeManager.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchangemgr/ExchangeManager.java
@@ -29,10 +29,12 @@ package gov.hhs.fha.nhinc.exchangemgr;
 import gov.hhs.fha.nhinc.connectmgr.persistance.dao.ExchangeInfoDAOFileImpl;
 import gov.hhs.fha.nhinc.exchange.ExchangeInfoType;
 import gov.hhs.fha.nhinc.exchange.ExchangeType;
+import gov.hhs.fha.nhinc.exchange.directory.EndpointConfigurationListType;
 import gov.hhs.fha.nhinc.exchange.directory.EndpointConfigurationType;
 import gov.hhs.fha.nhinc.exchange.directory.EndpointType;
 import gov.hhs.fha.nhinc.exchange.directory.OrganizationType;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
+import gov.hhs.fha.nhinc.nhinclib.NhincConstants.EXCHANGE_TYPE;
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants.UDDI_SPEC_VERSION;
 import gov.hhs.fha.nhinc.util.HomeCommunityMap;
 import java.math.BigInteger;
@@ -55,6 +57,7 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
     private static final Logger LOG = LoggerFactory.getLogger(ExchangeManager.class);
     private ExchangeInfoType exInfo = null;
     private Map<String, Map<String, OrganizationType>> exCache = new HashMap<>();
+    private ExchangeType overrideExchange;
     private boolean exCacheLoaded = false;
     private long exFileLastUpdateTime;
     private static final ExchangeManager INSTANCE = new ExchangeManager();
@@ -84,14 +87,10 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
                 exCache.clear();
                 if (null != exInfo.getExchanges() && CollectionUtils.isNotEmpty(exInfo.getExchanges().getExchange())) {
                     for (ExchangeType ex : exInfo.getExchanges().getExchange()) {
-                        if (null != ex.getOrganizationList()
-                            && CollectionUtils.isNotEmpty(ex.getOrganizationList().getOrganization())
-                            && StringUtils.isNotEmpty(ex.getType()) && StringUtils.isNotEmpty(ex.getName())) {
-                            Map<String, OrganizationType> innerMap = new HashMap<>();
-                            for (OrganizationType org : ex.getOrganizationList().getOrganization()) {
-                                innerMap.put(org.getHcid(), org);
-                            }
-                            exCache.put(ex.getName(), innerMap);
+                        if (isOverrideExchange(ex)) {
+                            overrideExchange = ex;
+                        } else {
+                            updateExchangeCache(ex);
                         }
                     }
                     exCacheLoaded = true;
@@ -131,27 +130,8 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
 
     @Override
     public OrganizationType getOrganization(String exchangeName, String hcid) throws ExchangeManagerException {
-        refreshExchangeCacheIfRequired();
-        String hcidWithoutPrefix = HomeCommunityMap.formatHomeCommunityId(hcid);
-        String hcidWithPrefix = HomeCommunityMap.getHomeCommunityIdWithPrefix(hcid);
-        if (StringUtils.isEmpty(hcidWithoutPrefix) || StringUtils.isEmpty(hcidWithPrefix)) {
-            return null;
-        }
-        Map<String, OrganizationType> map = null;
-        if (StringUtils.isNotEmpty(exchangeName)) {
-            map = exCache.get(exchangeName);
-        } else if (StringUtils.isNotEmpty(getDefaultExchange())) {
-            map = exCache.get(getDefaultExchange());
-        } else {
-            if (null != getCache().values()) {
-                map = extractHcidOrganizationMap();
-            }
-        }
-        if (null != map) {
-            return map.get(hcidWithPrefix) != null ? map.get(hcidWithPrefix) : map.get(hcidWithoutPrefix);
-        } else {
-            return null;
-        }
+        OrganizationType org = retrieveOrganizationFromCache(exchangeName, hcid);
+        return syncWithOverrides(org, hcid);
     }
 
     @Override
@@ -248,7 +228,7 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
                 exchangeFound.setTLSVersions(exchangeUpdate.getTLSVersions());
                 exchangeFound.setType(exchangeUpdate.getType());
                 exchangeFound.setUrl(exchangeUpdate.getUrl());
-            }else{
+            } else {
                 LOG.info("saveExchange--adding-exchangeUpdate");
                 exchanges.add(exchangeUpdate);
             }
@@ -325,5 +305,197 @@ public class ExchangeManager extends AbstractExchangeManager<UDDI_SPEC_VERSION> 
 
     public boolean isRefreshLocked() {
         return getExchangeInfoDAO().isRefreshLocked();
+    }
+
+    public boolean toggleIsDisabledFor(String forExchangeName) throws ExchangeManagerException {
+        boolean bSave = false;
+        getRefreshExceptionFor("toggle-disable");
+        if (StringUtils.isBlank(forExchangeName)) {
+            return bSave;
+        }
+        try {
+            if (null == exInfo) {
+                loadExchangeInfo();
+            }
+            List<ExchangeType> exchanges = ExchangeManagerHelper.getExchangeTypeBy(exInfo, true);
+            ExchangeType exchangeFound = ExchangeManagerHelper.findExchangeTypeBy(exchanges, forExchangeName);
+            if (null != exchangeFound) {
+                if (exchangeFound.isDisabled()) {
+                    exchangeFound.setDisabled(false);
+                } else {
+                    exchangeFound.setDisabled(true);
+                }
+            }
+            saveExchangeInfo();
+            bSave = true;
+        } catch (ExchangeManagerException e) {
+            LOG.error("unable to toggle-exchange-isDisable: {}", e.getLocalizedMessage(), e);
+        }
+        return bSave;
+    }
+
+    private static boolean isOverrideExchange(ExchangeType exchange) {
+        return null != exchange.getOrganizationList()
+            && CollectionUtils.isNotEmpty(exchange.getOrganizationList().getOrganization())
+            && StringUtils.isNotEmpty(exchange.getType()) && StringUtils.equalsIgnoreCase(
+            EXCHANGE_TYPE.OVERRIDES.toString(), exchange.getType());
+    }
+
+    private void updateExchangeCache(ExchangeType ex) {
+        if (null != ex.getOrganizationList()
+            && CollectionUtils.isNotEmpty(ex.getOrganizationList().getOrganization())
+            && StringUtils.isNotEmpty(ex.getType()) && StringUtils.isNotEmpty(ex.getName())) {
+            Map<String, OrganizationType> innerMap = new HashMap<>();
+            for (OrganizationType org : ex.getOrganizationList().getOrganization()) {
+                innerMap.put(org.getHcid(), org);
+            }
+            exCache.put(ex.getName(), innerMap);
+        }
+    }
+
+    private OrganizationType lookupOrganizationInOverrideExchange(String hcid) {
+        if (null != overrideExchange && null != overrideExchange.getOrganizationList() && CollectionUtils.isNotEmpty(
+            overrideExchange.getOrganizationList().getOrganization())) {
+            for (OrganizationType org : overrideExchange.getOrganizationList().getOrganization()) {
+                if (null != org.getHcid() && verifyHcid(org.getHcid(), hcid)) {
+                    return org;
+                }
+            }
+        }
+        return null;
+    }
+
+    private OrganizationType retrieveOrganizationFromCache(String exchangeName, String hcid) throws
+        ExchangeManagerException {
+        refreshExchangeCacheIfRequired();
+        String hcidWithoutPrefix = HomeCommunityMap.formatHomeCommunityId(hcid);
+        String hcidWithPrefix = HomeCommunityMap.getHomeCommunityIdWithPrefix(hcid);
+        if (StringUtils.isEmpty(hcidWithoutPrefix) || StringUtils.isEmpty(hcidWithPrefix)) {
+            return null;
+        }
+        Map<String, OrganizationType> map = null;
+        if (StringUtils.isNotEmpty(exchangeName)) {
+            map = exCache.get(exchangeName);
+        } else if (StringUtils.isNotEmpty(getDefaultExchange())) {
+            map = exCache.get(getDefaultExchange());
+        } else {
+            if (null != getCache().values()) {
+                map = extractHcidOrganizationMap();
+            }
+        }
+        if (null != map) {
+            return map.get(hcidWithPrefix) != null ? map.get(hcidWithPrefix) : map.get(hcidWithoutPrefix);
+        } else {
+            return null;
+        }
+    }
+
+    private OrganizationType syncWithOverrides(OrganizationType originalOrg, String hcid) {
+        OrganizationType overrideOrg = lookupOrganizationInOverrideExchange(hcid);
+        if (null == originalOrg && null == overrideOrg) {
+            return null;
+        }
+        //if overrides org does not have any endpoints, return the org without sync
+        if (null == overrideOrg || null == overrideOrg.getEndpointList() || CollectionUtils.
+            isEmpty(overrideOrg.getEndpointList().getEndpoint())) {
+            return originalOrg;
+        }
+        //if org does not have any endpoints, return the OverrrideOrg with out sync
+        if (null == originalOrg || null == originalOrg.getEndpointList() || CollectionUtils.isEmpty(originalOrg.
+            getEndpointList().getEndpoint())) {
+            return overrideOrg;
+        }
+        //synch up the Org with overrides.
+        return syncEndpoints(originalOrg, overrideOrg);
+    }
+
+    private static OrganizationType syncEndpoints(OrganizationType originalOrg, OrganizationType overrideOrg) {
+        Map<String, EndpointType> overrideEpMap = buildOverrideMap(overrideOrg.getEndpointList().getEndpoint());
+
+        for (EndpointType orEpType : originalOrg.getEndpointList().getEndpoint()) {
+            String serviceName = ExchangeManagerHelper.getNhinServiceName(orEpType.getName());
+            if (overrideEpMap.containsKey(serviceName)) {
+                EndpointType overrideEp = overrideEpMap.get(serviceName);
+                syncEndpointType(orEpType, overrideEp);
+                overrideEpMap.remove(serviceName);
+            }
+        }
+        if (!overrideEpMap.isEmpty()) {
+            for (Map.Entry<String, EndpointType> obj : overrideEpMap.entrySet()) {
+                originalOrg.getEndpointList().getEndpoint().add(overrideEpMap.get(obj.getKey()));
+            }
+        }
+        return originalOrg;
+    }
+
+    private static Map<String, EndpointType> buildOverrideMap(List<EndpointType> epOverrideList) {
+        Map<String, EndpointType> overrideEpMap = new HashMap<>();
+
+        for (EndpointType overriderEpType : epOverrideList) {
+            String serviceName = ExchangeManagerHelper.getNhinServiceName(overriderEpType.getName());
+            if (StringUtils.isNotEmpty(serviceName) && CollectionUtils.isNotEmpty(ExchangeManagerHelper.getSpecVersions(
+                overriderEpType))) {
+                overrideEpMap.put(serviceName, overriderEpType);
+            }
+        }
+        return overrideEpMap;
+    }
+
+    private static EndpointType syncEndpointType(EndpointType originalEpType, EndpointType overrideEpType) {
+        if (null == originalEpType || null == originalEpType.getEndpointConfigurationList()) {
+            return overrideEpType;
+        }
+        if (null == overrideEpType || null == overrideEpType.getEndpointConfigurationList()) {
+            return originalEpType;
+        }
+        EndpointConfigurationListType synchedEpListType = new EndpointConfigurationListType();
+        List<EndpointConfigurationType> synchedEpList = new ArrayList<>();
+
+        List<EndpointConfigurationType> overridesEpConfigList = overrideEpType.getEndpointConfigurationList().
+            getEndpointConfiguration();
+        List<EndpointConfigurationType> epConfigList = originalEpType.getEndpointConfigurationList().
+            getEndpointConfiguration();
+
+        if (CollectionUtils.isEmpty(epConfigList) && CollectionUtils.isNotEmpty(overridesEpConfigList)) {
+            synchedEpList.addAll(overridesEpConfigList);
+        } else if (CollectionUtils.isNotEmpty(epConfigList) && CollectionUtils.isEmpty(overridesEpConfigList)) {
+            synchedEpList.addAll(epConfigList);
+        } else {
+            synchedEpList.addAll(buildEndpointConfig(epConfigList, overridesEpConfigList));
+        }
+        synchedEpListType.getEndpointConfiguration().addAll(synchedEpList);
+        originalEpType.setEndpointConfigurationList(synchedEpListType);
+        return originalEpType;
+    }
+
+    private static List<EndpointConfigurationType> buildEndpointConfig(
+        List<EndpointConfigurationType> originalEpConfigList,
+        List<EndpointConfigurationType> overridesEpConfigList) {
+        List<EndpointConfigurationType> syncConfigList = new ArrayList<>();
+        Map<String, String> overrideMap = new HashMap<>();
+        for (EndpointConfigurationType config : originalEpConfigList) {
+            if (StringUtils.isNotBlank(config.getVersion())) {
+                overrideMap.put(config.getVersion(), config.getUrl());
+            }
+        }
+
+        for (EndpointConfigurationType config : overridesEpConfigList) {
+            overrideMap.put(config.getVersion(), config.getUrl());
+        }
+
+        for (Map.Entry<String, String> obj : overrideMap.entrySet()) {
+            EndpointConfigurationType epConfig = new EndpointConfigurationType();
+            epConfig.setVersion(obj.getKey());
+            epConfig.setUrl(overrideMap.get(obj.getKey()));
+            syncConfigList.add(epConfig);
+        }
+        return syncConfigList;
+    }
+
+    private static boolean verifyHcid(String overrideHcid, String requestHcid) {
+        String hcidWithoutPrefix = HomeCommunityMap.formatHomeCommunityId(requestHcid);
+        String hcidWithPrefix = HomeCommunityMap.getHomeCommunityIdWithPrefix(requestHcid);
+        return StringUtils.equalsIgnoreCase(hcidWithoutPrefix, overrideHcid) || StringUtils.endsWithIgnoreCase(
+            hcidWithPrefix, overrideHcid);
     }
 }
