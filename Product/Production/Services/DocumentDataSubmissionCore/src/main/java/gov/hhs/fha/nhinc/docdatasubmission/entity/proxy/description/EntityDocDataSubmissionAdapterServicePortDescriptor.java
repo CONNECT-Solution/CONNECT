bc03cb29b3857824e030dd8e6619f532bc7c2c1a
trantang <ttang@amdexcorp.com>
2018-06-01 09:42:39 -0400

    impl-adapter-portDescriptor

--- Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/entity/proxy/description/EntityDocDataSubmissionAdapterServicePortDescriptor.java
+++ Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/entity/proxy/description/EntityDocDataSubmissionAdapterServicePortDescriptor.java
@@ -1,10 +1,11 @@
 /**
- * 
+ *
  */
 package gov.hhs.fha.nhinc.docdatasubmission.entity.proxy.description;
 
 import gov.hhs.fha.nhinc.entityxds.EntityXDSPortType;
 import gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor;
+import javax.xml.ws.soap.SOAPBinding;
 
 /**
  * @author CONNECT New Staff
@@ -12,31 +13,19 @@ import gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor;
  */
 public class EntityDocDataSubmissionAdapterServicePortDescriptor implements ServicePortDescriptor<EntityXDSPortType> {
 
-    /* (non-Javadoc)
-     * @see gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor#getWSAddressingAction()
-     */
     @Override
     public String getWSAddressingAction() {
-        // TODO Auto-generated method stub
-        return null;
+        return "urn:gov:hhs:fha:nhinc:adapterxds:RegisterDocumentSet-b";
     }
 
-    /* (non-Javadoc)
-     * @see gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor#getPortClass()
-     */
     @Override
     public Class<EntityXDSPortType> getPortClass() {
-        // TODO Auto-generated method stub
-        return null;
+        return EntityXDSPortType.class;
     }
 
-    /* (non-Javadoc)
-     * @see gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor#getSOAPBindingVersion()
-     */
     @Override
     public String getSOAPBindingVersion() {
-        // TODO Auto-generated method stub
-        return null;
+        return SOAPBinding.SOAP12HTTP_BINDING;
     }
 
 }
