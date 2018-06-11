db341071953f2181a9225ec709d8d97b6e31832a
tabassum_jafri <tabassum.jafri@cgifederal.com>
2018-06-11 09:40:45 -0400

    Added org type, description and updated active to boolean value

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchange/transform/uddi/UDDITransform.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchange/transform/uddi/UDDITransform.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -84,6 +84,7 @@ public class UDDITransform implements ExchangeTransforms<BusinessDetail> {
                 OrganizationType org = new OrganizationType();
                 org.setName(buildOrganizationName(bEntity));
                 org.setHcid(getHomeCommunityID(bEntity));
+                org.getDescription().addAll(buildDescritpion(bEntity));
                 buildContacts(bEntity, org);
                 buildEndpointList(bEntity, org);
                 buildTargetRegions(bEntity, org);
@@ -297,5 +298,15 @@ public class UDDITransform implements ExchangeTransforms<BusinessDetail> {
             return contact.getUseType();
         }
         return null;
+    }
+
+    private static List<String> buildDescritpion(BusinessEntity entity) {
+        List<String> descList = new ArrayList<>();
+        if (null != entity && CollectionUtils.isNotEmpty(entity.getDescription())) {
+            for (Description obj : entity.getDescription()) {
+                descList.add(obj.getValue());
+            }
+        }
+        return descList;
     }
 }
