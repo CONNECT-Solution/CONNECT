ebf15ffa60c22c48676c077a691e5cc112ba9941
tabassum_jafri <tabassum.jafri@cgifederal.com>
2018-07-16 13:42:06 -0400

    Update WSAddressingTest to assert on soapAction and WSAddressignAction in ServicePortDescriptor class

--- Product/Production/Services/DocumentRetrieveCore/src/main/java/gov/hhs/fha/nhinc/docretrieve/entity/proxy/service/EntityDocRetrieveSecuredServicePortDescriptor.java
+++ Product/Production/Services/DocumentRetrieveCore/src/main/java/gov/hhs/fha/nhinc/docretrieve/entity/proxy/service/EntityDocRetrieveSecuredServicePortDescriptor.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -34,10 +34,10 @@ import gov.hhs.fha.nhinc.messaging.service.port.SOAP12ServicePortDescriptor;
  *
  */
 public class EntityDocRetrieveSecuredServicePortDescriptor extends
-        SOAP12ServicePortDescriptor<EntityDocRetrieveSecuredPortType> {
+    SOAP12ServicePortDescriptor<EntityDocRetrieveSecuredPortType> {
 
-    private static final String NAMESPACE_URI = "urn:gov:hhs:fha:nhinc:entitydocretrievesecured";
-    private static final String WS_ADDRESSING_ACTION = NAMESPACE_URI + ":RespondingGateway_CrossGatewayRetrieve";
+    private static final String WS_ADDRESSING_ACTION
+        = "urn:gov:hhs:fha:nhinc:entitydocretrievesecured:RespondingGateway_CrossGatewayRetrieveRequestMessage";
 
     /*
      * (non-Javadoc)
