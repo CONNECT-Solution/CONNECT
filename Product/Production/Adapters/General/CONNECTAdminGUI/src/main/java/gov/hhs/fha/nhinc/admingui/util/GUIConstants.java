d8939790c73ada3893c4b97864d109e2c65a53b5
trantang <ttang@amdexcorp.com>
2018-07-19 08:43:10 -0400

    CONN-2220 add-auditLog-eventType

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/util/GUIConstants.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/util/GUIConstants.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -41,7 +41,8 @@ public class GUIConstants {
         PatientDiscovery("PD"), PatientDiscoveryDeferredReq("PDDefReq"), PatientDiscoveryDeferredResp("PDDefResp"),
         QueryForDocuments("QD"), RetrieveDocuments("RD"), DocSubmission("DS"), DocSubmissionDeferredReq("DSDefReq"),
         DocSubmissionDeferredResp("DSDefResp"), AdminDistribution("AD"), CORE_X12DSRealTime("X12RealTime"),
-        CORE_X12DSGenericBatchRequest("X12BatchReq"), CORE_X12DSGenericBatchResponse("X12BatchResp");
+        CORE_X12DSGenericBatchRequest("X12BatchReq"), CORE_X12DSGenericBatchResponse("X12BatchResp"),
+        DocDataSubmission("DDS"), PatientLocationQuery("PLQ");
 
         public String abbServiceName = null;
 
