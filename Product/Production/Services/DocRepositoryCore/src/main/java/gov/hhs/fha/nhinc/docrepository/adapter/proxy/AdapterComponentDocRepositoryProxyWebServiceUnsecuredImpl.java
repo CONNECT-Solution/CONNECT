e067876e9498493f9c2051d1f7c190a379305f10
plobre <plobre@hotmail.com>
2018-10-03 15:09:20 -0400

    CONN_2261: Document Retrieve refactor. Throwing ErrorEventException in proxies

--- Product/Production/Services/DocRepositoryCore/src/main/java/gov/hhs/fha/nhinc/docrepository/adapter/proxy/AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl.java
+++ Product/Production/Services/DocRepositoryCore/src/main/java/gov/hhs/fha/nhinc/docrepository/adapter/proxy/AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,6 +29,7 @@ package gov.hhs.fha.nhinc.docrepository.adapter.proxy;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 import gov.hhs.fha.nhinc.docrepository.adapter.proxy.service.AdapterComponentDocRepositoryServicePortDescriptor;
 import gov.hhs.fha.nhinc.document.DocumentConstants;
+import gov.hhs.fha.nhinc.event.error.ErrorEventException;
 import gov.hhs.fha.nhinc.messaging.client.CONNECTCXFClientFactory;
 import gov.hhs.fha.nhinc.messaging.client.CONNECTClient;
 import gov.hhs.fha.nhinc.messaging.service.port.ServicePortDescriptor;
@@ -79,7 +80,7 @@ public class AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl implement
                 NhincConstants.ADAPTER_DOC_REPOSITORY_SERVICE_NAME);
 
             if (msg == null) {
-                LOG.error("Message was null");
+                throw new IllegalArgumentException("Request Message must be provided");
             } else {
                 ServicePortDescriptor<DocumentRepositoryPortType> portDescriptor = new AdapterComponentDocRepositoryServicePortDescriptor();
 
@@ -102,6 +103,7 @@ public class AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl implement
             registryError.setSeverity(NhincConstants.XDS_REGISTRY_ERROR_SEVERITY_ERROR);
             regResp.getRegistryErrorList().getRegistryError().add(registryError);
             response.setRegistryResponse(regResp);
+            throw new ErrorEventException(ex,response, "Unable to call Document Repository Adapter");
         }
 
         LOG.debug("End retrieveDocument");
@@ -120,7 +122,7 @@ public class AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl implement
                 NhincConstants.ADAPTER_DOC_REPOSITORY_SERVICE_NAME);
 
             if (msg == null) {
-                LOG.error("Message was null");
+                throw new IllegalArgumentException("Request Message must be provided");
             } else {
                 ServicePortDescriptor<DocumentRepositoryPortType> portDescriptor = new AdapterComponentDocRepositoryServicePortDescriptor();
 
@@ -132,6 +134,7 @@ public class AdapterComponentDocRepositoryProxyWebServiceUnsecuredImpl implement
             }
         } catch (Exception ex) {
             LOG.error("Error sending message to the adapter document repository: " + ex.getMessage(), ex);
+            throw new ErrorEventException(ex,response, "Unable to call Document Repository Adapter");
         }
         return response;
     }
