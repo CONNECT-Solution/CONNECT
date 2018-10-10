097d4992b6c42f0ad7825faabf0480370dc369ee
trantang <ttang@amdexcorp.com>
2018-10-10 14:16:48 -0400

    sonarqube-refactor and primeface-sort

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/event/dao/DatabaseEventLoggerDao.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/event/dao/DatabaseEventLoggerDao.java
@@ -27,8 +27,10 @@
 package gov.hhs.fha.nhinc.event.dao;
 
 import static gov.hhs.fha.nhinc.util.GenericDBUtils.closeSession;
+import static gov.hhs.fha.nhinc.util.GenericDBUtils.findAllBy;
 import static gov.hhs.fha.nhinc.util.GenericDBUtils.getEventDTO;
-import static gov.hhs.fha.nhinc.util.GenericDBUtils.rollbackTransaction;
+import static gov.hhs.fha.nhinc.util.GenericDBUtils.readBy;
+import static gov.hhs.fha.nhinc.util.GenericDBUtils.save;
 
 import gov.hhs.fha.nhinc.event.model.DatabaseEvent;
 import gov.hhs.fha.nhinc.event.model.EventDTO;
@@ -43,10 +45,9 @@ import java.util.Set;
 import org.apache.commons.lang.StringUtils;
 import org.apache.cxf.common.util.CollectionUtils;
 import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.ProjectionList;
 import org.hibernate.criterion.Projections;
@@ -65,7 +66,6 @@ public class DatabaseEventLoggerDao {
     private static final String DATE_NAME = "eventTime";
     private static final String SERVICE_TYPE = "serviceType";
 
-
     private static class SingletonHolder {
 
         public static final DatabaseEventLoggerDao INSTANCE = new DatabaseEventLoggerDao();
@@ -74,139 +74,65 @@ public class DatabaseEventLoggerDao {
         }
     }
 
-    /**
-     * Get an instance of DatabaseEventLoggerDao.
-     *
-     * @return singleton instance of DatabaseEventLoggerDao
-     */
     public static DatabaseEventLoggerDao getInstance() {
         LOG.debug("getInstance()...");
         return SingletonHolder.INSTANCE;
     }
 
-    /**
-     * Insert an event in the database.
-     *
-     * @param databaseEvent to be added to the database
-     * @return true if the event was successfully inserted.
-     */
     public boolean insertEvent(final DatabaseEvent databaseEvent) {
-
-        SessionFactory sessionFactory = null;
-        Session session = null;
-        Transaction tx = null;
-        boolean result = true;
-
-        try {
-            sessionFactory = getSessionFactory();
-            if (sessionFactory != null) {
-                session = sessionFactory.openSession();
-                tx = session.beginTransaction();
-
-                session.persist(databaseEvent);
-
-                tx.commit();
-            }
-
-        } catch (final HibernateException e) {
-            result = false;
-            rollbackTransaction(tx);
-            LOG.error("Exception during insertion caused by : {}", e.getMessage(), e);
-        } finally {
-            closeSession(session);
-        }
-
-        return result;
+        return save(getSession(), databaseEvent);
     }
 
     /**
-     * Hibernate Query for event counts for a given Event type grouping the provided
-     * column names
+     * Hibernate Query for event counts for a given Event type grouping the provided column names
      *
      * @param eventType a list of event types to include, or null for all event types
      * @return List of Object[] with [0] the count, [n] the provided grpProjections[n]
      */
     public List getCounts(final List<String> eventTypes, String... grpProjections) {
-        final SessionFactory sessionFactory = getSessionFactory();
+
         List results = new ArrayList<>();
+        Session session = null;
+        try {
+            session = getSession();
 
-        if (sessionFactory != null) {
-            Session session = null;
-            try {
-                session = sessionFactory.openSession();
+            ProjectionList projList = Projections.projectionList();
+            projList.add(Projections.rowCount());
 
-                ProjectionList projList = Projections.projectionList();
-                projList.add(Projections.rowCount());
-
-                for (String projection : grpProjections) {
-                    projList.add(Projections.groupProperty(projection));
-                }
-
-                Criteria criteria = session.createCriteria(DatabaseEvent.class);
-                if (CollectionUtils.isEmpty(eventTypes)) {
-                    criteria.add(Restrictions.in(EVENT_TYPE_NAME, eventTypes));
-                }
-
-                results = criteria.setProjection(projList)
-                    .list();
-            } finally {
-                closeSession(session);
+            for (String projection : grpProjections) {
+                projList.add(Projections.groupProperty(projection));
             }
+
+            Criteria criteria = session.createCriteria(DatabaseEvent.class);
+            if (CollectionUtils.isEmpty(eventTypes)) {
+                criteria.add(Restrictions.in(EVENT_TYPE_NAME, eventTypes));
+            }
+
+            results = criteria.setProjection(projList).list();
+        } finally {
+            closeSession(session);
         }
 
         return results;
     }
 
     public DatabaseEvent getLatestEvent(final String eventType) {
-
-        DatabaseEvent event = null;
-        final SessionFactory sessionFactory = getSessionFactory();
-        if (sessionFactory != null) {
-            Session session = null;
-            try {
-                session = sessionFactory.openSession();
-                event = (DatabaseEvent) session.createCriteria(DatabaseEvent.class)
-                    .add(Restrictions.eq(EVENT_TYPE_NAME, eventType)).addOrder(Order.desc(DATE_NAME)).setMaxResults(1)
-                    .uniqueResult();
-            } finally {
-                closeSession(session);
-            }
-        }
-
-        return event;
-    }
-
-    /**
-     * Get Failure messages based on event id
-     *
-     * @param id
-     * @return
-     */
-    public EventDTO getEventById(Long id) {
-        DatabaseEvent event = null;
         Session session = null;
         try {
-            final SessionFactory sessionFactory = getSessionFactory();
-            if (sessionFactory != null) {
-                session = sessionFactory.openSession();
-                event = (DatabaseEvent) session.createCriteria(DatabaseEvent.class)
-                    .add(Restrictions.eq("id", id))
-                    .uniqueResult();
-            }
-        } catch (HibernateException e) {
-            LOG.error("Exception getting database event by Id caused by :{}", e.getLocalizedMessage(), e);
+            session = getSession();
+            return (DatabaseEvent) session.createCriteria(DatabaseEvent.class)
+                .add(Restrictions.eq(EVENT_TYPE_NAME, eventType)).addOrder(Order.desc(DATE_NAME)).setMaxResults(1)
+                .uniqueResult();
         } finally {
             closeSession(session);
         }
-
-        return getEventDTO(event);
     }
 
-    /**
-     * Get exception class for messages processing failures events
-     *
-     * @return
-     */
+    public EventDTO getEventById(Long id) {
+        return getEventDTO(readBy(getSession(), DatabaseEvent.class, id));
+    }
+
+
     public Set<String> getExceptions() {
         Set<String> exceptions = new HashSet<>();
         for (EventDTO event : getAllFailureMessages(null, null, null, null)) {
@@ -215,60 +141,13 @@ public class DatabaseEventLoggerDao {
         return exceptions;
     }
 
-    /**
-     * Get all failure events for certain criteria
-     *
-     * @param serviceType
-     * @param startDate
-     * @param endDate
-     * @return
-     */
-    public List<EventDTO> getAllFailureMessages(String serviceType, String exceptionType, Date startDate, Date endDate) {
-        Session session = null;
-        LinkedList<EventDTO> retEvents =  new LinkedList<>();;
 
-        try {
-            final SessionFactory sessionFactory = getSessionFactory();
-            if (sessionFactory != null) {
-                session = sessionFactory.openSession();
-                Criteria criteria = session.createCriteria(DatabaseEvent.class);
-
-                criteria.add(Restrictions.eq(EVENT_TYPE_NAME, "MESSAGE_PROCESSING_FAILED"));
-
-                if (startDate != null) {
-                    criteria.add(Restrictions.ge(DATE_NAME, startDate));
-                }
-
-                if (endDate != null) {
-                    criteria.add(Restrictions.le(DATE_NAME, endDate));
-                }
-
-                if (StringUtils.isNotBlank(serviceType)) {
-                    criteria.add(Restrictions.eq(SERVICE_TYPE, serviceType));
-                }
-
-                List<DatabaseEvent> events = criteria.list();
-
-                for (DatabaseEvent event : events) {
-                    EventDTO dto = getEventDTO(event, true);
-                    if (StringUtils.isNotBlank(exceptionType)) {
-                        if (exceptionType.equals(dto.getExceptionType())) {
-                            retEvents.add(dto); // filter-list
-                        }
-                    } else {
-                        retEvents.add(dto); // all-list
-                    }
-                }
-            }
-        } catch (HibernateException e) {
-            LOG.error("Exception getting failure messages caused by :{}", e.getLocalizedMessage(), e);
-        } finally {
-            closeSession(session);
-        }
-
-        return retEvents;
+    public List<EventDTO> getAllFailureMessages(String serviceType, String exceptionType, Date startDate,
+        Date endDate) {
+        return filterEventsByException(
+            findAllBy(getSession(), DatabaseEvent.class, buildCriterionFor(serviceType, startDate, endDate)),
+            exceptionType);
     }
-
 
     protected SessionFactory getSessionFactory() {
         SessionFactory fact = null;
@@ -277,5 +156,44 @@ public class DatabaseEventLoggerDao {
             fact = util.getSessionFactory();
         }
         return fact;
+    }
+
+    protected Session getSession() {
+        return getSessionFactory().openSession();
+    }
+
+    private static List<EventDTO> filterEventsByException(List<DatabaseEvent> events, String exceptionType) {
+        LinkedList<EventDTO> list = new LinkedList<>();
+
+        for (DatabaseEvent event : events) {
+            EventDTO dto = getEventDTO(event, true);
+            if (StringUtils.isNotBlank(exceptionType)) {
+                if (exceptionType.equals(dto.getExceptionType())) {
+                    list.add(dto); // filter-list
+                }
+            } else {
+                list.add(dto); // all-list
+            }
+        }
+        return list;
+    }
+
+    private static List<Criterion> buildCriterionFor(String serviceType, Date startDate, Date endDate) {
+        List<Criterion> critList = new LinkedList<>();
+
+        critList.add(Restrictions.eq(EVENT_TYPE_NAME, "MESSAGE_PROCESSING_FAILED"));
+
+        if (startDate != null) {
+            critList.add(Restrictions.ge(DATE_NAME, startDate));
+        }
+
+        if (endDate != null) {
+            critList.add(Restrictions.le(DATE_NAME, endDate));
+        }
+
+        if (StringUtils.isNotBlank(serviceType)) {
+            critList.add(Restrictions.eq(SERVICE_TYPE, serviceType));
+        }
+        return critList;
     }
 }
