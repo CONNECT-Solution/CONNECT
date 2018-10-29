149b91937a4309febc3bec64f0f900fdebff66d9
Minh-Hai Nguyen <minh-hai.nguyen@cgi.com>
2018-10-29 17:16:56 -0400

    update readme/history and build script

--- Product/Install/JBoss/startServer.sh
+++
@@ -1,8 +0,0 @@
-# Set the JBoss Environment Variables
-. /nhin/CI/JBOSS_Variables.sh
-
-
-#Command to start JBoss 7.1.1.final App Server
-
-/nhin/jboss-as-7.1.1.Final/bin/standalone.sh &
-
--- Product/Install/JBoss/stopServer.sh
+++
@@ -1,7 +0,0 @@
-#Command to set JBoss Environmant Variable
-. /nhin/CI/JBOSS_Variables.sh
-
-#Command to stop JBoss 7.1.1.final Server
-/nhin/jboss-as-7.1.1.Final/bin/jboss-cli.sh --connect command=:shutdown
-
-
