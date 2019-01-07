3b97acbcb0e931a853ee8981d6ec2e126734ffd4
ptambelliniAMDEX <ptambellini@amdexcorp.com>
2019-01-07 17:05:36 -0500

    CONN-2391 Updated copywrite license to 2009-2019

--- Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/transform/policy/XDRPolicyTransformHelperTest.java
+++ Product/Production/Common/CONNECTCoreLib/src/test/java/gov/hhs/fha/nhinc/transform/policy/XDRPolicyTransformHelperTest.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
- * All rights reserved.
- * 
+ * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
+ *  All rights reserved.
+ *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -23,7 +23,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+*/
 package gov.hhs.fha.nhinc.transform.policy;
 
 import static org.junit.Assert.assertEquals;
