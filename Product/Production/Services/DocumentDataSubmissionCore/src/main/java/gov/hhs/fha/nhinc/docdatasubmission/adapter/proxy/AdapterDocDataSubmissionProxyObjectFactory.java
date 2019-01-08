dead66012863c10f7be91e9826331f4e6597c8b5
ptambelliniAMDEX <ptambellini@amdexcorp.com>
2019-01-08 09:30:18 -0500

    CONN-2391 Updated remaining CONNECT copywrite licenses

--- Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/adapter/proxy/AdapterDocDataSubmissionProxyObjectFactory.java
+++ Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/adapter/proxy/AdapterDocDataSubmissionProxyObjectFactory.java
@@ -27,7 +27,7 @@
 package gov.hhs.fha.nhinc.docdatasubmission.adapter.proxy;
 
 /*
- * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
+ * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
