fbd05449b66bec60414a4d562ee56fc51bf68b28
tabassum_jafri <tabassum.jafri@cgifederal.com>
2019-01-08 16:35:57 -0500

    copyright updates

--- Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/nhin/proxy/NhinDocDataSubmissionProxyObjectFactory.java
+++ Product/Production/Services/DocumentDataSubmissionCore/src/main/java/gov/hhs/fha/nhinc/docdatasubmission/nhin/proxy/NhinDocDataSubmissionProxyObjectFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
+ * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
