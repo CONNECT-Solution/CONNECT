337ffcfc0daeb1bff2d8273d81ad22a704942f80
ptambelliniAMDEX <ptambellini@amdexcorp.com>
2019-01-08 08:42:30 -0500

    CONN-2391 Removed a leading space on 2nd line

--- Product/Production/Services/DocumentQueryCore/src/main/java/gov/hhs/fha/nhinc/docquery/entity/OutboundDocQueryOrchestratable_a0.java
+++ Product/Production/Services/DocumentQueryCore/src/main/java/gov/hhs/fha/nhinc/docquery/entity/OutboundDocQueryOrchestratable_a0.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
- *  All rights reserved.
+ * All rights reserved.
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
