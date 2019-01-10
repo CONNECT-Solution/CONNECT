8318d1500f374009699d76fbcfdc42da76bd0a19
tabassum_jafri <tabassum.jafri@cgifederal.com>
2019-01-10 11:04:02 -0500

    Fixed copyright

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/servlet/CustomServletListener.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/servlet/CustomServletListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2018, United States Government, as represented by the Secretary of Health and Human Services.
+ * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -41,6 +41,7 @@ import org.slf4j.LoggerFactory;
  * @author mpnguyen
  */
 public class CustomServletListener extends StartupServletContextListener {
+
     private static final Logger LOG = LoggerFactory.getLogger(CustomServletListener.class);
 
     @Override
