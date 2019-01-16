4c2caf0c381147a1e05a7fc866ac142a54847655
tabassum_jafri <tabassum.jafri@cgifederal.com>
2019-01-16 14:47:02 -0500

    SNI support for FHIR Directory client

--- Product/Production/Services/ConnectionManagerCore/src/main/java/gov/hhs/fha/nhinc/exchangemgr/fhir/FhirClient.java
+++ Product/Production/Services/ConnectionManagerCore/src/main/java/gov/hhs/fha/nhinc/exchangemgr/fhir/FhirClient.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,36 +23,18 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 package gov.hhs.fha.nhinc.exchangemgr.fhir;
 
-import gov.hhs.fha.nhinc.callback.opensaml.CertificateManager;
-import gov.hhs.fha.nhinc.callback.opensaml.CertificateManagerImpl;
-import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
-import gov.hhs.fha.nhinc.properties.PropertyAccessException;
-import gov.hhs.fha.nhinc.properties.PropertyAccessor;
-import gov.hhs.fha.nhinc.util.StreamUtils;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.security.GeneralSecurityException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -63,99 +45,32 @@ public class FhirClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(FhirClient.class);
 
-    public String sendRequest(HttpUriRequest request) throws FhirClientException {
-        LOG.info("sendRequest: {}", request);
-        HttpClientBuilder client = HttpClientBuilder.create();
-        try (CloseableHttpClient httpClient = client.setSSLSocketFactory(createSSLConnectionSocketFactory()).build()) {
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                return unmarshallResponse(response);
-            }
-        } catch (IOException | NoSuchAlgorithmException | KeyManagementException ex) {
-            throw new FhirClientException("Error sending Http Request: " + ex.getLocalizedMessage(), ex);
-        }
-    }
-
-    private static String getTLSVersionsFromProperties() {
+    public String doGet(String url, String sniName, MimeType mimeType) throws FhirClientException {
         try {
-            return PropertyAccessor.getInstance().getProperty(NhincConstants.GATEWAY_PROPERTY_FILE,
-                NhincConstants.FHIR_TLS);
-        } catch (PropertyAccessException ex) {
-            LOG.error("Unable to retrieve {} from {}: {}", NhincConstants.FHIR_TLS,
-                NhincConstants.GATEWAY_PROPERTY_FILE, ex.getLocalizedMessage(), ex);
+            HttpURLConnection client = ConnectionUtil.createHttpUrlConnection(url, HttpMethod.GET, sniName, mimeType);
+            client.connect();
+            String response = readResponse(client.getResponseCode(), client.getInputStream());
+            client.disconnect();
+            return response;
+        } catch (IOException | GeneralSecurityException ex) {
+            throw new FhirClientException("Error performing Http Request: " + ex.getLocalizedMessage(), ex);
         }
-        return null;
     }
 
-    private static SSLConnectionSocketFactory createSSLConnectionSocketFactory() throws NoSuchAlgorithmException,
-        KeyManagementException {
-        String tls = getTLSVersionsFromProperties();
-        SSLContext sslContext;
-        if (StringUtils.isNotEmpty(tls)) {
-            sslContext = SSLContext.getInstance(tls);
+    private static String readResponse(int responseCode, InputStream data) throws FhirClientException, IOException {
+        StringBuilder response = new StringBuilder();
+        LOG.debug("Server returned {} response ", responseCode);
+        if (responseCode == HttpStatus.OK.value()) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(data))) {
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+            }
+            return response.toString();
         } else {
-            sslContext = SSLContext.getDefault();
+            throw new FhirClientException("Unsuccessful repsonse returned by the server");
         }
-        CertificateManager cm = CertificateManagerImpl.getInstance();
-        sslContext.init(getKeyManager(cm), getTrustManager(cm), null);
-        return new SSLConnectionSocketFactory(sslContext);
-    }
-
-    private static KeyManager[] getKeyManager(CertificateManager cm) {
-        try {
-            KeyManagerFactory keyFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            keyFactory.init(cm.getKeyStore(), getKeystorePassword());
-            return keyFactory.getKeyManagers();
-        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
-            LOG.error("Could not initialize key and trust managers: {} ", e.getLocalizedMessage(), e);
-        }
-        return new KeyManager[0];
-    }
-
-    private static TrustManager[] getTrustManager(CertificateManager cm) {
-        try {
-            TrustManagerFactory trustFactory = TrustManagerFactory.
-                getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustFactory.init(cm.getTrustStore());
-            return trustFactory.getTrustManagers();
-        } catch (KeyStoreException | NoSuchAlgorithmException e) {
-            LOG.error("Could not initialize key and trust managers: {} ", e.getLocalizedMessage(), e);
-        }
-        return new TrustManager[0];
-    }
-
-    private static char[] getKeystorePassword() throws UnrecoverableKeyException {
-        String keystorePassword = System.getProperty("javax.net.ssl.keyStorePassword");
-        if (keystorePassword == null || keystorePassword.isEmpty()) {
-            throw new UnrecoverableKeyException("Password for key is null or empty.");
-        }
-
-        return keystorePassword.toCharArray();
-    }
-
-    private static String unmarshallResponse(CloseableHttpResponse response) throws FhirClientException {
-        if (null != response) {
-            if ((null != response.getStatusLine())
-                && (response.getStatusLine().getStatusCode() == HttpStatus.OK.value())) {
-                return unmarshall(response.getEntity());
-            } else {
-                throw new FhirClientException("Unsuccessful repsonse returned by the server");
-            }
-        }
-        throw new FhirClientException("Null response returned by the server");
-    }
-
-    private static String unmarshall(HttpEntity entity) throws FhirClientException {
-        InputStream instream = null;
-        try {
-            if (null != entity && null != entity.getContent()) {
-                instream = entity.getContent();
-                return IOUtils.toString(instream, FhirConstants.DEFAULT_CHARSET);
-            }
-        } catch (IOException ex) {
-            throw new FhirClientException("Unable to read response, " + ex.getLocalizedMessage(), ex);
-        } finally {
-            StreamUtils.closeStreamSilently(instream);
-        }
-        return null;
     }
 }
