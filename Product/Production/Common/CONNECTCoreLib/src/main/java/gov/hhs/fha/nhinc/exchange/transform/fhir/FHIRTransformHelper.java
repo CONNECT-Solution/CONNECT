708b01920085585b8cfd08bf0c4f1f4a1749a1c9
plobre <plobre@hotmail.com>
2019-03-11 15:27:40 -0400

    CONN_2426: fixing Narrative parsing and display for Admin GUI

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchange/transform/fhir/FHIRTransformHelper.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/exchange/transform/fhir/FHIRTransformHelper.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -186,13 +186,13 @@ public class FHIRTransformHelper {
         List<String> desc = null;
         if (null != narritive) {
             desc = new ArrayList<>();
-            desc.add(narritive.toString());
+            desc.add(narritive.getDivAsString());
         }
         return desc;
     }
 
     public static String getOrgType(List<CodeableConcept> types) {
-        return ((CollectionUtils.isNotEmpty(types)) && (null != types.get(0).getCodingFirstRep()))
+        return CollectionUtils.isNotEmpty(types) && null != types.get(0).getCodingFirstRep()
             ? types.get(0).getCodingFirstRep().getCode() : null;
     }
 
