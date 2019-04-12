3c4ca6dc34f8e969d1994bd262a0480fda87485d
plobre <plobre@hotmail.com>
2019-04-12 13:03:55 -0400

    CONN_2447: Adding new adapters, entity, and nwhin endpoints based on new requirements

---
+++ Product/Production/Adapters/DocumentQuery_a0/src/main/java/gov/hhs/fha/nhinc/docquery/deferred/adapter/AdapterDeferredResultsOptionResponse.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of the United States Government nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE UNITED STATES GOVERNMENT BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package gov.hhs.fha.nhinc.docquery.deferred.adapter;
+
+import gov.hhs.fha.nhinc.dq.adapterdeferredresultoption.AdapterDocQueryDeferredResultsOptionPortType;
+import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+
+/**
+ * Adapter webservice to process deferred documents into a deferred response option message to send back to the Initiating
+ * Gateway at the endpoint specified in the initial request.
+ */
+
+public class AdapterDeferredResultsOptionResponse implements AdapterDocQueryDeferredResultsOptionPortType{
+
+    @Override
+    public RegistryResponseType respondingGatewayCrossGatewayQueryResults(AdhocQueryResponse arg0) {
+        return null;
+    }
+
+
+}
