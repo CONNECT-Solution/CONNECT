c74911d78b5d7e7422602074c2fc9e41b8ceeab3
trantang <ttang@amdexcorp.com>
2019-05-10 11:26:24 -0400

    CONN-2416 atna log Deferred Result

--- Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/util/GUIConstants.java
+++ Product/Production/Adapters/General/CONNECTAdminGUI/src/main/java/gov/hhs/fha/nhinc/admingui/util/GUIConstants.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,7 +23,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 package gov.hhs.fha.nhinc.admingui.util;
 
 import gov.hhs.fha.nhinc.nhinclib.NhincConstants;
@@ -49,6 +49,7 @@ public class GUIConstants {
         PATIENT_DISCOVERY_DEF_REQ(NhincConstants.PATIENT_DISCOVERY_DEFERRED_REQ_SERVICE_NAME,"PDDefReq"),
         PATIENT_DISCOVERY_DEF_RESP(NhincConstants.PATIENT_DISCOVERY_DEFERRED_RESP_SERVICE_NAME,"PDDefResp"),
         QUERY_FOR_DOCUMENTS(NhincConstants.DOC_QUERY_SERVICE_NAME,"QD"),
+        QUERY_FOR_DOCUMENTS_DEF_RESP(NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME, "QDDefResp"),
         RETRIEVE_DOCUMENTS(NhincConstants.DOC_RETRIEVE_SERVICE_NAME,"RD"),
         DOCUMENT_SUBMISSION(NhincConstants.NHINC_XDR_SERVICE_NAME,"DS"),
         DOCUMENT_SUBMISISON_DEF_REQ(NhincConstants.NHINC_XDR_REQUEST_SERVICE_NAME,"DSDefReq"),
--- Product/Production/Services/DocumentQueryCore/src/test/java/gov/hhs/fha/nhinc/docquery/audit/transform/DocQueryDeferredResponseAuditTransformsTest.java
+++ Product/Production/Services/DocumentQueryCore/src/test/java/gov/hhs/fha/nhinc/docquery/audit/transform/DocQueryDeferredResponseAuditTransformsTest.java
@@ -29,7 +29,6 @@ package gov.hhs.fha.nhinc.docquery.audit.transform;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 import com.services.nhinc.schema.auditmessage.ParticipantObjectIdentificationType;
@@ -48,22 +47,24 @@ import java.net.UnknownHostException;
 import java.util.Properties;
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
-import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryRequest;
 import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.AdhocQueryType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExtrinsicObjectType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectListType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.SlotType1;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.ValueListType;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * @author ttang
  *
- * @author tjafri
  */
-public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryRequest, AdhocQueryResponse> {
+public class DocQueryDeferredResponseAuditTransformsTest
+extends AuditTransformsTest<AdhocQueryResponse, RegistryResponseType> {
 
     private final String REMOTE_HCID_WITHOUT_PREFIX = "2.2";
     private final String REMOTE_HCID_WITH_PREFIX = "urn:oid:2.2";
@@ -73,13 +74,7 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
     private final String REMOTE_OBJECT_URL = "http://" + REMOTE_IP + ":9090/source/AuditService";
     private final String WS_REEQUEST_URL = "http://" + REMOTE_IP + ":9090/AuditService";
 
-    @Before
-    public void setup() {
-        // propertiesAccessor
-        System.setProperty("nhinc.properties.dir", System.getProperty("user.dir") + "/src/test/resources");
-    }
-
-    public DocQueryAuditTransformsTest() {
+    public DocQueryDeferredResponseAuditTransformsTest() {
     }
 
     @AfterClass
@@ -87,36 +82,36 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
     }
 
     @Test
-    public void testRequestAuditMsgHCIDWithOutPrefix() throws ConnectionManagerException, UnknownHostException,
-    CertificateManagerException {
-        transformRequestToAuditMsg(createAdhocQueryRequest(), createNhinTarget(REMOTE_HCID_WITHOUT_PREFIX),
+    public void testRequestAuditMsgHCIDWithOutPrefix()
+        throws ConnectionManagerException, UnknownHostException, CertificateManagerException {
+        transformRequestToAuditMsg(createRequest(), createNhinTarget(REMOTE_HCID_WITHOUT_PREFIX),
             REMOTE_HCID_WITH_PREFIX);
     }
 
     @Test
-    public void testRequestAuditMsgHCIDWithPrefix() throws ConnectionManagerException, UnknownHostException,
-    CertificateManagerException {
-        transformRequestToAuditMsg(createAdhocQueryRequest(), createNhinTarget(REMOTE_HCID_WITH_PREFIX),
+    public void testRequestAuditMsgHCIDWithPrefix()
+        throws ConnectionManagerException, UnknownHostException, CertificateManagerException {
+        transformRequestToAuditMsg(createRequest(), createNhinTarget(REMOTE_HCID_WITH_PREFIX),
             REMOTE_HCID_WITH_PREFIX);
     }
 
     @Test
     public void testResponseAuditMsgHCIDWithOutPrefix() throws ConnectionManagerException, UnknownHostException {
-        transformResponseToAuditMsg(createAdhocQueryRequest(), createAdhocQueryResponse(), LOCAL_HCID_WITH_PREFIX);
+        transformResponseToAuditMsg(createRequest(), createResponse(), LOCAL_HCID_WITH_PREFIX);
     }
 
     @Test
     public void testResponseAuditMsgHCIDWithPrefix() throws ConnectionManagerException, UnknownHostException {
-        transformResponseToAuditMsg(createAdhocQueryRequest(), createAdhocQueryResponse(), LOCAL_HCID_WITH_PREFIX);
+        transformResponseToAuditMsg(createRequest(), createResponse(), LOCAL_HCID_WITH_PREFIX);
     }
 
-    private void transformRequestToAuditMsg(AdhocQueryRequest request, NhinTargetSystemType target, String hcid) throws
-    ConnectionManagerException, UnknownHostException, CertificateManagerException {
+    private void transformRequestToAuditMsg(AdhocQueryResponse request, NhinTargetSystemType target, String hcid)
+        throws ConnectionManagerException, UnknownHostException, CertificateManagerException {
 
         Properties webContextProperties = new Properties();
         webContextProperties.setProperty(NhincConstants.WEB_SERVICE_REQUEST_URL, WS_REEQUEST_URL);
         webContextProperties.setProperty(NhincConstants.REMOTE_HOST_ADDRESS, REMOTE_IP);
-        DocQueryAuditTransforms transforms = new DocQueryAuditTransforms() {
+        DocQueryDeferredResponseAuditTransforms transforms = new DocQueryDeferredResponseAuditTransforms() {
             @Override
             protected String getLocalHostAddress() {
                 return LOCAL_IP;
@@ -124,8 +119,8 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
 
             @Override
             protected String getRemoteHostAddress(Properties webContextProperties) {
-                if (webContextProperties != null && !webContextProperties.isEmpty() && webContextProperties
-                    .getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
+                if (webContextProperties != null && !webContextProperties.isEmpty()
+                    && webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
 
                     return webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS);
                 }
@@ -142,25 +137,26 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
 
         LogEventRequestType auditRequest = transforms.transformRequestToAuditMsg(request, assertion, target,
             NhincConstants.AUDIT_LOG_OUTBOUND_DIRECTION, NhincConstants.AUDIT_LOG_NHIN_INTERFACE, Boolean.TRUE, null,
-            NhincConstants.DOC_QUERY_SERVICE_NAME);
+            NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME);
 
-        testGetEventIdentificationType(auditRequest, NhincConstants.DOC_QUERY_SERVICE_NAME, Boolean.TRUE);
+        testGetEventIdentificationType(auditRequest, NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME,
+            Boolean.TRUE);
         testGetActiveParticipantSource(auditRequest, Boolean.TRUE, webContextProperties, LOCAL_IP);
         testGetActiveParticipantDestination(auditRequest, Boolean.TRUE, webContextProperties, REMOTE_OBJECT_URL);
         testAuditSourceIdentification(auditRequest.getAuditMessage().getAuditSourceIdentification(), assertion);
         testCreateActiveParticipantFromUser(auditRequest, Boolean.TRUE, assertion);
         assertParticipantObjectIdentification(auditRequest, hcid);
         assertEquals("AuditMessage.Request ServiceName mismatch", auditRequest.getEventType(),
-            NhincConstants.DOC_QUERY_SERVICE_NAME);
+            NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME);
     }
 
-    private void transformResponseToAuditMsg(AdhocQueryRequest request, AdhocQueryResponse response, String hcid) throws
-    ConnectionManagerException, UnknownHostException {
+    private void transformResponseToAuditMsg(AdhocQueryResponse request, RegistryResponseType response, String hcid)
+        throws ConnectionManagerException, UnknownHostException {
         Properties webContextProperties = new Properties();
         webContextProperties.setProperty(NhincConstants.WEB_SERVICE_REQUEST_URL, WS_REEQUEST_URL);
         webContextProperties.setProperty(NhincConstants.REMOTE_HOST_ADDRESS, REMOTE_IP);
 
-        DocQueryAuditTransforms transforms = new DocQueryAuditTransforms() {
+        DocQueryDeferredResponseAuditTransforms transforms = new DocQueryDeferredResponseAuditTransforms() {
             @Override
             protected String getLocalHostAddress() {
                 return LOCAL_IP;
@@ -168,8 +164,8 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
 
             @Override
             protected String getRemoteHostAddress(Properties webContextProperties) {
-                if (webContextProperties != null && !webContextProperties.isEmpty() && webContextProperties.
-                    getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
+                if (webContextProperties != null && !webContextProperties.isEmpty()
+                    && webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS) != null) {
 
                     return webContextProperties.getProperty(NhincConstants.REMOTE_HOST_ADDRESS);
                 }
@@ -185,29 +181,28 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
         AssertionType assertion = createAssertion();
         LogEventRequestType auditResponse = transforms.transformResponseToAuditMsg(request, response, assertion, null,
             NhincConstants.AUDIT_LOG_INBOUND_DIRECTION, NhincConstants.AUDIT_LOG_NHIN_INTERFACE, Boolean.FALSE,
-            webContextProperties, NhincConstants.DOC_QUERY_SERVICE_NAME);
+            webContextProperties, NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME);
 
-        testGetEventIdentificationType(auditResponse, NhincConstants.DOC_QUERY_SERVICE_NAME, Boolean.FALSE);
+        testGetEventIdentificationType(auditResponse, NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME,
+            Boolean.FALSE);
         testAuditSourceIdentification(auditResponse.getAuditMessage().getAuditSourceIdentification(), assertion);
         testGetActiveParticipantSource(auditResponse, Boolean.FALSE, webContextProperties, LOCAL_IP);
         testGetActiveParticipantDestination(auditResponse, Boolean.FALSE, webContextProperties, REMOTE_OBJECT_URL);
         assertParticipantObjectIdentification(auditResponse, hcid);
         assertEquals("AuditMessage.Response ServiceName mismatch", auditResponse.getEventType(),
-            NhincConstants.DOC_QUERY_SERVICE_NAME);
+            NhincConstants.DOC_QUERY_DEFERRED_RESULTS_SERVICE_NAME);
     }
 
     private void assertParticipantObjectIdentification(LogEventRequestType auditRequest, String hcid) {
         assertNotNull("auditRequest is null", auditRequest);
         assertNotNull("AuditMessage is null", auditRequest.getAuditMessage());
-        assertNotNull("ParticipantObjectIdentification is null", auditRequest.getAuditMessage()
-            .getParticipantObjectIdentification());
-        assertFalse("ParticipantObjectIdentification list is missing test values", auditRequest.getAuditMessage()
-            .getParticipantObjectIdentification().size() < 2);
+        assertNotNull("ParticipantObjectIdentification is null",
+            auditRequest.getAuditMessage().getParticipantObjectIdentification());
+        assertFalse("ParticipantObjectIdentification list is missing test values",
+            auditRequest.getAuditMessage().getParticipantObjectIdentification().size() < 1);
 
         ParticipantObjectIdentificationType participantPatient = auditRequest.getAuditMessage()
             .getParticipantObjectIdentification().get(0);
-        ParticipantObjectIdentificationType participantQuery = auditRequest.getAuditMessage()
-            .getParticipantObjectIdentification().get(1);
 
         assertEquals("ParticipantPatient.ParticipantObjectID mismatch", "D123401^^^&1.1&ISO",
             participantPatient.getParticipantObjectID());
@@ -229,66 +224,53 @@ public class DocQueryAuditTransformsTest extends AuditTransformsTest<AdhocQueryR
             DocQueryAuditTransformsConstants.PARTICIPANT_PATIENT_OBJ_ID_TYPE_DISPLAY_NAME,
             participantPatient.getParticipantObjectIDTypeCode().getDisplayName());
 
-        assertSame("ParticipantQuery.ParticipantObjectTypeCode object reference mismatch",
-            DocQueryAuditTransformsConstants.PARTICIPANT_QUERY_OBJ_TYPE_CODE_SYSTEM,
-            participantQuery.getParticipantObjectTypeCode());
-        assertSame("ParticipantQuery.ParticipantObjectTypeCodeRole object reference mismatch",
-            DocQueryAuditTransformsConstants.PARTICIPANT_QUERY_OBJ_TYPE_CODE_ROLE,
-            participantQuery.getParticipantObjectTypeCodeRole());
-        assertEquals("ParticipantQuery.ParticipantObjectIDTypeCode.Code mismatch",
-            DocQueryAuditTransformsConstants.PARTICIPANT_QUERY_OBJ_ID_TYPE_CODE,
-            participantQuery.getParticipantObjectIDTypeCode().getCode());
-        assertEquals("ParticipantQuery.ParticipantObjectIDTypeCode.CodeSystemName mismatch",
-            DocQueryAuditTransformsConstants.PARTICIPANT_QUERY_OBJ_ID_TYPE_CODE_SYSTEM,
-            participantQuery.getParticipantObjectIDTypeCode().getCodeSystemName());
-        assertEquals("ParticipantQuery.ParticipantObjectIDTypeCode.DisplayName mismatch",
-            DocQueryAuditTransformsConstants.PARTICIPANT_QUERY_OBJ_ID_TYPE_DISPLAY_NAME,
-            participantQuery.getParticipantObjectIDTypeCode().getDisplayName());
-        assertNull("ParticipantPatient.ParticipantObjectName is not null",
-            participantPatient.getParticipantObjectName());
-        assertNull("ParticipantQuery.ParticipantObjectName is not null",
-            participantQuery.getParticipantObjectName());
-        assertNotNull("ParticipantQuery.ParticipantObjectQuery is null",
-            participantQuery.getParticipantObjectQuery());
-        assertEquals("ParticipantQuery.ParticipantObjectDetail HomeCommunityId mismatch", hcid,
-            new String(participantQuery.getParticipantObjectDetail().get(1).getValue()));
     }
 
-    private AdhocQueryResponse createAdhocQueryResponse() {
-        AdhocQueryResponse response = new AdhocQueryResponse();
-        response.setRegistryObjectList(new RegistryObjectListType());
-        response.getRegistryObjectList().getIdentifiable().add(createIdentifiable(new RegistryObjectType()));
+    private RegistryResponseType createResponse() {
+        RegistryResponseType response = new AdhocQueryResponse();
+        response.setStatus("urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success");
         return response;
     }
 
-    private AdhocQueryRequest createAdhocQueryRequest() {
-        ValueListType val = new ValueListType();
-        val.getValue().add("D123401^^^&1.1&ISO");
+    private SlotType1 createSlot(String name, String... values) {
+        SlotType1 pidSlot = null;
+        if (StringUtils.isNotBlank(name) && ArrayUtils.isNotEmpty(values)) {
+            pidSlot = new SlotType1();
+            pidSlot.setName(name);
+            for (String item : values) {
+                ValueListType val = new ValueListType();
+                val.getValue().add(item);
+                pidSlot.setValueList(val);
+            }
+        }
+        return pidSlot;
+    }
 
-        SlotType1 pidSlot = new SlotType1();
-        pidSlot.setName(DocQueryAuditTransformsConstants.XDS_DOCUMENT_ENTRY_PATIENT_ID);
-        pidSlot.setValueList(val);
+    private static <T> JAXBElement<T> createJAXBElement(Class<T> clazz, T value) {
+        return new JAXBElement<>(new QName("urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", "Identifiable"), clazz,
+            value);
+    }
 
-        AdhocQueryRequest request = new AdhocQueryRequest();
-        request.setAdhocQuery(new AdhocQueryType());
-        request.getAdhocQuery().getSlot().add(pidSlot);
-        request.getAdhocQuery().setId("urn:uuid:14d4debf-8f97-4251-9a74-a90016b0af0d");
+    private AdhocQueryResponse createRequest() {
+
+        ExtrinsicObjectType extrinsic = new ExtrinsicObjectType();
+        extrinsic.getSlot().add(createSlot("sourcePatientId", "D123401^^^&1.1&ISO"));
+
+        AdhocQueryResponse request = new AdhocQueryResponse();
+        request.setStatus("urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success");
+        request.setRequestId("urn:uuid:14d4debf-8f97-4251-9a74-a90016b0af0d");
+        request.setRegistryObjectList(new RegistryObjectListType());
+        request.getRegistryObjectList().getIdentifiable().add(createJAXBElement(ExtrinsicObjectType.class, extrinsic));
+        request.getRegistryObjectList().getIdentifiable()
+        .add(createJAXBElement(RegistryObjectType.class, new RegistryObjectType()));
+
         return request;
     }
 
-    /**
-     * @param registryObjectType
-     * @return
-     */
-    private JAXBElement<RegistryObjectType> createIdentifiable(RegistryObjectType registryObjectType) {
-        return new JAXBElement<>(new QName(
-            "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", "Identifiable"), RegistryObjectType.class,
-            registryObjectType);
-    }
 
     @Override
-    protected AuditTransforms<AdhocQueryRequest, AdhocQueryResponse> getAuditTransforms() {
-        return new DocQueryAuditTransforms();
+    protected AuditTransforms<AdhocQueryResponse, RegistryResponseType> getAuditTransforms() {
+        return new DocQueryDeferredResponseAuditTransforms();
     }
 
     private NhinTargetSystemType createNhinTarget(String hcid) {
