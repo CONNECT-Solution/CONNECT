6371a261d3a4e6599a7444f76040effb2383a60f
plobre <plobre@hotmail.com>
2019-05-17 08:29:38 -0400

    CONN_2416: changing CXF to take into account deferred capable requests for the port builder. Addressed issue with Error Handler for deferred response adapter

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/messaging/service/port/CachingCXFUnsecuredServicePortBuilder.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/messaging/service/port/CachingCXFUnsecuredServicePortBuilder.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2009-2019, United States Government, as represented by the Secretary of Health and Human Services.
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above
@@ -12,7 +12,7 @@
  *     * Neither the name of the United States Government nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,8 +26,7 @@
 */
 package gov.hhs.fha.nhinc.messaging.service.port;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.collections.map.MultiKeyMap;
 
 /**
  * @author akong
@@ -35,7 +34,7 @@ import java.util.Map;
  */
 public class CachingCXFUnsecuredServicePortBuilder<T> extends CachingCXFServicePortBuilder<T> {
 
-    private static Map<Class<?>, Object> CACHED_PORTS = new HashMap<>();
+    private static MultiKeyMap CACHED_PORTS = new MultiKeyMap();
 
     /**
      * Constructor.
@@ -52,7 +51,7 @@ public class CachingCXFUnsecuredServicePortBuilder<T> extends CachingCXFServiceP
      * @see gov.hhs.fha.nhinc.messaging.service.port.CachingCXFServicePortBuilder#getCache()
      */
     @Override
-    protected Map<Class<?>, Object> getCache(String gatewayAlias) {
+    protected MultiKeyMap getCache(String gatewayAlias, boolean isDeferredCapable) {
         return CACHED_PORTS;
     }
 
